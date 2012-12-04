59efe4a0dc9beb02f9be98da1ecf56a526911806
Laurence Rowe <lrowe@stanford.edu>
2012-12-03 17:53:32 -0800

    Use require.js for loading javascript.

---
+++ src/encodb/static/js/app.js
@@ -0,0 +1,14 @@
+define([
+    'jquery', 'underscore', 'backbone'],
+    function module($, _, Backbone) {
+
+    var app = _.extend({
+
+        start: function start() {
+            
+        }
+
+    });
+
+    return app;
+});
--- src/encodb/static/js/main.js
+++ src/encodb/static/js/main.js
@@ -1,1 +1,49 @@
+require.config({
+    paths: {
+        app: 'app',
+
+        // Libraries
+        backbone: 'vendor/backbone.min',
+        bootstrap: 'vendor/bootstrap.min',
+        jquery: 'vendor/jquery.min',
+        modernizr: 'vendor/modernizr.min',
+        underscore: 'vendor/underscore.min'
+    },
+
+    shim: {
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+
+        bootstrap: {
+            deps: ['jquery']
+        },
+
+        // Modernizr loads later than normal with Require.js, so don't use the
+        // new HTML5 elements in the HTML file itself.
+        modernizr: {
+            exports: 'Modernizr'
+        },
+
+        underscore: {
+            exports: '_'
+        }
+    }
+});
+
+define([
+    'jquery', 'app', 'bootstrap', 'modernizr'],
+    function module($, app) {
+
+    // Treat the jQuery ready function as the entry point to the application.
+    // Inside this function, kick-off all initialization, everything up to this
+    // point should be definitions.
+    $(function ready() {
+        app.start();
+
+        // Simplify debugging
+        window.app = app;
+    });
+});
 
