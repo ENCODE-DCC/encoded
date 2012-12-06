c02d31f2eb4fcab6320042cc7e2b594cf0e60960
Laurence Rowe <lrowe@stanford.edu>
2012-12-05 16:42:42 -0800

    Routing and history support.

--- src/encodb/static/js/app.js
+++ src/encodb/static/js/app.js
@@ -1,14 +1,51 @@
-define([
-    'jquery', 'underscore', 'backbone'],
-    function module($, _, Backbone) {
+define(['exports', 'jquery', 'underscore', 'backbone', 'home'],
+function app(exports, $, _, Backbone, home) {
 
-    var app = _.extend({
+    exports.start = function start() {
+        exports.router = new exports.Router();
+        exports.setupNavigation();
+    };
 
-        start: function start() {
-            
+    exports.setupNavigation = function setupNavigation() {
+        // Trigger the initial route and enable HTML5 History API support
+        Backbone.history.start({pushState: true});
+
+        // All navigation that is relative should be passed through the navigate
+        // method, to be processed by the router.  If the link has a data-bypass
+        // attribute, bypass the delegation completely.
+        $(document).on('click', 'a:not([data-bypass])', function click(evt) {
+            // Get the absolute anchor href.
+            var href = {prop: $(this).prop("href"), attr: $(this).attr("href")};
+            // Get the absolute root.
+            var root = location.protocol + "//" + location.host + '/';
+            // Ensure the root is part of the anchor href, meaning it's relative.
+            if (href.prop && href.prop.slice(0, root.length) === root) {
+                // Stop the default event to ensure the link will not cause a page
+                // refresh.
+                evt.preventDefault();
+
+                // `Backbone.history.navigate` is sufficient for all Routers and will
+                // trigger the correct events. The Router's internal `navigate` method
+                // calls this anyways.  The fragment is sliced from the root.
+                Backbone.history.navigate(href.attr, true);
+            }
+        });
+    };
+
+
+    // The Router is responsible for mapping URLs to views
+    exports.Router = Backbone.Router.extend({
+
+        routes: {
+            '': 'routeHome'
+        },
+
+        routeHome: function routeHome() {
+            var view = new home.HomeView({el: $('#content')});
+            view.render();
         }
 
     });
 
-    return app;
+    return exports;
 });
