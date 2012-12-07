fe26907b763feb3841f51241d8e20c87daafc0c5
GregRR <gregroe@stanford.edu>
2012-12-07 14:33:43 -0800

    Adding SASS and Compass required files - includes files from github.com/jlong/sass-twitter-bootstrap with additional compass config file, /config.rb, and updated .gitignore

---
+++ config.rb
@@ -0,0 +1,28 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "src/encoded/static/css"
+sass_dir = "src/encoded/static/sass"
+images_dir = "src/encoded/static/img"
+javascripts_dir = "src/encoded/static/modules"
+fonts_dir = "src/encoded/static/fonts"
+
+# To export minified css, uncomment :compress, and comments out :nested
+output_style = :compressed
+# output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass src/encoded/static/sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
