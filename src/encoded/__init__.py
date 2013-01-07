5b055b63c7f46d1342f5b4e3f4416881026693c4
Laurence Rowe <lrowe@stanford.edu>
2013-01-07 09:51:01 -0800

    Use a fallback view to return the html page equivalent to the json api.

--- src/encoded/__init__.py
+++ src/encoded/__init__.py
@@ -4,17 +4,28 @@ STATIC_MAX_AGE = 0
 routes = {
     '': 'home',
     'antibodies/': 'antibodies',
-    'favicon.ico': 'favicon',
+    'antibodies/{antibody}': 'antibody',
     }
+
+
+def api_routes(config):
+    for pattern, name in routes.items():
+        config.add_route(name, pattern)
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    # Static resources
     config.add_static_view('static', 'static', cache_max_age=STATIC_MAX_AGE)
     config.add_static_view('tests/js', 'tests/js', cache_max_age=STATIC_MAX_AGE)
-    for pattern, name in routes.items():
-        config.add_route(name, pattern)
+    config.add_route('favicon', 'favicon.ico')
+
+    config.include(api_routes, route_prefix='/api')
+
+    config.add_route('fallback', '*path')
+
     config.scan(ignore='encoded.tests')
     return config.make_wsgi_app()
--- src/encoded/views.py
+++ src/encoded/views.py
@@ -6,11 +6,50 @@ from pyramid.view import view_config
 index_html = resource_filename('encoded', 'index.html')
 favicon_ico = resource_filename('encoded', 'static/img/favicon.ico')
 
+MODELS = [
+    {
+        'uuid': 'a0000000000000000000000000000001',
+        'title': 'First model',
+        },
+    {
+        'uuid': 'b0000000000000000000000000000002',
+        'title': 'Second model',
+        },
+    {
+        'uuid': 'c0000000000000000000000000000003',
+        'title': 'Third model',
+        },
+    ]
 
-@view_config(route_name='antibodies')
-@view_config(route_name='home')
-def single_page(request):
+uuid_model = {model['uuid']: model for model in MODELS}
+
+
+@view_config(route_name='fallback', request_method='GET')
+def html_page(request):
+    path = request.matchdict['path']
+    if path and path[0] == 'api':
+        raise KeyError(request.path_info)
+    subreq = request.copy()
+    subreq.path_info = '/api' + request.path_info
+    response = request.invoke_subrequest(subreq)
     return FileResponse(index_html, request=request)
+
+
+@view_config(route_name='home', renderer='json')
+def home(request):
+    return {}
+
+
+@view_config(route_name='antibodies', renderer='json')
+def antibodies(request):
+    return MODELS
+
+
+@view_config(route_name='antibody', renderer='json')
+def antibody(request):
+    antibody_id = request.matchdict['antibody']
+    model = uuid_model[antibody_id]
+    return model
 
 
 @view_config(route_name='favicon', http_cache=(3600, {'public': True}))
