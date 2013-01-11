3c98c0669a4077ac135620f4c92a8cf53c44e2e3
Laurence Rowe <laurence@lrowe.co.uk>
2013-01-11 10:56:34 -0800

    Configure engine correctly for sqlite. Ensure tables exist on connection.

--- src/encoded/__init__.py
+++ src/encoded/__init__.py
@@ -1,6 +1,9 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-from .storage import DBSession
+from .storage import (
+    Base,
+    DBSession,
+    )
 STATIC_MAX_AGE = 0
 
 
@@ -25,13 +28,27 @@ def tests_js(config):
     config.add_static_view('tests/js', 'tests/js', cache_max_age=STATIC_MAX_AGE)
 
 
+def configure_engine(settings):
+    engine_url = settings.get('sqlalchemy.url')
+    engine_opts = {}
+    # http://docs.sqlalchemy.org/en/rel_0_8/dialects/sqlite.html#using-a-memory-database-in-multiple-threads
+    if engine_url == 'sqlite://':
+        from sqlalchemy.pool import StaticPool
+        engine_opts.update(
+            connect_args={'check_same_thread': False},
+            poolclass=StaticPool,
+            )
+    engine = engine_from_config(settings, 'sqlalchemy.', **engine_opts)
+    Base.metadata.create_all(engine)
+    DBSession.configure(bind=engine)
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     config.include('pyramid_tm')
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
+    configure_engine(settings)
 
     # Render an HTML page to browsers and a JSON document for API clients
     config.add_renderer(None, 'encoded.renderers.PageOrJSON')
