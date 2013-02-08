6cef0c6115d3ab989228028307ae892f39ad0ab3
Laurence Rowe <laurence@lrowe.co.uk>
2013-02-08 13:50:38 -0800

    Update test data. Lookup validation uuids from document. Skip incomplete test data. Test fetching antibodies list.

--- src/encoded/tests/test_views.py
+++ src/encoded/tests/test_views.py
@@ -61,3 +61,5 @@ def test_load_workbook(testapp):
     from ..loadxl import load_all
     from pkg_resources import resource_filename
     load_all(testapp, resource_filename('encoded', 'tests/data/AntibodySubmissionsENCODE3.xlsx'))
+    res = testapp.get('/antibodies/', headers={'Accept': 'application/json'}, status=200)
+    assert res.json['_embedded']['items']
