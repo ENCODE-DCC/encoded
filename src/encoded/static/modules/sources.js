796070607f32f38bf932717e77b9971017acfb8b
Laurence Rowe <laurence@lrowe.co.uk>
2013-03-12 18:20:23 -0700

    Editing of sources

--- src/encoded/static/modules/sources.js
+++ src/encoded/static/modules/sources.js
@@ -45,11 +45,64 @@ function sources(exports, $, _, base, table_sorter, table_filter, home_template,
         model_factory: exports.Source
     });
 
-    exports.SourceEditOverlay = base.Modal.extend({
-        title: "Edit",
-        template2: _.template('Hello')
+    var SourceEditOverlay = exports.SourceEditOverlay = base.Modal.extend({
+        tagName: 'form',
+        events: {'submit': 'submit'},
+        className: base.Modal.prototype.className + ' form-horizontal',
+        initialize: function initialize(options) {
+            var name = options.route_args[0];
+            this.action = _.find(this.model._links.actions, function(item) {
+                return item.name === name;
+            });
+            this.title = this.action.title;
+            this.deferred = $.get(this.action.profile);
+            this.deferred.done(_.bind(function (data) {
+                this.schema = data;
+            }, this));
+        },
+        render: function render() {
+            this.value = this.model.toJSON();
+            SourceEditOverlay.__super__.render.apply(this, arguments);
+            this.form = this.$('.modal-body').jsonForm({
+                schema: this.schema,
+                form: _.without(_.keys(this.schema.properties), '_uuid'),
+                value: this.value,
+                submitEvent: false,
+                onSubmitValid: _.bind(this.send, this)
+            });
+            return this;
+        },
+        submit: function submit(evt) {
+            this.form.submit(evt);
+        },
+        send: function send(value)  {
+            this.value = value;
+            // Essentially just $.ajax but triggering some events
+            this.model.sync(null, this.model, {
+                url: this.action.href,
+                type: this.action.method,
+                contentType: 'application/json',
+                data: JSON.stringify(value),
+                dataType: 'json'
+            }).done(_.bind(function (data) {
+                // close, refresh
+                console.log(data);
+                var url = data._links.items[0].href;
+                // force a refresh
+                app.view_registry.history.path = null;
+                app.view_registry.history.navigate(url, {trigger: true});
+            }, this)).fail(_.bind(function (data) {
+                // flag errors, try again
+                console.log(data);
+            }, this));
+            // Stop event propogation
+            return false;
+        }
     }, {
-        route_name: 'edit'
+        route_name: 'edit',
+        model_factory: function model_factory(attrs, options) {
+            return app.view_registry.current_views.content.model;
+        }
     });
 
     return exports;
