f5292c8b66571716c3ba400bf579fca390d0f165
Laurence Rowe <laurence@lrowe.co.uk>
2013-03-20 15:53:56 -0700

    NamespacedAuthenticationPolicy

---
+++ src/encoded/authentication.py
@@ -0,0 +1,60 @@
+from pyramid.path import (
+    DottedNameResolver,
+    caller_package,
+    )
+
+
+class NamespacedAuthenticationPolicy(object):
+    """ Wrapper for authentication policy classes
+
+    As userids are included in the list of principals, it seems good practice
+    to namespace them to avoid clashes.
+
+    Constructor Arguments
+
+    ``namespace``
+
+        The namespace used (string).
+
+    ``base``
+
+        The base authentication policy (class or dotted name).
+
+    Remaining arguments are passed to the ``base`` constructor.
+
+    Example
+
+    To make a ``REMOTE_USER`` 'admin' be 'user:admin'
+
+    .. code-block:: python
+
+        policy = NamespacedAuthenticationPolicy('user',
+            'pyramid.authentication.RemoteUserAuthenticationPolicy')
+    """
+
+    def __new__(cls, namespace, base, *args, **kw):
+        # Dotted name support makes it easy to configure with pyramid_multiauth
+        name_resolver = DottedNameResolver(caller_package())
+        base = name_resolver.maybe_resolve(base)
+        # Dynamically create a subclass
+        name = 'Namespaced_%s_%s' % (namespace, base.__name__)
+        klass = type(name, (cls, base), {'_namespace_prefix': namespace + ':'})
+        return super(NamespacedAuthenticationPolicy, klass) \
+                                                .__new__(klass, *args, **kw)
+
+    def __init__(self, namespace, base, *args, **kw):
+        super(NamespacedAuthenticationPolicy, self).__init__(*args, **kw)
+
+    def unauthenticated_userid(self, request):
+        userid = super(NamespacedAuthenticationPolicy, self) \
+                                            .unauthenticated_userid(request)
+        if userid is not None:
+            userid = self._namespace_prefix + userid
+        return userid
+
+    def remember(self, request, principal, **kw):
+        if not principal.startswith(self._namespace_prefix):
+            return []
+        principal = principal[len(self._namespace_prefix):]
+        return super(NamespacedAuthenticationPolicy, self) \
+                                            .remember(request, principal, **kw)
---
+++ src/encoded/tests/test_authentication.py
@@ -0,0 +1,102 @@
+import unittest
+from pyramid.tests.test_authentication import DummyRequest
+
+
+class TestNamespacedAuthenticationPolicy(unittest.TestCase):
+    """ This is a modified version of TestRemoteUserAuthenticationPolicy
+    """
+    def _getTargetClass(self):
+        from encoded.authentication import NamespacedAuthenticationPolicy
+        return NamespacedAuthenticationPolicy
+
+    def _makeOne(self, namespace='user',
+                base='pyramid.authentication.RemoteUserAuthenticationPolicy',
+                *args, **kw):
+        return self._getTargetClass()(namespace, base, *args, **kw)
+
+    def test_class_implements_IAuthenticationPolicy(self):
+        from zope.interface.verify import verifyClass
+        from pyramid.interfaces import IAuthenticationPolicy
+        klass = self._makeOne().__class__
+        verifyClass(IAuthenticationPolicy, klass)
+
+    def test_instance_implements_IAuthenticationPolicy(self):
+        from zope.interface.verify import verifyObject
+        from pyramid.interfaces import IAuthenticationPolicy
+        verifyObject(IAuthenticationPolicy, self._makeOne())
+
+    def test_unauthenticated_userid_returns_None(self):
+        request = DummyRequest({})
+        policy = self._makeOne()
+        self.assertEqual(policy.unauthenticated_userid(request), None)
+
+    def test_unauthenticated_userid(self):
+        request = DummyRequest({'REMOTE_USER':'fred'})
+        policy = self._makeOne()
+        self.assertEqual(policy.unauthenticated_userid(request), 'user:fred')
+
+    def test_authenticated_userid_None(self):
+        request = DummyRequest({})
+        policy = self._makeOne()
+        self.assertEqual(policy.authenticated_userid(request), None)
+
+    def test_authenticated_userid(self):
+        request = DummyRequest({'REMOTE_USER':'fred'})
+        policy = self._makeOne()
+        self.assertEqual(policy.authenticated_userid(request), 'user:fred')
+
+    def test_effective_principals_None(self):
+        from pyramid.security import Everyone
+        request = DummyRequest({})
+        policy = self._makeOne()
+        self.assertEqual(policy.effective_principals(request), [Everyone])
+
+    def test_effective_principals(self):
+        from pyramid.security import Everyone
+        from pyramid.security import Authenticated
+        request = DummyRequest({'REMOTE_USER':'fred'})
+        policy = self._makeOne()
+        self.assertEqual(policy.effective_principals(request),
+                         [Everyone, Authenticated, 'user:fred'])
+
+    def test_remember(self):
+        request = DummyRequest({'REMOTE_USER':'fred'})
+        policy = self._makeOne()
+        result = policy.remember(request, 'fred')
+        self.assertEqual(result, [])
+
+    def test_forget(self):
+        request = DummyRequest({'REMOTE_USER':'fred'})
+        policy = self._makeOne()
+        result = policy.forget(request)
+        self.assertEqual(result, [])
+
+    # From TestSessionAuthenticationPolicy
+
+    def test_session_remember(self):
+        request = DummyRequest()
+        policy = self._makeOne(
+                    base='pyramid.authentication.SessionAuthenticationPolicy',
+                    prefix='')
+        result = policy.remember(request, 'user:fred')
+        self.assertEqual(request.session.get('userid'), 'fred')
+        self.assertEqual(result, [])
+        self.assertEqual(policy.unauthenticated_userid(request), 'user:fred')
+
+    def test_session_forget(self):
+        request = DummyRequest(session={'userid':'fred'})
+        policy = self._makeOne(
+                    base='pyramid.authentication.SessionAuthenticationPolicy',
+                    prefix='')
+        result = policy.forget(request)
+        self.assertEqual(request.session.get('userid'), None)
+        self.assertEqual(result, [])
+
+    def test_session_forget_no_identity(self):
+        request = DummyRequest()
+        policy = self._makeOne(
+                    base='pyramid.authentication.SessionAuthenticationPolicy',
+                    prefix='')
+        result = policy.forget(request)
+        self.assertEqual(request.session.get('userid'), None)
+        self.assertEqual(result, [])
