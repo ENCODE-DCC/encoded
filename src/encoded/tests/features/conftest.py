d937b97e0421f71a4c587063f11bed9e1a0feadd
Laurence Rowe <laurence@lrowe.co.uk>
2013-04-02 20:06:48 -0700

    Refactor pytest/behave integration to use pytest reporting. Would be nice to add some nicer reporting on features/scenarios, but bin/test -v -v -m bdd works well for now.

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -9,81 +9,105 @@ from _pytest.python import FixtureRequest, FuncFixtureInfo
 
 @contextmanager
 def fixture_context(fixture_request):
-    from pytest.behave import _fixture_requests
+    """ Manages the fixture_request context
+    """
+    from pytest.bdd import _fixture_requests
     _fixture_requests.append(fixture_request)
     yield
     popped = _fixture_requests.pop()
     assert popped is fixture_request
 
 
-# pytest.behave.getfixture
+# pytest.bdd.getfixture
 def getfixture(name):
-    from pytest.behave import _fixture_requests
+    from pytest.bdd import _fixture_requests
     fixture_request = _fixture_requests[-1]
     return fixture_request.getfuncargvalue(name)
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "behave: denotes a behave BDD test.")
+    config.addinivalue_line("markers", "bdd: denotes a BDD test.")
+    bdd = BDDPlugin(config)
+    config.pluginmanager.register(bdd, 'bdd')
 
 
-def pytest_namespace():
-    """ Behave currently relies on a single global step registry
-    """
-    # from behave.matchers import step_matcher
-    from behave.step_registry import registry
-    hooks = {}
+class BDDPlugin(object):
+    def __init__(self, config):
+        self.config = config
 
-    def _make_hook(hook_name):
-        def decorator(wrapped):
-            hooks.setdefault(hook_name, []).append(wrapped)
-            return wrapped
+    @property
+    def reporter(self):
+        return self.config.pluginmanager.getplugin('terminalreporter')
 
-        decorator.__name__ = hook_name
-        return decorator
+    @pytest.mark.tryfirst
+    def pytest_runtest_logstart(self, nodeid, location):
+        # ensure that the path is printed before the
+        # 1st test of a module starts running
+        reporter = self.reporter
+        if not reporter.showlongtestinfo:
+            return
+        #fspath = nodeid.split("::")[0]
 
-    _make_hook.__hooks__ = hooks
+    def pytest_runtest_setup(self, item):
+        pass
 
-    ns = {
-        # 'step_matcher': step_matcher,
-        '_registry': registry,
-        '_make_hook': _make_hook,  # Can't simply be a dict
-        '_fixture_requests': [],  # Set later
-        'getfixture': getfixture,
-    }
+    def pytest_collect_file(self, parent, path):
+        if path.ext == '.feature':
+            return FeatureFile(path, parent, self)
+        dirpath = path.dirpath()
+        if path.ext == '.py' and dirpath.basename == 'steps':
+            path.pyimport()
+            return
+        if dirpath.basename == 'features' and \
+                path.basename == 'environment.py':
+            path.pyimport()
+            return
 
-    for step_type in ('given', 'when', 'then', 'step'):
-        decorator = registry.make_decorator(step_type)
-        ns[step_type] = decorator
-        ns[step_type.title()] = decorator
+    def pytest_namespace(self):
+        """ Behave currently relies on a single global step registry
+        """
+        # from behave.matchers import step_matcher
+        from behave.step_registry import registry
+        hooks = {}
 
-    # Make before_step, etc. decorators
-    for timing in ['before', 'after']:
-        for place in ['step', 'scenario', 'feature', 'tag', 'all']:
-            name = '%s_%s' % (timing, place)
-            ns[name] = _make_hook(name)
+        def _make_hook(hook_name):
+            def decorator(wrapped):
+                hooks.setdefault(hook_name, []).append(wrapped)
+                return wrapped
 
-    return {'behave': ns}
+            decorator.__name__ = hook_name
+            return decorator
 
+        _make_hook.__hooks__ = hooks
 
-def pytest_collect_file(parent, path):
-    if path.ext == '.feature':
-        return FeatureFile(path, parent)
-    dirpath = path.dirpath()
-    if path.ext == '.py' and dirpath.basename == 'steps':
-        path.pyimport()
-        return
-    if dirpath.basename == 'features' and \
-            path.basename == 'environment.py':
-        path.pyimport()
-        return
+        ns = {
+            # 'step_matcher': step_matcher,
+            '_registry': registry,
+            '_make_hook': _make_hook,  # Can't simply be a dict
+            '_fixture_requests': [],  # Set later
+            'getfixture': getfixture,
+        }
+
+        for step_type in ('given', 'when', 'then', 'step'):
+            decorator = registry.make_decorator(step_type)
+            ns[step_type] = decorator
+            ns[step_type.title()] = decorator
+
+        # Make before_step, etc. decorators
+        for timing in ['before', 'after']:
+            for place in ['step', 'scenario', 'feature', 'tag', 'all']:
+                name = '%s_%s' % (timing, place)
+                ns[name] = _make_hook(name)
+
+        return {'bdd': ns}
 
 
 class FeatureFile(pytest.File):
-    def __init__(self, path, parent):
+    def __init__(self, path, parent, bdd_plugin):
         super(FeatureFile, self).__init__(path, parent)
         # Set a keyword for all behave tests
-        self.keywords['behave'] = pytest.mark.behave
+        self.keywords['bdd'] = pytest.mark.bdd
+        self.bdd_plugin = bdd_plugin
 
     # Must be a separate object for the child fixture request to work.
     def collect(self):
@@ -112,12 +136,18 @@ class FixtureRequestMixin(object):
 
 
 class Feature(FixtureRequestMixin, pytest.Collector):
-    def __init__(self, feature, parent):
-        super(Feature, self).__init__(feature.name, parent)
-        self.feature = feature
+    outline = None
+
+    def __init__(self, model, parent):
+        name = '%s: %s' % (model.keyword, model.name)
+        super(Feature, self).__init__(name, parent)
+        self.feature = self.model = model
         for tag in self.feature.tags:
             marker = getattr(pytest.mark, tag)
             self.keywords[marker.markname] = marker
+
+    def reportinfo(self):
+        return self.fspath, self.model.line, self.name
 
     def collect(self):
         from behave import model
@@ -135,11 +165,7 @@ class Feature(FixtureRequestMixin, pytest.Collector):
     def setup(self):
         runner = self.runner
         feature = self.feature
-        from behave.formatter import formatters
         runner.feature = feature
-        stream = runner.config.output
-        runner.formatter = formatters.get_formatter(runner.config, stream)
-        runner.formatter.uri(feature.filename)
 
         runner.context._push()
         runner.context.feature = feature
@@ -147,15 +173,10 @@ class Feature(FixtureRequestMixin, pytest.Collector):
         # current tags as a set
         runner.context.tags = set(feature.tags)
 
-        runner.formatter.feature(feature)
-
         with fixture_context(self.fixture_request):
             for tag in feature.tags:
                 runner.run_hook('before_tag', runner.context, tag)
             runner.run_hook('before_feature', runner.context, feature)
-
-        if feature.background:
-            runner.formatter.background(feature.background)
 
     def teardown(self):
         runner = self.runner
@@ -168,24 +189,15 @@ class Feature(FixtureRequestMixin, pytest.Collector):
 
         runner.context._pop()
 
-        runner.formatter.eof()
-        runner.formatter.stream.write('\n')
-
-        stream = runner.config.output
-        runner.formatter.close()
-        stream.write('\n')
-
-        for reporter in runner.config.reporters:
-            reporter.feature(feature)
-
-    def reportinfo(self):
-        return self.fspath, 0, "Feature: %s" % self.name
-
 
 class ScenarioOutline(FixtureRequestMixin, pytest.Collector):
-    def __init__(self, outline, parent):
-        super(ScenarioOutline, self).__init__(outline.name, parent)
-        self.outline = outline
+    def __init__(self, model, parent):
+        name = '%s: %s' % (model.keyword, model.name)
+        super(ScenarioOutline, self).__init__(name, parent)
+        self.outline = self.model = model
+
+    def reportinfo(self):
+        return self.fspath, self.model.line, self.name
 
     @property
     def runner(self):
@@ -201,14 +213,22 @@ class ScenarioOutline(FixtureRequestMixin, pytest.Collector):
 
 
 class Scenario(FixtureRequestMixin, pytest.Collector):
-    def __init__(self, scenario, parent):
-        super(Scenario, self).__init__(scenario.name, parent)
-        self.scenario = scenario
+    def __init__(self, model, parent):
+        name = '%s: %s' % (model.keyword, model.name)
+        row = getattr(model, '_row', None)
+        if row is not None:
+            name += '[%s]' % '-'.join(row.cells)
+
+        super(Scenario, self).__init__(name, parent)
+        self.scenario = self.model = model
         for tag in self.scenario.tags:
             marker = getattr(pytest.mark, tag)
             self.keywords[marker.markname] = marker
         self.undefined = []
         self.run_steps = True
+
+    def reportinfo(self):
+        return self.fspath, self.model.line, self.name
 
     @property
     def runner(self):
@@ -217,6 +237,10 @@ class Scenario(FixtureRequestMixin, pytest.Collector):
     @property
     def feature(self):
         return self.parent.feature
+
+    @property
+    def outline(self):
+        return self.parent.outline
 
     def collect(self):
         for step in self.scenario:
@@ -229,8 +253,6 @@ class Scenario(FixtureRequestMixin, pytest.Collector):
         scenario = self.scenario
 
         tags = feature.tags + scenario.tags
-
-        runner.formatter.scenario(scenario)
 
         row = getattr(scenario, '_row', None)
         if row is not None:
@@ -248,7 +270,6 @@ class Scenario(FixtureRequestMixin, pytest.Collector):
             runner.run_hook('before_scenario', runner.context, scenario)
 
         for step in scenario:
-            runner.formatter.step(step)
             step.match = registry.find_match(step)
             if step.match is None:
                 self.undefined.append(step)
@@ -258,8 +279,8 @@ class Scenario(FixtureRequestMixin, pytest.Collector):
         runner = self.runner
         scenario = self.scenario
 
-        for step in self.undefined:
-            runner.undefined.append(step)
+        #for step in self.undefined:
+        #    runner.undefined.append(step)
 
         with fixture_context(self.fixture_request):
             runner.run_hook('after_scenario', runner.context, scenario)
@@ -271,9 +292,13 @@ class Scenario(FixtureRequestMixin, pytest.Collector):
 
 
 class Step(FixtureRequestMixin, pytest.Item):
-    def __init__(self, step, parent):
-        super(Step, self).__init__(step.name, parent)
-        self.step = step
+    def __init__(self, model, parent):
+        name = '%s %s' % (model.keyword, model.name)
+        super(Step, self).__init__(name, parent)
+        self.step = self.model = model
+
+    def reportinfo(self):
+        return self.fspath, self.model.line, self.name
 
     @property
     def runner(self):
@@ -284,19 +309,20 @@ class Step(FixtureRequestMixin, pytest.Item):
         return self.parent.feature
 
     @property
+    def outline(self):
+        return self.parent.outline
+
+    @property
     def scenario(self):
         return self.parent.scenario
 
     def setup(self):
-        from behave.model import NoMatch
         runner = self.runner
         step = self.step
         match = step.match
 
         if match is None:
             self.parent.run_steps = False
-            runner.formatter.match(NoMatch())
-            runner.formatter.result(self)
             pytest.skip('No match')
             return
 
@@ -304,121 +330,47 @@ class Step(FixtureRequestMixin, pytest.Item):
             pytest.skip('Skipped')
             return
 
-        runner.formatter.match(match)
-
         with fixture_context(self.fixture_request):
             runner.run_hook('before_step', runner.context, self)
 
     def runtest(self):
-        import time
         runner = self.runner
         step = self.step
         match = step.match
         try:
-            start = time.time()
-            try:
-                if step.text:
-                    runner.context.text = step.text
-                if step.table:
-                    runner.context.table = step.table
-                match.run(runner.context)
-                step.status = 'passed'
-            except Exception:
-                step.status = 'failed'
-                self.parent.run_steps = False
-                runner.context._set_root_attribute('failed', True)
-                raise
-        finally:
-            step.duration = time.time() - start
+            if step.text:
+                runner.context.text = step.text
+            if step.table:
+                runner.context.table = step.table
+            match.run(runner.context)
+            step.status = 'passed'
+        except Exception:
+            step.status = 'failed'
+            self.parent.run_steps = False
+            runner.context._set_root_attribute('failed', True)
+            raise
 
     def teardown(self):
         runner = self.runner
         step = self.step
 
-        runner.formatter.result(step)
         with fixture_context(self.fixture_request):
             runner.run_hook('after_step', runner.context, step)
 
 
 @pytest.fixture(scope='session')
-def behave_config():
-    # FIXME: read values from pytestconfig
-    import sys
-    from behave.reporter.junit import JUnitReporter
-    from behave.reporter.summary import SummaryReporter
-    from behave.tag_expression import TagExpression
-
-    class Configuration(object):
-        color = sys.platform != 'win32'
-        stdout_capture = False
-        stderr_capture = False
-        show_snippets = True
-        show_skipped = True
-        log_capture = False
-        dry_run = False
-        show_source = True
-        show_timings = True
-        logging_format = '%(levelname)s:%(name)s:%(message)s'
-        summary = False
-        junit = False
-        junit_directory = 'reports'
-        format = ['plain']  # ['pretty']
-        output = sys.stdout
-        wip = False
-        quiet = False
-        show_multiline = False
-        stop = False
-        verbose = False
-        expand = False
-
-        def __init__(self, **kw):
-            self.formatters = []
-            self.reporters = []
-            self.tags = []
-
-            self.__dict__.update(kw)
-
-            if self.wip:
-                # Only run scenarios tagged with "wip". Additionally: use the
-                # "plain" formatter, do not capture stdout or logging output and
-                # stop at the first failure.
-                self.format = ['plain']
-                self.tags = ['wip']
-                self.stop = True
-                self.log_capture = False
-                self.stdout_capture = False
-
-            self.tags = TagExpression(self.tags or [])
-
-            if self.quiet:
-                self.show_source = False
-                self.show_snippets = False
-
-            if self.junit:
-                # Buffer the output (it will be put into Junit report)
-                self.stdout_capture = True
-                self.stderr_capture = True
-                self.log_capture = True
-                self.reporters.append(JUnitReporter(self))
-            if self.summary:
-                self.reporters.append(SummaryReporter(self))
-
-    config = Configuration()
-    return config
-
-
-@pytest.fixture(scope='session')
-def behave_runner(request, behave_config):
+def behave_runner(request):
     import behave.runner
-    import pytest.behave
+    import pytest.bdd
 
-    class Runner(behave.runner.Runner):
-        def __init__(self, config):
-            super(Runner, self).__init__(config)
+    class Runner(object):
+        config = None
+
+        def __init__(self):
             self.context = behave.runner.Context(self)
-            self.hooks = pytest.behave._make_hook.__hooks__
+            self.hooks = pytest.bdd._make_hook.__hooks__
             self.context._set_root_attribute('getfixture',
-                                             pytest.behave.getfixture)
+                                             pytest.bdd.getfixture)
 
         def run_hook(self, name, context, *args):
             with fixture_context(request):
@@ -428,14 +380,12 @@ def behave_runner(request, behave_config):
                         for hook in self.hooks[name]:
                             hook(context, *args)
 
-    runner = Runner(behave_config)
+    runner = Runner()
 
     @request.addfinalizer
     def after_all():
         with fixture_context(request):
             runner.run_hook('after_all', runner.context)
-        for reporter in runner.config.reporters:
-            reporter.end()
 
     with fixture_context(request):
         runner.run_hook('before_all', runner.context)
--- src/encoded/tests/features/environment.py
+++ src/encoded/tests/features/environment.py
@@ -2,25 +2,25 @@ from behaving.web import environment as webenv
 import pytest
 
 
-@pytest.behave.before_all
+@pytest.bdd.before_all
 def before_all(context):
     webenv.before_all(context)
-    server = pytest.behave.getfixture('_server')
-    pytest.behave.getfixture('testdata')
+    server = pytest.bdd.getfixture('_server')
+    pytest.bdd.getfixture('testdata')
     context.base_url = server.application_url
 
 
-@pytest.behave.after_all
+@pytest.bdd.after_all
 def after_all(context):
     webenv.after_all(context)
 
 
-@pytest.behave.before_scenario
+@pytest.bdd.before_scenario
 def before_scenario(context, scenario):
-    pytest.behave.getfixture('external_tx')
+    pytest.bdd.getfixture('external_tx')
     webenv.before_scenario(context, scenario)
 
 
-@pytest.behave.after_scenario
+@pytest.bdd.after_scenario
 def after_scenario(context, scenario):
     webenv.after_scenario(context, scenario)
