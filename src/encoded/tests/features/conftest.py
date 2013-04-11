d495b6870317139d3ef831617f7d2cf3a6b0091e
Laurence Rowe <laurence@lrowe.co.uk>
2013-04-10 18:45:34 -0700

    Notes to self.

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -8,26 +8,32 @@ def scenario_tx(external_tx):
     pass
 
 
+#@pytest.fixture(scope='session')
+def browser(context, before_all):
+    from behaving.web.steps.browser import given_a_browser
+    # context.default_browser = 'remote'
+    given_a_browser(context)
+
+
 # These are equivalent to the environment.py hooks
 #@pytest.fixture(scope='session', autouse=True)
 def before_all(request, _server, context):
-    from behaving.web import environment as webenv
-    webenv.before_all(context)
+    import behaving.web
+    behaving.web.setup(context)
     context.base_url = _server.application_url
 
     @request.addfinalizer
     def after_all():
-        webenv.after_all(context)
+        behaving.web.teardown(context)
 
 
-#@pytest.fixture(autouse=True)
+#@pytest.fixture(scope='function', autouse=True)
 def before_scenario(request, context, scenario):
-    from behaving.web import environment as webenv
-    webenv.before_scenario(context, scenario)
+    pass
 
     @request.addfinalizer
     def after_scenario():
-        webenv.after_scenario(context, scenario)
+        pass
 
 
 #@pytest.fixture(scope='subfunction', autouse=True)
