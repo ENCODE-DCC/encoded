e54fc8a9f1ab92724a7dec5aaac23ca99f3aabf1
Laurence Rowe <laurence@lrowe.co.uk>
2013-04-16 15:14:17 -0700

    Increase wait_time for travis tests.

--- src/encoded/tests/__init__.py
+++ src/encoded/tests/__init__.py
@@ -2,7 +2,14 @@
 
 
 def pytest_addoption(parser):
+
+    def int_arg(options, opt, value, parser):
+        parser.values.ensure_value(options.dest, [])
+        value = (value[0], int(value[1]))
+        getattr(parser.values, options.dest).append(value)
+
     parser.addoption('--engine-url', dest='engine_url', default='sqlite://')
     parser.addoption('--browser', dest='browser', default=None)
-    parser.addoption('--browser-arg', nargs=2, dest='browser_args', action='append')
+    parser.addoption('--browser-arg', nargs=2, dest='browser_args', action='append', type='string')
+    parser.addoption('--browser-arg-int', nargs=2, dest='browser_args', action='callback', callback=int_arg, type='string')
     parser.addoption('--remote-webdriver', dest='remote_webdriver', action='store_true', default=False)
