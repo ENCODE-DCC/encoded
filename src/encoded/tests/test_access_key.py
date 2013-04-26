883cd72c33ae434f8452ca6923eaa2aa8dfd5f3d
Laurence Rowe <laurence@lrowe.co.uk>
2013-04-26 14:41:27 -0700

    Access key tests.

--- src/encoded/tests/test_access_key.py
+++ src/encoded/tests/test_access_key.py
@@ -1,7 +1,19 @@
 import pytest
 
-@pytest.fixture
-def users(testapp):
+
+def basic_auth(username, password):
+    from base64 import b64encode
+    return 'Basic ' + b64encode('%s:%s' % (username, password))
+
+
+@pytest.datafixture
+def access_keys(app):
+    from webtest import TestApp
+    environ = {
+        'HTTP_ACCEPT': 'application/json',
+        'REMOTE_USER': 'TEST',
+    }
+    testapp = TestApp(app, environ)
     from .sample_data import URL_COLLECTION
     url = '/users/'
     users = []
@@ -18,17 +30,6 @@ def users(testapp):
             'effective_principals': sorted(principals),
             '_uuid': item['_uuid'],
         })
-    return users
-
-
-@pytest.fixture
-def user(users):
-    return users[0]
-
-
-@pytest.fixture
-def access_keys(testapp, users):
-    from base64 import b64encode
     access_keys = []
     for user in users:
         description = 'My programmatic key'
@@ -39,8 +40,7 @@ def access_keys(testapp, users):
             'location': res.location,
             'access_key_id': res.json['access_key_id'],
             'secret_access_key': res.json['secret_access_key'],
-            'auth_header': 'Basic ' + b64encode(
-                '%s:%s' % (res.json['access_key_id'], res.json['secret_access_key'])),
+            'auth_header': basic_auth(res.json['access_key_id'], res.json['secret_access_key']),
             'user_uuid': user['_uuid'],
             'description': description,
             'user': user,
@@ -53,7 +53,7 @@ def access_key(access_keys):
     return access_keys[0]
 
 
-def test_access_key_post(anontestapp, execute_counter, access_key):
+def test_access_key_principals(anontestapp, execute_counter, access_key):
     headers = {'Authorization': access_key['auth_header']}
     with execute_counter.expect(2):
         res = anontestapp.get('/@@testing-user', headers=headers)
@@ -63,6 +63,39 @@ def test_access_key_post(anontestapp, execute_counter, access_key):
         'accesskey:' + access_key['access_key_id'],
     ] + access_key['user']['effective_principals']
 
+
+def test_access_key_reset(anontestapp, access_key):
+    headers = {'Authorization': access_key['auth_header']}
+    res = anontestapp.post_json(access_key['location'] + '/@@reset-secret', {}, headers=headers)
+    new_headers = {'Authorization': basic_auth(access_key['access_key_id'], res.json['secret_access_key'])}
+
+    res = anontestapp.get('/@@testing-user', headers=headers)
+    assert res.json['authenticated_userid'] is None
+
+    res = anontestapp.get('/@@testing-user', headers=new_headers)
+    assert res.json['authenticated_userid'] == 'accesskey:' + access_key['access_key_id']
+
+
+def test_access_key_disable(anontestapp, access_key):
+    headers = {'Authorization': access_key['auth_header']}
+    res = anontestapp.post_json(access_key['location'] + '/@@disable-secret', {}, headers=headers)
+
+    res = anontestapp.get('/@@testing-user', headers=headers)
+    assert res.json['authenticated_userid'] is None
+
+
+def test_access_key_edit(anontestapp, access_key):
+    headers = {'Authorization': access_key['auth_header']}
+    NEW_DESCRIPTION = 'new description'
+    properties = {'description': NEW_DESCRIPTION}
+    anontestapp.post_json(access_key['location'], properties, headers=headers)
+
+    res = anontestapp.get(access_key['location'], properties, headers=headers)
+    assert res.json['description'] == NEW_DESCRIPTION
+
+
+@pytest.mark.xfail
+def test_access_key_view_hides_secret_access_key_hash(anontestapp, access_key):
+    headers = {'Authorization': access_key['auth_header']}
     res = anontestapp.get(access_key['location'], headers=headers)
-    assert 'description' in res.json
-    #assert 'secret_access_key_hash' not in res.json
+    assert 'secret_access_key_hash' not in res.json
