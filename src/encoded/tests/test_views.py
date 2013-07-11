5614ca4e7d186977d32443fee0e390d30f919173
Laurence Rowe <laurence@lrowe.co.uk>
2013-07-10 22:34:50 -0700

    Fix load workbook test to count with limit=all

--- src/encoded/tests/test_views.py
+++ src/encoded/tests/test_views.py
@@ -90,7 +90,7 @@ def __test_sample_data(testapp):
 def test_load_workbook(workbook, testapp, url, length):
     # testdata must come before testapp in the funcargs list for their
     # savepoints to be correctly ordered.
-    res = testapp.get(url, status=200)
+    res = testapp.get(url + '?limit=all', status=200)
     assert len(res.json['items']) >= length
     # extra guys are fine
 
