e706ba14a47eaf4637623d541d1960c9da83b728
Laurence Rowe <laurence@lrowe.co.uk>
2013-07-29 17:51:55 -0700

    Support for :object columns.

--- src/encoded/typedsheets.py
+++ src/encoded/typedsheets.py
@@ -1,51 +1,70 @@
 from pyramid.settings import asbool
 
 
-def parse_array(value):
-    return [v.strip() for v in value.decode('utf-8').split(';') if v.strip()]
+def parse_array(types, value):
+    return [cast(types, v) for v in value.split(';') if v.strip()]
 
 
-def decode_utf8(value):
+def parse_object(types, value):
+    items = (part.split(':', 1) for part in value.split(',') if value.strip())
+    return {k.strip(): cast(types, v) for k, v in items}
+
+
+def parse_string(types, value):
+    assert not types
     return value.decode('utf-8')
 
 
-def ignore(value):
+def parse_ignore(types, value):
     return None
 
 
-def parse_number(value):
+def parse_number(types, value):
+    assert not types
     try:
         return int(value)
     except ValueError:
         return float(value)
 
 
+def parse_integer(types, value):
+    assert not types
+    return int(value)
+
+
+def parse_boolean(types, value):
+    assert not types
+    return asbool(value)
+
+
 TYPE_BY_NAME = {
-    'string': decode_utf8,
+    'string': parse_string,
     'number': parse_number,
-    'boolean': asbool,
-    'integer': int,
+    'boolean': parse_boolean,
+    'integer': parse_integer,
+    'ignore': parse_ignore,
     'array': parse_array,
-    'ignore': ignore,
+    'object': parse_object,
 }
 
 
-def convert(name, value):
-    """ fieldname:<cast>, value -> fieldname, cast(value)
-    """
-    parts = name.rsplit(':', 1)
-    name = parts[0]
-    if len(parts) == 2:
-        type_name = parts[1]
-    else:
-        type_name = 'string'
+def cast(types, value):
+    types = list(types) or ['string']
+    type_name = types.pop()
+    value = value.strip()
     if value.lower() == 'null':
-        return name, None
-    if type_name != 'string' and value == '':
-        return name, None
-    cast = TYPE_BY_NAME[type_name]
-    value = cast(value)
-    return name, value
+        return None
+    if value == '' and type_name != 'string':
+        return None
+    parse = TYPE_BY_NAME[type_name]
+    return parse(types, value)
+
+
+def convert(name, value):
+    """ fieldname:<cast>[:<cast>...], value -> fieldname, cast(value)
+    """
+    parts = name.split(':')
+    return parts[0], cast(parts[1:], value)
 
 
 def cast_row_values(dictrows):
