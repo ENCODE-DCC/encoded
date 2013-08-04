d9d043b217571c565aa9e7e27fdf4afda845276f
Laurence Rowe <laurence@lrowe.co.uk>
2013-08-04 16:25:05 -0700

    Add Encode and UCSC logos to footer; Make footer smaller.

--- src/encoded/static/components/footer.js
+++ src/encoded/static/components/footer.js
@@ -13,10 +13,8 @@ function (React) {
             console.log('render footer');
             return (
                 <footer id="page-footer">
-                    <div id="footer-top">
+                    <div class="text">
                         <div class="container">
-                            <a href="http://www.stanford.edu"><img src="/static/img/su-logo.png" alt="Stanford University" id="su-logo" /></a>
-                            <a href="http://med.stanford.edu/"><img src="/static/img/som-logo.png" alt="Stanford University School of Medicine" id="som-logo" /></a>
                             <div id="footer-links">
                                 <ul>
                                     <li><a href="/">ENCODE</a></li>
@@ -25,13 +23,16 @@ function (React) {
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                    <div id="footer-bottom">
                         <div class="container">
                             &copy;2013. <a href="http://www.stanford.edu/">Stanford University</a>.
                             <a href="http://med.stanford.edu/">School of Medicine</a>.
                             <a href="http://genetics.stanford.edu/">Department of Genetics</a>.
                         </div>
+                    </div>
+                    <div class="container logos">
+                        <a class="left" href="http://encodeproject.org/"><img src="/static/img/encode-logo.png" alt="ENCODE" /></a>
+                        <a class="right" href="http://www.stanford.edu/"><img src="/static/img/su-logo.png" alt="Stanford University" /></a>
+                        <a class="right" href="http://www.ucsc.edu/"><img src="/static/img/ucsc-logo.png" alt="University of California Santa Cruz" /></a>
                     </div>
                 </footer>
             );
