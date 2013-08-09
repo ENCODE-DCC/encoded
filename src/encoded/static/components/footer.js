e5853f3f98cf948563c0f2b5a2fe5f12010afa17
GregRR <gregroe@stanford.edu>
2013-08-09 10:04:22 -0700

    Updated footer with logos.

--- src/encoded/static/components/footer.js
+++ src/encoded/static/components/footer.js
@@ -16,16 +16,19 @@ function (React) {
 					<div class="container">
 						<div id="footer-links">
 							<ul>
-								<li><a href="/">ENCODE</a></li>
-								<li><a href="">Contact</a></li>
+								<li><a href="/"><img src="/static/img/encode-logo-small.png" alt="ENCODE" id="encode-logo" /></a></li>
+								<li><a href="#contact">Contact</a></li>
 								<li><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
+								<li>&copy;2013. Stanford University.</li>
 							</ul>
 						</div>
 						<div id="footer-logos">
 							<ul>
+								
+								<li><a href="http://www.ucsc.edu"><img src="/static/img/ucsc-logo-white-alt.png" alt="UC Santa Cruz" id="ucsc-logo" /></a>
+								</li>
 								<li><a href="http://www.stanford.edu"><img src="/static/img/su-logo-white.png" alt="Stanford University" id="su-logo" /></a></li>
-								<li><a href="http://med.stanford.edu/"><img src="/static/img/som-logo-red.png" alt="Stanford University School of Medicine" id="som-logo" /></a></li>
-								<li>&copy;2013. Stanford University.</li>
+								
 							</ul>
 						</div>
 					</div>
