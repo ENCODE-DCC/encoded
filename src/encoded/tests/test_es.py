6fb3c99cc8326b3f13604fa24965b05f8afb7347
nikhilRP <nikhilrp@stanford.edu>
2013-09-04 15:15:23 -0700

    Added one more test for ElasticSearch indexing and checking the mapping

--- src/encoded/tests/test_es.py
+++ src/encoded/tests/test_es.py
@@ -1,4 +1,4 @@
-from pyelasticsearch import ElasticSearch
+from pyelasticsearch import ElasticSearch, IndexAlreadyExistsError
 
 
 def test_es_conn():
@@ -16,3 +16,67 @@ def test_delete_index():
     connection = ElasticSearch('http://localhost:9200')
     result = connection.delete_index('test_index')
     assert result == {'acknowledged': True, 'ok': True}
+
+
+def test_data_index(workbook, testapp):
+    ''' method tests the ElasticSearch connection by putting in the mapping and indexing the documents '''
+    
+    connection = ElasticSearch('http://localhost:9200')
+    mapping = {
+        'basic': {
+            'properties': {
+                'target': {
+                    'properties': {
+                        'lab': {
+                            'properties': {
+                                'title': {
+                                    'type': 'string',
+                                    'index': 'not_analyzed'
+                                }
+                            }
+                        }
+                    }
+                },
+                'antibody': {
+                    'properties': {
+                        'host_organism': {
+                            'properties': {
+                                'name': {
+                                    'type': 'string',
+                                    'index': 'not_analyzed'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    res = testapp.get('/antibodies/?limit=all').maybe_follow(status=200)
+    items = res.json['@graph']
+
+    index = 'test'
+    try:
+        indexer = connection.create_index(index)
+        assert indexer == {'acknowledged': True, 'ok': True}
+    except IndexAlreadyExistsError:
+        connection.delete_index(index)
+        connection.create_index(index)
+    mapper = connection.put_mapping(index, 'basic', mapping)
+    assert mapper == {'acknowledged': True, 'ok': True}
+
+    counter = 0
+    for item in items:
+        item_json = testapp.get(str(item['@id']), headers={'Accept': 'application/json'}, status=200)
+        document_id = str(item_json.json['@id'])[-37:-1]
+        document = item_json.json
+        connection.index('test', 'basic', document, document_id)
+        counter = counter + 1
+        if counter % 50 == 0:
+            # flushing ElasticSearch(JDK) memory after indexing 50 docs
+            connection.flush(index)
+
+    connection.refresh(index)
+    count = connection.count('*:*', index='test')
+    assert count['count'] == len(items)
