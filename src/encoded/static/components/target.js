5aad3cccbeb1ef352eb05185f7beadedcab3c7f4
GregRR <gregroe@stanford.edu>
2013-09-11 15:05:38 -0700

    HGNC link was already available.

--- src/encoded/static/components/target.js
+++ src/encoded/static/components/target.js
@@ -9,14 +9,16 @@ function (target, React, globals, dbxref) {
         render: function() {
             var context = this.props.context;
             var itemClass = globals.itemClass(context, 'view-detail panel key-value');
-            var geneCardBaseURL = "http://www.genecards.org/cgi-bin/carddisp.pl?gene=";
+           
             return (
                 <dl class={itemClass}>
                     <dt>Target name</dt>
                     <dd class="no-cap">{context.label}</dd>
 
                     <dt>Target Gene</dt>
-                    <dd><a href={geneCardBaseURL + context.gene_name}>{context.gene_name}</a></dd>
+                    <dd><a href={globals.dbxref_prefix_map.HGNC + context.gene_name}>{context.gene_name}</a></dd>
+                    
+                    
 
                     <dt>External Resources</dt>
                     <dd>
