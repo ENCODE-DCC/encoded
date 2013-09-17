ba98f8f1be42282129310b0aeab2020effe29295
GregRR <gregroe@stanford.edu>
2013-09-17 15:55:58 -0700

    Target genes for mice should point to uniprot for now; Remine #637;

--- src/encoded/static/components/target.js
+++ src/encoded/static/components/target.js
@@ -4,19 +4,29 @@ function (target, React, globals, dbxref) {
     'use strict';
 
     var DbxrefList = dbxref.DbxrefList;
-
+	var Dbxref = dbxref.Dbxref;
+	
     var Panel = target.Panel = React.createClass({
         render: function() {
             var context = this.props.context;
             var itemClass = globals.itemClass(context, 'view-detail panel key-value');
-           
+            var geneLink;
+            
+            if (context.organism.name == "human") {
+            	geneLink = globals.dbxref_prefix_map.HGNC + context.gene_name;
+            } else if (context.organism.name == "mouse") {
+            	var uniProtValue = JSON.stringify(context.dbxref);
+            	var sep = uniProtValue.indexOf(":") + 1;
+            	var uniProtID = uniProtValue.substring(sep, uniProtValue.length - 2);
+            	geneLink = globals.dbxref_prefix_map.UniProtKB + uniProtID;
+            }
             return (
                 <dl class={itemClass}>
                     <dt>Target name</dt>
                     <dd class="no-cap">{context.label}</dd>
 
                     <dt>Target gene</dt>
-                    <dd><a href={globals.dbxref_prefix_map.HGNC + context.gene_name}>{context.gene_name}</a></dd>
+                    <dd><a href={geneLink}>{context.gene_name}</a></dd>
 
                     <dt>External resources</dt>
                     <dd>
