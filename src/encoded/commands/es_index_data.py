068d249ae83eedd502170d64e6f6b03f47b8ca72
nikhilRP <Nikhil@angus.local>
2013-10-02 18:52:45 -0700

    Changed the ES indexing script. Note: For now it is still incomplete

--- src/encoded/commands/es_index_data.py
+++ src/encoded/commands/es_index_data.py
@@ -1,54 +1,62 @@
 from pyramid import paster
 from pyelasticsearch import ElasticSearch, IndexAlreadyExistsError
 from collections import OrderedDict
+from ..schema_utils import (
+    load_schema,
+)
 
 ES_URL = 'http://localhost:9200'
 DOCTYPE = 'basic'
 es = ElasticSearch(ES_URL)
 
-# Mapping will be moved to schemas eventually
-basic_mapping = {'basic': {}}
-libraries_mapping = {'basic': {'properties': {'size_range': {'type': 'string'}}}}
-replicates_mapping = {'basic': {'properties': {'library': {'properties': {'size_range': {'type': 'string'}}}}}}
-donors_mapping = {'basic': {'properties': {'age': {'type': 'string'}}}}
-
-targets_mapping = {'basic': {'properties': {'lab': {'properties': {'title': {'type': 'string', 'index': 'not_analyzed'}}}, 'organism': {'properties': {'name': {'type': 'string', 'index': 'not_analyzed'}}}}}}
-biosamples_mapping = {'basic': {'properties': {'source': {'properties': {'title': {'type': 'string', 'index': 'not_analyzed'}}}, 'lab': {'properties': {'title': {'type': 'string', 'index': 'not_analyzed'}}}, 'system_slims': {'type': 'string', 'index': 'not_analyzed'}, 'organ_slims': {'type': 'string', 'index': 'not_analyzed'}, 'biosample_type': {'type': 'string', 'index': 'not_analyzed'}, 'treatments': {'type': 'nested'}, 'constructs': {'type': 'nested'}}}}
-experiments_mapping = {'basic': {'properties': {'assay_term_name': {'type': 'string', 'index': 'not_analyzed'}, 'target': {'properties': {'organism': {'properties': {'name': {'type': 'string', 'index': 'not_analyzed'}}}}}, 'files': {'type': 'nested', 'properties': {'replicate': {'properties': {'library': {'properties': {'size_range': {'type': 'string'}}}}}}}, 'lab': {'properties': {'title': {'type': 'string', 'index': 'not_analyzed'}}}, 'replicates': {'type': 'nested', 'properties': {'library': {'properties': {'size_range': {'type': 'string'}}}, 'library id (sanity)': {'type': 'string'}}}}}}
-antibodies_mapping = {'basic': {'properties': {'target': {'properties': {'lab': {'properties': {'title': {'type': 'string', 'index': 'not_analyzed'}}}}}, 'antibody': {'properties': {'host_organism': {'properties': {'name': {'type': 'string', 'index': 'not_analyzed'}}}}}}}}
-
 # Part of this will be moved to schemas and other part should be in a proper dict
 COLLECTION_URL = OrderedDict([
-    ('/users/', ['user', basic_mapping]),
-    ('/awards/', ['award', basic_mapping]),
-    ('/labs/', ['lab', basic_mapping]),
-    ('/organisms/', ['organism', basic_mapping]),
-    ('/sources/', ['source', basic_mapping]),
-    ('/targets/', ['target', targets_mapping]),
-    ('/antibody-lots/', ['antibody_lot', basic_mapping]),
-    ('/antibody-characterizations/', ['antibody_characterization', basic_mapping]),
-    ('/antibodies/', ['antibody_approval', antibodies_mapping]),
-    ('/mouse-donors/', ['mouse_donor', donors_mapping]),
-    ('/human-donors/', ['human_donor', donors_mapping]),
-    ('/documents/', ['document', basic_mapping]),
-    ('/treatments/', ['treatment', basic_mapping]),
-    ('/constructs/', ['construct', basic_mapping]),
-    ('/construct-characterizations/', ['construct_characterization', basic_mapping]),
-    ('/rnais/', ['rnai', basic_mapping]),
-    ('/rnai-characterizations/', ['rnai_characterization', basic_mapping]),
-    ('/biosamples/', ['biosample', biosamples_mapping]),
-    ('/biosample-characterizations/', ['biosample_characterization', basic_mapping]),
-    ('/platforms/', ['platform', basic_mapping]),
-    ('/libraries/', ['library', basic_mapping]),
-    ('/experiments/', ['experiment', experiments_mapping]),
-    ('/replicates/', ['replicate', replicates_mapping])
+    ('user', '/users/'),
+    ('access_key', '/access-keys/'),
+    ('award', '/awards/'),
+    ('lab', '/labs/'),
+    ('organism', '/organisms/'),
+    ('source', '/sources/'),
+    ('target', '/targets/'),
+    ('antibody_lot', '/antibody-lots/'),
+    ('antibody_characterization', '/antibody-characterizations/'),
+    ('antibody_approval', '/antibodies/'),
+    ('mouse_donor', '/mouse-donors/'),
+    ('human_donor', '/human-donors/'),
+    ('treatment', '/treatments/'),
+    ('construct', '/constructs/'),
+    ('construct_characterization', '/construct-characterizations/'),
+    ('rnai', '/rnais/'),
+    ('rnai_characterization', '/rnai-characterizations/'),
+    ('biosample', '/biosamples/'),
+    ('biosample_characterization', '/biosample-characterizations/'),
+    ('platform', '/platforms/'),
+    ('library', '/libraries/'),
+    ('experiment', '/experiments/'),
+    ('replicate', '/replicates/'),
+    ('file', '/files/')
 ])
+
+
+def generate_mapping(index):
+    ''' Generates ElasticSearch mapping for each index '''
+
+    schema = load_schema(index + '.json')
+    mapping = dict({'basic': {}})
+    try:
+        facets = schema['facets']
+    except:
+        pass
+    else:
+        for facet in facets:
+            print facet
+    return mapping
 
 
 def main():
     ''' Indexes app data loaded to th elasticsearch '''
 
-    app = paster.get_app('production.ini')
+    app = paster.get_app('dev-masterdata.ini')
     from webtest import TestApp
     environ = {
         'HTTP_ACCEPT': 'application/json',
@@ -63,31 +71,30 @@ def main():
     print
 
     for url in COLLECTION_URL:
-        print "Indexing " + COLLECTION_URL.get(url)[0] + " ...."
-        res = testapp.get(url + '?limit=all&collection_source=database', headers={'Accept': 'application/json'}, status=200)
+        print "Indexing " + url + " ...."
+        res = testapp.get(COLLECTION_URL[url] + '?limit=all&collection_source=database', headers={'Accept': 'application/json'}, status=200)
         items = res.json['@graph']
 
         # try creating index, if it exists already delete it and create it again and generate mapping
-        index = COLLECTION_URL.get(url)[0]
+        index = url
         try:
             es.create_index(index)
         except IndexAlreadyExistsError:
             pass
         else:
-            es.put_mapping(index, DOCTYPE, COLLECTION_URL.get(url)[1])
-
+            es.put_mapping(index, DOCTYPE, generate_mapping(index))
+        
         counter = 0
         for item in items:
             try:
                 item_json = testapp.get(str(item['@id']), headers={'Accept': 'application/json'}, status=200)
+                document_id = str(item_json.json['uuid'])
+                document = item_json.json
             except Exception as e:
                 print e
             else:
-                document_id = str(item_json.json['uuid'])
-                document = item_json.json
-                
                 # For biosamples getting organ_slim and system_slim from ontology index
-                if COLLECTION_URL.get(url)[0] == 'biosample':
+                if url == 'biosample':
                     if document['biosample_term_id']:
                         try:
                             document['organ_slims'] = (es.get('ontology', 'basic', document['biosample_term_id']))['_source']['organs']
@@ -106,7 +113,7 @@ def main():
 
         es.refresh(index)
         count = es.count('*:*', index=index)
-        print "Finished indexing " + str(count['count']) + " " + COLLECTION_URL.get(url)[0]
+        print "Finished indexing " + str(count['count'])
         print ""
 
 if __name__ == '__main__':
