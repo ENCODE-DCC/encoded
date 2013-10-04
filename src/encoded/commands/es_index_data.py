5fdf82dfffbe9c7b52d7c63e82adaf0b164fc52d
nikhilRP <nikhilrp@stanford.edu>
2013-10-04 14:56:55 -0700

    Changed indexing script to include developmental slim for biosamples and added few other facets

--- src/encoded/commands/es_index_data.py
+++ src/encoded/commands/es_index_data.py
@@ -100,9 +100,11 @@ def main():
                         try:
                             document['organ_slims'] = (es.get('ontology', 'basic', document['biosample_term_id']))['_source']['organs']
                             document['system_slims'] = (es.get('ontology', 'basic', document['biosample_term_id']))['_source']['systems']
+                            document['developmental_slims'] = (es.get('ontology', 'basic', document['biosample_term_id']))['_source']['developmental']
                         except:
                             document['organ_slims'] = []
                             document['system_slims'] = []
+                            document['developmental_slims'] = []
                             print "ID not found - " + document['biosample_term_id']
                     else:
                         document['organ_slims'] = []
