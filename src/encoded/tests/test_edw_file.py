8e12f1a48c06cebb072c07112cff8a62d762969d
Kate Rosenbloom <kate@soe.ucsc.edu>
2013-10-09 10:13:27 -0700

    Preliminary commit of functional test test_format_app_fileinfo.  Not expected to pass

--- src/encoded/tests/test_edw_file.py
+++ src/encoded/tests/test_edw_file.py
@@ -6,12 +6,17 @@ import json
 
 # globals
 
+# TEST_HASHES = {}
+#pytest.mark.parametrize(('field1', 'field2'), TEST_HASHES.items())
+# def test_fxn(field1, field2)
+
 EDW_FILE_TEST_DATA_DIR = 'src/encoded/tests/data/edw_file'
 
 def test_app_file_show():
     assert(1 == 1)
 
 def test_format_app_fileinfo():
+    # Test extracting EDW-relevant fields from encoded file.json
     EDW_TEST_FILE = 'format_app_file_in.json'
 
     # load input
@@ -24,5 +29,18 @@ def test_format_app_fileinfo():
     set_app = set(file_dict.items())
     set_edw = set(format_app_file_out.items())
     assert len(set_edw ^ set_app) == 0
+
+
+def test_get_app_fileinfo(workbook, testapp):
+    app_files = encoded.commands.read_edw_fileinfo.get_app_fileinfo(testapp)
+
+
+#def test_new(workbook, app):
+    # Test find of new files (accession at EDW but not encoded)
+
+
+#def test_import(workbook, app)
+    # Test import of new file to encoded
+
 
 
