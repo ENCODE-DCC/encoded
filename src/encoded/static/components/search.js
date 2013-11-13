a0a74c220218272e9b78439343927acaca1d9add
nikhilRP <nikhilrp@stanford.edu>
2013-11-13 14:20:02 -0800

    Collection page redirects to search page

--- src/encoded/static/components/search.js
+++ src/encoded/static/components/search.js
@@ -26,15 +26,9 @@ function (exports, React, url, globals) {
                 }
                 if(counter < 4) {
                     if(count == result_count) {
-                        return <li>
-                                <label><small>{id} ({count})</small></label>
-                            </li>
+                        return <li>{id}<span className="pull-right">{count}</span></li>
                     }else {
-                        return <li>
-                                <a href={href_search+'&'+field+'='+id}>
-                                    <label><small>{id} ({count})</small></label>
-                                </a>
-                            </li>
+                        return <li><a href={href_search+'&'+field+'='+id}>{id}<span className="pull-right">{count}</span></a></li>
                     }
                 } 
             };
@@ -53,15 +47,9 @@ function (exports, React, url, globals) {
                 }
                 if (counter1 >= 4) {
                    if(count == result_count) {
-                        return <li>
-                                <label><small>{id} ({count})</small></label>
-                            </li>
+                        return <li>{id}<span className="pull-right">{count}</span></li>
                     }else {
-                        return <li>
-                                <a href={href_search+'&'+field+'='+id}>
-                                    <label><small>{id} ({count})</small></label>
-                                </a>
-                            </li>
+                        return <li><a href={href_search+'&'+field+'='+id}>{id}<span className="pull-right">{count}</span></a></li>
                     }
                 }
             };
@@ -79,28 +67,28 @@ function (exports, React, url, globals) {
                     }
                 }
                 return <div>
-                        <legend><small>{term}</small></legend>
-                        <ul className="facet-list">
-                            {terms.length ?
-                                terms.map(buildTerms)
-                            : null}
-                        </ul>
-                        {terms.length > 3 ?
-                            <ul className="facet-list">
+                        <h5>{term}</h5>
+                        <ul className="facet-list nav">
+                            <div>
+                                {terms.length ?
+                                    terms.map(buildTerms)
+                                : null}
+                            </div>
+                            {terms.length > 3 ?
                                 <div id={termID} className="collapse">
                                     {terms.length ?
                                         terms.map(buildCollapsingTerms)
                                     : null}
                                 </div>
-                            </ul>
-                        : null}
-                        {terms.length > 3 ?
-                            <label className="pull-right">
-                                    <small>
-                                        <button type="button" className="btn btn-link collapsed" data-toggle="collapse" data-target={'#'+termID} />
-                                    </small>
-                            </label>
-                        : null}
+                            : null}
+                            {terms.length > 3 ?
+                                <label className="pull-right">
+                                        <small>
+                                            <button type="button" className="btn btn-link collapsed" data-toggle="collapse" data-target={'#'+termID} />
+                                        </small>
+                                </label>
+                            : null}
+                        </ul>
                     </div>
             };
             return (
@@ -120,44 +108,55 @@ function (exports, React, url, globals) {
             var href_search = url.parse(this.props.href).search || '';
             var facets = context['facets'];
             var count = context['count'];
+            var columns = context['columns'];
 
             var resultsView = function(result) {
                 var highlight = result['highlight'];
                 switch (result['@type'][0]) {
                     case "biosample":
                         return <li className="post">
-                                    <div className="accession"><a href={result['@id']}>{result['accession']}</a></div>
-                                    <small>{result['biosample_term_name']} - {result['biosample_term_id']} - {result['lab.title']}
-                                        <br />
-                                        <div className="highlight"dangerouslySetInnerHTML={{__html: highlight.toString()}} />
-                                    </small>
+                                    <h4><a href={result['@id']}>{result['accession']}</a></h4>
+                                    <div className="well">
+                                        <strong>{columns['biosample_term_name']}</strong>: {result['biosample_term_name']}
+                                        <span className="pull-right"><strong>{columns['biosample_type']}</strong>: {result['biosample_type']}</span><br />
+                                        <strong>{columns['organism.name']}</strong>: {result['organism.name']}
+                                        <span className="pull-right"><strong>{columns['source.title']}</strong>: {result['source.title']}</span><br />
+                                        <strong>{columns['lab.title']}</strong>: {result['lab.title']}
+                                    </div>
                             </li>
                         break;
                     case "experiment":
                         return <li className="post">
-                                    <div className="accession"><a href={result['@id']}>{result['accession']}</a></div>
-                                    <small>{result['description']} - {result['assay_term_name']} - {result['lab.title']}
-                                        <br />
-                                        <div className="highlight" dangerouslySetInnerHTML={{__html: highlight.toString()}} />
-                                    </small>
+                                    <h4><a href={result['@id']}>{result['accession']}</a></h4>
+                                    <div className="well">
+                                        <strong>{columns['assay_term_name']}</strong>: {result['assay_term_name']}
+                                        <span className="pull-right"><strong>{columns['target.label']}</strong>: {result['target.label']}</span><br />
+                                        <strong>{columns['biosample_term_name']}</strong>: {result['biosample_term_name']}
+                                        <span className="pull-right"><strong>{columns['lab.title']}</strong>: {result['lab.title']}</span><br />
+                                        <strong>{columns['award.rfa']}</strong>: {result['award.rfa']}
+                                    </div>
                             </li>
                         break;
                     case "antibody_approval":
                         return <li className="post">
-                                <div className="accession"><a href={result['@id']}>{result['antibody.accession']}</a></div>
-                                <small>{result['target.label']} - {result['antibody.source.title']}
-                                    <br />
-                                    <div className="highlight" dangerouslySetInnerHTML={{__html: highlight.toString()}} />
-                                </small>
+                                    <h4><a href={result['@id']}>{result['antibody.accession']}</a></h4>
+                                    <div className="well">
+                                        <strong>{columns['target.label']}</strong>: {result['target.label']}
+                                        <span className="pull-right"><strong>{columns['target.organism.name']}</strong>: {result['target.organism.name']}</span><br />
+                                        <strong>{columns['antibody.source.title']}</strong>: {result['antibody.source.title']}
+                                        <span className="pull-right"><strong>{columns['antibody.product_id']}</strong>: {result['antibody.product_id']}</span><br />
+                                        <strong>{columns['antibody.lot_id']}</strong>: {result['antibody.lot_id']}
+                                        <span className="pull-right"><strong>{columns['status']}</strong>: {result['status']}</span>
+                                    </div>
                             </li>
                         break;
                     case "target":
                         return <li className="post">
-                                <div className="accession"><a href={result['@id']}>{result['label']}</a></div>
-                                <small>{result['organism.name']}
-                                    <br />
-                                    <div  className="highlight" dangerouslySetInnerHTML={{__html: highlight.toString()}} />
-                                </small>
+                                    <h4><a href={result['@id']}>{result['label']}</a></h4>
+                                    <div className="well">
+                                        <strong>{columns['organism.name']}</strong>: {result['organism.name']}<br />
+                                        <strong>{columns['dbxref']}</strong>: {result['dbxref']}
+                                    </div>
                             </li>
                         break;
                 }
@@ -165,57 +164,44 @@ function (exports, React, url, globals) {
             return (
                     <div>
                         {results.length ?
-                            <div className="panel data-display">
-                                <div className="row">
-                                    <div className="span3" id="facets">
-                                        <h4>Filter Results</h4>
-                                        <section className="facet box">
-                                            <div>
-                                                <legend><small>Data Type</small></legend>
-                                                <ul className="facet-list">
-                                                    {count['antibodies'] ?
-                                                        <li>
-                                                            <span className="badge pull-right">{count['antibodies']}</span>
-                                                            <a href={href_search+'&type=antibodies'}><small>Antibodies</small></a>
-                                                        </li>
-                                                    : null}
-                                                    {count['biosamples'] ?
-                                                        <li>
-                                                            <span className="badge pull-right">{count['biosamples']}</span>
-                                                            <a href={href_search+'&type=biosamples'}><small>Biosamples</small></a>
-                                                        </li>
-                                                    : null}
-                                                    {count['experiments'] ?
-                                                        <li>
-                                                            <span className="badge pull-right">{count['experiments']}</span>
-                                                            <a href={href_search+'&type=experiments'}><small>Experiments</small></a>
-                                                        </li>
-                                                    : null}
-                                                    {count['targets'] ?
-                                                        <li>
-                                                            <span className="badge pull-right">{count['targets']}</span>
-                                                            <a href={href_search+'&type=targets'}><small>Targets</small></a>
-                                                        </li>
-                                                    : null}
-                                                </ul>
-                                            </div>
-                                            {facets.length ?
-                                                this.transferPropsTo(<FacetBuilder />)
-                                            :null }
-                                        </section>
+                            <div className="row">
+                                <div className="span3">
+                                    <div>
+                                        <ul className="nav nav-tabs nav-stacked">
+                                            {count['antibodies'] ?
+                                                <li>
+                                                    <a href={href_search+'&type=antibodies'}>Antibodies<span className="pull-right">{count['antibodies']}</span></a>
+                                                </li>
+                                            : null}
+                                            {count['biosamples'] ?
+                                                <li>
+                                                    <a href={href_search+'&type=biosamples'}>Biosamples<span className="pull-right">{count['biosamples']}</span></a>
+                                                </li>
+                                            : null}
+                                            {count['experiments'] ?
+                                                <li>
+                                                    <a href={href_search+'&type=experiments'}>Experiments<span className="pull-right">{count['experiments']}</span></a>
+                                                </li>
+                                            : null}
+                                            {count['targets'] ?
+                                                <li>
+                                                    <a href={href_search+'&type=targets'}>Targets<span className="pull-right">{count['targets']}</span></a>
+                                                </li>
+                                            : null}
+                                        </ul>
                                     </div>
-                                    <div className="span8">
-                                        <legend>{results.length} Results Found</legend>
-                                        <div className="results">
-                                            <ul className="nav">
-                                                {results.length ?
-                                                    results.map(resultsView)
-                                                : null}
-                                            </ul>
-                                        </div>
-                                    </div>
+                                    {facets.length ?
+                                        this.transferPropsTo(<FacetBuilder />)
+                                    :null }
                                 </div>
-                            </div>  
+                                <div className="span8">
+                                    <ul className="nav">
+                                        {results.length ?
+                                            results.map(resultsView)
+                                        : null}
+                                    </ul>
+                                </div>
+                            </div>
                         : null }
                     </div>  
                 );
@@ -232,14 +218,18 @@ function (exports, React, url, globals) {
             var context = this.props.context;
             var results = context['@graph'];
             return (
-                <div >
-                    <form className="input-prepend">
-                        <span className="add-on"><i className="icon-search"></i></span>
-                        <input id='inputValidate' className="input-xxlarge" type="text" placeholder="Search ENCODE" name="searchTerm" defaultValue={this.state.text} />
-                    </form>
-                    {Object.keys(results).length ?
-                        this.transferPropsTo(<ResultTable />)
-                    : <h4>Please enter a search term </h4>}
+                <div>
+                    <div className="panel data-display">
+                        <form className="input-prepend">
+                            <span className="add-on"><i className="icon-search"></i></span>
+                            <input id='inputValidate' className="input-xxlarge" type="text" placeholder="Search ENCODE" name="searchTerm" defaultValue={this.state.text} />
+                        </form>
+                    </div>
+                    <div className="panel data-display"> 
+                        {Object.keys(results).length ?
+                            this.transferPropsTo(<ResultTable />)
+                        : <h4>Please enter a search term </h4>}
+                    </div>
                 </div>
             );
         }
--- src/encoded/views/search.py
+++ src/encoded/views/search.py
@@ -4,25 +4,7 @@ from ..contentbase import (
     Root
 )
 from pyelasticsearch import ElasticSearch
-from ..schema_utils import (
-    load_schema,
-)
-
 es = ElasticSearch('http://localhost:9200')
-schemas = {
-    'biosamples': 'biosample.json',
-    'experiments': 'experiment.json',
-    'antibodies': 'antibody_approval.json',
-    'targets': 'target.json'
-}
-
-# This will eventually move to schemas
-data = {
-    'biosamples': ['@id', '@type', 'accession', 'biosample_term_id', 'biosample_term_name', 'lab.title'],
-    'experiments': ['@id', '@type', 'accession', 'description', 'assay_term_name', 'lab.title'],
-    'antibodies': ['@id', '@type', 'antibody.accession', 'target.label', 'antibody.source.title'],
-    'targets': ['@id', '@type', 'label', 'organism.name', 'lab.title']
-}
 
 
 # Query class should be improved to accomodate filters and other ES functionality
@@ -85,94 +67,105 @@ def search(context, request):
     ''' Search view connects to ElasticSearch and returns the results'''
 
     result = context.__json__(request)
+    params = request.params
+    root = request.root
     result.update({
         '@id': '/search/',
         '@type': ['search'],
         'title': 'Search ENCODE',
-        'count': {},
         'facets': [],
-        '@graph': []
+        '@graph': [],
+        'columns': {},
+        'count': {}
     })
-    params = request.params
 
-    if 'type' not in params:
-        schema = load_schema('biosample.json')
-        facets = schema['facets']
-    else:
-        schema = load_schema(schemas[params.get('type')])
-        facets = schema['facets']
-
-    query = Query()
-    index = ''
-
-    if params['searchTerm']:
-        if 'type' in params:
-            index = schemas[params.get('type')][:-5]
-            if len(params) > 2:
-                query = FilteredQuery()
-                query.__setterm__(params.get('searchTerm'))
-                query.fields = data[params.get('type')]
-                for key, value in params.iteritems():
-                    if value == 'other':
-                        query.__setmissingfilter__(key)
-                    elif key != 'searchTerm' and key != 'type':
-                        query.__setfilter__(key, value)
-            else:
-                query.fields = data[params.get('type')]
-                query.query = {'query_string': {'query': params.get('searchTerm')}}
-
-            if len(facets.keys()):
-                for facet in facets:
-                    face = {'terms': {'field': '', 'size': 999999}}
-                    face['terms']['field'] = facets[facet]
-                    query.facets[facet] = face
-
-            s = es.search(query, index=index, size=999999)
-            facet_results = s['facets']
-            
-            for facet in facet_results:
-                face = {}
-                face['field'] = facet_results[facet]
-                face[facet] = []
-                for term in facet_results[facet]['terms']:
-                    face[facet].append({term['term']: term['count']})
-                if facet_results[facet]['missing'] != 0:
-                    face[facet].append({'other': facet_results[facet]['missing']})
-                result['facets'].append(face)
-
-            for key, value in schemas.items():
-                if value == index + '.json':
-                    result['count'][key] = len(s['hits']['hits'])
-            
-            for dataS in s['hits']['hits']:
-                data_highlight = dataS['fields']
-                if 'highlight' in dataS:
-                    for key in dataS['highlight'].keys():
-                        data_highlight['highlight'] = dataS['highlight'][key]
-                else:
-                    data_highlight['highlight'] = []
-                result['@graph'].append(data_highlight)
-            return result
-        else:
-            # This code block executes the search for all the types of data
-            query.query = {'query_string': {'query': params.get('searchTerm')}}
-            for d in data:
-                query.fields = data[d]
-                # Should have some limit on size to have better
-                s = es.search(query, index=schemas[d][:-5], size=999999)
-                for key, value in schemas.items():
-                    if value == schemas[d]:
-                        result['count'][key] = len(s['hits']['hits'])
-                for dataS in s['hits']['hits']:
-                    data_highlight = dataS['fields']
-                    if 'highlight' in dataS:
-                        for key in dataS['highlight'].keys():
-                            data_highlight['highlight'] = dataS['highlight'][key]
-                    else:
-                        data_highlight['highlight'] = []
-                    result['@graph'].append(data_highlight)
-            return result
-    else:
+    size = 999999
+    
+    # if no searchTerm
+    try:
+        search_term = params['searchTerm']
+    except:
         return result
-    
-    
+
+    try:
+        search_type = params['type']
+    except:
+        query = Query()
+        # This code block executes the search for all the types of data
+        query.query = {'query_string': {'query': search_term}}
+        indices = ['antibody_approval', 'biosample', 'experiment', 'target']
+        fields = ['@id', '@type']
+        for index in indices:
+            collection = root[index]
+            result['columns'].update(collection.columns)
+            for column in collection.columns:
+                fields.append(column)
+
+        query.fields = list(set(fields))
+        # Should have some limit on size to have better
+        s = es.search(query, index=indices, size=999999)
+        antibody_count = biosample_count = experiment_count = target_count = 0
+        for dataS in s['hits']['hits']:
+            data_highlight = dataS['fields']
+            if dataS['fields']['@type'][0] == 'antibody_approval':
+                antibody_count = antibody_count + 1
+            elif dataS['fields']['@type'][0] == 'biosample':
+                biosample_count = biosample_count + 1
+            elif dataS['fields']['@type'][0] == 'experiment':
+                experiment_count = experiment_count + 1
+            elif dataS['fields']['@type'][0] == 'target':
+                target_count = target_count + 1
+            result['count']['biosamples'] = biosample_count
+            result['count']['antibodies'] = antibody_count
+            result['count']['experiments'] = experiment_count
+            result['count']['targets'] = target_count
+            if 'highlight' in dataS:
+                for key in dataS['highlight'].keys():
+                    data_highlight['highlight'] = dataS['highlight'][key]
+            else:
+                data_highlight['highlight'] = []
+            result['@graph'].append(data_highlight)
+        return result
+    else:
+        collections = root.by_item_type
+        fields = ['@id', '@type']
+        for collection_name in collections:
+            if search_type == root.by_item_type[collection_name].__name__:
+                collection = root[collection_name]
+                index = collection_name
+                schema = collection.schema
+                result['columns'] = columns = collection.columns
+                break
+        query = Query()
+        for column in columns:
+            fields.append(column)
+        query.fields = fields
+        query.query = {'query_string': {'query': search_term}}
+
+        for facet in schema['facets']:
+            face = {'terms': {'field': '', 'size': size}}
+            face['terms']['field'] = schema['facets'][facet]
+            query.facets[facet] = face
+
+        results = es.search(query, index=index, size=size)
+        facet_results = results['facets']
+        for facet in facet_results:
+            face = {}
+            face['field'] = facet_results[facet]
+            face[facet] = []
+            for term in facet_results[facet]['terms']:
+                face[facet].append({term['term']: term['count']})
+            if facet_results[facet]['missing'] != 0:
+                face[facet].append({'other': facet_results[facet]['missing']})
+            result['facets'].append(face)
+
+        for dataS in results['hits']['hits']:
+            data_highlight = dataS['fields']
+            if 'highlight' in dataS:
+                for key in dataS['highlight'].keys():
+                    data_highlight['highlight'] = dataS['highlight'][key]
+            else:
+                data_highlight['highlight'] = []
+            result['@graph'].append(data_highlight)
+        result['count'][search_type] = len(results['hits']['hits'])
+        return result
