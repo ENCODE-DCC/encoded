a8af672d65e8739b60beceffaa9cea3576043bd7
Cherry Lab Build User <build@demo-a.encodedcc.org>
2014-01-06 14:41:11 -0800

    Modified cloud-config.yml and src/encoded/commands/deploy.py to implement various configurations including setting up wal-e environment for AWS.

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -2,14 +2,23 @@
 import boto.ec2
 import time
 import sys
+import os
 
 conn = boto.ec2.connect_to_region("us-west-1")
 
+user_data = open('cloud-config.yml').read()
+user_data=user_data % {
+    'AWS_ID': os.environ["S3_READ_ID"],
+    'AWS_KEY': os.environ["S3_READ_KEY"],
+    'AWS_SERVER': os.environ["S3_SERVER"],
+}
+
+# Use 'ami-b698a9f3' - ubuntu/images/ebs/ubuntu-saucy-13.10-amd64-server-20131204
 reservation = conn.run_instances(
-    'ami-7e37033b',
+    'ami-b698a9f3',
     instance_type='m3.xlarge',
     security_group_ids=['sg-df30f19b'],
-    user_data=open('cloud-config.yml').read(),
+    user_data=user_data,
 )
 
 instance = reservation.instances[0] # Instance:i-34edd56f
@@ -26,4 +35,3 @@ print
 print instance.state
 
 print instance.public_dns_name # u'ec2-54-219-26-167.us-west-1.compute.amazonaws.com'
-
