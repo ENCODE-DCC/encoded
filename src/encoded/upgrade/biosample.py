39e51bc75877e408c8305f99dd592105e1319524
Laurence Rowe <laurence@lrowe.co.uk>
2014-01-13 20:46:45 -0800

    Make biosample starting_amount a number. Refs #794.

--- src/encoded/upgrade/biosample.py
+++ src/encoded/upgrade/biosample.py
@@ -1,10 +1,17 @@
 from ..migrator import upgrade_step
 
 
+def number(value):
+    try:
+        return int(value)
+    except ValueError:
+        return float(value)
+
+
 @upgrade_step('biosample', '', '2')
 def biosample_0_2(value, system):
     # http://redmine.encodedcc.org/issues/794
     if 'starting_amount' in value:
-        new_value = int(value['starting_amount'])
+        new_value = number(value['starting_amount'])
         value['starting_amount'] = new_value
 
