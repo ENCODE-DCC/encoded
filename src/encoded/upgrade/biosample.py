ccf7767dcb4089ee8551c813817a9a857e0b792f
Laurence Rowe <laurence@lrowe.co.uk>
2014-01-16 15:15:48 -0800

    Remove 'unknown' values for starting_amount; handle exponents.

--- src/encoded/upgrade/biosample.py
+++ src/encoded/upgrade/biosample.py
@@ -2,6 +2,10 @@ from ..migrator import upgrade_step
 
 
 def number(value):
+    value = value.lower().replace(' ', '')
+    value = value.replace('x10^', 'e')
+    if value == 'unknown':
+        return None
     try:
         return int(value)
     except ValueError:
@@ -13,5 +17,7 @@ def biosample_0_2(value, system):
     # http://redmine.encodedcc.org/issues/794
     if 'starting_amount' in value:
         new_value = number(value['starting_amount'])
-        value['starting_amount'] = new_value
-
+        if new_value is None:
+            del value['starting_amount']
+        else:
+            value['starting_amount'] = new_value
