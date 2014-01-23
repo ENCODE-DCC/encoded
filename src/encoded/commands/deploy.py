736077e421b565b460441115f29279cdf005767c
ENCODE Admin <encoded@demo-a.encodedcc.org>
2014-01-22 17:24:24 -0800

    More changes in cloud-config.yml and deploy.py to add the following - deployment from any git branch/tag - elasticsearch indexing - EBS volumes to store postgres and elasticsearch data - resolve integration issues

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -1,37 +1,67 @@
 
-import boto.ec2
-import time
-import sys
-import os
+#import logging
 
-conn = boto.ec2.connect_to_region("us-west-1")
+#DOCTYPE = 'basic'
 
-user_data = open('cloud-config.yml').read()
-user_data=user_data % {
-    'AWS_ID': os.environ["S3_READ_ID"],
-    'AWS_KEY': os.environ["S3_READ_KEY"],
-    'AWS_SERVER': os.environ["S3_SERVER"],
-}
+#EPILOG = __doc__
 
-# Use 'ami-b698a9f3' - ubuntu/images/ebs/ubuntu-saucy-13.10-amd64-server-20131204
-reservation = conn.run_instances(
-    'ami-b698a9f3',
-    instance_type='m3.xlarge',
-    security_group_ids=['sg-df30f19b'],
-    user_data=user_data,
-)
+#log = logging.getLogger(__name__)
 
-instance = reservation.instances[0] # Instance:i-34edd56f
-print instance
-print instance.state,
+def run(branch):
+    import boto.ec2
+    import time
+    import sys
+    import os
 
-while instance.state == 'pending':
-    sys.stdout.write('.')
-    sys.stdout.flush()
-    time.sleep(1)
-    instance.update()
+    conn = boto.ec2.connect_to_region("us-west-1")
+    bdm = boto.ec2.blockdevicemapping.BlockDeviceMapping()
+    bdm['/dev/sdf'] = boto.ec2.blockdevicemapping.BlockDeviceType(snapshot_id='snap-9bdfcd91', delete_on_termination=True)
+    bdm['/dev/sdg'] = boto.ec2.blockdevicemapping.BlockDeviceType(snapshot_id='snap-9bdfcd91', delete_on_termination=True)
 
-print
-print instance.state
+    user_data = open('cloud-config.yml').read()
+    user_data=user_data % {
+        'AWS_ID': os.environ["S3_READ_ID"],
+        'AWS_KEY': os.environ["S3_READ_KEY"],
+        'AWS_SERVER': os.environ["S3_SERVER"],
+        'BRANCH': branch,
+    }
 
-print instance.public_dns_name # u'ec2-54-219-26-167.us-west-1.compute.amazonaws.com'
+    # Use 'ami-b698a9f3' - ubuntu/images/ebs/ubuntu-saucy-13.10-amd64-server-20131204
+    reservation = conn.run_instances(
+        'ami-b698a9f3',
+        instance_type='m3.xlarge',
+        security_group_ids=['sg-df30f19b'],
+        user_data=user_data,
+        block_device_map=bdm,
+        instance_initiated_shutdown_behavior='terminate',
+    )
+
+    instance = reservation.instances[0] # Instance:i-34edd56f
+    print instance
+    print instance.state,
+
+    while instance.state == 'pending':
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(1)
+        instance.update()
+
+    print
+    print instance.state
+
+    print instance.public_dns_name # u'ec2-54-219-26-167.us-west-1.compute.amazonaws.com'
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Deploy ENCODE on AWS", 
+    )
+    parser.add_argument('-b', '--branch', default='master', help="Git branch or tag")
+    args = parser.parse_args()
+
+    return run(args.branch)
+
+
+if __name__ == '__main__':
+    main()
