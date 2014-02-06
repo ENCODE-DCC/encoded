4437300cce79c5e2f02b8e1729477324d29da81c
Laurence Rowe <laurence@lrowe.co.uk>
2014-02-06 15:33:31 -0800

    Add more keys to silence React complaints.

--- src/encoded/static/components/search.js
+++ src/encoded/static/components/search.js
@@ -277,7 +277,7 @@ var Dbxref = dbxref.Dbxref;
                         }
                     }
                     var url_unfacet = href_search.replace(unfacet_url, "");
-                    return <a className="btn btn-small btn-info" href={url_unfacet}>{filter[key] + ' '}<i className="icon-remove-sign"></i></a>
+                    return <a key={key} className="btn btn-small btn-info" href={url_unfacet}>{filter[key] + ' '}<i className="icon-remove-sign"></i></a>
                 }
             };
             return (
