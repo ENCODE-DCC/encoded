eac4988f833867854a89b0ee3139cc5ef88e30ff
Laurence Rowe <laurence@lrowe.co.uk>
2014-02-25 20:50:06 -0800

    Use html lang attribute instead of meta tag.

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -129,10 +129,9 @@ var App = React.createClass({
         }
 
         return (
-            <html>
+            <html lang="en">
                 <head>
                     <meta charSet="utf-8" />
-                    <meta httpEquiv="content-language" content="en" />
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <title>{title}</title>
