acf895176cccfa90eb6fde7e266563c6aedd85af
Laurence Rowe <laurence@lrowe.co.uk>
2014-02-25 20:32:06 -0800

    Monkey patch ReactDOM fixes.

--- src/encoded/static/components/index.js
+++ src/encoded/static/components/index.js
@@ -1,5 +1,15 @@
 /** @jsx React.DOM */
 'use strict';
+
+// https://github.com/facebook/react/pull/1183
+var DefaultDOMPropertyConfig = require('react/lib/DefaultDOMPropertyConfig');
+DefaultDOMPropertyConfig.DOMAttributeNames.httpEquiv = 'http-equiv';
+
+// https://github.com/facebook/react/pull/1181
+var ReactDOM = require('react/lib/ReactDOM');
+ReactDOM.link.type.prototype._tagClose = '';
+
+
 // Require all components to ensure javascript load ordering
 require('./antibody');
 require('./app');
