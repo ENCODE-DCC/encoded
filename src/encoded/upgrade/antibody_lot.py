22130a8943f2bbbd68894efc9ab70e36d81240e0
Venkat Malladi <vmalladi@stanford.edu>
2014-03-01 10:07:29 -0800

    Pluralized the array property in upgrade script.

--- src/encoded/upgrade/antibody_lot.py
+++ src/encoded/upgrade/antibody_lot.py
@@ -4,11 +4,11 @@ from ..migrator import upgrade_step
 @upgrade_step('antibody_lot', '', '2')
 def antibody_lot_0_2(value, system):
     # http://redmine.encodedcc.org/issues/817
-    value['dbxref'] = []
+    value['dbxrefs'] = []
    
     if 'encode2_dbxrefs' in value:
         for encode2_dbxref in value['encode2_dbxrefs']:
             new_dbxref = 'ucsc_encode_db:' + encode2_dbxref
-            value['dbxref'].append(new_dbxref)
+            value['dbxrefs'].append(new_dbxref)
         del value['encode2_dbxrefs']
         
