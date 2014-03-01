26c1ba113343fd789b8f2322b24bf6897ff16f46
Venkat Malladi <vmalladi@stanford.edu>
2014-03-01 10:40:36 -0800

    Simplified test data.

--- src/encoded/tests/test_upgrade_dataset.py
+++ src/encoded/tests/test_upgrade_dataset.py
@@ -15,15 +15,15 @@ def experiment_1(root, experiment, files):
     return properties
 
 @pytest.fixture
-def experiment_2(root, experiment):
-    item = root.get_by_uuid(experiment['uuid'])
-    properties = item.properites.copy()
-    properties.update({
-        'schema_version': '2',
+def experiment_2(lab,award):
+    return {
+        'award': award['uuid'],
+        'lab': lab['uuid'],
         'encode2_dbxrefs': ['wgEncodeEH0020303'],
-        'geo_dbxrefs': ['GSM999292'],
-    })
-    return properties
+        'geo_dbxrefs': ['GSM999292']
+    }
+
+
 
 def test_experiment_upgrade(root, registry, experiment, experiment_1, files, threadlocals, dummy_request):
     migrator = registry['migrator']
@@ -34,11 +34,9 @@ def test_experiment_upgrade(root, registry, experiment, experiment_1, files, thr
     assert 'files' not in value
     assert value['related_files'] == [files[1]['uuid']]
 
-def test_experiment_upgrade_dbxrefs(root, registry, experiment, experiment_2, threadlocals, dummy_request):
+def test_experiment_upgrade_dbxrefs(app, experiment_2):
     migrator = registry['migrator']
-    context = root.get_by_uuid(experiment['uuid'])
-    dummy_request.context = context
-    value = migrator.upgrade('experiment', experiment_2, target_version='3', context=context)
+    value = migrator.upgrade('experiment', experiment_2, target_version='3')
     assert value['schema_version'] == '3'
     assert 'encode2_dbxrefs' not in value
     assert 'geo_dbxrefs' not in value
