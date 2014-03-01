9117124955adc0ae5e3dd25f3ff8c407166f2862
Venkat Malladi <vmalladi@stanford.edu>
2014-02-28 20:05:31 -0800

    Fixed reference in upgrade scripts.

--- src/encoded/upgrade/dataset.py
+++ src/encoded/upgrade/dataset.py
@@ -26,11 +26,11 @@ def dataset_2_3(value, system):
     if 'encode2_dbxrefs' in value:
         for encode2_dbxref in value['encode2_dbxrefs']:
             new_dbxref = 'ucsc_encode_db:' + encode2_dbxref
-            value['dbxref'].append(new_dbxref)
+            value['dbxrefs'].append(new_dbxref)
         del value['encode2_dbxrefs']
     
     if 'geo_dbxrefs' in value:
         for geo_dbxref in value['geo_dbxrefs']:
             new_dbxref = 'geo_db:' + geo_dbxref
-            value['dbxref'].append(new_dbxref)
+            value['dbxrefs'].append(new_dbxref)
         del value['geo_dbxrefs']
