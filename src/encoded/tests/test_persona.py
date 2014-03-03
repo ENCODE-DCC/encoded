033704ca10eeb899ae262f278e56afe2c2040350
Laurence Rowe <laurence@lrowe.co.uk>
2014-03-03 01:00:31 -0800

    Fix tests, test setup to reflect new code.

--- src/encoded/tests/test_persona.py
+++ src/encoded/tests/test_persona.py
@@ -32,23 +32,23 @@ def persona_bad_assertion():
 
 def test_login_no_csrf(anontestapp, persona_assertion):
     res = anontestapp.post_json('/login', persona_assertion, status=400)
-    assert 'Set-Cookie' not in res.headers
+    assert 'Set-Cookie' in res.headers
 
 
 def test_login_unknown_user(anontestapp, persona_assertion):
     res = anontestapp.get('/session')
-    csrf_token = str(res.json['csrf_token'])
+    csrf_token = str(res.json['_csrft_'])
     headers = {'X-CSRF-Token': csrf_token}
     res = anontestapp.post_json('/login', persona_assertion, headers=headers, status=403)
-    assert 'Set-Cookie' not in res.headers
+    assert 'Set-Cookie' in res.headers
 
 
 def test_login_bad_audience(anontestapp, persona_bad_assertion):
     res = anontestapp.get('/session')
-    csrf_token = str(res.json['csrf_token'])
+    csrf_token = str(res.json['_csrft_'])
     headers = {'X-CSRF-Token': csrf_token}
-    res = anontestapp.post_json('/login', persona_bad_assertion, headers=headers, status=422)
-    assert 'Set-Cookie' not in res.headers
+    res = anontestapp.post_json('/login', persona_bad_assertion, headers=headers, status=403)
+    assert 'Set-Cookie' in res.headers
 
 
 def test_login_logout(testapp, anontestapp, persona_assertion):
@@ -64,15 +64,15 @@ def test_login_logout(testapp, anontestapp, persona_assertion):
 
     # Log in
     res = anontestapp.get('/session')
-    csrf_token = str(res.json['csrf_token'])
+    csrf_token = str(res.json['_csrft_'])
     headers = {'X-CSRF-Token': csrf_token}
     res = anontestapp.post_json('/login', persona_assertion, headers=headers, status=200)
     assert 'Set-Cookie' in res.headers
     res = anontestapp.get('/session')
-    assert res.json['persona'] == email
+    assert res.json['auth.userid'] == email
 
     # Log out
     res = anontestapp.get('/logout?redirect=false', headers=headers, status=200)
     assert 'Set-Cookie' in res.headers
     res = anontestapp.get('/session')
-    assert res.json['persona'] is None
+    assert 'auth.userid' not in res.json
