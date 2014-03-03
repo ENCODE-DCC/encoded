de2f9fd685fd7f2649765664be4104a80dd897b2
Laurence Rowe <laurence@lrowe.co.uk>
2014-03-03 00:02:02 -0800

    Refactor login and session to avoid extra roundtrips and give a nicer experience when session expires.

--- src/encoded/persona.py
+++ src/encoded/persona.py
@@ -1,4 +1,5 @@
 from browserid.errors import TrustError
+from pyramid.authentication import CallbackAuthenticationPolicy
 from pyramid.config import ConfigurationError
 from pyramid.httpexceptions import (
     HTTPForbidden,
@@ -18,11 +19,8 @@ from pyramid.view import (
     view_config,
 )
 from .contentbase import make_subrequest
-from .storage import (
-    DBSession,
-    Key,
-)
-from .validation import ValidationFailure
+
+_marker = object()
 
 
 AUDIENCES_MESSAGE = """\
@@ -49,74 +47,108 @@ class LoginDenied(HTTPForbidden):
     title = 'Login failure'
 
 
-# in a perfect world these would inherit from Classes shared by api module
-def verify_assertion(request):
-    """Verifies the assertion in the given request.
-
-    Returns the email of the user if everything is valid, otherwise raises
-    a ValidationFailure
+class PersonaAuthenticationPolicy(CallbackAuthenticationPolicy):
     """
-    verifier = request.registry['persona.verifier']
-    try:
-        assertion = request.json['assertion']
-    except KeyError:
-        msg = 'Missing assertion.'
-        raise ValidationFailure('body', ['assertion'], msg)
-    try:
-        data = verifier.verify(assertion)
-    except (ValueError, TrustError) as e:
-        msg = 'Invalid assertion: %s (%s)' % (e, type(e).__name__)
-        raise ValidationFailure('body', ['assertion'], msg)
-    else:
-        request.validated = data
+    Checks assertion during authentication so login can construct user session.
+    """
+    view_name = 'login'
+    method = 'POST'
+
+    def unauthenticated_userid(self, request):
+        if request.method != self.method or \
+                getattr(request, 'view_name', None) != self.view_name:
+            return None
+
+        cached = getattr(request, '_persona_authenticated', _marker)
+        if cached is not _marker:
+            return cached
+
+        verifier = request.registry['persona.verifier']
+        try:
+            assertion = request.json['assertion']
+        except (ValueError, TypeError, KeyError):
+            if self.debug:
+                self._log(
+                    'Missing assertion.',
+                    'unauthenticated_userid',
+                    request)
+            request._persona_authenticated = None
+            return None
+        try:
+            data = verifier.verify(assertion)
+        except (ValueError, TrustError) as e:
+            if self.debug:
+                self._log(
+                    ('Invalid assertion: %s (%s)', (e, type(e).__name__)),
+                    'unauthenticated_userid',
+                    request)
+            request._persona_authenticated = None
+            return None
+
+        email = request._persona_authenticated = data['email'].lower()
+        return email
+
+    def remember(self, request, principal, **kw):
+        return []
+
+    def forget(self, request):
+        return []
 
 
 # Unfortunately, X-Requested-With is not sufficient.
 # http://lists.webappsec.org/pipermail/websecurity_lists.webappsec.org/2011-February/007533.html
 # Checking the CSRF token in middleware is easier
 @view_config(name='login', physical_path='/', request_method='POST',
-             subpath_segments=0, permission=NO_PERMISSION_REQUIRED,
-             validators=[verify_assertion])
+             subpath_segments=0, permission=NO_PERMISSION_REQUIRED)
 def login(request):
     """View to check the persona assertion and remember the user"""
-    data = request.validated
-    email = data['email'].lower()
-    session = DBSession()
-    model = session.query(Key).get(('user:email', email))
-    if model is None:
+    login = authenticated_userid(request)
+    if login is None:
+        namespace = userid = None
+    else:
+        namespace, userid = login.split('.', 1)
+    if namespace != 'persona':
+        request.session['user_properties'] = {}
+        request.response.headerlist.extend(forget(request))
         raise LoginDenied()
-    login = 'mailto.' + email
-    request.response.headerlist.extend(remember(request, login))
-    return data
+    subreq = make_subrequest(request, '/current-user')
+    subreq.remote_user = userid
+    subreq.override_renderer = 'null_renderer'
+    request.session['user_properties'] = request.invoke_subrequest(subreq)
+    request.response.headerlist.extend(remember(request, 'mailto.' + userid))
+    return request.session
 
 
 @view_config(name='logout', physical_path='/',
              subpath_segments=0, permission=NO_PERMISSION_REQUIRED)
 def logout(request):
     """View to forget the user"""
+    request.session.get_csrf_token()
+    request.session['user_properties'] = {}
     request.response.headerlist.extend(forget(request))
     if asbool(request.params.get('redirect', True)):
         raise HTTPFound(location=request.resource_path(request.root))
-    return {'status': 'okay'}
+    return request.session
 
 
 @view_config(name='session', physical_path='/', request_method='GET',
              subpath_segments=0, permission=NO_PERMISSION_REQUIRED)
 def session(context, request):
-    """ Give the user a CSRF token
+    """ Possibly refresh the user's session cookie
     """
-    token = request.session.get_csrf_token()
+    request.session.get_csrf_token()
+    if not request.params.get('reload'):
+        return request.session
+    # Reload the user's session cookie
     login = authenticated_userid(request)
-    result = {'csrf_token': token, 'persona': None, 'user_properties': {}}
     if login is None:
-        return result
-
-    namespace, userid = login.split('.', 1)
+        namespace = userid = None
+    else:
+        namespace, userid = login.split('.', 1)
     if namespace != 'mailto':
-        raise HTTPForbidden()
-
-    result['persona'] = userid
+        return request.session
     subreq = make_subrequest(request, '/current-user')
+    subreq.remote_user = userid
     subreq.override_renderer = 'null_renderer'
-    result['user_properties'] = request.invoke_subrequest(subreq)
-    return result
+    request.session['user_properties'] = request.invoke_subrequest(subreq)
+    return request.session
