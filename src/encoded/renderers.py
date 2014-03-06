efb5f3e7348304ae83bff981e098e54967349588
Laurence Rowe <laurence@lrowe.co.uk>
2014-03-05 18:00:17 -0800

    X-If-Match-User header requires userid to match.

--- src/encoded/renderers.py
+++ src/encoded/renderers.py
@@ -1,4 +1,3 @@
-from .json_script_escape import json_script_escape
 from pkg_resources import resource_filename
 from pyramid.events import (
     NewRequest,
@@ -6,9 +5,9 @@ from pyramid.events import (
 )
 from pyramid.decorator import reify
 from pyramid.httpexceptions import (
-    HTTPBadRequest,
-    HTTPServerError,
     HTTPMovedPermanently,
+    HTTPPreconditionFailed,
+    HTTPServerError,
     HTTPUnauthorized,
     HTTPUnsupportedMediaType,
 )
@@ -218,6 +217,15 @@ def choose_format(event):
 
     # Discriminate based on Accept header or format parameter
     request = event.request
+
+    login = None
+    expected_user = request.headers.get('X-If-Match-User')
+    if expected_user is not None:
+        login = authenticated_userid(request)
+        if login != 'mailto.' + expected_user:
+            detail = 'X-If-Match-User does not match'
+            raise HTTPPreconditionFailed(detail)
+
     if request.method not in ('GET', 'HEAD'):
         request.environ['encoded.format'] = 'json'
         if request.content_type != 'application/json':
@@ -230,7 +238,9 @@ def choose_format(event):
             if token == dict.get(request.session, '_csrft_', None):
                 return
             raise CSRFTokenError('Incorrect CSRF token')
-        login = authenticated_userid(request)
+
+        if login is None:
+            login = authenticated_userid(request)
         if login is not None:
             namespace, userid = login.split('.', 1)
             if namespace != 'mailto':
