543dbc05ac00db534315192274221e0918a389a9
Laurence Rowe <laurence@lrowe.co.uk>
2014-03-10 15:17:59 -0700

    Sort json in edit view.

--- src/encoded/static/components/edit.js
+++ src/encoded/static/components/edit.js
@@ -6,6 +6,24 @@ var parseError = require('./mixins').parseError;
 var FetchedData = require('./fetched').FetchedData;
 var _ = require('underscore');
 
+
+var sorted_json = module.exports.sorted_json = function (obj) {
+    if (obj instanceof Array) {
+        return obj.map(function (value) {
+            return sorted_json(value);
+        });
+    } else if (obj instanceof Object) {
+        var sorted = {};
+        Object.keys(obj).sort().forEach(function (key) {
+            sorted[key] = obj[key];
+        });
+        return sorted;
+    } else {
+        return obj;
+    }
+};
+
+
 var ItemEdit = module.exports.ItemEdit = React.createClass({
     render: function() {
         var context = this.props.context;
@@ -58,7 +76,7 @@ var EditForm = module.exports.EditForm = React.createClass({
         var ace = require('brace');
         require('brace/mode/json');
         require('brace/theme/solarized_light');
-        var value = JSON.stringify(this.props.data, null, 4)
+        var value = JSON.stringify(sorted_json(this.props.data), null, 4)
         var editor = ace.edit(this.refs.editor.getDOMNode());
         var session = editor.getSession()
         session.setMode('ace/mode/json');
