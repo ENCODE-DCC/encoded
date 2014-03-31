4a5e56d4dbf6697f2612345d5a48b0d9147708e3
Laurence Rowe <l@lrowe.co.uk>
2014-03-31 15:49:36 -0700

    Remove stray file

--- src/encoded/upgrade/biosample_characterization.py
+++
@@ -1,8 +0,0 @@
-from ..migrator import upgrade_step
-
-
-#@upgrade_step('biosample_characterization', '', '2')
-def biosample_characterization_0_2(value, system):
-    # http://redmine.encodedcc.org/issues/794
-    if value.get('characterization_method') == "immunofluorescense":
-        value['characterization_method'] = "immunofluorescence"
