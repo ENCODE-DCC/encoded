767bd1e8fed38e52da44a52ed9df30f14654d7d2
Venkat Malladi <vmalladi@stanford.edu>
2014-03-31 15:35:05 -0700

    Fixed flake8 issues.

--- src/encoded/tests/test_upgrade_treatment.py
+++ src/encoded/tests/test_upgrade_treatment.py
@@ -6,8 +6,9 @@ def treatment():
     return{
         'treatment_type': 'chemical',
         'treatment_term_name': 'estradiol',
-        'treatment_term_id': 'CHEBI:23965'	
+        'treatment_term_id': 'CHEBI:23965'
     }
+
 
 @pytest.fixture
 def treatment_1(treatment, award):
@@ -26,7 +27,8 @@ def test_treatment_upgrade(app, treatment_1):
     assert value['schema_version'] == '2'
     assert 'encode2_dbxrefs' not in value
     assert value['dbxrefs'] == ['UCSC-ENCODE-cv:Estradiol_1nM']
-    assert 'award' not in value  
+    assert 'award' not in value
+
 
 def test_treatment_upgrade_encode_dbxref(app, treatment_1):
     treatment_1['encode2_dbxrefs'] = ["encode:hESC to endoderm differentiation treatment"]
@@ -35,4 +37,4 @@ def test_treatment_upgrade_encode_dbxref(app, treatment_1):
     assert value['schema_version'] == '2'
     assert 'encode2_dbxrefs' not in value
     assert value['dbxrefs'] == ['UCSC-ENCODE-cv:hESC to endoderm differentiation treatment']
-    assert 'award' not in value  
+    assert 'award' not in value
