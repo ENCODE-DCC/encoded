3e4fc50873fb58ee779462d06a318798973d73da
Venkat Malladi <vmalladi@stanford.edu>
2014-04-10 11:03:18 -0700

    Updated definition of the upgrade script.

--- src/encoded/upgrade/access_key.py
+++ src/encoded/upgrade/access_key.py
@@ -2,6 +2,6 @@ from ..migrator import upgrade_step
 
 
 @upgrade_step('access_key', '', '2')
-def access_key_0_3(value, system):
+def access_key_0_2(value, system):
     # http://encode.stanford.edu/issues/1295
     value['status'] =  'current'
