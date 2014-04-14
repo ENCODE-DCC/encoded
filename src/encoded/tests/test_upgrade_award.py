ef911658382ff0bff5c25024ffda800617cfc8cc
Venkat Malladi <vmalladi@stanford.edu>
2014-04-14 15:34:00 -0700

    Updated award and organism to use shared status.

--- src/encoded/tests/test_upgrade_award.py
+++ src/encoded/tests/test_upgrade_award.py
@@ -13,13 +13,21 @@ def award_1(award):
     item = award.copy()
     item.update({
         'schema_version': '1',
-        'status': 'CURRENT',
+        'rfa': "ENCODE2"
     })
     return item
 
 
 def test_award_upgrade(app, award_1):
     migrator = app.registry['migrator']
+    value = migrator.upgrade('award', award_1, target_version='2')
+    assert value['schema_version'] == '2'
+    assert value['status'] == 'disabled'
+
+
+def test_award_upgrade_encode3(app, award_1):
+    migrator = app.registry['migrator']
+    award_1['rfa'] = 'ENCODE3'
     value = migrator.upgrade('award', award_1, target_version='2')
     assert value['schema_version'] == '2'
     assert value['status'] == 'current'
--- src/encoded/upgrade/award.py
+++ src/encoded/upgrade/award.py
@@ -6,6 +6,10 @@ def award_0_2(value, system):
     # http://redmine.encodedcc.org/issues/1295
     # http://redmine.encodedcc.org/issues/1307
 
-    if 'status' in value:
-    	value['status'] = value['status'].lower()
+    rfa_mapping = ['ENCODE2', 'ENCODE2-Mouse']
+
+    if value['rfa'] in rfa_mapping:
+    	value['status'] = 'disabled'
+    else:
+    	value['status'] = 'current'
     	
