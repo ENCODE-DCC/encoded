daa52934e59019c69bbb400f3e98b084511f7ed9
Venkat Malladi <vmalladi@stanford.edu>
2014-04-24 16:52:44 -0700

    Updated experiment model to handle files

--- src/encoded/upgrade/dataset.py
+++ src/encoded/upgrade/dataset.py
@@ -69,9 +69,16 @@ def dataset_3_4(value, system):
             if value['award'] in ENCODE2_AWARDS:
                 value['status'] = 'released'
             elif value['award'] not in ENCODE2_AWARDS:
-                value['status'] = 'submitted'
+                if 'original_files' in value:
+                    value['status'] = 'submitted'
+                else:
+                    value['status'] = 'proposed'
+
     else:
         if value['award'] in ENCODE2_AWARDS:
             value['status'] = 'released'
         elif value['award'] not in ENCODE2_AWARDS:
-            value['status'] = 'submitted'
+            if 'original_files' in value:
+                value['status'] = 'submitted'
+            else:
+                value['status'] = 'proposed'
