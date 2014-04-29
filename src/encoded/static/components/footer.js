6b0efff16eda981fc88123a301e56d81d36a00b7
Forrest Tanaka <ftanaka@icloud.com>
2014-04-29 14:08:06 -0700

    Fix up footer styles so logos and text don't spill outside width.

--- src/encoded/static/components/footer.js
+++ src/encoded/static/components/footer.js
@@ -11,22 +11,24 @@ var Footer = React.createClass({
         console.log('render footer');
         return (
             <footer id="page-footer" className="page-footer">
-                <div className="container-fluid">
-                    <div className="col-sm-6 col-sm-push-6">
-                        <ul className="footer-links">
-                            <li><a href="mailto:encode-help@lists.stanford.edu">Contact</a></li>
-                            <li><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
-                        </ul>
-                        <p className="copy-notice">&copy;{new Date().getFullYear()} Stanford University.</p>
-                    </div>
-                    
-                    <div className="col-sm-6 col-sm-pull-6">
-                        <ul className="footer-logos">
-                            <li><a href="/"><img src="/static/img/encode-logo-small-2x.png" alt="ENCODE" id="encode-logo" height="45px" width="78px" /></a></li>
-                            <li><a href="http://www.ucsc.edu"><img src="/static/img/ucsc-logo-white-alt-2x.png" alt="UC Santa Cruz" id="ucsc-logo" width="107px" height="42px" /></a>
-                            </li>
-                            <li><a href="http://www.stanford.edu"><img src="/static/img/su-logo-white-2x.png" alt="Stanford University" id="su-logo" width="105px" height="49px" /></a></li>
-                        </ul>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 col-sm-push-6">
+                            <ul className="footer-links">
+                                <li><a href="mailto:encode-help@lists.stanford.edu">Contact</a></li>
+                                <li><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
+                            </ul>
+                            <p className="copy-notice">&copy;{new Date().getFullYear()} Stanford University.</p>
+                        </div>
+                        
+                        <div className="col-sm-6 col-sm-pull-6">
+                            <ul className="footer-logos">
+                                <li><a href="/"><img src="/static/img/encode-logo-small-2x.png" alt="ENCODE" id="encode-logo" height="45px" width="78px" /></a></li>
+                                <li><a href="http://www.ucsc.edu"><img src="/static/img/ucsc-logo-white-alt-2x.png" alt="UC Santa Cruz" id="ucsc-logo" width="107px" height="42px" /></a>
+                                </li>
+                                <li><a href="http://www.stanford.edu"><img src="/static/img/su-logo-white-2x.png" alt="Stanford University" id="su-logo" width="105px" height="49px" /></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </footer>
