cadf4fe0fc18ef727560e829a127096ab0fa4192
esther <esther@genome.stanford.edu>
2014-05-19 11:03:38 -0700

    Updates to views.py for model organism life stages

--- src/encoded/views/views.py
+++ src/encoded/views/views.py
@@ -214,7 +214,7 @@ class Source(Collection):
     }
     item_name_key = 'name'
     unique_key = 'source:name'
-    item_keys =  ALIAS_KEYS + ['name']
+    item_keys = ALIAS_KEYS + ['name']
 
 
 class DonorItem(Collection.Item):
@@ -361,7 +361,8 @@ class Biosample(Collection):
             'age': {'$value': '{age}', '$templated': True},
             'age_units': {'$value': '{age_units}', '$templated': True},
             'health_status': {'$value': '{health_status}', '$templated': True},
-            'life_stage': {'$value': '{life_stage}', '$templated': True}
+            'life_stage': {'$value': '{life_stage}', '$templated': True},
+            'synchronization': {'$value': '{synchronization}', '$templated': True}
         }
         embedded = set([
             'donor',
@@ -415,7 +416,7 @@ class Biosample(Collection):
                     ns['organ_slims'] = ns['system_slims'] = ns['developmental_slims'] = ns['synonyms'] = []
             else:
                 ns['organ_slims'] = ns['system_slims'] = ns['developmental_slims'] = ns['synonyms'] = []
-            
+
             root = find_root(self)
             donor = root.get_by_uuid(self.properties['organism'])
             human_donor_properties = [
@@ -425,12 +426,28 @@ class Biosample(Collection):
                 "health_status",
                 "life_stage"
             ]
-            model_biosample_properties = {
+            mouse_biosample_properties = {
                 "sex": "model_organism_sex",
                 "age": "model_organism_age",
                 "age_units": "model_organism_age_units",
                 "health_status": "model_organism_health_status",
-                "life_stage": "model_organism_life_stage"
+                "life_stage": "mouse_life_stage"
+            }
+            fly_biosample_properties = {
+                "sex": "model_organism_sex",
+                "age": "model_organism_age",
+                "age_units": "model_organism_age_units",
+                "health_status": "model_organism_health_status",
+                "life_stage": "fly_organism_life_stage",
+                "synchronization": "fly_synchronization_stage",
+            }
+            worm_biosample_properties = {
+                "sex": "model_organism_sex",
+                "age": "model_organism_age",
+                "age_units": "model_organism_age_units",
+                "health_status": "model_organism_health_status",
+                "life_stage": "worm_organism_life_stage",
+                "synchronization": "worm_synchronization_stage",
             }
             if ns['organism'] == '7745b647-ff15-4ff3-9ced-b897d4e2983c':
                 for value in human_donor_properties:
@@ -438,8 +455,30 @@ class Biosample(Collection):
                         ns[value] = donor.properties[value]
                     else:
                         ns[value] = ''
+            elif ns['organism'] in ["ab546d43-8e2a-4567-8db7-a217e6d6eea0",
+                                    "5be68469-94ba-4d60-b361-dde8958399ca",
+                                    "74144f1f-f3a6-42b9-abfd-186a1ca93198",
+                                    "c3cc08b7-7814-4cae-a363-a16b76883e3f",
+                                    "d1072fd2-8374-4f9b-85ce-8bc2c61de122",
+                                    "b9ce90a4-b791-40e9-9b4d-ffb1c6a5aa2b",
+                                    "0bdd955a-57f0-4e4b-b93d-6dd1df9b766c"]:
+                for key, value in fly_biosample_properties.items():
+                    if value in ns:
+                        ns[key] = ns[value]
+                    else:
+                        ns[key] = ''
+            elif ns['organism'] in ["2732dfd9-4fe6-4fd2-9d88-61b7c58cbe20",
+                                    "e3ec4c1b-a203-4fe7-a013-96c2d45ab242",
+                                    "69efae2b-4df5-4957-81da-346f1b93cb98",
+                                    "a7e711b9-534c-44a3-a782-2a15af620739",
+                                    "451f9e49-685d-40d5-ad89-760b2512262a"]:
+                for key, value in worm_biosample_properties.items():
+                    if value in ns:
+                        ns[key] = ns[value]
+                    else:
+                        ns[key] = ''
             else:
-                for key, value in model_biosample_properties.items():
+                for key, value in mouse_biosample_properties.items():
                     if value in ns:
                         ns[key] = ns[value]
                     else:
@@ -637,7 +676,7 @@ class Dataset(Collection):
         'title': 'Datasets',
         'description': 'Listing of datasets',
     }
-    
+
     class Item(Collection.Item):
         template = {
             'files': [
@@ -686,7 +725,7 @@ class Experiment(Dataset):
         'title': 'Experiments',
         'description': 'Listing of Experiments',
     }
-    
+
     class Item(Dataset.Item):
         base_types = [Dataset.item_type] + Dataset.Item.base_types
         template = {
