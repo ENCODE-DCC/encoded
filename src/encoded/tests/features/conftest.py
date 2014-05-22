6d3868fb2b6ad482a9ce83ac72fec06f7700928b
David Glick <david@glicksoftware.com>
2014-05-21 23:36:48 -0700

    try to fix splinter's cookies another way

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -3,6 +3,16 @@ import pytest
 pytest_plugins = 'encoded.tests.bdd'
 
 
+# patch cookie support back into spliter's remote webdriver
+from splinter.driver.webdriver.remote import WebDriver
+from splinter.driver.webdriver.cookie_manager import CookieManager
+old__init__ = WebDriver.__init__
+def __init__(self, *args, **kw):
+    old__init__(self, *args, **kw)
+    self._cookie_manager = CookieManager(self.driver)
+WebDriver.__init__ = __init__
+
+
 @pytest.mark.fixture_lock('encoded.storage.DBSession')
 @pytest.fixture(scope='session')
 def app_settings(server_host_port, elasticsearch_server, postgresql_server):
