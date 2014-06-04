8feacec9b33bac8c060f2eebfd2fef0b9625e559
David Glick <david@glicksoftware.com>
2014-06-04 14:43:14 -0700

    handle drag/drop file upload

--- src/encoded/static/components/image.js
+++ src/encoded/static/components/image.js
@@ -86,21 +86,39 @@ var FileInput = React.createClass({
     },
 
     render: function() {
+        var mimetype = this.state.value.type;
+        var preview = (mimetype && mimetype.indexOf('image/') === 0) ? <img src={this.state.value.href} width="128" /> : '';
         return (
-            <div>
-                <input ref="input" type="file" onChange={this.onChange} />
-                <img src={this.state.value.href} width="128" />
+            <div className="dropzone" onDragOver={this.onDragOver} onDrop={this.onDrop}>
+                <div className="drop">Drop a file here.
+                    <div>{preview}</div>
+                </div>
+                <div className="browse">Or <input ref="input" type="file" onChange={this.onChange} /></div>
             </div>
         );
     },
 
-    onChange: function() {
-        var input = this.refs.input.getDOMNode();
-        var file = input.files[0];
+    onDragOver: function(e) {
+        e.dataTransfer.dropEffect = 'copy';
+        return false;  // indicate we are going to handle the drop
+    },
+
+    onDrop: function(e) {
+        var file = e.dataTransfer.files[0];
+        this.onChange(file);
+        return false;
+    },
+
+    onChange: function(file) {
+        if (file === undefined) {
+            var input = this.refs.input.getDOMNode();
+            file = input.files[0];
+        }
         var reader = new FileReader();
         reader.onloadend = function() {
             var value = {
                 download: file.name,
+                type: file.type,
                 href: reader.result
             }
             this.props.onChange(value);
