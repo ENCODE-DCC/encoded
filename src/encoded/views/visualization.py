3af0464ccfe8b703d9f14fac9378a9f58ceb6842
nikhilRP <nikhilrp@stanford.edu>
2014-06-05 14:59:05 -0700

    Added graphType

--- src/encoded/views/visualization.py
+++ src/encoded/views/visualization.py
@@ -132,7 +132,7 @@ def hub(context, request):
                 else:
                     signal_view = signal_view + newline
                 signal_view = signal_view + newline + (2 * tab) + getTrack(f, long_label, 'SIG-view') + \
-                    '\ngraphTypeDefault bars'
+                    newline + (2 * tab) + 'graphTypeDefault bars'
                 signal_count = signal_count + 1
         if signal_view == '':
             parent = parent + (newline * 2) + tab + peak_view
