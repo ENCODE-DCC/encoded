cb447ec00243847ee8dbf404a62456f4a4e2d149
Laurence Rowe <laurence@lrowe.co.uk>
2014-06-05 18:37:34 -0700

    Update sample data to avoid creation of aws credentials.

--- src/encoded/tests/sample_data.py
+++ src/encoded/tests/sample_data.py
@@ -296,6 +296,7 @@ FILES = [
         'file_format': 'fastq',
         'md5sum': 'd41d8cd98f00b204e9800998ecf8427e',
         'output_type': 'rawData',
+        'status': 'current',
     },
     {
         'uuid': '0a38fea0-c067-415a-9710-c887aff95767',
@@ -304,6 +305,7 @@ FILES = [
         'file_format': 'fastq',
         'md5sum': 'd41d8cd98f00b204e9800998ecf8427e',
         'output_type': 'rawData',
+        'status': 'current',
     },
 ]
 
