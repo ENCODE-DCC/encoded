0867fe0d35bfc256c44e9e4d5242fe7e2f87b4ff
David Glick <david@glicksoftware.com>
2014-06-05 17:22:27 -0700

    add fallback block edit form for blocks we don't have specific forms for yet

--- src/encoded/static/components/blocks/fallback.js
+++ src/encoded/static/components/blocks/fallback.js
@@ -5,6 +5,8 @@ var globals = require('../globals');
 var item = require('../item');
 
 
+// Fallback block view
+
 var BlockViewFallback = module.exports.BlockViewFallback = React.createClass({
     render: function() {
         var Panel = item.Panel;
@@ -18,7 +20,29 @@ var BlockViewFallback = module.exports.BlockViewFallback = React.createClass({
 });
 
 
-// Also use this view as a fallback for anything we haven't registered
-globals.block_views.fallback = function () {
-    return BlockViewFallback;
+// Fallback block edit form
+
+var ReactForms = require('react-forms');
+var Form = ReactForms.Form;
+var Property = ReactForms.schema.Property;
+
+var JsonType = {
+    serialize: function(value) { return JSON.stringify(value, null, 4); },
+    deserialize: function(value) { return (typeof value === 'string') ? JSON.parse(value) : value; },
+}
+
+var BlockSchema = (
+    <Property label="JSON" type={JsonType} input={<textarea rows="15" cols="80" />} />
+);
+
+var BlockEditFallback = React.createClass({
+    render: function() {
+        return this.transferPropsTo(<Form schema={BlockSchema} value={this.props.value} />);
+    }
+});
+
+
+// Also use these views as a fallback for anything we haven't registered
+globals.block_views.fallback = function (obj, name) {
+    return (name == 'edit') ? BlockEditFallback : BlockViewFallback;
 };
