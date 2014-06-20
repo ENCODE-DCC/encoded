af4774611ab13f949049ef3c68b784b28837a5ac
Laurence Rowe <laurence@lrowe.co.uk>
2014-06-20 15:27:50 -0700

    Use ES6 classes (transpiled by jstransform.)

--- src/encoded/static/components/globals.js
+++ src/encoded/static/components/globals.js
@@ -3,19 +3,19 @@
 var Registry = require('registry');
 
 // Item pages
-module.exports.content_views = Registry();
+module.exports.content_views = new Registry();
 
 // Panel detail views
-module.exports.panel_views = Registry();
+module.exports.panel_views = new Registry();
 
 // Listing detail views
-module.exports.listing_views = Registry();
+module.exports.listing_views = new Registry();
 
 // Cell name listing titles
-module.exports.listing_titles = Registry();
+module.exports.listing_titles = new Registry();
 
 // Blocks
-module.exports.blocks = Registry();
+module.exports.blocks = new Registry();
 
 
 var itemClass = module.exports.itemClass = function (context, htmlClass) {
