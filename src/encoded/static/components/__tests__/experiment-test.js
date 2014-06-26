5ce55fdf8b4567d28c12d540daa69c5bf2013d73
Forrest Tanaka <forrest@forrest-tanaka.com>
2014-06-26 11:56:26 -0700

    More complete Jest tests of experiment.js, including test data.

--- src/encoded/static/components/__tests__/experiment-test.js
+++ src/encoded/static/components/__tests__/experiment-test.js
@@ -21,6 +21,14 @@ jest.dontMock('../mixins.js');
 jest.dontMock('../jestdata/experiment.js');
 jest.dontMock('../jestdata/file.js');
 jest.dontMock('../jestdata/document.js');
+jest.dontMock('../jestdata/replicate.js');
+jest.dontMock('../jestdata/platform.js');
+jest.dontMock('../jestdata/library.js');
+jest.dontMock('../jestdata/biosample.js');
+jest.dontMock('../jestdata/human_donor.js');
+jest.dontMock('../jestdata/mouse_donor.js');
+jest.dontMock('../jestdata/organism.js');
+jest.dontMock('../jestdata/source.js');
 
 
 describe('experiment', function() {
@@ -28,10 +36,31 @@ describe('experiment', function() {
     var url = require('url');
     var TestUtils = require('react/lib/ReactTestUtils');
     require('../biosample.js');
+
+    // Load up a single experiment with test data
     var Experiment = require('../experiment.js').Experiment;
     var context = require('../jestdata/experiment.js').experiment_basic;
     context.files = require('../jestdata/file.js').file_basic;
     context.documents = require('../jestdata/document.js').document_basic;
+    context.replicates = require('../jestdata/replicate.js').replicate_basic;
+    context.replicates[0].platform = context.replicates[1].platform = require('../jestdata/platform.js').platform[0];
+    var library = require('../jestdata/library.js').library;
+    var biosamples = require('../jestdata/biosample.js').biosample;
+    var human_donor = require('../jestdata/human_donor.js').human_donors[0];
+    var mouse_donor = require('../jestdata/mouse_donor.js').mouse_donors[0];
+    var source = require('../jestdata/source.js').sources[0];
+    var organisms = require('../jestdata/organism.js').organisms;
+    human_donor.organism = organisms[0];
+    mouse_donor.organism = organisms[1];
+    biosamples[0].donor = human_donor;
+    biosamples[1].donor = mouse_donor;
+    biosamples[0].source = biosamples[1].source = source;
+    biosamples[0].organism = human_donor.organism;
+    biosamples[1].organism = mouse_donor.organism;
+    library[0].biosample = biosamples[0];
+    library[1].biosample = biosamples[1];
+    context.replicates[0].library = library[0];
+    context.replicates[1].library = library[1];
 
     it('displays proper variants of experiments', function() {
         // Render a basic experiment
@@ -42,12 +71,13 @@ describe('experiment', function() {
         var summary = TestUtils.scryRenderedDOMComponentsWithClass(experiment, 'data-display');
         expect(summary.length).toEqual(1);
         var defTerms = summary[0].getDOMNode().getElementsByTagName('dt');
-        expect(defTerms.length).toEqual(7);
+        expect(defTerms.length).toEqual(8);
         var defDescs = summary[0].getDOMNode().getElementsByTagName('dd');
-        expect(defDescs.length).toEqual(7);
+        expect(defDescs.length).toEqual(8);
+        expect(defDescs[2].textContent).toContain('Homo sapiens and Mus musculus');
 
         // Proper links in dbxrefs key-value
-        var dbxrefs = defDescs[6].getElementsByTagName('a');
+        var dbxrefs = defDescs[7].getElementsByTagName('a');
         expect(dbxrefs.length).toEqual(2);
         expect(dbxrefs[0].getAttribute('href')).toEqual('http://genome.ucsc.edu/cgi-bin/hgTracks?tsCurTab=advancedTab&tsGroup=Any&tsType=Any&hgt_mdbVar1=dccAccession&hgt_tSearch=search&hgt_tsDelRow=&hgt_tsAddRow=&hgt_tsPage=&tsSimple=&tsName=&tsDescr=&db=hg19&hgt_mdbVal1=wgEncodeEH003317');
         expect(dbxrefs[1].getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM1010811');
