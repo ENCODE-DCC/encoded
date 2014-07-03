c1dfe383994b1eebc9deed0b0c7ddf719dc00862
Laurence Rowe <laurence@lrowe.co.uk>
2014-07-03 15:42:18 -0700

    Fix file_extension mapping for upload to s3 take 2.

--- src/encoded/views/file.py
+++ src/encoded/views/file.py
@@ -26,7 +26,7 @@ def external_creds(parent, properties):
     bucket = registry.settings['file_upload_bucket']
     mapping = parent.schema['file_format_file_extension']
     file_extension = mapping[properties['file_format']]
-    key = '{uuid}/{accession}{file_extension}'.format(**properties)
+    key = '{uuid}/{accession}{file_extension}'.format(file_extension=file_extension, **properties)
     policy = {
         'Version': '2012-10-17',
         'Statement': [
