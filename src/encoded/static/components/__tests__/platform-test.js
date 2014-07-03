6041cf6e130dc4f90070c13b7469a60b79402cb9
Forrest Tanaka <ftanaka@icloud.com>
2014-07-02 21:01:09 -0700

    Revert jest test to master.

--- src/encoded/static/components/__tests__/platform-test.js
+++ src/encoded/static/components/__tests__/platform-test.js
@@ -10,18 +10,13 @@ jest.dontMock('../globals.js');
 jest.dontMock('../dbxref.js');
 jest.dontMock('../../libs/registry.js');
 
-describe('Platform', function() {
-    var TestUtils;
-    var platformPanel;
-    var defTerms;
-    var defDescs;
-
-    beforeEach(function() {
+describe('platform', function() {
+    it('shows proper text and links', function() {
         var React = require('react');
         var Panel = require('../platform.js').Panel;
-        TestUtils = require('react/lib/ReactTestUtils');
+        var TestUtils = require('react/lib/ReactTestUtils');
 
-        // Set up context object to be rendered
+        // Set up context object
         var context = {
             url: 'http://www3.appliedbiosystems.com/cms/groups/mcb_marketing/documents/generaldocuments/cms_072050.pdf',
             title: 'Applied Biosystems SOLiD System 3 Plus',
@@ -29,49 +24,44 @@ describe('Platform', function() {
             dbxrefs: ['UCSC-ENCODE-cv:AB_SOLiD_3.5', 'GEO:GPL9442']
         };
 
-        // Render platform panel into jsnode
+        // Render a platform panel in the document
         var platformPanel = <Panel context={context} />;
         TestUtils.renderIntoDocument(platformPanel);
 
-        // Get the <dt> and <dd> terms needed for all tests
-        defTerms = TestUtils.scryRenderedDOMComponentsWithTag(platformPanel, 'dt');
-        defDescs = TestUtils.scryRenderedDOMComponentsWithTag(platformPanel, 'dd');
-    });
-
-    it('has three <dt> and <dd> elements', function() {
+        // Verify we have three <dt> and <dd> elements
+        var defTerms = TestUtils.scryRenderedDOMComponentsWithTag(platformPanel, 'dt');
         expect(defTerms.length).toEqual(3);
+        var defDescs = TestUtils.scryRenderedDOMComponentsWithTag(platformPanel, 'dd');
         expect(defDescs.length).toEqual(3);
-    });
 
-    describe('External References (third key-value) Item', function() {
-        var defDescNode, unorderedList, listItems, anchors;
+        // Verify the first <dd> (Platform name) has a link to www3.appliedbiosystems.com
+        var defDescNode = defDescs[0].getDOMNode();
+        expect(defDescNode.hasChildNodes()).toEqual(true);
+        expect(defDescNode.childNodes.length).toEqual(1);
+        var anchor = defDescNode.childNodes[0];
+        expect(anchor.getAttribute('href')).toEqual('http://www3.appliedbiosystems.com/cms/groups/mcb_marketing/documents/generaldocuments/cms_072050.pdf');
 
-        beforeEach(function() {
-            defDescNode = defDescs[2].getDOMNode();
-            unorderedList = defDescNode.childNodes[0];
-            listItems = unorderedList.childNodes;
-            anchors = unorderedList.getElementsByTagName('a');
-        });
+        // Verify the third <dd> (dbxrefs) has an unordered list with two items
+        defDescNode = defDescs[2].getDOMNode();
+        expect(defDescNode.hasChildNodes()).toEqual(true);
+        expect(defDescNode.childNodes.length).toEqual(1);
+        var unordered = defDescNode.childNodes[0];
+        expect(unordered.hasChildNodes()).toEqual(true);
+        expect(unordered.childNodes.length).toEqual(2);
 
-        it('has an unordered list with two items', function() {
-            expect(defDescNode.hasChildNodes()).toBeTruthy();
-            expect(defDescNode.childNodes.length).toEqual(1);
-            expect(unorderedList.hasChildNodes()).toBeTruthy();
-            expect(unorderedList.childNodes.length).toEqual(2);
-        });
+        // Make sure the two items got the proper text
+        var listItem = unordered.childNodes;
+        expect(listItem[0].textContent).toEqual('UCSC-ENCODE-cv:AB_SOLiD_3.5');
+        expect(listItem[1].textContent).toEqual('GEO:GPL9442');
 
-        it('has proper link text', function() {
-            expect(listItems[0].textContent).toEqual('UCSC-ENCODE-cv:AB_SOLiD_3.5');
-            expect(listItems[1].textContent).toEqual('GEO:GPL9442');
-        });
-
-        it('has links to the proper places', function() {
-            expect(listItems[0].hasChildNodes()).toBeTruthy();
-            expect(listItems[0].childNodes.length).toEqual(1);
-            expect(listItems[1].hasChildNodes()).toBeTruthy();
-            expect(listItems[1].childNodes.length).toEqual(1);
-            expect(anchors[0].getAttribute('href')).toEqual('http://genome.cse.ucsc.edu/cgi-bin/hgEncodeVocab?ra=encode%2Fcv.ra&term="AB_SOLiD_3.5"');
-            expect(anchors[1].getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GPL9442');
-        });
+        // Make sure the two items contain links to the proper places
+        expect(listItem[0].hasChildNodes()).toEqual(true);
+        expect(listItem[0].childNodes.length).toEqual(1);
+        expect(listItem[1].hasChildNodes()).toEqual(true);
+        expect(listItem[1].childNodes.length).toEqual(1);
+        anchor = listItem[0].childNodes[0];
+        expect(anchor.getAttribute('href')).toEqual('http://genome.cse.ucsc.edu/cgi-bin/hgEncodeVocab?ra=encode%2Fcv.ra&term="AB_SOLiD_3.5"');
+        anchor = listItem[1].childNodes[0];
+        expect(anchor.getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GPL9442');
     });
 });
