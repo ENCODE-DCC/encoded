a4b5390c21cce1e4b3ff9194a0c4a17ac428250d
estherc <etchan@stanford.edu>
2014-07-21 14:19:08 -0700

    Add donor in biosample checks

--- src/encoded/audit/biosample.py
+++ src/encoded/audit/biosample.py
@@ -26,3 +26,21 @@ def audit_biosample_term(value, system):
     if ontology_term_name != term_name and term_name not in ontology[term_id]['synonyms']:
         detail = '{} - {} - {}'.format(term_id, term_name, ontology_term_name)
         raise AuditFailure('term name mismatch', detail, level='ERROR')
+
+
+@audit_checker('biosample')
+def audit_biosample_culture_date(value, system):
+    if value['culture_harvest_date'] <= value['culture_start_date']:
+        detail = 'culture_harvest_date precedes culture_start_date'
+        raise AuditFailure('invalid dates', detail, level='ERROR')
+
+
+@audit_checker('biosample')
+def audit_biosample_donor(value, system):
+    if (not value['donor']) and (not value['pooled_from']):
+        detail = 'biosample donor is missing'
+        raise AuditFailure('missing donor', detail, level='ERROR')
+
+    if value['organism']['name'] != value['donor']['organism']['name']:
+        detail = 'biosample and donor organism mismatch'
+        raise AuditFailure('organism mismatch') 
--- src/encoded/tests/test_audit_biosample.py
+++ src/encoded/tests/test_audit_biosample.py
@@ -2,20 +2,78 @@ import pytest
 
 
 @pytest.fixture
-def ntr_biosample(submitter, lab, award, source, organism):
-    return {
+def base_biosample(testapp, source, award, lab, organism):
+    item = {
         'award': award['uuid'],
-        'biosample_term_id': 'NTR:0000022',
-        'biosample_term_name': 'myocyte',
-        'biosample_type': 'in vitro differentiated cells',
         'lab': lab['uuid'],
-        'organism': organism['uuid'],
         'source': source['uuid'],
+        'organism': organism['uuid']
     }
+    return testapp.post_json('biosample', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def base_human(testapp, name, taxon_id, scientific_name):
+    item = {
+        'name': 'human',
+        'taxon_id': '9606',
+        'scientific_name': 'Homo sapiens'
+    }
+    return testapp.post_json('organism', item, status=201).json['@graph'][0]
+
+@pytest.fixture
+def base_fly(testapp, name, taxon_id, scientific_name):
+    item = {
+        'name': 'dmelanogaster',
+        'taxon_id': '7227',
+        'scientific_name': 'Drosophila melanogaster'
+    }
+    return testapp.post_json('organism', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def base_human_donor(testapp, award, lab, base_human):
+    item = {
+        'award': award['uuid'],
+        'lab': lab['uuid']
+        'organism': base_human['@id']
+    }
+    return testapp.post_json('human-donors', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def base_fly_donor(testapp, award, lab, base_fly):
+    item = {
+        'award': award['uuid'],
+        'lab': lab['uuid']
+        'organism': base_fly['@id']
+    }
+    return testapp.post_json('fly-donors', item, status=201).json['@graph'][0]
 
 
 def test_audit_biosample_term_ntr(testapp, ntr_biosample):
-    res = testapp.post_json('/biosample', ntr_biosample)
+    res = testapp.patch_json(base_biosample['@id'], {'biosample_term_id': 'NTR:0000022', 'biosample_term_name': 'myocyte', 'biosample_type': 'in vitro differentiated cells'})
     res = testapp.get(res.location + '@@index-data')
     error, = res.json['audit']
     assert error['category'] == 'NTR'
+
+
+def test_audit_biosample_culture_dates(testapp, base_biosample):
+    res = testapp.patch_json(base_biosample['@id'], {'culture_start_date': '2014-06-30', 'culture_harvest_date': '2014-06-25'})
+    res = testapp.get(res.location + '@@index-data')
+    error, = res.json['audit']
+    assert error['category'] == 'invalid dates'
+
+
+def test_audit_biosample_donor(testapp, base_biosample):
+    res = testapp.get(res.location + '@@index-data')
+    error, = res.json['audit']
+    assert error['category'] == 'missing donor'
+
+
+def test_audit_biosample_donor_organism(testapp, base_biosample, base_human_donor, base_fly):
+    res = testapp.patch_json(base_biosample['@id'], {'donor': base_human_donor['@id'], 'organism': base_fly['@id']})
+    res = testapp.get(res.location + '@@index-data')
+    error, = res.json['audit']
+    assert error['category'] == 'organism mismatch'
+    
