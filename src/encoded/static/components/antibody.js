e8cc603ab04e51025f27340472ef9fb7eff87e33
Forrest Tanaka <ftanaka@icloud.com>
2014-07-24 15:28:12 -0700

    Small change to spacing of document link in antibodies.

--- src/encoded/static/components/antibody.js
+++ src/encoded/static/components/antibody.js
@@ -170,9 +170,8 @@ var Characterization = module.exports.Characterization = React.createClass({
             attachmentHref = url.resolve(context['@id'], context.attachment.href);
             download = (
                 <div className="dl-link">
-                    <i className="icon icon-download"></i>&nbsp;
-                    <a data-bypass="true" href={attachmentHref} download={context.attachment.download}>
-                        <div>{context.attachment.download}</div>
+                    <i className="icon icon-download"></i>&nbsp;<a data-bypass="true" href={attachmentHref} download={context.attachment.download}>
+                        {context.attachment.download}
                     </a>
                 </div>
             );
