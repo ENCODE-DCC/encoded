dbd95999de9514ef2f54c24a63e6bed8ea901377
Cricket Sloan <cricketsloan@gmail.com>
2014-07-24 08:03:47 -0700

    Adding in the upgrade

--- src/encoded/tests/test_upgrade_file.py
+++ src/encoded/tests/test_upgrade_file.py
@@ -28,3 +28,26 @@ def test_file_upgrade(app, file_1):
     value = migrator.upgrade('file', file_1, target_version='2')
     assert value['schema_version'] == '2'
     assert value['status'] == 'current'
+
+
+def test_file_upgrade2(app, file_1):
+
+    # Set the experiment to released
+    file_1['dataset'].update({
+        'status': 'released'
+        })
+
+    migrator = app.registry['migrator']
+    value = migrator.upgrade('file', file_1, target_version='3')
+    assert value['schema_version'] == '3'
+    assert value['status'] == 'released'
+
+    # Reset the file and experiment to NOT released
+    file_1['status'] = 'current'
+    file_1['dataset'].update({
+        'status': 'release_ready'
+        })
+
+    value = migrator.upgrade('file', file_1, target_version='3')
+    assert value['schema_version'] == '3'
+    assert value['status'] == 'in progress'    
--- src/encoded/upgrade/file.py
+++ src/encoded/upgrade/file.py
@@ -7,3 +7,14 @@ def file_0_2(value, system):
     # http://redmine.encodedcc.org/issues/1307
     if 'status' in value:
         value['status'] = value['status'].lower()
+
+
+@upgrade_step('file', '2', '3')
+def file_2_3(value, system):
+    # http://redmine.encodedcc.org/issues/1429
+    # http://redmine.encodedcc.org/issues/
+    if value.get('status') == 'current':
+        if value['experiment'].get('status') == 'released':
+            value['status'] = 'released'
+        else:
+            value['status'] = 'in progress'
