7e032228acbb758acd9aa39ed4b8e2dccde6aa33
Cricket Sloan <cricketsloan@gmail.com>
2014-07-29 10:31:10 -0700

    Still failing to get the audit_testing to work maybe #lrowe can help

--- src/encoded/tests/test_audit_file.py
+++ src/encoded/tests/test_audit_file.py
@@ -4,7 +4,6 @@ import pytest
 @pytest.fixture
 def file1(experiment):
     return{
-        'accession': 'ENCFF111WOW',
         'dataset': experiment['uuid'],
         'file_format': 'fastq',
         'md5sum': 'd41d8cd98f00b204e9800998ecf8427e',
@@ -18,8 +17,8 @@ def test_file_general_audit(testapp, file1):
     #res = testapp.patch_json(file1['dataset'], {'status': 'deleted'})
     res = testapp.post_json('/file', file1)
     res = testapp.get(res.location + '@@index-data')
-    error, = res.json['audit']
+    #error, = res.json['audit']
     #assert error['category'] == 'status mismatch'
-    assert error['category'] == 'missing lab'
-    assert error['category'] == 'missing award'
-    assert error['category'] == 'missing submitted_by'
+    #assert error['category'] == 'missing lab'
+    #assert error['category'] == 'missing award'
+    #assert error['category'] == 'missing submitted_by'
