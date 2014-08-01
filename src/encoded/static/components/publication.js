bb2a9e3f17968e912a13f888721c4e5ac499fca6
Forrest Tanaka <ftanaka@icloud.com>
2014-08-01 04:01:39 -0700

    Handle publications without authors in search.

--- src/encoded/static/components/publication.js
+++ src/encoded/static/components/publication.js
@@ -59,7 +59,7 @@ var Listing = React.createClass({
     mixins: [search.PickerActionsMixin],
     render: function() {
         var context = this.props.context;
-        var authorList = context.authors.split(', ', 4);
+        var authorList = context.authors ? context.authors.split(', ', 4) : [];
         var authors = authorList.length === 4 ? authorList.splice(0, 3).join(', ') + ', et al' : context.authors;
         return (<li>
                     <div>
@@ -71,7 +71,7 @@ var Listing = React.createClass({
                         <div className="accession"><a href={context['@id']}>{context.title}</a></div>
                     </div>
                     <div className="data-row">
-                        <p className="list-author">{authors}.</p>
+                        {authors ? <p className="list-author">{authors}.</p> : null}
                         <p className="list-citation"><i>{context.journal}</i>. {context.date_published};{context.volume}{context.issue ? '(' + context.issue + ')' : '' }:{context.page}.</p>
                         {context.references.length ? <DbxrefList values={context.references} className="list-reference" /> : '' }
                     </div>
