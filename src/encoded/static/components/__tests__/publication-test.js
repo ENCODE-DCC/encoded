3f7c27b5f6c17b20fa9bc1855af7d9f992219811
Forrest Tanaka <forrest@forrest-tanaka.com>
2014-08-01 08:54:17 -0700

    Update publication Jest test to check PMCID links.

--- src/encoded/static/components/__tests__/publication-test.js
+++ src/encoded/static/components/__tests__/publication-test.js
@@ -63,7 +63,7 @@ describe('Publication', function() {
 
         beforeEach(function() {
             var context_ref = _.clone(context);
-            context_ref.references = [require('../testdata/references/PMID19352372'), require('../testdata/references/PMID20150147')];
+            context_ref.references = ['PMID:19352372', 'PMCID:3735843'];
             publication = <Panel context={context_ref} />;
             TestUtils.renderIntoDocument(publication);
         });
@@ -76,7 +76,7 @@ describe('Publication', function() {
             var anchor = li[0].getElementsByTagName('a');
             expect(anchor[0].getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/pubmed/?term=19352372');
             anchor = li[1].getElementsByTagName('a');
-            expect(anchor[0].getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/pubmed/?term=20150147');
+            expect(anchor[0].getAttribute('href')).toEqual('http://www.ncbi.nlm.nih.gov/pmc/articles/3735843');
         });
     });
 });
