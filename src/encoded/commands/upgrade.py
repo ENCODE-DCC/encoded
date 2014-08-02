8ba3b8d76eef11ddd1afbfb42f6626864378364a
Laurence Rowe <laurence@lrowe.co.uk>
2014-08-02 14:16:52 -0700

    Add progress logging for upgrade.

--- src/encoded/commands/upgrade.py
+++ src/encoded/commands/upgrade.py
@@ -56,6 +56,8 @@ def run(testapp, collections):
                     except Exception:
                         logger.exception('Upgrade failed for: /%s/%s', collection_name, uuid)
                         errors += 1
+                if count % 1000:
+                    logger.info('Upgrading %s: %d', collection_name, count)
             logger.info('Upgraded %s: %d (errors: %d)', collection_name, count, errors)
 
 
