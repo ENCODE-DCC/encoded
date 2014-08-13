0780178deb741383c51c066941d56cdc326f1a60
cricketsloan <cricketsloan@stanford.edu>
2014-08-13 10:33:54 -0700

    Update test_audit_file.py
    moving rejected changes from 1699 to 1714

--- src/encoded/tests/test_audit_file.py
+++ src/encoded/tests/test_audit_file.py
@@ -2,11 +2,13 @@ import pytest
 
 
 @pytest.fixture
-def file1(experiment):
+def file1(experiment, award, lab):
     return{
         'dataset': experiment['uuid'],
         'file_format': 'fastq',
         'md5sum': 'd41d8cd98f00b204e9800998ecf8427e',
         'output_type': 'reads',
+        'award': award['uuid'],
+        'lab': lab['uuid']
         'status': 'released'
     }
