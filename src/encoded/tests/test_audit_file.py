950505234441c7ee0c2af37b2a5299d78dacfe3c
Cricket Sloan <cricketsloan@gmail.com>
2014-08-14 12:19:48 -0700

    Audits need the testapp

--- src/encoded/tests/test_audit_file.py
+++ src/encoded/tests/test_audit_file.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture
-def file1(experiment, award, lab):
+def file1(experiment, award, lab, testapp):
     item = {
         'dataset': experiment['uuid'],
         'file_format': 'fastq',
@@ -20,7 +20,7 @@ def test_audit_paired_with(testapp, file1):
     errors = res.json['audit']
     assert any(error['category'] == 'missing paired_with' for error in errors)
 
-def test_audit_paired_with(testapp, file1):
+def test_audit_file_size(testapp, file1):
     res = testapp.get(file1['@id'] + '@@index-data')
     errors = res.json['audit']
-    assert any(error['category'] == 'missing file_size' for error in errors)
+    assert any(error['category'] == 'missing file_size' for error in errors) 
