ec85d500a89e6efe1a16c90f1ee629f22110b31d
Venkat Malladi <vmalladi@stanford.edu>
2014-08-19 13:35:40 -0700

    If attachement, must have method.

--- src/encoded/tests/test_schema_antibody_characterization.py
+++ src/encoded/tests/test_schema_antibody_characterization.py
@@ -1,5 +1,8 @@
 import pytest
 
+RED_DOT = """data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA
+AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+9TXL0Y4OHwAAAABJRU5ErkJggg=="""
 
 @pytest.fixture
 def antibody_characterization(submitter, award, lab, antibody_lot, target):
@@ -15,3 +18,9 @@ def test_antibody_characterization_review(testapp, antibody_characterization):
     antibody_characterization['characterization_review'] = [ {"organism": "human", "lane": 2, "biosample_type": "immortalized cell line", "biosample_term_name": "K562", "biosample_term_id": "EFO:0002067"}]
     testapp.post_json('/antibody_characterization', antibody_characterization, status=422)
 
+
+def test_antibody_characterizaton_two_methods(testapp, antibody_characterization):
+    antibody_characterization['primary_characterization_method'] = 'immunoblot'
+    antibody_characterization['secondary_characterization_method'] = 'dot blot assay'
+    antibody_characterization['attachment'] =  {'download': 'red-dot.png', 'href': RED_DOT}
+    testapp.post_json('/antibody_characterization', antibody_characterization, status=422)
