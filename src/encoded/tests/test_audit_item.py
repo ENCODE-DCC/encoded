6a1d670df522e875042a9aef271ecc3a29a1de0f
Laurence Rowe <laurence@lrowe.co.uk>
2014-08-21 16:21:57 -0700

    Some more tests for schema validation audit.

--- src/encoded/tests/test_audit_item.py
+++ src/encoded/tests/test_audit_item.py
@@ -5,8 +5,30 @@ def test_audit_item_schema_validation(testapp, organism):
     assert any(error['category'] == 'validation error' for error in errors)
 
 
-def test_audit_item_schema_upgrade(testapp, organism):
+def test_audit_item_schema_upgrade_failure(testapp, organism):
     testapp.patch_json(organism['@id'] + '?validate=false', {'schema_version': '999'})
     res = testapp.get(organism['@id'] + '@@index-data')
     errors = res.json['audit']
     assert any(error['category'] == 'upgrade failure' for error in errors)
+
+
+def test_audit_item_schema_upgrade_ok(testapp, organism):
+    patch = {
+        'schema_version': '1',
+        'status': 'CURRENT',
+    }
+    testapp.patch_json(organism['@id'] + '?validate=false', patch)
+    res = testapp.get(organism['@id'] + '@@index-data')
+    errors = res.json['audit']
+    assert not errors
+
+
+def test_audit_item_schema_upgrade_validation_failure(testapp, organism):
+    patch = {
+        'schema_version': '1',
+        'status': 'UNKNOWN',
+    }
+    testapp.patch_json(organism['@id'] + '?validate=false', patch)
+    res = testapp.get(organism['@id'] + '@@index-data')
+    errors = res.json['audit']
+    assert any(error['category'] == 'validation error: status' for error in errors)
