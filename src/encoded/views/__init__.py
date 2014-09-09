8b2dcfdffc887f196d50faa2eb31da426b15d940
Laurence Rowe <laurence@lrowe.co.uk>
2014-09-08 21:06:55 -0700

    Attempt at defining an ontology from our schemas. See: https://gist.github.com/stain/7690362 Problems: We have a term conflict between dataset the property and dataset the type, maybe we need to capitalize types...

--- src/encoded/views/__init__.py
+++ src/encoded/views/__init__.py
@@ -11,6 +11,7 @@ from ..contentbase import (
 def includeme(config):
     config.registry['encoded.processid'] = os.getppid()
     config.add_route('schema', '/profiles/{item_type}.json')
+    config.add_route('jsonld_terms', '/terms')
     config.add_route('jsonld_context', '/context.jsonld')
     config.add_route('graph', '/profiles/graph.dot')
     config.scan()
