1c229ab1cd98eb9f7379fc5dfc112a30d59338fa
nikhilrp <nikhilrp@stanford.edu>
2014-11-10 20:38:51 -0800

    Added "hgHub_do_firstDb" flag for trackhub

--- src/encoded/views/search.py
+++ src/encoded/views/search.py
@@ -289,7 +289,7 @@ def search(context, request, search_type=None):
             if facet['count'] > 0:
                 hub = request.url.replace('search/?', 'batch_hub/') + '/hub.txt'
                 hub = hub.replace('&', ',,')
-                hgConnect = 'http://genome.ucsc.edu/cgi-bin/hgHubConnect?hgHub_do_redirect=on&hgHubConnect.remakeTrackHub=on&'
+                hgConnect = 'http://genome.ucsc.edu/cgi-bin/hgHubConnect?hgHub_do_redirect=on&hgHubConnect.remakeTrackHub=on&hgHub_do_firstDb=1&'
                 result['batch_hub'] = hgConnect + '&'.join([
                     'hubUrl=' + hub
                 ])
