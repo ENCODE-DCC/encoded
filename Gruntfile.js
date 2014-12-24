21b4c1057111b23aaf16c667a7a8a06dfd6913a0
David Glick <david@glicksoftware.com>
2014-12-23 16:46:00 -0800

    use watchify to speed browserification

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,6 @@ module.exports = function(grunt) {
                     'brace/mode/json',
                     'brace/theme/solarized_light',
                 ],
-                options: {
-                    debug: true,
-                },
                 plugin: [
                     ['minifyify', {
                         map: 'brace.js.map',
@@ -40,9 +37,6 @@ module.exports = function(grunt) {
                 src: [
                     './src/encoded/static/inline.js',
                 ],
-                options: {
-                    debug: true,
-                },
                 require: [
                     'scriptjs',
                     'google-analytics',
@@ -69,9 +63,6 @@ module.exports = function(grunt) {
                     './src/encoded/static/libs/respond.js',
                     './src/encoded/static/browser.js',
                 ],
-                options: {
-                    debug: true,
-                },
                 external: [
                     'brace',
                     'brace/mode/json',
@@ -105,7 +96,6 @@ module.exports = function(grunt) {
                 src: ['./src/encoded/static/server.js'],
                 options: {
                     builtins: false,
-                    debug: true,
                     detectGlobals: false,
                 },
                 transform: [
@@ -149,13 +139,23 @@ module.exports = function(grunt) {
 
     grunt.registerMultiTask('browserify', function () {
         var browserify = require('browserify');
+        var watchify = require('watchify');
+        var _ = grunt.util._;
         var path = require('path');
         var fs = require('fs');
         var data = this.data;
-        var done = this.async();
-        var options = data.options || {};
+        var watch = (process.env.NODE_ENV !== 'production');
+        var options = _.extend({
+            debug: true,
+            cache: {},
+            packageCache: {},
+            fullPaths: watch
+        }, data.options);
 
         var b = browserify(options);
+        if (watch) {
+            b = watchify(b);
+        }
 
         var i;
         var reqs = [];
@@ -200,13 +200,29 @@ module.exports = function(grunt) {
         var dest = data.dest;
         grunt.file.mkdir(path.dirname(dest));
 
-        var out = fs.createWriteStream(dest);
-        b.bundle().pipe(out);
-        out.on('close', done);
+        var bundle = function(done) {
+            var out = fs.createWriteStream(dest);
+            b.bundle().pipe(out);
+            out.on('close', function() {
+                grunt.log.write('Wrote ' + dest + '\n');
+                if (done !== undefined) done();
+            });
+        };
+        b.on('update', function() { bundle(); });
+        var done = this.async();
+        bundle(done);
+    });
 
+    grunt.registerTask('wait', function() {
+        grunt.log.write('Waiting for changes...\n');
+        this.async();
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['browserify', 'copy']);
+    if (process.env.NODE_ENV === 'production') {
+        grunt.registerTask('default', ['browserify', 'copy']);
+    } else {
+        grunt.registerTask('default', ['browserify', 'copy', 'wait']);
+    }
 };
