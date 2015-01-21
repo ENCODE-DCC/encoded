0b944d38df007c5b62f42f9b368dd0aced5b80bd
Laurence Rowe <laurence@lrowe.co.uk>
2015-01-20 17:46:37 -0800

    Use ssd ebs volumes

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -25,9 +25,12 @@ def run(wale_s3_prefix, branch=None, name=None, persistent=False):
 
     conn = boto.ec2.connect_to_region("us-west-2")
     bdm = BlockDeviceMapping()
+    bdm['/dev/xvda1'] = BlockDeviceType(volume_type='gp2', delete_on_termination=True)
     if persistent:
-        bdm['/dev/xvdf'] = BlockDeviceType(snapshot_id='snap-8f90c779', delete_on_termination=True)
-        bdm['/dev/xvdg'] = BlockDeviceType(snapshot_id='snap-8f90c779', delete_on_termination=True)
+        bdm['/dev/xvdf'] = BlockDeviceType(
+            volume_type='gp2', snapshot_id='snap-8f90c779', delete_on_termination=True)
+        bdm['/dev/xvdg'] = BlockDeviceType(
+            volume_type='gp2', snapshot_id='snap-8f90c779', delete_on_termination=True)
     else:
         bdm['/dev/xvdf'] = BlockDeviceType(ephemeral_name='ephemeral0')
         bdm['/dev/xvdg'] = BlockDeviceType(ephemeral_name='ephemeral1')
