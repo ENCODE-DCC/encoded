89f15ce21a7b65987ee9964c4dc4252d958ea4e6
nikhilrp <nikhilrp@stanford.edu>
2015-02-02 10:11:30 -0800

    First phase of batch download

--- src/encoded/views/download.py
+++ src/encoded/views/download.py
@@ -6,6 +6,52 @@ from urllib.parse import (
     urlencode,
 )
 
+_exp_columns = [
+    'accession',
+    'assay_term_id',
+    'assay_term_name',
+    'biosample_term_id',
+    'biosample_term_name',
+    'biosample_type',
+    'target'
+]
+
+
+@view_config(route_name='metadata', request_method='GET')
+def metadata_csv(context, request):
+
+    param_list = parse_qs(request.matchdict['search_params'].encode('utf-8'))
+    param_list['frame'] = ['object']
+    param_list['field'] = _exp_columns
+    param_list['limit'] = ['all']
+
+    path = '/search/?%s' % urlencode(param_list, True)
+    results = embed(request, path, as_user=True)
+
+    data = []
+    for row in results['@graph']:
+        data_row = ''
+        for column in _exp_columns:
+            row_column = ''
+            if column in row:
+                row_column = row[column]
+            if data_row == '':
+                data_row = '{value}'.format(
+                    data_row=data_row,
+                    value=row_column
+                )
+            else:
+                data_row = '{data_row},{value}'.format(
+                    data_row=data_row,
+                    value=row_column
+                )
+        data.append(data_row)
+    return Response(
+        content_type='text/csv',
+        body='\n'.join(data),
+        content_disposition='attachment; filename="%s"' % 'metadata.csv'
+    )
+
 
 @view_config(route_name='batch_download', request_method='GET')
 def batch_download(context, request):
@@ -14,10 +60,11 @@ def batch_download(context, request):
     param_list = parse_qs(request.matchdict['search_params'].encode('utf-8'))
     param_list['field'] = ['files.href', 'files.md5sum', 'files.file_size', 'files.file_format']
     param_list['files.status'] = ['released']
+    param_list['limit'] = ['all']
 
     path = '/search/?%s' % urlencode(param_list, True)
     results = embed(request, path, as_user=True)
-    metadata_link = '{host_url}/@@metadata/{search_params}'.format(
+    metadata_link = '{host_url}/metadata/{search_params}'.format(
         host_url=request.host_url,
         search_params=request.matchdict['search_params']
     )
