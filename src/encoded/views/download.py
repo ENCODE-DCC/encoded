b473535043c6d8d45366f233b95219bcea6f0795
nikhilRP <nikhilrp@stanford.edu>
2015-02-02 14:24:29 -0800

    few more changes to the downloads

--- src/encoded/views/download.py
+++ src/encoded/views/download.py
@@ -8,13 +8,31 @@ from urllib.parse import (
 
 _exp_columns = [
     'accession',
+    'description',
     'assay_term_id',
     'assay_term_name',
     'biosample_term_id',
     'biosample_term_name',
     'biosample_type',
-    'target'
+    'target',
+    'run_type',
+    'assembly',
+    'month_released',
+    'lab.title',
+    'files.accession'
 ]
+
+import collections
+
+def flatten(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 
 @view_config(route_name='metadata', request_method='GET')
@@ -30,11 +48,22 @@ def metadata_csv(context, request):
 
     data = []
     for row in results['@graph']:
+        row = flatten(row)
         data_row = ''
         for column in _exp_columns:
-            row_column = ''
+            row_column = []
             if column in row:
-                row_column = row[column]
+                if isinstance(row[column], list):
+                    row_column = row[column]
+                else:
+                    row_column = [row[column]]
+            elif column.split('.')[0] in row:
+                for c in row[column.split('.')[0]]:
+                    row_column.append(flatten(c).values()[0])
+
+            row_column = '"{column}"'.format(
+                column=','.join(row_column)
+            )
             if data_row == '':
                 data_row = '{value}'.format(
                     data_row=data_row,
@@ -58,7 +87,7 @@ def batch_download(context, request):
 
     # adding extra params to get requied columsn
     param_list = parse_qs(request.matchdict['search_params'].encode('utf-8'))
-    param_list['field'] = ['files.href', 'files.md5sum', 'files.file_size', 'files.file_format']
+    param_list['field'] = ['files.href', 'files.file_format']
     param_list['files.status'] = ['released']
     param_list['limit'] = ['all']
 
@@ -73,20 +102,16 @@ def batch_download(context, request):
         for exp in results['@graph']:
             for f in exp['files']:
                 if f['file_format'] in param_list['file_format']:
-                    files.append('{host_url}{href}#md5={md5};size={size}'.format(
+                    files.append('{host_url}{href}'.format(
                         host_url=request.host_url,
-                        href=f['href'],
-                        md5=f['md5sum'],
-                        size=f['file_size']
+                        href=f['href']
                     ))
     else:
         for exp in results['@graph']:
             for f in exp['files']:
                 files.append('{host_url}{href}#md5={md5};size={size}'.format(
                     host_url=request.host_url,
-                    href=f['href'],
-                    md5=f['md5sum'],
-                    size=f['file_size']
+                    href=f['href']
                 ))
     return Response(
         content_type='text/plain',
