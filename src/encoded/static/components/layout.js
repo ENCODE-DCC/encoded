7644a588f95f444fd16764c7088f7029a82f58b3
Laurence Rowe <laurence@lrowe.co.uk>
2015-03-11 18:37:13 -0700

    Fix block editing.

--- src/encoded/static/components/layout.js
+++ src/encoded/static/components/layout.js
@@ -60,6 +60,7 @@ var BlockEditModal = React.createClass({
     },
 
     onChange: function(value) {
+        if (value.toJS !== undefined) value = value.toJS();
         this.setState({value: value});
     },
 
@@ -71,7 +72,7 @@ var BlockEditModal = React.createClass({
     },
 
     save: function() {
-        this.props.onChange(this.state.value.toJS());
+        this.props.onChange(this.state.value);
         this.props.onRequestHide();
     }
 
