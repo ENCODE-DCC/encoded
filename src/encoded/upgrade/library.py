1dc7dd40ef9f40502d7e7dba384160b6f16194cf
estherc <etchan@stanford.edu>
2015-04-07 16:56:51 -0700

    Add upgrade for fragmentation_enum mapping

--- src/encoded/upgrade/library.py
+++ src/encoded/upgrade/library.py
@@ -24,3 +24,56 @@ def library_3_4(value, system):
 
     if 'paired_ended' in value:
         del value['paired_ended']
+
+    covaris_generic = [
+        'A combination of random priming and covaris shearing',
+        'covaris sheering',
+        'covarius'
+        ]
+
+    sonication_generic = [
+        'probe in',
+        'probe-in',
+        'sonication'
+    ]
+
+    chemical_generic = [
+        'magnesium-catalyzed hydrolysis',
+        'chemical'
+    ]
+
+    tagmentation = [
+        'Illumina/Nextera tagmentation',
+        'Nextera Tagmentation'
+    ]
+
+    bioruptor_twin = [
+        'biorupter twin',
+        'bioruptor twin'
+    ]
+
+    if 'fragmentation_method' in value:
+        if value['fragmentation_method'] in covaris_generic:
+            value['fragmentation_method'] = 'shearing (Covaris generic)'
+        elif value['fragmentation_method'] == 'Bioruptor 300':
+            value['fragmentation_method'] = 'sonication (Bioruptor Plus)'
+        elif value['fragmentation_method'] == 'Covaris S2':
+            value['fragmentation_method'] = 'shearing (Covaris S2)'
+        elif value['fragmentation_method'] == 'Diagenode Bioruptor':
+            value['fragmentation_method'] = 'sonication (Bioruptor generic)'
+        elif value['fragmentation_method'] in tagmentation:
+            value['fragmentation_method'] = 'chemical (Nextera tagmentation)'
+        elif value['fragmentation_method'] == 'None':
+            value['fragmentation_method'] = 'none'
+        elif value['fragmentation_method'] in bioruptor_twin:
+            value['fragmentation_method'] = 'sonication (Bioruptor Twin)'
+        elif value['fragmentation_method'] in chemical_generic:
+            value['fragmentation_method'] = 'chemical (generic)'
+        elif value['fragmentation_method'] == 'chemical (part of Illumina TruSeq mRNA Kit)':
+            value['fragmentation_method'] = 'chemical (Illumina TruSeq)'
+        elif value['fragmentation_method'] == 'not applicable':
+            value['fragmentation_method'] = 'n/a'
+        elif value['fragmentation_method'] in sonication_generic:
+            value['fragmentation_method'] = 'sonication (generic)'
+        else:
+            value['fragmentation_method'] = 'see document'
