7ecd946ebe896b746f56fd8dab2bce402dee68c8
Laurence Rowe <laurence@lrowe.co.uk>
2015-04-22 19:26:55 -0700

    Refactor JSON item editor to use new components.

--- src/encoded/static/components/edit.js
+++ src/encoded/static/components/edit.js
@@ -4,9 +4,10 @@ var globals = require('./globals');
 var parseAndLogError = require('./mixins').parseAndLogError;
 var fetched = require('./fetched');
 var _ = require('underscore');
-var $script = require('scriptjs');
 var ga = require('google-analytics');
-
+var Ace = require('./ace');
+var ScriptReady = require('./scriptready');
+var Spinner = require('./spinner');
 
 var sorted_json = module.exports.sorted_json = function (obj) {
     if (obj instanceof Array) {
@@ -56,12 +57,23 @@ var EditForm = module.exports.EditForm = React.createClass({
         var error = this.state.error;
         return (
             <div>
-                <div ref="editor" style={{
-                    position: "relative !important",
-                    border: "1px solid lightgray",
-                    margin: "auto",
-                    width: "100%"
-                }}></div>
+                <ScriptReady scripts={['brace']} spinner={<Spinner />}>
+                    <Ace
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        onChangeAnnotation={this.hasErrors}
+                        mode="ace/mode/json"
+                        options={{
+                            maxLines: 1000,
+                            minLines: 24
+                        }}
+                        style={{
+                            position: "relative !important",
+                            border: "1px solid lightgray",
+                            margin: "auto",
+                            width: "100%"
+                        }} />
+                </ScriptReady>
                 <div style={{"float": "right", "margin": "10px"}}>
                     <a href="" className="btn btn-default">Cancel</a>
                     {' '}
@@ -76,30 +88,12 @@ var EditForm = module.exports.EditForm = React.createClass({
         );
     },
 
-    componentDidMount: function () {
-        $script('brace', this.setupEditor);
+    handleChange: function (value) {
+        this.setState({value: value});
     },
 
-    setupEditor: function () {
-        var ace = require('brace');
-        require('brace/mode/json');
-        require('brace/theme/solarized_light');
-        var value = JSON.stringify(sorted_json(this.props.data), null, 4);
-        var editor = ace.edit(this.refs.editor.getDOMNode());
-        var session = editor.getSession();
-        session.setMode('ace/mode/json');
-        editor.setValue(value);
-        editor.setOptions({
-            maxLines: 1000,
-            minLines: 24
-        });
-        editor.clearSelection();
-        this.setState({editor: editor});
-        session.on("changeAnnotation", this.hasErrors);
-    },
-
-    hasErrors: function () {
-        var annotations = this.state.editor.getSession().getAnnotations();
+    hasErrors: function (editor) {
+        var annotations = editor.getSession().getAnnotations();
         var has_error = annotations.reduce(function (value, anno) {
             return value || (anno.type === "error");
         }, false);
@@ -111,13 +105,14 @@ var EditForm = module.exports.EditForm = React.createClass({
             communicating: false,
             data: undefined,
             putRequest: undefined,
-            erred: false
+            erred: false,
+            value: JSON.stringify(sorted_json(this.props.data), null, 4)
         };
     },
 
     save: function (e) {
         e.preventDefault();
-        var value = this.state.editor.getValue();
+        var value = this.state.value;
         var url = this.props.context['@id'];
         var request = this.context.fetch(url, {
             method: 'PUT',
