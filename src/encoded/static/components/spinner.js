533551851311af9caa7b0419638fc7ba37a0ea57
Laurence Rowe <laurence@lrowe.co.uk>
2015-04-22 19:23:29 -0700

    Loading spinner component.

---
+++ src/encoded/static/components/spinner.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var Spinner = module.exports = React.createClass({
+    render: function() {
+        return (<div className="loading-spinner communicating"></div>);
+    }
+});
