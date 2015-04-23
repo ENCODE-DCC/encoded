cb303837ac42bbd74f50721e6942ef0f5d4b9b8a
Laurence Rowe <laurence@lrowe.co.uk>
2015-04-22 19:24:09 -0700

    ScriptReady component

---
+++ src/encoded/static/components/scriptready.js
@@ -0,0 +1,53 @@
+/* Wrapper component to require async script loading.
+
+Example::
+
+    <ScriptReady scripts={['brace']}>
+        <Ace />
+    </ScriptReady>
+
+*/
+
+var React = require('react');
+var $script = require('scriptjs');
+var ScriptReady = module.exports = React.createClass({
+    propTypes: {
+        scripts: React.PropTypes.array,
+        spinner: React.PropTypes.element
+    },
+    getDefaultProps: function() {
+        return {
+            scripts: [],
+            spinner: null,
+        }
+    },
+    getInitialState: function() {
+        return {loaded: {}};
+    },
+    componentDidMount: function() {
+        var scripts = this.props.scripts;
+        $script(scripts, this.onScriptLoad.bind(null, scripts));
+    },
+    componentWillReceiveProps: function(nextProps) {
+        var loaded = this.state.loaded;
+        var scripts = nextProps.scripts;
+        if (scripts.filter(name => !loaded[name]).length > 0) {
+            $script(scripts, this.onScriptLoad.bind(null, scripts));
+        }
+    },
+    onScriptLoad: function(scripts) {
+        var loaded = this.state.loaded;
+        scripts.forEach(name => {
+            loaded[name] = true;
+        });
+        this.setState({loaded: loaded});
+    },
+    render: function() {
+        var loaded = this.state.loaded;
+        var scripts = this.props.scripts;
+        if (scripts.filter(name => !loaded[name]).length > 0) {
+            return this.props.spinner;
+        }
+        return React.Children.only(this.props.children);
+    }
+});
