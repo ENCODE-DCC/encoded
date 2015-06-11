1dbbc361c4db866eea66663940ab5e7c78c3887c
Laurence Rowe <laurence@lrowe.co.uk>
2015-06-10 18:13:20 -0700

    Use pytest-bdd and pytest-splinter instead of custom pytest/behave plugin.

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -1,17 +1,5 @@
 import pytest
 
-pytest_plugins = 'encoded.tests.bdd'
-
-
-# patch cookie support back into spliter's remote webdriver
-from splinter.driver.webdriver.remote import WebDriver
-from splinter.driver.webdriver.cookie_manager import CookieManager
-old__init__ = WebDriver.__init__
-def __init__(self, *args, **kw):
-    old__init__(self, *args, **kw)
-    self._cookie_manager = CookieManager(self.driver)
-WebDriver.__init__ = __init__
-
 
 @pytest.fixture(scope='session')
 def app_settings(server_host_port, elasticsearch_server, postgresql_server):
@@ -30,9 +18,8 @@ def app(app_settings):
 
 # Though this is expensive, set up first within browser tests to avoid remote
 # browser timeout
-# XXX Ideally this wouldn't be autouse...
 @pytest.mark.fixture_cost(-1)
-@pytest.yield_fixture(scope='session', autouse=True)
+@pytest.yield_fixture(scope='session')
 def workbook(app):
     from webtest import TestApp
     environ = {
@@ -52,67 +39,21 @@ def workbook(app):
     # XXX cleanup
 
 
-@pytest.yield_fixture(scope='session')
-def admin_user(context, browser):
-    context.browser.visit(context.base_url)  # need to be on domain to set cookie
-    context.browser.cookies.add({'REMOTE_USER': 'TEST'})
-    yield
-    context.browser.cookies.delete('REMOTE_USER')
+@pytest.fixture(scope='session')
+def base_url(_server):
+    return _server
 
 
-@pytest.fixture(scope='session', autouse=True)
-def set_webdriver(request, context):
-    context.default_browser = request.config.option.browser
-    context.remote_webdriver = request.config.option.remote_webdriver
-    context.browser_args = dict(request.config.option.browser_args or ())
+@pytest.fixture(scope='session')
+def splinter_browser_load_condition():
+
+    def condition(browser):
+        return browser.is_element_not_present_by_css("#application") or \
+            browser.is_element_not_present_by_css(".communicating")
+
+    return condition
 
 
-@pytest.mark.fixture_cost(1000)
-@pytest.fixture(scope='session', autouse=True)
-def browser(context, before_all, set_webdriver):
-    from behaving.web.steps.browser import given_a_browser
-    # context.default_browser = 'remote'
-    given_a_browser(context)
-    context.browser.driver.set_window_size(1024, 768)
-
-
-# These are equivalent to the environment.py hooks
-@pytest.fixture(scope='session', autouse=True)
-def before_all(request, _server, context):
-    import behaving.web
-    behaving.web.setup(context)
-    context.base_url = _server
-    from selenium.common.exceptions import WebDriverException
-
-    @request.addfinalizer
-    def after_all():
-        try:
-            behaving.web.teardown(context)
-        except WebDriverException:
-            pass  # remote webdriver may already have gone away
-
-
-#@pytest.fixture(scope='function', autouse=True)
-def before_scenario(request, context, scenario):
-    pass
-
-    @request.addfinalizer
-    def after_scenario():
-        pass
-
-
-@pytest.fixture(scope='subfunction', autouse=True)
-def before_step(request, context, step):
-
-    @request.addfinalizer
-    def after_step():
-        import textwrap
-        if step.status != 'failed':
-            return
-        print('')
-        print("=" * 70)
-        print("  Screenshot URL: %s" % context.browser.url)
-        print("-" * 70)
-        screenshot = context.browser.driver.get_screenshot_as_base64()
-        print('\n'.join(textwrap.wrap('data:image/png;base64,' + screenshot)))
-        print("=" * 70)
+@pytest.fixture(scope='session')
+def splinter_driver_kwargs(request):
+    return dict(request.config.option.browser_args or ())
