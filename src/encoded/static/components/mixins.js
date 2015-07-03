8cdde987c62a7450b780e5846d28db19ea7ed367
Laurence Rowe <laurence@lrowe.co.uk>
2015-07-02 17:52:02 -0700

    Make loading persona optional for easier testing.

--- src/encoded/static/components/mixins.js
+++ src/encoded/static/components/mixins.js
@@ -97,8 +97,9 @@ module.exports.Persona = {
 
     componentDidMount: function () {
         // Login / logout actions must be deferred until persona is ready.
-        this.extractSessionCookie();
-        $script.ready('persona', this.configurePersona);
+        if (!this.parseSessionCookie(this.extractSessionCookie()).disable_persona) {
+          $script.ready('persona', this.configurePersona);
+        }
     },
 
     ajaxPrefilter: function (options, original, xhr) {
@@ -153,6 +154,7 @@ module.exports.Persona = {
         if (this.props.session_cookie !== session_cookie) {
             this.setProps({session_cookie: session_cookie});
         }
+        return session_cookie;
     },
 
     componentWillReceiveProps: function (nextProps) {
