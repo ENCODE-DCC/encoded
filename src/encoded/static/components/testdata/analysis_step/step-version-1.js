8683034f82a4d545917077c7f5a645350999314f
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-07-12 12:01:38 -0700

    Reenable graph Jest tests
    Disabled graph tests previously. Rewrote them for the new objects and relationships, and reenabled them.

---
+++ src/encoded/static/components/testdata/analysis_step/step-version-1.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "@id": "/analysis-step-versions/871a52b2-6874-4927-98c6-1d0b74bbc194/",
+    "@type": ["analysis_step_version", "item"],
+    "status": "released",
+    "aliases": [],
+    "schema_version": "1",
+    "version": 1,
+    "analysis_step": require('../analysis_step/encode-2-step'),
+    "software_versions": [],
+    "uuid": "871a52b2-6874-4927-98c6-1d0b74bbc194"
+};
