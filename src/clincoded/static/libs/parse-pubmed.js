ec0c5339f76f1a007f149912481887bd5d5256c4
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-07-15 11:31:54 -0700

    Use new PubMed-parsing code
    Now using an adaptation of a GitHub-residing project to parse PubMed more reliably, taking into account variations in the PubMed data.

--- src/clincoded/static/libs/parse-pubmed.js
+++ src/clincoded/static/libs/parse-pubmed.js
@@ -3,199 +3,94 @@
 // https://github.com/standard-analytics/pubmed-schema-org/blob/master/lib/pubmed.js
 
 var _ = require('underscore');
+var moment = require('moment');
 
 module.exports.parsePubmed = parsePubmed;
 
 /**
  * see http://www.nlm.nih.gov/bsd/licensee/elements_descriptions.html
  */
-function parsePubmed(xml, pmid){
-    var pkg;
+function parsePubmed(xml){
+    var article = {};
     var doc = new DOMParser().parseFromString(xml, 'text/xml');
 
     var $PubmedArticle = doc.getElementsByTagName('PubmedArticle')[0];
     if($PubmedArticle){
+        var publicationData = '';
 
+        // Get the PMID
+        var $PMID = $PubmedArticle.getElementsByTagName('PMID')[0];
+        if($PMID) {
+            article.pmid = $PMID.textContent;
+        }
+
+        // Get the journal name
         var $Journal = $PubmedArticle.getElementsByTagName('Journal')[0];
-        var jsDate, periodical;
         if($Journal){
-            jsDate = pubmedDatePublished($Journal);
-            periodical = pubmedPeriodical($Journal);
-        }
-        var authors = pubmedAuthors($PubmedArticle);
-
-        var pkgId = [];
-
-        if(periodical && periodical.alternateName){
-            pkgId.push(periodical.alternateName.toLowerCase());
+            article.date = pubmedDatePublished($Journal).toISOString();
+            article.date = moment(article.date).format('YYYY-MMM');
+            article.journal = pubmedPeriodical($Journal);
         }
 
-        if(authors.author && authors.author.familyName){
-            pkgId.push(authors.author.familyName.toLowerCase());
-        }
-
-        if(jsDate){
-            pkgId.push(jsDate.getFullYear());
-        }
-
-        if(pkgId.length>=2){
-            pkgId = pkgId.join('-');
-        } else {
-            pkgId = pmid.toString();
-        }
-
-        pkg = {
-            '@context': '',
-            '@id': pkgId,
-            '@type': 'MedicalScholarlyArticle'
-        };
-        pkg.version = '0.0.0';
-
-        var sameAs = ['http://www.ncbi.nlm.nih.gov/pubmed/' + pmid];
-        var doi = pubmedDoi($PubmedArticle);
-        if (doi) { sameAs.push('http://doi.org/' + doi); }
-
-
-        var keywords = pubmedKeywords($PubmedArticle);
-        if(keywords){
-            pkg.keywords = keywords;
-        }
-
-        if(jsDate){ pkg.datePublished = jsDate.toISOString(); }
+        article.firstAuthor = pubmedAuthors($PubmedArticle);
 
         var $ArticleTitle = $PubmedArticle.getElementsByTagName('ArticleTitle')[0];
         if($ArticleTitle){
-            pkg.headline = $ArticleTitle.textContent; //remove [] Cf http://www.nlm.nih.gov/bsd/licensee/elements_descriptions.html#articletitle
-        }
-
-        if(authors.author){ pkg.author = authors.author; }
-        if(authors.contributor){ pkg.contributor = authors.contributor; }
-
-        var $CopyrightInformation = $PubmedArticle.getElementsByTagName('CopyrightInformation')[0];
-        if($CopyrightInformation){
-            pkg.copyrightHolder = { description: $CopyrightInformation.textContent };
-        }
-
-        pkg.provider = {
-            '@type': 'Organization',
-            '@id': 'http://www.ncbi.nlm.nih.gov/pubmed/',
-            description: 'From MEDLINE®/PubMed®, a database of the U.S. National Library of Medicine.'
-        };
-
-        var sourceOrganization = pubmedSourceOrganization($PubmedArticle);
-        if(sourceOrganization){
-            pkg.sourceOrganization = sourceOrganization;
+            article.title = $ArticleTitle.textContent; //remove [] Cf http://www.nlm.nih.gov/bsd/licensee/elements_descriptions.html#articletitle
         }
 
         var abstracts = pubmedAbstract($PubmedArticle);
         if(abstracts){
-            pkg.abstract = abstracts;
+            article.abstract = abstracts;
         }
 
         //issue, volume, periodical, all nested...
         if($Journal){
-            var isPartOf;
 
             var publicationIssue = pubmedPublicationIssue($Journal);
-            if(publicationIssue){
-                isPartOf = publicationIssue;
-            }
-
             var publicationVolume = pubmedPublicationVolume($Journal);
-            if(publicationVolume){
-                if(publicationIssue){
-                    publicationIssue.isPartOf = publicationVolume;
-                } else {
-                    isPartOf = publicationVolume;
-                }
-            }
-
-            if(periodical){
-                if(publicationVolume){
-                    publicationVolume.isPartOf = periodical;
-                } else if (publicationIssue){
-                    publicationIssue.isPartOf = periodical;
-                } else {
-                    isPartOf = periodical;
-                }
-            }
-
-            if(isPartOf){ pkg.isPartOf = isPartOf; }
+            var publicationPgn = '';
 
             //pages (bibo:pages (bibo:pages <-> schema:pagination) or bibo:pageStart and bibo:pageEnd e.g <Pagination> <MedlinePgn>12-9</MedlinePgn>)
             var $Pagination = $PubmedArticle.getElementsByTagName('Pagination')[0];
             if($Pagination){
                 var $MedlinePgn = $Pagination.getElementsByTagName('MedlinePgn')[0];
                 if($MedlinePgn){
-                    var medlinePgn = $MedlinePgn.textContent || '';
-                    var rePage = /^(\d+)-(\d+)$/;
-                    var matchPage = medlinePgn.match(rePage);
-                    if(matchPage){ //fix ranges like 1199-201 or 12-9
-                        var pageStart = matchPage[1];
-                        var pageEnd = matchPage[2];
-                        if(pageEnd.length < pageStart.length){
-                            pageEnd = pageStart.substring(0, pageStart.length - pageEnd.length) + pageEnd;
-                        }
-                        pkg.pageStart = parseInt(pageStart, 10);
-                        pkg.pageEnd = parseInt(pageEnd, 10);
-                    } else {
-                        pkg.pagination = medlinePgn;
-                    }
+                    publicationPgn = $MedlinePgn.textContent;
                 }
             }
+            publicationData = publicationVolume + (publicationIssue ? '(' + publicationIssue + ')' : '')  + ':' + publicationPgn;
+        }
+        if (publicationData) {
+            article.date += ';' + publicationData;
         }
     }
 
-    return pkg;
+    return article;
 }
 
 function pubmedAuthors($PubmedArticle){
     var authors = {};
+    var firstAuthor = '';
 
     var $AuthorList = $PubmedArticle.getElementsByTagName('AuthorList')[0];
     if($AuthorList){
         var $Authors = $AuthorList.getElementsByTagName('Author');
-        if($Authors){
-            Array.prototype.forEach.call($Authors, function($Author, i){
-                var person = { '@type': 'Person' };
+        var $FirstAuthor = $Authors[0];
 
-                var $LastName = $Author.getElementsByTagName('LastName')[0];
-                if($LastName){
-                    person.familyName = $LastName.textContent;
-                }
-
-                var $ForeName = $Author.getElementsByTagName('ForeName')[0];
-                if($ForeName){
-                    person.givenName = $ForeName.textContent;
-                }
-
-                if(person.familyName && person.givenName ){
-                    person.name = person.givenName + ' ' + person.familyName;
-                }
-
-                var $Affiliation = $Author.getElementsByTagName('Affiliation')[0];
-                if($Affiliation){
-                    person.affiliation = {
-                        '@type': 'Organization',
-                        description: $Affiliation.textContent
-                    };
-                }
-
-                if(Object.keys(person).length > 1){
-                    if(i === 0){
-                        authors.author = person;
-                    } else {
-                        if(!authors.contributor){
-                            authors.contributor = [];
-                        }
-                        authors.contributor.push(person);
-                    }
-                }
-
-            });
+        var $LastName = $FirstAuthor.getElementsByTagName('LastName')[0];
+        if($LastName){
+            firstAuthor = $LastName.textContent;
         }
+
+        var $Initials = $FirstAuthor.getElementsByTagName('Initials')[0];
+        if($Initials){
+            firstAuthor += (firstAuthor ? ' ' : '') + $Initials.textContent;
+        }
+    } else {
+        firstAuthor = 'Anonymous';
     }
-    return authors;
+    return firstAuthor;
 }
 
 function pubmedDoi($PubmedArticle){
@@ -268,48 +163,27 @@ function pubmedPublicationIssue($Journal){
 
     var $issue = $Journal.getElementsByTagName('Issue')[0];
     if($issue){
-        return {
-            '@type': 'PublicationIssue',
-            issueNumber: parseInt($issue.textContent, 10)
-        };
+        return $issue.textContent;
     }
-
+    return '';
 }
 
 function pubmedPublicationVolume($Journal){
 
     var $volume = $Journal.getElementsByTagName('Volume')[0];
     if($volume){
-        return {
-            '@type': 'PublicationVolume',
-            volumeNumber: parseInt($volume.textContent, 10)
-        };
+        return $volume.textContent;
     }
-
+    return '';
 }
 
 function pubmedPeriodical($Journal){
 
-    var periodical = { '@type': 'Periodical' };
-
     var $Title = $Journal.getElementsByTagName('Title')[0];
     if($Title){
-        periodical.name = $Title.textContent;
+        return $Title.textContent;
     }
-
-    var $ISOAbbreviation = $Journal.getElementsByTagName('ISOAbbreviation')[0];
-    if($ISOAbbreviation){
-        periodical.alternateName = $ISOAbbreviation.textContent;
-    }
-
-    var $ISSN = $Journal.getElementsByTagName('ISSN')[0];
-    if($ISSN){
-        periodical.issn = $ISSN.textContent;
-    }
-
-    if(Object.keys(periodical).length > 1){
-        return periodical;
-    }
+    return '';
 
 }
 
@@ -321,43 +195,20 @@ function pubmedPeriodical($Journal){
  */
 
 function pubmedAbstract($PubmedArticle){
+    var abstractText = '';
 
     var $Abstracts = $PubmedArticle.getElementsByTagName('Abstract');
     if($Abstracts && $Abstracts.length){
 
-        return Array.prototype.map.call($Abstracts, function($Abstract){
+        var $AbstractTexts = $Abstracts[0].getElementsByTagName('AbstractText');
+        if($AbstractTexts && $AbstractTexts.length){
 
-            var myAbstract = { '@type': 'Abstract' };
+            abstractText = $AbstractTexts[0].textContent;
 
-            var $AbstractTexts = $Abstract.getElementsByTagName('AbstractText');
-            if($AbstractTexts && $AbstractTexts.length){
-
-                var parts = Array.prototype.map.call($AbstractTexts, function($AbstractText){
-                    var part = { '@type': 'Abstract' };
-                    var nlmCategory = $AbstractText.getAttribute('NlmCategory') || $AbstractText.getAttribute('Label');
-                    if(nlmCategory){
-                        part.headline = nlmCategory.trim().toLowerCase();
-                    }
-                    part.abstractBody = $AbstractText.textContent;
-                    return part;
-                });
-
-                if(parts.length === 1){
-                    if(parts[0].headline){
-                        myAbstract.headline = parts[0].headline;
-                    }
-                    myAbstract.abstractBody = parts[0].abstractBody;
-                } else {
-                    myAbstract.hasPart = parts;
-                }
-
-            }
-
-            return myAbstract;
-
-        });
+        }
 
     }
+    return abstractText;
 
 }
 
