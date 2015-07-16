350613753ed7d251deb8b527c97c9a98a422ef62
Laurence Rowe <laurence@lrowe.co.uk>
2015-07-16 15:18:10 -0700

    Remove pipeline name, version, end_points #3093

--- src/encoded/upgrade/pipeline.py
+++ src/encoded/upgrade/pipeline.py
@@ -7,3 +7,11 @@ def pipeline_0_2(value, system):
 
     value['lab'] = "a558111b-4c50-4b2e-9de8-73fd8fd3a67d"  # /labs/encode-processing-pipeline/
     value['award'] = "b5736134-3326-448b-a91a-894aafb77876"  # /awards/ENCODE/
+
+
+@upgrade_step('pipeline', '2', '3')
+def pipeline_2_3(value, system):
+    # http://redmine.encodedcc.org/issues/3093
+    value.pop('name', None)
+    value.pop('version', None)
+    value.pop('end_points', None)
