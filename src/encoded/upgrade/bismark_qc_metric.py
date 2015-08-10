e4d0eef62d67dcbb0d8f6d9ff52ff78e714ce2a2
Tim Dreszer <tdreszer@stanford.org>
2015-08-10 15:37:01 -0700

    With Laurences's help, the bismark upgrade should now work.

--- src/encoded/upgrade/bismark_qc_metric.py
+++ src/encoded/upgrade/bismark_qc_metric.py
@@ -3,11 +3,9 @@ from contentbase import (
     upgrade_step,
 )
 
-
 @upgrade_step('bismark_qc_metric', '1', '2')
 def bismark_qc_metric_1_2(value, system):
     # http://redmine.encodedcc.org/issues/3114
     root = system['registry'][ROOT]
-    if 'applies_to' in value:
-        value['relates_to'] = value.get('applies_to')
-        del value['applies_to']
+    step_run = root.get_by_uuid(value['step_run'])
+    value['relates_to'] = step_run.get_rev_links('output_files')
