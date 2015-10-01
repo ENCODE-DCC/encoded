94d84faf63a4d963d0c79a6801d60dcd18588eef
nikhilrp <nikhilrp@stanford.edu>
2015-10-01 15:31:39 -0700

    typo

--- src/encoded/commands/es_index_listener.py
+++ src/encoded/commands/es_index_listener.py
@@ -114,7 +114,7 @@ def run(testapp, timeout=DEFAULT_TIMEOUT, dry_run=False, control=None, update_st
                     if result.get('indexed', 0):
                         update_status(result=result)
                         log.info(result)
-                        updated = result.get('updated', []):
+                        updated = result.get('updated', [])
                             for uuid in updated:
                                 testapp.post_json('/%s/@@index_file' % uuid)
                 update_status(
