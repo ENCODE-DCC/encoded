788e6272cfe8b1da71e1c21aec29ad2bdefb39ea
estherc <etchan@stanford.edu>
2015-10-19 14:08:45 -0700

    Fixed step_run audit logic again x 2

--- src/encoded/audit/file.py
+++ src/encoded/audit/file.py
@@ -272,11 +272,13 @@ def audit_modERN_ChIP_pipeline_steps(value, system):
             detail = 'bigBed {} is linked to the wrong step_run: {}'.format(value['@id'], step['aliases'][0])
             raise AuditFailure('wrong step_run for bigBed peaks', detail, level='WARNING')
 
-    if (value['output_type'] == 'peaks') and (step['aliases'][0] != 'modern:chip-seq-spp-peak-calling-step-run-v-1-virtual'):
+    if (value['output_type'] == 'peaks') and (value['file_format'] == 'bed'):
+        if (value['file_format_type'] == 'narrowPeak') and (step['aliases'][0] != 'modern:chip-seq-spp-peak-calling-step-run-v-1-virtual'):
             detail = 'Peaks {} is linked to the wrong step_run: {}'.format(value['@id'], step['aliases'][0])
             raise AuditFailure('wrong step_run for peaks', detail, level='WARNING')
 
-    if (value['output_type'] == 'optimal idr thresholded peaks') and (step['aliases'][0] != 'modern:chip-seq-optimal-idr-step-run-v-1-virtual'):
+    if (value['output_type'] == 'optimal idr thresholded peaks') and (value['file_format'] == 'bed'):
+        if (value['file_format_type'] == 'narrowPeak') and (step['aliases'][0] != 'modern:chip-seq-optimal-idr-step-run-v-1-virtual'):
             detail = 'Optimal IDR thresholded peaks {} is linked to the wrong step_run: {}'.format(value['@id'], step['aliases'][0])
             raise AuditFailure('wrong step_run for IDR peaks', detail, level='WARNING')
 
