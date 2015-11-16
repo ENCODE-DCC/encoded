a129fac8ce72f4e1fc20cfa6e2a7831a527a0ed6
Laurence Rowe <laurence@lrowe.co.uk>
2015-11-16 13:17:16 -0800

    Bump timeout to 30s as we're still seeing errors for all files search.

--- src/contentbase/elasticsearch/__init__.py
+++ src/contentbase/elasticsearch/__init__.py
@@ -29,7 +29,7 @@ def includeme(config):
         serializer=PyramidJSONSerializer(json_renderer),
         connection_class=TimedUrllib3HttpConnection,
         retry_on_timeout=True,
-        timeout=20,
+        timeout=30,
     )
 
     config.include('.cached_views')
