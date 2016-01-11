dba28a754cb0784bd80c9394fdc7fb79745d5bea
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2016-01-11 10:58:31 -0800

    changing audit.DCC_ACTION.detail field mapping to analyzed

--- src/contentbase/elasticsearch/create_mapping.py
+++ src/contentbase/elasticsearch/create_mapping.py
@@ -209,7 +209,7 @@ def audit_mapping():
         },
         'detail': {
             'type': 'string',
-            'index': 'analyzed',
+            'index': 'analyzed', 
         },
         'level_name': {
             'type': 'string',
