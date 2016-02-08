226b2cf4fb85263abc403115582eb910d03525c0
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-02-08 10:20:13 -0800

    Display contributing file details

--- src/encoded/static/components/experiment.js
+++ src/encoded/static/components/experiment.js
@@ -1242,6 +1242,7 @@ var assembleGraph = module.exports.assembleGraph = function(context, session, in
                 shape: 'rect',
                 cornerRadius: 16,
                 parentNode: replicateNode,
+                contributing: fileContributed,
                 ref: fileRef
             }, metricsInfo);
 
