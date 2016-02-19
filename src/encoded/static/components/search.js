ab8714543a7d8426d5bff607d822d40c2d3ccc28
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2016-02-19 15:29:40 -0800

    debug modal

--- src/encoded/static/components/search.js
+++ src/encoded/static/components/search.js
@@ -820,7 +820,7 @@ var AuditMixin = audit.AuditMixin;
           },
 
           handleToggle: function () {
-            console.log(this.state.isModalOpen);
+            console.log(this.state.isModalOpen)
             this.setState({
               isModalOpen: !this.state.isModalOpen
             });
