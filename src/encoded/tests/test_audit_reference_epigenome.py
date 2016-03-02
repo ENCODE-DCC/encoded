8f0cff9d02d0eb44fe58f5f3c791d22aed9573ad
Idan Gabdank <gabdank@stanford.edu>
2016-03-02 10:30:37 -0800

    added tests for multiple donors and biosample term names to reference epigenomes

--- src/encoded/tests/test_audit_reference_epigenome.py
+++ src/encoded/tests/test_audit_reference_epigenome.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture
 def reference_epigenome_1(testapp, lab, award):
     item = {
@@ -215,6 +216,28 @@ def test_reference_epigenome_without_required_assays(testapp, reference_epigenom
     assert any(error['category'] == 'missing IHEC required assay' for error in errors_list)
 
 
+@pytest.fixture
+def replicate_RNA_seq(testapp, reference_experiment_RNA_seq, library_1):
+    item = {
+        'experiment': reference_experiment_RNA_seq['@id'],
+        'library': library_1['@id'],
+        'biological_replicate_number': 1,
+        'technical_replicate_number': 1,
+    }
+    return testapp.post_json('/replicate', item).json['@graph'][0]
+
+
+@pytest.fixture
+def replicate_RRBS(testapp, reference_experiment_RRBS, library_2):
+    item = {
+        'experiment': reference_experiment_RRBS['@id'],
+        'library': library_2['@id'],
+        'biological_replicate_number': 1,
+        'technical_replicate_number': 1,
+    }
+    return testapp.post_json('/replicate', item).json['@graph'][0]
+
+
 def test_reference_epigenome_with_required_assays(testapp, reference_epigenome_1,
                                                   reference_experiment_RNA_seq,
                                                   reference_experiment_RRBS,
@@ -232,8 +255,10 @@ def test_reference_epigenome_with_required_assays(testapp, reference_epigenome_1
                                                        reference_experiment_RRBS['@id'],
                                                        reference_experiment_WGBS['@id'],
                                                        reference_experiment_chip_seq_control['@id'],
-                                                       reference_experiment_chip_seq_H3K27me3['@id'],
-                                                       reference_experiment_chip_seq_H3K36me3['@id'],
+                                                       reference_experiment_chip_seq_H3K27me3[
+                                                       '@id'],
+                                                       reference_experiment_chip_seq_H3K36me3[
+                                                       '@id'],
                                                        reference_experiment_chip_seq_H3K4me1['@id'],
                                                        reference_experiment_chip_seq_H3K4me3['@id'],
                                                        reference_experiment_chip_seq_H3K27ac['@id'],
@@ -245,3 +270,59 @@ def test_reference_epigenome_with_required_assays(testapp, reference_epigenome_1
     for error_type in errors:
         errors_list.extend(errors[error_type])
     assert all(error['category'] != 'missing IHEC required assay' for error in errors_list)
+
+
+def test_reference_epigenome_multiple_donors(testapp, reference_epigenome_1,
+                                             reference_experiment_RNA_seq,
+                                             reference_experiment_RRBS,
+                                             replicate_RNA_seq,
+                                             replicate_RRBS,
+                                             library_1,
+                                             library_2,
+                                             biosample_1,
+                                             biosample_2,
+                                             donor_1,
+                                             donor_2):
+    testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
+    testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id']})
+    testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
+    testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
+    testapp.patch_json(reference_epigenome_1['@id'], {'related_datasets':
+                                                      [reference_experiment_RNA_seq['@id'],
+                                                       reference_experiment_RRBS['@id']]})
+    res = testapp.get(reference_epigenome_1['@id'] + '@@index-data')
+    errors = res.json['audit']
+    errors_list = []
+    for error_type in errors:
+        errors_list.extend(errors[error_type])
+    assert any(error['category'] == 'multiple donors in reference epigenome' for
+               error in errors_list)
+
+
+def test_reference_epigenome_multiple_biosample_term_names(testapp, reference_epigenome_1,
+                                                           reference_experiment_RNA_seq,
+                                                           reference_experiment_RRBS,
+                                                           replicate_RNA_seq,
+                                                           replicate_RRBS,
+                                                           library_1,
+                                                           library_2,
+                                                           biosample_1,
+                                                           biosample_2,
+                                                           donor_1,
+                                                           donor_2):
+    testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id'],
+                                            'biosample_term_name': 'liver'})
+    testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id'],
+                                            'biosample_term_name': 'aorta'})
+    testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
+    testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
+    testapp.patch_json(reference_epigenome_1['@id'], {'related_datasets':
+                                                      [reference_experiment_RNA_seq['@id'],
+                                                       reference_experiment_RRBS['@id']]})
+    res = testapp.get(reference_epigenome_1['@id'] + '@@index-data')
+    errors = res.json['audit']
+    errors_list = []
+    for error_type in errors:
+        errors_list.extend(errors[error_type])
+    assert any(error['category'] == 'multiple biosample term names in reference epigenome' for
+               error in errors_list)
