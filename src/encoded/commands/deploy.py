8143cbeaa24496088d275d500e069548c11f8e6f
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2016-03-24 14:31:31 -0700

    amazon AMI HVM complains about device names

--- src/encoded/commands/deploy.py
+++ src/encoded/commands/deploy.py
@@ -73,6 +73,8 @@ def run(wale_s3_prefix, image_id, instance_type, elasticsearch,
         security_groups = ['elasticsearch-https']
         del bdm[0]['Ebs']
         bdm[0]['VirtualName'] = 'ephemeral0'
+        bdm[0]['DeviceName'] = '/dev/sda'
+        print(bdm)
         if profile_name == 'production':
             instance_type = 'r3.xlarge'
         else:
