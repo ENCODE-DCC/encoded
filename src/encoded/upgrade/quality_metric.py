65da93d25f3a21832eac63ac7d5daf3a5ae2a52d
Idan Gabdank <gabdank@stanford.edu>
2016-04-07 15:30:48 -0700

    started the upgrade of attribution

---
+++ src/encoded/upgrade/quality_metric.py
@@ -0,0 +1,78 @@
+from snovault import (
+    CONNECTION,
+    upgrade_step,
+)
+
+
+@upgrade_step('bismark_quality_metric', '1', '2')
+def bismark_quality_metric_1_2(value, system):
+    # http://redmine.encodedcc.org/issues/3114
+    conn = system['registry'][CONNECTION]
+    step_run = conn.get_by_uuid(value['step_run'])
+    output_files = conn.get_rev_links(step_run.model, 'step_run', 'File')
+    value['quality_metric_of'] = [str(uuid) for uuid in output_files]
+
+
+@upgrade_step('bismark_quality_metric', '2', '3')
+def bismark_quality_metric_2_3(value, system):
+    # http://redmine.encodedcc.org/issues/3063
+    if 'aliases' in value:
+        value['aliases'] = list(set(value['aliases']))
+
+    if 'quality_metric_of' in value:
+        value['quality_metric_of'] = list(set(value['quality_metric_of']))
+
+@upgrade_step('bismark_quality_metric', '3', '4')
+def bismark_quality_metric_3_4(value, system):
+    # http://redmine.encodedcc.org/issues/3897
+    # get from the file the lab and award for the attribution!!!
+    context = system['context']
+    root = find_root(context)
+    publications = root['quality_metric_of']
+
+
+    value['award'] = new_award
+    value['lab'] = new_lab
+
+    context = system['context']
+    root = find_root(context)
+    publications = root['publications']
+    if 'references' in value:
+        new_references = []
+        for ref in value['references']:
+            if re.match('doi', ref):
+                new_references.append(REFERENCES_UUID[ref])
+            else:
+                item = publications[ref]
+                new_references.append(str(item.uuid))
+        value['references'] = new_references
+
+
+
+
+    '''
+    bigwigcorrelate_quality_metric.json V 3
+
+    chipseq_filter_quality_metric.json V 3
+    cpg_correlation_quality_metric.json V 2
+    dnase_peak_quality_metric.json V 3
+    edwbamstats_quality_metric.json V 3
+    edwcomparepeaks_quality_metric.json V 3
+    encode2_chipseq_quality_metric.json V 3
+    fastqc_quality_metric.json V 3
+    generic_quality_metric.json V 3
+
+    hotspot_quality_metric.json V 3
+    idr_quality_metric.json V 2
+    idr_summary_quality_metric.json V 3
+    
+    mad_quality_metric.json V 2 
+    pbc_quality_metric.json V 3
+    phantompeaktools_spp_quality_metric.json V 3
+
+    quality_metric.json ****
+
+    samtools_flagstats_quality_metric.json V 3
+    samtools_stats_quality_metric.json V 3
+    star_quality_metric.json V 3
+    '''
