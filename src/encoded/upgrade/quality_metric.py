480ffd19dd235048e04b142f809cd52c2c47e65e
Idan Gabdank <gabdank@stanford.edu>
2016-04-07 17:26:26 -0700

    checking validation

---
+++ src/encoded/upgrade/quality_metric.py
@@ -0,0 +1,37 @@
+from snovault import (
+    CONNECTION,
+    upgrade_step,
+)
+
+
+@upgrade_step('bismark_quality_metric', '1', '2')
+def bismark_quality_metric_1_2(value, system):
+    # http://redmine.encodedcc.org/issues/3114
+    conn = system['registry'][CONNECTION]
+    step_run = conn.get_by_uuid(value['step_run'])
+    output_files = conn.get_rev_links(step_run.model, 'step_run', 'File')
+    value['quality_metric_of'] = [str(uuid) for uuid in output_files]
+
+
+@upgrade_step('bismark_quality_metric', '2', '3')
+def bismark_quality_metric_2_3(value, system):
+    # http://redmine.encodedcc.org/issues/3063
+    if 'aliases' in value:
+        value['aliases'] = list(set(value['aliases']))
+
+    if 'quality_metric_of' in value:
+        value['quality_metric_of'] = list(set(value['quality_metric_of']))
+
+
+#@upgrade_step('fastqc_quality_metric', '2', '3')
+#@upgrade_step('pbc_quality_metric', '2', '3')
+'''@upgrade_step('generic_quality_metric', '2', '3')
+def quality_metric_2_3(value, system):
+    # http://redmine.encodedcc.org/issues/3936
+    if 'quality_metric_of' not in value or \
+       len(value['quality_metric_of']) == 0:
+        conn = system['registry'][CONNECTION]
+        step_run = conn.get_by_uuid(value['step_run'])
+        output_files = conn.get_rev_links(step_run.model, 'step_run', 'File')
+        value['quality_metric_of'] = list(set([str(uuid) for uuid in output_files]))
+'''
