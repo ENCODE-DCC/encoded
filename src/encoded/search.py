e98bd903902b90ae657b9f589b9c6b0b7d90eaa6
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-04-15 08:27:08 -0700

    DCC_ACTION audit facet only if logged in
    Now applies to matrix too. Already applied to search and report.

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -636,9 +636,7 @@ def search(context, request, search_type=None, return_generator=False):
     if len(doc_types) == 1 and 'facets' in types[doc_types[0]].schema:
         facets.extend(types[doc_types[0]].schema['facets'].items())
 
-    import pprint;
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(request.has_permission('search_audit'))
+    # Display all audits if logged in, or all but DCC_ACTION if logged out
     for audit_facet in audit_facets:
         if search_audit and 'group.submitter' in principals or 'DCC_ACTION' not in audit_facet[0]:
             facets.append(audit_facet)
@@ -771,6 +769,7 @@ def matrix(context, request):
         'filters': [],
         'notification': '',
     }
+    search_audit = request.has_permission('search_audit')
 
     doc_types = request.params.getall('type')
     if len(doc_types) != 1:
@@ -835,9 +834,12 @@ def matrix(context, request):
     # Adding facets to the query
     facets = [(field, facet) for field, facet in schema['facets'].items() if
               field in matrix['x']['facets'] or field in matrix['y']['facets']]
-    if request.has_permission('search_audit'):
-        for audit_facet in audit_facets:
+
+    # Display all audits if logged in, or all but DCC_ACTION if logged out
+    for audit_facet in audit_facets:
+        if search_audit and 'group.submitter' in principals or 'DCC_ACTION' not in audit_facet[0]:
             facets.append(audit_facet)
+
     query['aggs'] = set_facets(facets, used_filters, principals, doc_types)
 
     # Group results in 2 dimensions
