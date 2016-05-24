0db1a7734b33367363e9040d7eb857f5f2350061
Ben Hitz <hitz@stanford.edu>
2016-05-24 11:06:51 -0400

    continue purge for sample database

--- src/snowflakes/tests/test_search.py
+++ src/snowflakes/tests/test_search.py
@@ -29,27 +29,3 @@ def test_report_view(workbook, testapp):
     assert 'columns' in res
     assert '@graph' in res
 
-
-def test_matrix_view(workbook, testapp):
-    res = testapp.get('/matrix/?type=Experiment').json
-    assert res['@type'] == ['Matrix']
-    assert res['@id'] == '/matrix/?type=Experiment'
-    assert res['@context'] == '/terms/'
-    assert res['notification'] == 'Success'
-    assert res['title'] == 'Experiment Matrix'
-    assert res['total'] > 0
-    assert 'facets' in res
-    assert 'filters' in res
-    assert 'matrix' in res
-    assert res['matrix']['max_cell_doc_count'] > 0
-    assert res['matrix']['search_base'] == '/search/?type=Experiment'
-    assert res['matrix']['x']['group_by'] == 'assay_title'
-    assert res['matrix']['x']['label'] == 'Assay'
-    assert res['matrix']['x']['limit'] == 20
-    assert len(res['matrix']['x']['buckets']) > 0
-    assert len(res['matrix']['x']['facets']) > 0
-    assert res['matrix']['y']['group_by'] == ['replicates.library.biosample.biosample_type', 'biosample_term_name']
-    assert res['matrix']['y']['label'] == 'Biosample'
-    assert res['matrix']['y']['limit'] == 5
-    assert len(res['matrix']['y']['replicates.library.biosample.biosample_type']['buckets']) > 0
-    assert len(res['matrix']['y']['replicates.library.biosample.biosample_type']['buckets'][0]['biosample_term_name']['buckets']) > 0
