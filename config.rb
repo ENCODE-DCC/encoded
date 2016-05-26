28187d1b5bf097955e57ee757a049e9fcd05f87c
Ben Hitz <hitz@stanford.edu>
2016-05-26 08:29:54 -0700

    config changes

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "src/encoded/static/css"
-sass_dir = "src/encoded/static/scss"
-images_dir = "src/encoded/static/img"
-javascripts_dir = "src/encoded/static/modules"
-fonts_dir = "src/encoded/static/fonts"
+css_dir = "src/snowflakes/static/css"
+sass_dir = "src/snowflakes/static/scss"
+images_dir = "src/snowflakes/static/img"
+javascripts_dir = "src/snowflakes/static/modules"
+fonts_dir = "src/snowflakes/static/fonts"
 
 # To export minified css, uncomment :compress, and comments out :nested
 output_style = :compressed
@@ -24,5 +24,5 @@ color_output = false
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass src/encoded/static/sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass src/snowflakes/static/sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
