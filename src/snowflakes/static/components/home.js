378c18e5793c81a7033f6125256be1dfa4636298
Ben Hitz <hitz@stanford.edu>
2016-06-30 14:02:45 -0700

    fix non bdd tests

--- src/snowflakes/static/components/home.js
+++ src/snowflakes/static/components/home.js
@@ -10,11 +10,11 @@ var Home = module.exports.Home = React.createClass({
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="project-info site-title">
-                                <h1>ENCODE: The Encyclopedia of DNA Elements</h1>
+                                <h1>Snowflakes: Where every object is unique</h1>
                             </div>
                             <div id="info-box" className="project-info text-panel">
-                                <h4>Preview the new ENCODE Portal</h4>
-                                <p>Enter a search term like "skin", "ChIP-seq", or "CTCF" or select a data type in the toolbar above.</p>
+                                <h4>Snowflake Portal</h4>
+                                <p>Enter a search term in the toolbar above.</p>
                             </div>
                         </div>
                     </div>
