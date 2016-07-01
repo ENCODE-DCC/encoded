e44512be4a3a709ef79107c0712c757bb0ea2f69
kjain21 <kritijn21@gmail.com>
2016-07-01 14:23:19 -0700

    temp

--- src/encoded/static/components/home.js
+++ src/encoded/static/components/home.js
@@ -154,15 +154,15 @@ var HomepageChart = React.createClass({
             var ctx = canvas.getContext("2d")
 
 
-            var width = 400,
-                height = 400;
+            // var width = 400,
+            //     height = 400;
 
-            var text = "Total: " + totalDocCount,
-                textX = Math.round((width - ctx.measureText(text).width) / 2),
-                textY = height / 2;
+            // var text = "Total: " + totalDocCount,
+            //     textX = Math.round((width - ctx.measureText(text).width) / 2),
+            //     textY = height / 2;
 
-            ctx.fillText(text, textX, textY);
-            ctx.save();
+            // ctx.fillText(text, textX, textY);
+            // ctx.save();
 
 
 
