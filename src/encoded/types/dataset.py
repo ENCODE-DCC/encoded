6639dc2798cbdcef360608d9522a8a250e77c6d0
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-07-22 11:48:28 -0700

    Allow archived files while logged out
    Involves changing dataset.py to include archived along with released files. Should be reviewed.

--- src/encoded/types/dataset.py
+++ src/encoded/types/dataset.py
@@ -129,10 +129,10 @@ class Dataset(Item):
         },
     })
     def files(self, request, original_files, status):
-        if status in ('release ready', 'released'):
+        if status in ('release ready', 'released', 'archived'):
             return paths_filtered_by_status(
                 request, original_files,
-                include=('released',),
+                include=('released', 'archived'),
             )
         else:
             return paths_filtered_by_status(
