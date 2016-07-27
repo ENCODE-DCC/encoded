aefe3f8a3ba472fca7157f44ee2c1b1d5c452707
Idan Gabdank <gabdank@stanford.edu>
2016-07-27 12:41:34 -0700

    still failing

--- src/encoded/audit/makeunicode.py
+++ src/encoded/audit/makeunicode.py
@@ -1,10 +1,9 @@
 import sys
 
 if sys.version_info < (3,):
-    import codecs
 
     def u(x):
-        return codecs.decode(x, encoding='utf-8')
+        return x.decode('utf-8')
 else:
 
     def u(x):
