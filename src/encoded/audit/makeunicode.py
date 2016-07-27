68f96247d7ccf7dc0a19af4a2d79d9b5fd0ded76
Idan Gabdank <gabdank@stanford.edu>
2016-07-27 14:26:45 -0700

    fixing a typo

--- src/encoded/audit/makeunicode.py
+++ src/encoded/audit/makeunicode.py
@@ -4,7 +4,7 @@ if sys.version_info < (3,):
 
     def u(x):
         if isinstance(x, basestring):
-            if not isinstance(x, 'utf-8'):
+            if not isinstance(x, unicode):
                 x = unicode(x, 'utf-8')
         return x
 else:
