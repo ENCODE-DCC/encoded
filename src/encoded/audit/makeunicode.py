84518e48fe024685e6959f258ce26d2e38dc441f
Idan Gabdank <gabdank@stanford.edu>
2016-07-27 13:51:16 -0700

    another attempt with 2.7

--- src/encoded/audit/makeunicode.py
+++ src/encoded/audit/makeunicode.py
@@ -3,7 +3,10 @@ import sys
 if sys.version_info < (3,):
 
     def u(x):
-        return x.decode('utf-8', 'replace')
+        if isinstance(x, basestring):
+            if not isinstance(x, 'utf-8'):
+                x = unicode(x, 'utf-8')
+        return x
 else:
 
     def u(x):
