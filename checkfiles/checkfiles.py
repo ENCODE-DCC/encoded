724d12c4c600499d70c69868242fa96c16e0f133
Idan Gabdank <gabdank@users.noreply.github.com>
2016-07-29 14:47:39 -0700

    chekfiles fix (#1462)
    * fixing checkfiles
    
    (1) excluding ‘replaced’ files when searching for content_md5sum
    conflicts
    (2) not reporting self-clashes of content_md5sum
    
    * making ADN instead of multiple if statements
    
    * avoiding multiprocessing error
    
    * fixing assumption that there were no errors
    
    * uuid is not working, changing into @id
    
    * trying with accessions
    
    * fixing the accession using code and removing print statements
    
    * not reporting error for self clash of content_md5sum
    
    * refactoring check_contentmd5sum_conflict function

--- checkfiles/checkfiles.py
+++ checkfiles/checkfiles.py
@@ -183,6 +183,35 @@ def check_format(encValData, job, path):
         result['validateFiles'] = output.decode(errors='replace').rstrip('\n')
 
 
+def check_for_contentmd5sum_conflicts(item, result, output, errors, session, url):
+    result['content_md5sum'] = output[:32].decode(errors='replace')
+    try:
+        int(result['content_md5sum'], 16)
+    except ValueError:
+        errors['content_md5sum'] = output.decode(errors='replace').rstrip('\n')
+    else:
+        query = '/search/?type=File&status!=replaced&content_md5sum=' + result[
+            'content_md5sum']
+        r = session.get(urljoin(url, query))
+        r_graph = r.json().get('@graph')
+        if len(r_graph) > 0:
+            conflicts = []
+            for entry in r_graph:
+                if 'accession' in entry and 'accession' in item:
+                    if entry['accession'] != item['accession']:
+                        conflicts.append(
+                            'checked %s is conflicting with content_md5sum of %s' % (
+                                result['content_md5sum'],
+                                entry['accession']))
+                else:
+                    conflicts.append(
+                        'checked %s is conflicting with content_md5sum of %s' % (
+                            result['content_md5sum'],
+                            entry['accession']))
+            if len(conflicts) > 0:
+                errors['content_md5sum'] = str(conflicts)
+
+
 def check_file(config, session, url, job):
     item = job['item']
     errors = job['errors']
@@ -253,23 +282,7 @@ def check_file(config, session, url, job):
             except subprocess.CalledProcessError as e:
                 errors['content_md5sum'] = e.output.decode(errors='replace').rstrip('\n')
             else:
-                result['content_md5sum'] = output[:32].decode(errors='replace')
-                try:
-                    int(result['content_md5sum'], 16)
-                except ValueError:
-                    errors['content_md5sum'] = output.decode(errors='replace').rstrip('\n')
-                else:
-                    query = '/search/?type=File&status!=replaced&content_md5sum=' + result['content_md5sum']
-                    r = session.get(urljoin(url, query))
-                    r_graph = r.json().get('@graph')
-                    if len(r_graph) > 0:
-                        conflicts = []
-                        for entry in r_graph:
-                            conflicts.append(
-                                'checked %s is conflicting with content_md5sum of %s' % (
-                                    result['content_md5sum'],
-                                    entry['accession']))
-                        errors['content_md5sum'] = str(conflicts)
+                check_for_contentmd5sum_conflicts(item, result, output, errors, session, url)
 
                 if os.path.exists(unzipped_original_bed_path):
                     try:
@@ -288,37 +301,25 @@ def check_file(config, session, url, job):
             except subprocess.CalledProcessError as e:
                 errors['content_md5sum'] = e.output.decode(errors='replace').rstrip('\n')
             else:
-
-                result['content_md5sum'] = output[:32].decode(errors='replace')
-                try:
-                    int(result['content_md5sum'], 16)
-                except ValueError:
-                    errors['content_md5sum'] = output.decode(errors='replace').rstrip('\n')
-                else:
-                    query = '/search/?type=File&content_md5sum=' + result['content_md5sum']
-                    r = session.get(urljoin(url, query))
-                    r_graph = r.json().get('@graph')
-                    if len(r_graph) > 0:
-                        conflicts = []
-                        for entry in r_graph:
-                            conflicts.append(
-                                'checked %s is conflicting with content_md5sum of %s' % (
-                                    result['content_md5sum'],
-                                    entry['accession']))
-                        errors['content_md5sum'] = str(conflicts)
+                check_for_contentmd5sum_conflicts(item, result, output, errors, session, url)
     if not errors:
         if item['file_format'] == 'bed':
             check_format(config['encValData'], job, unzipped_modified_bed_path)
         else:
             check_format(config['encValData'], job, local_path)
-
-    if item['file_format'] == 'bed':
-        if os.path.exists(unzipped_modified_bed_path):
+    else:
+        if item['file_format'] == 'bed':
             try:
-                os.remove(unzipped_modified_bed_path)
-            except OSError as e:
-                errors['file_remove_error'] = 'OS could not remove the file ' + \
-                                              unzipped_modified_bed_path
+                unzipped_modified_bed_path = unzipped_modified_bed_path
+                if os.path.exists(unzipped_modified_bed_path):
+                    try:
+                        os.remove(unzipped_modified_bed_path)
+                    except OSError as e:
+                        errors['file_remove_error'] = 'OS could not remove the file ' + \
+                                                      unzipped_modified_bed_path
+            except NameError:
+                pass
+
     if item['status'] != 'uploading':
         errors['status_check'] = \
             "status '{}' is not 'uploading'".format(item['status'])
