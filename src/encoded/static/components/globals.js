58fd1aa0f5a937775b8f36e20992d9d25051684e
Forrest Tanaka <fytanaka@stanford.edu>
2016-07-29 13:06:37 -0700

    3829 Paired raw file tables (#1463)
    * In progress
    
    * First implementation
    
    * Updates from Ben’s suggestions
    
    http://redmine.encodedcc.org/issues/3829#note-34
    
    * Post-merge change
    
    Bottom borders no longer being drawn after merging in master. Redmine #3829
    
    * Add back file download link to raw file table
    
    Also remove the old raw table configuration object. Redmine #3829
    
    * Fix bioreplicate sorting for non-paired files
    
    Need to sort numerically, not alphabetically. That means need to also sort in multiple replicates per file.
    
    * Factor out sorting function
    
    * Fix paired biorep sorting bug
    
    Also added line between paired and unpaired portions of raw file table, and aline between paired bioreplicate numbers and library. Redmine #3829.
    
    * Meke stricter condition for unpaired top border

--- src/encoded/static/components/globals.js
+++ src/encoded/static/components/globals.js
@@ -130,6 +130,13 @@ String.prototype.uppercaseFirstChar = function(string) {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
+// Convert the number `n` to a string, zero-filled to `digits` digits. Maximum of four zeroes.
+// http://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript#answer-2998822
+module.exports.zeroFill = function(n) {
+    var filled = '0000' + n;
+    return filled.substr(filled.length - 4);
+};
+
 // Order that antibody statuses should be displayed
 module.exports.statusOrder = [
     'eligible for new data',
