6cc0a65de36800baa94093d4ba1ebf32d484cbaa
estherc <etchan@stanford.edu>
2016-08-15 21:21:16 -0700

    Change file upgrade 8to9 test

--- src/encoded/tests/test_upgrade_file.py
+++ src/encoded/tests/test_upgrade_file.py
@@ -78,14 +78,27 @@ def file_7(file_base):
 
 
 @pytest.fixture
-def file_8(file_base):
+def file_8a(file_base):
     item = file_base.copy()
     item.update({
-        'file_format': 'fastq',
+        'file_format': 'bam',
         'assembly': 'hg19',
         'schema_version': '8'
     })
     return item
+
+
+@pytest.fixture
+def file_8b(file_base, file_8a):
+    item = file_base.copy()
+    item.update({
+        'file_format': 'bed',
+        'file_format_type': 'narrowPeak',
+        'schema_version': '8',
+        'derived_from': [file_8a]
+    })
+    return item
+
 
 
 def test_file_upgrade(upgrader, file_1):
@@ -142,7 +155,7 @@ def test_file_upgrade7(upgrader, file_7):
     assert value['schema_version'] == '8'
 
 
-def test_file_upgrade8(upgrader, file_8):
-    value = upgrader.upgrade('file', file_8, current_version='8', target_version='9')
+def test_file_upgrade8(upgrader, file_8a, file_8b):
+    value = upgrader.upgrade('file', file_8b, current_version='8', target_version='9')
     assert value['schema_version'] == '9'
-    assert 'assembly' not in value
+    assert 'assembly' in value
