183665ddf917c14dd73b4bdc81e7b1a69dcedc8b
Idan Gabdank <gabdank@stanford.edu>
2016-08-22 16:47:47 -0700

    removing old property of modifiction_description

--- src/encoded/upgrade/genetic_modification.py
+++ src/encoded/upgrade/genetic_modification.py
@@ -6,3 +6,4 @@ def genetic_modification_1_2(value, system):
     # http://redmine.encodedcc.org/issues/3063
     if 'modifiction_description' in value:
         value['modification_description'] = value['modifiction_description']
+        value.pop('modifiction_description')
