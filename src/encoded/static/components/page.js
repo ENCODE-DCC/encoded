35008bd0dd1466e47aa6d287b31cb4339db5d69f
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-09-12 23:45:55 -0700

    Format blog posts
    Redmine #3278

--- src/encoded/static/components/page.js
+++ src/encoded/static/components/page.js
@@ -1,5 +1,7 @@
 'use strict';
 var React = require('react');
+var moment = require('moment');
+var {Panel} = require('../libs/bootstrap/panel');
 var Layout = require('./layout').Layout;
 var globals = require('./globals');
 var _ = require('underscore');
@@ -8,6 +10,19 @@ var _ = require('underscore');
 var Page = module.exports.Page = React.createClass({
     render: function() {
         var context = this.props.context;
+        if (context.blog) {
+            return (
+                <Panel addClasses="blog-post">
+                    <div className="blog-post-header">
+                        <h1>{context.title}</h1>
+                        <h2>{moment.utc(context.date_created).format('MMMM D, YYYY')}</h2>
+                    </div>
+                    <Layout value={context.layout} />
+                </Panel>
+            )
+        }
+
+        // Non-blog page; render as title, then content box
         return (
             <div>
                 <header className="row">
