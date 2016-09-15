2b8453fd8c952958ec48ae2d5c1c31c3cd83724a
Idan Gabdank <gabdank@stanford.edu>
2016-09-15 13:52:13 -0700

    removing pipeline array audits
    since assay details become schema required, removing the test of audit
    monitoring situation in which they are not present

--- src/encoded/tests/test_audit_pipeline.py
+++ src/encoded/tests/test_audit_pipeline.py
@@ -1,27 +1,5 @@
-def test_audit_pipeline_assay_term_id(testapp, pipeline):
-    res = testapp.get(pipeline['@id'] + '@@index-data')
-    errors = res.json['audit']
-    errors_list = []
-    for error_type in errors:
-        errors_list.extend(errors[error_type])
-    assert any(error['category'] ==
-               'missing assay information' for error in errors_list)
-
-
-def test_audit_pipeline_assay_term_name(testapp, pipeline):
-    testapp.patch_json(pipeline['@id'], {'assay_term_id': 'NTR:0003082'})
-    res = testapp.get(pipeline['@id'] + '@@index-data')
-    errors = res.json['audit']
-    errors_list = []
-    for error_type in errors:
-        errors_list.extend(errors[error_type])
-    assert any(error['category'] ==
-               'missing assay information' for error in errors_list)
-
-
-def test_audit_pipeline_assay_term_name_mixed(testapp, pipeline):
-    testapp.patch_json(pipeline['@id'], {'assay_term_id': 'NTR:0003082',
-                                         'assay_term_name': 'RNA-seq'})
+def test_audit_pipeline_inconsistent_assay(testapp, pipeline):
+    testapp.patch_json(pipeline['@id'], {'assay_term_id': 'OBI:0001271'})
     res = testapp.get(pipeline['@id'] + '@@index-data')
     errors = res.json['audit']
     errors_list = []
