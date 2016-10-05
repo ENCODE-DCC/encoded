a877d5d01d3a927dcd9c376fce2cae32cee2c5e0
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2016-10-05 13:23:59 -0700

    tests auth0

--- src/encoded/tests/test_persona.py
+++ src/encoded/tests/test_persona.py
@@ -1,8 +1,8 @@
 import pytest
+import requests
 
-
+@pytest.fixture(scope='session')
 def auth0_access_token():
-    import requests
     fb_access_token = {
         'connection': 'Username-Password-Authentication',
         'scope': 'openid',
@@ -24,65 +24,63 @@ def auth0_access_token():
 
 
 @pytest.fixture(scope='session')
-def auth0_encode_user():
-    access_token = auth0_access_token()
-    return {'accessToken': access_token}
+def auth0_encode_user_token(auth0_access_token):
+    return {'accessToken': auth0_access_token}
+
+@pytest.fixture(scope='session')
+def auth0_encode_user_profile(auth0_access_token):
+    user_url = "https://{domain}/userinfo?access_token={access_token}" \
+        .format(domain='encode.auth0.com', access_token=auth0_access_token)
+    user_info = requests.get(user_url).json()
+    return user_info
 
 
-
-def test_login_no_csrf(anontestapp, auth0_encode_user):
-    res = anontestapp.post_json('/login', auth0_encode_user, status=400)
+def test_login_no_csrf(anontestapp, auth0_encode_user_token):
+    res = anontestapp.post_json('/login', auth0_encode_user_token, status=400)
     assert 'Set-Cookie' in res.headers
 
 
-def test_login_unknown_user(anontestapp, auth0_encode_user):
+def test_login_unknown_user(anontestapp, auth0_encode_user_token):
     res = anontestapp.get('/session')
     csrf_token = str(res.json['_csrft_'])
     headers = {'X-CSRF-Token': csrf_token}
-    res = anontestapp.post_json('/login', auth0_encode_user, headers=headers, status=403)
+    res = anontestapp.post_json('/login', auth0_encode_user_token, headers=headers, status=403)
     assert 'Set-Cookie' in res.headers
 
 
-# def test_login_bad_audience(anontestapp, persona_bad_assertion):
-#     res = anontestapp.get('/session')
-#     csrf_token = str(res.json['_csrft_'])
-#     headers = {'X-CSRF-Token': csrf_token}
-#     res = anontestapp.post_json('/login', persona_bad_assertion, headers=headers, status=403)
-#     assert 'Set-Cookie' in res.headers
+
+def test_login_logout(testapp, anontestapp, auth0_encode_user_token, auth0_encode_user_profile):
+    # Create a user with the persona email
+    url = '/users/'
+    email = auth0_encode_user_profile['email']
+    item = {
+        'email': email,
+        'first_name': 'Auth0',
+        'last_name': 'Test User',
+    }
+    testapp.post_json(url, item, status=201)
+
+    # Log in
+    res = anontestapp.get('/session')
+    csrf_token = str(res.json['_csrft_'])
+    headers = {'X-CSRF-Token': csrf_token}
+    res = anontestapp.post_json('/login', auth0_encode_user_token, headers=headers, status=200)
+    assert 'Set-Cookie' in res.headers
+    res = anontestapp.get('/session')
+    assert res.json['auth.userid'] == email
+
+    # Log out
+    res = anontestapp.get('/logout?redirect=false', headers=headers, status=200)
+    assert 'Set-Cookie' in res.headers
+    res = anontestapp.get('/session')
+    assert 'auth.userid' not in res.json
 
 
-# def test_login_logout(testapp, anontestapp, persona_assertion):
-#     # Create a user with the persona email
-#     url = '/users/'
-#     email = persona_assertion['email']
-#     item = {
-#         'email': email,
-#         'first_name': 'Persona',
-#         'last_name': 'Test User',
-#     }
-#     testapp.post_json(url, item, status=201)
-
-#     # Log in
-#     res = anontestapp.get('/session')
-#     csrf_token = str(res.json['_csrft_'])
-#     headers = {'X-CSRF-Token': csrf_token}
-#     res = anontestapp.post_json('/login', persona_assertion, headers=headers, status=200)
-#     assert 'Set-Cookie' in res.headers
-#     res = anontestapp.get('/session')
-#     assert res.json['auth.userid'] == email
-
-#     # Log out
-#     res = anontestapp.get('/logout?redirect=false', headers=headers, status=200)
-#     assert 'Set-Cookie' in res.headers
-#     res = anontestapp.get('/session')
-#     assert 'auth.userid' not in res.json
-
-
-# def test_impersonate_user(anontestapp, admin, submitter):
-#     res = anontestapp.post_json(
-#         '/impersonate-user', {'userid': submitter['email']},
-#         extra_environ={'REMOTE_USER': str(admin['email'])}, status=200)
-#     assert 'Set-Cookie' in res.headers
-#     assert res.json['auth.userid'] == submitter['email']
-#     res = anontestapp.get('/session')
-#     assert res.json['auth.userid'] == submitter['email']
+def test_impersonate_user(anontestapp, admin, submitter):
+    res = anontestapp.post_json(
+        '/impersonate-user', {'userid': submitter['email']},
+        extra_environ={'REMOTE_USER': str(admin['email'])}, status=200)
+    assert 'Set-Cookie' in res.headers
+    assert res.json['auth.userid'] == submitter['email']
+    res = anontestapp.get('/session')
+    assert res.json['auth.userid'] == submitter['email']
