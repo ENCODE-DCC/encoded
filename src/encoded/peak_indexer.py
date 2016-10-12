624edb2dc8a437316823088a05a800b664a13ec5
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2016-10-12 14:14:36 -0700

    count is int, not a list

--- src/encoded/peak_indexer.py
+++ src/encoded/peak_indexer.py
@@ -285,9 +285,9 @@ def index_file(request):
             '_source': False,
         })
         log.warn('      ')
-        log.warn('invalidated {}'.format(len(res['hits']['total'])))
+        log.warn('invalidated {}'.format(res['hits']['total']))
         if res['hits']['total'] > SEARCH_MAX:
-            log.warn('invalidated {} > SEARCH_MAX'.format(len(res['hits']['total'])))
+            log.warn('invalidated {} > SEARCH_MAX'.format(res['hits']['total']))
             invalidated = list(all_uuids(request.registry))
         else:
             referencing = {hit['_id'] for hit in res['hits']['hits']}
