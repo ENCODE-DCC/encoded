18e08ef4f698c0289287435c0261d7ee0247eff2
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-10-25 14:28:35 -0700

    Upgrade script for pages
    Removes any keywords not in the new schema enum for page.news_keywords.

---
+++ src/encoded/tests/test_upgrade_page.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+@pytest.fixture
+def page():
+    return{
+        'name': 'Fake Page',
+    }
+
+
+@pytest.fixture
+def page_1(page):
+    item = page.copy()
+    item.update({
+        'schema_version': '1',
+        'news_keywords': ['RNA binding', 'Experiment', 'DNA methylation', 'promoter-like regions', 'Conferences'],
+    })
+    return item
+
+
+@pytest.fixture
+def page_2(page):
+    item = page.copy()
+    item.update({
+        'schema_version': '1',
+        'news_keywords': ['Experiment', 'promoter-like regions'],
+    })
+    return item
+
+
+def test_page_upgrade_keep(upgrader, page_1):
+    value = upgrader.upgrade('page', page_1, target_version='2')
+    assert value['schema_version'] == '2'
+    assert value['news_keywords'] == ['RNA binding', 'DNA methylation', 'Conferences']
+
+
+def test_page_upgrade_empty(upgrader, page_2):
+    value = upgrader.upgrade('page', page_2, target_version='2')
+    assert value['schema_version'] == '2'
+    assert value['news_keywords'] == []
