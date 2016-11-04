cf959b1ea53aef2ef0b8b091521bfcc7b467cd27
Idan Gabdank <gabdank@stanford.edu>
2016-11-04 12:43:56 -0700

    refactored further
    made read_name_function
    and
    sequence_line_function

--- checkfiles/checkfiles.py
+++ checkfiles/checkfiles.py
@@ -265,6 +265,59 @@ def process_special_read_name(read_name,
         read_number + ':')
 
 
+def process_read_name_line(line,
+                           read_name_pattern,
+                           special_read_name_pattern,
+                           read_numbers_set,
+                           signatures_set,
+                           signatures_no_barcode_set,
+                           read_name_prefix,
+                           old_illumina_current_prefix,
+                           errors):
+    read_name = line.strip()
+    words_array = re.split(r'\s', read_name)
+    if read_name_pattern.match(read_name) is None:
+        if special_read_name_pattern.match(read_name) is not None:
+            process_special_read_name(read_name,
+                                      words_array,
+                                      read_numbers_set,
+                                      signatures_set,
+                                      signatures_no_barcode_set)
+        else:
+            # unrecognized read_name_format
+            # current convention is to include WHOLE
+            # readname at the end of the signature
+            if len(words_array) == 1:
+                if read_name_prefix.match(read_name) is not None:
+                    # new illumina without second part
+                    old_illumina_current_prefix = process_prefix_of_illumina_read_name(
+                        read_name,
+                        signatures_set,
+                        old_illumina_current_prefix)
+
+                elif len(read_name) > 3 and read_name.count(':') > 2:
+                    # assuming old illumina format
+                    old_illumina_current_prefix = process_old_illumina_format(
+                        read_name,
+                        read_numbers_set,
+                        old_illumina_current_prefix,
+                        signatures_set)
+                else:
+                    # unrecognized
+                    errors['fastq_format_readname'] = \
+                        'submitted fastq file does not ' + \
+                        'comply with illumina fastq read name format, ' + \
+                        'read name was : {}'.format(read_name)
+
+    else:  # found a match to the regex of "almost" illumina read_name
+        if len(words_array) == 2:
+            process_almost_new_illumina_read_name(read_name,
+                                                  read_numbers_set,
+                                                  signatures_set,
+                                                  signatures_no_barcode_set)
+    return old_illumina_current_prefix
+
+
 def process_sequence_line(line, read_lengths_dictionary):
     length = len(line.strip())
     if length not in read_lengths_dictionary:
@@ -290,7 +343,6 @@ def process_fastq_file(job, fastq_data_stream, session, url):
         '^(@[a-zA-Z\d]+[a-zA-Z\d_-]*:[a-zA-Z\d-]+:[a-zA-Z\d_-]' +
         '+:\d+:\d+:\d+:\d+[/1|/2]*[\s_][12]:[YXN]:[0-9]+:([ACNTG\+]*|[0-9]*))$'
     )
-    unrecognized_read_name = False
     read_numbers_set = set()
     signatures_set = set()
     signatures_no_barcode_set = set()
@@ -303,6 +355,17 @@ def process_fastq_file(job, fastq_data_stream, session, url):
             line = encoded_line.decode('utf-8')
             line_index += 1
             if line_index == 1:
+                old_illumina_current_prefix = process_read_name_line(
+                    line,
+                    read_name_pattern,
+                    special_read_name_pattern,
+                    read_numbers_set,
+                    signatures_set,
+                    signatures_no_barcode_set,
+                    read_name_prefix,
+                    old_illumina_current_prefix,
+                    errors)
+
                 read_name = line.strip()
                 words_array = re.split(r'\s', read_name)
                 if read_name_pattern.match(read_name) is None:
@@ -333,7 +396,7 @@ def process_fastq_file(job, fastq_data_stream, session, url):
                                     signatures_set)
                             else:
                                 # unrecognized
-                                unrecognized_read_name = read_name
+
                                 errors['fastq_format_readname'] = \
                                     'submitted fastq file does not ' + \
                                     'comply with illumina fastq read name format, ' + \
@@ -353,8 +416,6 @@ def process_fastq_file(job, fastq_data_stream, session, url):
     except IOError:
         errors['unzipped_fastq_streaming'] = 'Error occured, while streaming unzipped fastq.'
     else:
-        if unrecognized_read_name is not False:
-            print ('example of not illumina read_name ' + unrecognized_read_name)
         # read_count update
         result['read_count'] = read_count
 
