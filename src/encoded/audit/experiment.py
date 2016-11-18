a3b3212beb8e420ce0df518b3a76bf9b0d3a7bc5
Idan Gabdank <gabdank@stanford.edu>
2016-11-18 11:10:38 -0800

    any target that is not a control or histone mark will be audited as TF

--- src/encoded/audit/experiment.py
+++ src/encoded/audit/experiment.py
@@ -1734,7 +1734,7 @@ def check_file_chip_seq_read_depth(file_to_check,
             elif read_depth < 5000000:
                 yield AuditFailure('extremely low read depth',
                                    detail, level='ERROR')
-    elif 'transcription factor' in target_investigated_as:
+    else:
         if pipeline_title == 'Transcription factor ChIP-seq pipeline (modERN)':
             if read_depth < modERN_cutoff:
                 detail = 'modERN processed alignment file {} has {} '.format(file_to_check['@id'],
@@ -1747,7 +1747,8 @@ def check_file_chip_seq_read_depth(file_to_check,
                 yield AuditFailure('insufficient read depth',
                                    detail, level='NOT_COMPLIANT')
         else:
-            pipeline_object = get_pipeline_by_name(pipeline_objects, 'Transcription factor ChIP-seq')
+            pipeline_object = get_pipeline_by_name(pipeline_objects,
+                                                   'Transcription factor ChIP-seq')
             if pipeline_object:
                 if 'assembly' in file_to_check:
                     detail = 'Alignment file {} '.format(file_to_check['@id']) + \
