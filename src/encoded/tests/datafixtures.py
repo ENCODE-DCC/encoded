4651f6bca394c52d497be64bd730b23fc61ae767
estherc <etchan@stanford.edu>
2016-11-18 11:32:13 -0800

    Fixed more whitespace weirdness

--- src/encoded/tests/datafixtures.py
+++ src/encoded/tests/datafixtures.py
@@ -228,6 +228,7 @@ def experiment(testapp, lab, award):
     }
     return testapp.post_json('/experiment', item).json['@graph'][0]
 
+
 @pytest.fixture
 def base_experiment(testapp, lab, award):
     item = {
@@ -237,6 +238,7 @@ def base_experiment(testapp, lab, award):
         'status': 'started'
     }
     return testapp.post_json('/experiment', item, status=201).json['@graph'][0]
+
 
 @pytest.fixture
 def replicate(testapp, experiment, library):
