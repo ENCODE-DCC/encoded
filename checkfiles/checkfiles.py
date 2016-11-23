f18b943fc00a467778ce96a489654d3f0c280571
Idan Gabdank <gabdank@stanford.edu>
2016-11-23 11:19:29 -0800

    updating the version to be 1.02

--- checkfiles/checkfiles.py
+++ checkfiles/checkfiles.py
@@ -841,7 +841,7 @@ def run(out, err, url, username, password, encValData, mirror, search_query,
     except multiprocessing.NotImplmentedError:
         nprocesses = 1
 
-    version = '1.01'
+    version = '1.02'
 
     out.write("STARTING Checkfiles version %s (%s): with %d processes %s at %s\n" %
               (version, search_query, nprocesses, dr, datetime.datetime.now()))
