800e6cec81d2783dba62b3b3cab5856f1cbf514c
Idan Gabdank <gabdank@stanford.edu>
2016-12-02 12:33:22 -0800

    added replicate audit checking for match between constructs in biosample and experimental antibody target

--- src/encoded/audit/replicate.py
+++ src/encoded/audit/replicate.py
@@ -30,4 +30,78 @@ def audit_status_replicate(value, system):
             value['@id'],
             exp_status
             )
-        raise AuditFailure('mismatched status', detail, level='INTERNAL_ACTION')
+        yield AuditFailure('mismatched status', detail, level='INTERNAL_ACTION')
+    return
+
+
+@audit_checker(
+    'Replicate',
+    frame=[
+        'experiment',
+        'library',
+        'library.biosample',
+        'library.biosample.constructs',
+        'library.biosample.model_organism_donor_constructs',
+        'antibody',
+        'antibody.targets'])
+def audit_inconsistent_construct_tag(value, system):
+    if value['status'] in ['deleted', 'replaced', 'revoked']:
+        return
+    if value['experiment']['assay_term_id'] != 'OBI:0000716':
+        return  # not ChIP-seq
+    if 'library' in value and 'biosample' in value['library']:
+        matching_flag = False
+        tags_names = get_biosample_constructs_tags(value)
+        antibody_targets = get_ab_targets(value)
+        for ab_target in antibody_targets:
+            if 'recombinant protein' in ab_target['investigated_as']:
+                if ab_target['label'] in tags_names:
+                    matching_flag = True
+                    break
+        if not matching_flag:
+            detail = 'Replicate {}-{} in experiment {} '.format(
+                value['biological_replicate_number'],
+                value['technical_replicate_number'],
+                value['experiment']['@id']) + \
+                'specifies antibody {} that is inconsistent '.format(
+                value['antibody']['@id']) + \
+                'with biosample {} constructs tags {}.'.format(
+                value['biosample']['@id'],
+                tags_names)
+            yield AuditFailure('inconsistent construct tag', detail, level='INTERNAL_ACTION')
+    return
+
+
+def get_ab_targets(replicate):
+    uuids_set = set()
+    targtes_to_return = []
+    if 'antibody' in replicate and \
+       replicate['antibody']['status'] not in ['deleted', 'replaced', 'revoked']:
+        ab_targets = replicate['antibody']['targets']
+        for t in ab_targets:
+            if t['uuid'] not in uuids_set and t['status'] not in ['deleted', 'replaced', 'revoked']:
+                uuids_set.add(t['uuid'])
+                targtes_to_return.append(t)
+    return targtes_to_return
+
+
+def get_biosample_constructs_tags(replicate):
+    tags_names = set()
+    if 'library' in replicate and 'biosample' in replicate['library'] and \
+       replicate['library']['status'] not in ['deleted', 'replaced', 'revoked']:
+        biosample = replicate['library']['biosample']
+        if biosample['status'] not in ['deleted', 'replaced', 'revoked']:
+            if 'constructs' in biosample:
+                for construct in biosample['constructs']:
+                    if construct['status'] not in ['deleted', 'replaced', 'revoked'] and \
+                       'tags' in construct:
+                        for t in construct['tags']:
+                            tags_names.add(t['name'])
+            elif 'model_organism_donor_constructs' in biosample:
+                        for construct in biosample['model_organism_donor_constructs']:
+                            if construct['status'] not in ['deleted', 'replaced', 'revoked'] and \
+                               'tags' in construct:
+                                for t in construct['tags']:
+                                    tags_names.add(t['name'])
+
+    return tags_names
