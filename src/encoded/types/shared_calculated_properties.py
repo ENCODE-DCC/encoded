d3b89f4822c5485d4ef8588bba4f1646bd4cbbb4
estherc <etchan@stanford.edu>
2016-12-15 14:46:10 -0800

    Calculate assay_term_id from assay_term_name

--- src/encoded/types/shared_calculated_properties.py
+++ src/encoded/types/shared_calculated_properties.py
@@ -252,3 +252,61 @@ class CalculatedSeriesTarget:
     })
     def target(self, request, related_datasets):
         return request.select_distinct_values('target', *related_datasets)
+
+
+class CalculatedAssayTermID:
+    @calculated_property(condition='assay_term_name', schema={
+        "title": "Assay ID",
+        "description": "OBI (Ontology for Biomedical Investigations) ontology identifier for the assay.",
+        "type": "string",
+        "comment": "Calculated based on the choice of assay_term_name"
+    })
+    def assay_term_id(self, request, assay_term_name):
+        term_lookup = {
+            'ChIP-seq': 'OBI:0000716',
+            'RNA-seq': 'OBI:0001271',
+            'DNase-seq': 'OBI:0001853',
+            'eCLIP': 'NTR:0003027',
+            'shRNA knockdown followed by RNA-seq': 'NTR:0000762',
+            'RNA Bind-n-Seq': 'OBI:0002044',
+            'transcription profiling by array assay': 'OBI:0001463',
+            'DNA methylation profiling by array assay': 'OBI:0001332',
+            'whole-genome shotgun bisulfite sequencing': 'OBI:0001863',
+            'RRBS': 'OBI:0001862',
+            'siRNA knockdown followed by RNA-seq': 'NTR:0000763',
+            'RAMPAGE': 'OBI:0001864',
+            'comparative genomic hybridization by array': 'OBI:0001393',
+            'CAGE': 'OBI:0001674',
+            'single cell isolation followed by RNA-seq': 'NTR:0003082',
+            'Repli-seq': 'OBI:0001920',
+            'microRNA-seq': 'OBI:0001922',
+            'microRNA counts': 'NTR:0003660',
+            'MRE-seq': 'OBI:0001861',
+            'RIP-seq': 'OBI:0001857',
+            'Repli-chip': 'OBI:0001915',
+            'MeDIP-seq': 'OBI:0000693',
+            'ChIA-PET': 'OBI:0001848',
+            'FAIRE-seq': 'OBI:0001859',
+            'ATAC-seq': 'OBI:0002039',
+            'PAS-seq': 'OBI:0002045',
+            'RIP-chip': 'OBI:0001918',
+            'RNA-PET': 'OBI:0001850',
+            'genotyping by high throughput sequencing assay': 'OBI:0001247',
+            'CRISPR genome editing followed by RNA-seq': 'NTR:0003814',
+            'protein sequencing by tandem mass spectrometry assay': 'OBI:0001923',
+            '5C': 'OBI:0001919',
+            'HiC': 'OBI:0002042',
+            'TAB-seq': 'NTR:0002490',
+            'iCLIP': 'OBI:0002043',
+            'DNA-PET': 'OBI:0001849',
+            'Switchgear': 'NTR:0000612',
+            '5\' RLM RACE': 'NTR:0001684',
+            'MNase-seq': 'OBI:0001924'
+        }
+        term_id = list()
+        for term_name in assay_term_name:
+            if term_name in term_lookup:
+                term_id.append(term_lookup.get(term_name))
+            else:
+                term_id.append('Term ID unknown')
+        return term_id
