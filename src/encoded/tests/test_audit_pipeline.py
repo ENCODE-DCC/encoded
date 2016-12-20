0b8faf2168d31cf0732bf15279661703d590eaa3
estherc <etchan@stanford.edu>
2016-12-19 17:27:27 -0800

    Removed pipeline audit no longer needed as term id is calculated

--- src/encoded/tests/test_audit_pipeline.py
+++
@@ -1,8 +0,0 @@
-def test_audit_pipeline_assay_term_id(testapp, pipeline):
-    res = testapp.get(pipeline['@id'] + '@@index-data')
-    errors = res.json['audit']
-    errors_list = []
-    for error_type in errors:
-        errors_list.extend(errors[error_type])
-    assert any(error['category'] ==
-               'missing assay information' for error in errors_list)
