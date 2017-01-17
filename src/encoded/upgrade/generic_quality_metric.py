a37ae5a7d8ac8d25b534517f7bec1fab46c3921b
estherc <etchan@stanford.edu>
2017-01-17 11:35:55 -0800

    Add upgrades to all quality metrics

--- src/encoded/upgrade/generic_quality_metric.py
+++ src/encoded/upgrade/generic_quality_metric.py
@@ -22,3 +22,16 @@ def generic_quality_metric_2_3(value, system):
 @upgrade_step('generic_quality_metric', '3', '4')
 def generic_quality_metric_3_4(value, system):
     return
+
+
+@upgrade_step('generic_quality_metric', '4', '5')
+def generic_quality_metric_4_5(value, system):
+    # http://redmine.encodedcc.org/issues/2491
+    if 'assay_term_id' in value:
+        del value['assay_term_id']
+
+    if 'notes' in value:
+        if value['notes']:
+            value['notes'] = value['notes'].strip()
+        else:
+            del value['notes']
--- src/encoded/upgrade/idr_quality_metric.py
+++ src/encoded/upgrade/idr_quality_metric.py
@@ -21,3 +21,16 @@ def idr_quality_metric_1_2(value, system):
 @upgrade_step('idr_quality_metric', '2', '3')
 def idr_quality_metric_2_3(value, system):
     return
+
+
+@upgrade_step('idr_quality_metric', '3', '4')
+def idr_quality_metric_3_4(value, system):
+    # http://redmine.encodedcc.org/issues/2491
+    if 'assay_term_id' in value:
+        del value['assay_term_id']
+
+    if 'notes' in value:
+        if value['notes']:
+            value['notes'] = value['notes'].strip()
+        else:
+            del value['notes']
