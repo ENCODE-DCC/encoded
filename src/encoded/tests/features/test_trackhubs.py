434ada740b453df10045453a8b452551c8fd0db7
Tim Dreszer <tdreszer@stanford.org>
2017-01-17 16:03:47 -0800

    Adding batch_hub trackdb test.

--- src/encoded/tests/features/test_trackhubs.py
+++ src/encoded/tests/features/test_trackhubs.py
@@ -58,6 +58,44 @@ def test_dataset_trackDb(testapp, workbook, expected):
 
 
 @pytest.mark.parametrize('expected', [
+    "track chip",
+    "compositeTrack on",
+    "type bed 3",
+    "longLabel Collection of ENCODE ChIP-seq experiments",
+    "shortLabel ENCODE ChIP-seq",
+    "visibility full",
+    "html ChIP",
+    "subGroup1 view Views aOIDR=Optimal_IDR_thresholded_peaks bCIDR=Conservative_IDR_thresholded_peaks cRPKS=Replicated_peaks dPKS=Peaks eFCOC=Fold_change_over_control fSPV=Signal_p-value gSIG=Signal",
+    "subGroup2 BS Biosample GM12878=GM12878",
+    "subGroup3 EXP Experiment ENCSR000DZQ=ENCSR000DZQ",
+    "subGroup4 REP Replicates pool=Pooled",
+    "subGroup5 TARG Targets EBF1=EBF1",
+    "sortOrder BS=+ TARG=+ REP=+ view=+ EXP=+",
+    "dimensions dimA=REP",
+    "dimensionAchecked pool",
+    "    track chip_aOIDR_view",
+    "    parent chip on",
+    "    view aOIDR",
+    "    type bigBed 6 +",
+    "    visibility dense",
+    "    scoreFilter 100:1000",
+    "    spectrum on",
+    "        track ENCFF003COS",
+    "        parent chip_aOIDR_view on",
+    "        bigDataUrl /files/ENCFF003COS/@@download/ENCFF003COS.bigBed?proxy=true",
+    "        longLabel EBF1 ChIP-seq of GM12878 optimal idr thresholded peaks pool ENCSR000DZQ - ENCFF003COS",
+    "        shortLabel pool oIDR pk",
+    "        type bigBed 6 +",
+    "        color 153,38,0",
+    "        altColor 115,31,0",
+    "        subGroups BS=GM12878 EXP=ENCSR000DZQ REP=pool TARG=EBF1 view=aOIDR",
+])
+def test_genomes(testapp, workbook, expected):
+    res = testapp.get("/batch_hub/type%3Dexperiment%2C%2caccession%3DENCSR000DZQ%2C%2caccession%3DENCSRENCSR575ZXX/hg19/trackDb.txt")
+    assert expected in res.text
+
+
+@pytest.mark.parametrize('expected', [
     "track ENCSR727WCB",
     "compositeTrack on",
     "type bed 3",
