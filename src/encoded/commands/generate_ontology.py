390e705a3a007d99c7419f1d7c486d11d4acd840
jahilton <jahilton@stanford.edu>
2017-01-24 13:13:18 -0800

    slimming 3 RACE assays to transcription category

--- src/encoded/commands/generate_ontology.py
+++ src/encoded/commands/generate_ontology.py
@@ -132,6 +132,9 @@ slim_shims = {
         'OBI:0001923': 'Proteomics',  # OBI:0000615': 'MS-MS'
         'OBI:0001849': 'Genotyping',  # OBI:0000435 (DNA-PET)
         'OBI:0002044': 'RNA binding',  # OBI:0001854 (RNA-Bind-N-Seq)
+        'OBI:0002091': 'Transcription',
+        'OBI:0002092': 'Transcription',
+        'OBI:0002093': 'Transcription'
     }
 
 }
