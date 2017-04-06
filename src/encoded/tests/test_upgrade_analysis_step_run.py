3f9330707c3e87afd69e3ebbca8a11420ad82615
Esther <etchan@stanford.edu>
2017-04-05 21:22:23 -0700

    Fix aliases in fixtures

--- src/encoded/tests/test_upgrade_analysis_step_run.py
+++ src/encoded/tests/test_upgrade_analysis_step_run.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def analysis_step_version_with_alias(testapp, analysis_step, software_version):
     item = {
-        'aliases': ['versionof:' + analysis_step['name']],
+        'aliases': ['encode:' + analysis_step['name']],
         'analysis_step': analysis_step['@id'],
         'software_versions': [
             software_version['@id'],
