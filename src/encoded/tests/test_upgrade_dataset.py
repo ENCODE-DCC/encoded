39e6b1f68cdcae191c166fdf447e3edebe80ec6e
Esther <etchan@stanford.edu>
2017-04-25 11:46:29 -0700

    Confirm removal of ucsc_encode_db namespace in upgrade

--- src/encoded/tests/test_upgrade_dataset.py
+++ src/encoded/tests/test_upgrade_dataset.py
@@ -98,7 +98,8 @@ def experiment_10(root, experiment):
         'aliases': [
             'j-michael-cherry:Lib:XZ:20100107:11--ChIP:XZ:20100104:09:AdiposeNuclei:H3K4Me3',
             'roadmap-epigenomics:Bisulfite-Seq analysis of ucsf-4* stem cell line from UCSF-4||Tue Apr 16 16:10:36 -0500 2013||85822',
-            'encode:[this is]_qu#ite:bad" '
+            'encode:[this is]_qu#ite:bad" ',
+            'UCSC_encode_db:Illumina_HiSeq_2000'
         ]
     })
     return properties
@@ -236,5 +237,7 @@ def test_bad_dataset_alias_upgrade_10_11(root, upgrader, experiment_10):
     assert 'j-michael-cherry:Lib_XZ_20100107_11--ChIP_XZ_20100104_09_AdiposeNuclei_H3K4Me3' in value['aliases']
     assert 'roadmap-epigenomics:Bisulfite-Seq analysis of ucsf-4 stem cell line from UCSF-4_Apr-16-2013_85822' in value['aliases']
     assert 'encode:(this is)_quite_bad' in value['aliases']
+    assert 'encode:Illumina_HiSeq_2000' in value['aliases']
+    assert 'UCSC_encode_db:Illumina_HiSeq_2000' not in value['aliases']
     for alias in value['aliases']:
         assert len(alias.split(':')) == 2
