711781692ae4bf3a421c500b6515102f8df32364
Idan Gabdank <gabdank@stanford.edu>
2017-05-10 09:47:24 -0700

    removing audits

--- src/encoded/tests/test_audit_conditions.py
+++
@@ -1,9 +0,0 @@
-def test_rfa(award, biosample, root):
-    from ..audit.conditions import rfa
-    rfa_condition = rfa(award['rfa'])
-    context = root.get_by_uuid(biosample['uuid'])
-    system = {
-        "context": context,
-        "root": root,
-    }
-    assert rfa_condition(biosample, system)
