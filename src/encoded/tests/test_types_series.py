a321f59f99d406ef2f86070c957be35b86c48930
Esther <etchan@stanford.edu>
2017-06-01 15:34:31 -0700

    Clarified series assembly calcprop test

--- src/encoded/tests/test_types_series.py
+++ src/encoded/tests/test_types_series.py
@@ -56,9 +56,15 @@ def GRCh38_file(testapp, base_experiment, award, lab):
     return testapp.post_json('/file', item, status=201).json['@graph'][0]
 
 
-def test_assembly(testapp, base_reference_epigenome, base_experiment, hg19_file, GRCh38_file):
-    res = testapp.get(base_reference_epigenome['@id']+'@@index-data')
+def test_assembly_from_related_datasets(testapp, base_reference_epigenome, base_experiment, hg19_file, GRCh38_file):
+    # If only original file is present in series, assembly should be ['hg19']
+    res = testapp.get(base_reference_epigenome['@id'] + '@@index-data')
     assert res.json['object']['assembly'] == ['hg19']
+    # Adding a related_dataset with a file in GRCh38 should now give it both ['hg19', 'GRCh38']
     testapp.patch_json(base_reference_epigenome['@id'], {'related_datasets': [base_experiment['@id']]})
-    res = testapp.get(base_reference_epigenome['@id']+'@@index-data')
-    assert 'GRCh38' in res.json['object']['assembly'] and 'hg19' in res.json['object']['assembly']
+    res = testapp.get(base_reference_epigenome['@id'] + '@@index-data')
+    assert res.json['object']['assembly'].sort() == ['hg19', 'GRCh38'].sort()
+    # Setting the experiment in related_dataset to delete it should exclude its assembly from the calculation
+    testapp.patch_json(base_experiment['@id'], {'status': 'deleted'})
+    res = testapp.get(base_reference_epigenome['@id'] + '@@index-data')
+    assert res.json['object']['assembly'] == ['hg19']
