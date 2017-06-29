7ae7ad217f2b7a2dfcbeb5156044d442ce95acb1
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2017-06-28 18:08:47 -0700

    remove non-functional test

--- src/encoded/tests/test_post_put_patch.py
+++ src/encoded/tests/test_post_put_patch.py
@@ -130,21 +130,6 @@ def test_patch(content, testapp):
     assert res.json['@graph'][0]['simple2'] == 'supplied simple2'
 
 
-def test_patch_new_schema_version(content, root, testapp, monkeypatch):
-    collection = root['testing_post_put_patch']
-    properties = collection.type_info.schema['properties']
-
-    url = content['@id']
-    res = testapp.get(url)
-    assert res.json['schema_version'] == '1'
-
-    monkeypatch.setitem(properties['schema_version'], 'default', '2')
-    monkeypatch.setattr(collection.type_info, 'schema_version', '2')
-    monkeypatch.setitem(properties, 'new_property', {'default': 'new'})
-    res = testapp.patch_json(url, {}, status=200)
-    assert res.json['@graph'][0]['schema_version'] == '2'
-    assert res.json['@graph'][0]['new_property'] == 'new'
-
 
 def test_admin_put_protected_link(link_targets, testapp):
     res = testapp.post_json(COLLECTION_URL, item_with_link[0], status=201)
