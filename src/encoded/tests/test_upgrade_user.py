32d38126f6c40445f5a317d021250222d6f2d0ec
aditin14 <aditin14@stanford.edu>
2017-06-29 13:43:39 -0700

    adding user fields upgrade, tests, and merging in master

--- src/encoded/tests/test_upgrade_user.py
+++ src/encoded/tests/test_upgrade_user.py
@@ -40,3 +40,29 @@ def test_user_upgrade_viewing_groups(upgrader, user_3):
     value = upgrader.upgrade('user', user_3, target_version='4')
     assert value['schema_version'] == '4'
     assert value['viewing_groups'] == ['ENCODE3']
+
+
+@pytest.fixture
+def user_6(user):
+    item = user.copy()
+    item.update({
+        'schema_version': '5',
+        'phone1': '206-685-2672',
+        'phone2': '206-267-1098',
+        'fax': '206-267-1094',
+        'skype': 'fake_id',
+        'google': 'google',
+        'timezone': 'US/Pacific',
+    })
+    return item
+
+
+def test_user_upgrade_contact_info(upgrader, user_6):
+    value = upgrader.upgrade('user', user_6, target_version='6')
+    assert value['schema_version'] == '6'
+    assert value['phone1'] is None
+    assert value['phone2'] is None
+    assert value['fax'] is None
+    assert value['skype'] is None
+    assert value['google'] is None
+    assert value['timezone'] is None
--- src/encoded/upgrade/user.py
+++ src/encoded/upgrade/user.py
@@ -15,3 +15,22 @@ def user_3_4(value, system):
         'ENCODE3' if v == 'ENCODE' else v
         for v in value['viewing_groups']
     ]
+
+
+@upgrade_step('user', '5', '6')
+def user_5_6(value, system):
+    # https://encodedcc.atlassian.net/browse/ENCD-3409
+    if ['phone1'] in value:
+        value['phone1'] is None
+    if ['phone2'] in value:
+        value['phone2'] is None
+    if ['fax'] in value:
+        value['fax'] is None
+    if ['skype'] in value:
+        value['skype'] is None
+    if ['google'] in value:
+        value['google'] is None
+    if ['timezone'] in value:
+        value['timezone'] is None
+    if ['schema_version'] in value:
+        value['schema'] == '6'
