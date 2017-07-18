3d19dff204d2a4ab37b807ee61b49dd3aab7753c
Idan Gabdank <gabdank@stanford.edu>
2017-07-18 13:15:30 -0700

    fixing test to work with biosample summary and inserts for email address

--- src/encoded/tests/test_search.py
+++ src/encoded/tests/test_search.py
@@ -53,13 +53,13 @@ def test_matrix_view(workbook, testapp):
     assert len(res['matrix']['x']['buckets']) > 0
     assert len(res['matrix']['x']['facets']) > 0
     assert res['matrix']['y']['group_by'] == [
-        'biosample_type', 'biosample_term_name']
+        'biosample_summary', 'biosample_term_name']
     assert res['matrix']['y']['label'] == 'Biosample'
     assert res['matrix']['y']['limit'] == 5
     assert len(res['matrix']['y'][
-        'biosample_type']['buckets']) > 0
+        'biosample_summary']['buckets']) > 0
     assert len(res['matrix']['y'][
-        'biosample_type']['buckets'][0][
+        'biosample_summary']['buckets'][0][
         'biosample_term_name']['buckets']) > 0
 
 
