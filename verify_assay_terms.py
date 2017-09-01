d33275b66f7e1fc98654ba6eeef7f58645eb62e3
jahilton <jahilton@stanford.edu>
2017-09-01 15:19:16 -0700

    importing assay_terms dict.

--- verify_assay_terms.py
+++ verify_assay_terms.py
@@ -1,76 +1,24 @@
 import json
-# from assay_data.py import assay_terms
+from importlib.machinery import SourceFileLoader
 
-ont = json.load(open('ontology.json'))
-
-assay_terms = {
-      'ChIP-seq': 'OBI:0000716',
-      'RNA-seq': 'OBI:0001271',
-      'DNase-seq': 'OBI:0001853',
-      'eCLIP': 'OBI:0002111',
-      'shRNA knockdown followed by RNA-seq': 'NTR:0000762',
-      'RNA Bind-n-Seq': 'OBI:0002044',
-      'transcription profiling by array assay': 'OBI:0001463',
-      'DNA methylation profiling by array assay': 'OBI:0001332',
-      'whole-genome shotgun bisulfite sequencing': 'OBI:0001863',
-      'RRBS': 'OBI:0001862',
-      'siRNA knockdown followed by RNA-seq': 'NTR:0000763',
-      'RAMPAGE': 'OBI:0001864',
-      'comparative genomic hybridization by array': 'OBI:0001393',
-      'CAGE': 'OBI:0001674',
-      'single cell isolation followed by RNA-seq': 'NTR:0003082',
-      'Repli-seq': 'OBI:0001920',
-      'microRNA-seq': 'OBI:0001922',
-      'microRNA counts': 'NTR:0003660',
-      'MRE-seq': 'OBI:0001861',
-      'RIP-seq': 'OBI:0001857',
-      'Repli-chip': 'OBI:0001915',
-      'MeDIP-seq': 'OBI:0000693',
-      'ChIA-PET': 'OBI:0001848',
-      'FAIRE-seq': 'OBI:0001859',
-      'ATAC-seq': 'OBI:0002039',
-      'PAS-seq': 'OBI:0002045',
-      'RIP-chip': 'OBI:0001918',
-      'RNA-PET': 'OBI:0001850',
-      'genotyping by high throughput sequencing assay': 'OBI:0001247',
-      'CRISPR genome editing followed by RNA-seq': 'NTR:0003814',
-      'protein sequencing by tandem mass spectrometry assay': 'OBI:0001923',
-      '5C': 'OBI:0001919',
-      'HiC': 'OBI:0002042',
-      'TAB-seq': 'OBI:0002086',
-      'iCLIP': 'OBI:0002043',
-      'DNA-PET': 'OBI:0001849',
-      'Switchgear': 'NTR:0000612',
-      '5\' RLM RACE': 'OBI:0002093',
-      'MNase-seq': 'OBI:0001924',
-      '5\' RACE': 'OBI:0002091',
-      '3\' RACE': 'OBI:0002092',
-      'small RNA-seq': 'OBI:0002112',
-      'Bru-seq': 'OBI:0002113',
-      'BruChase-seq': 'OBI:0002114',
-      'genetic modification followed by DNase-seq': 'NTR:0004774',
-      'CRISPRi followed by RNA-seq': 'NTR:0004619',
-      'genotype phasing by HiC': 'NTR:0004875',
-      'Circulome-seq': 'NTR:0005023',
-      'whole genome sequencing assay': 'OBI:0002117',
-      'BruUV-seq': 'NTR:0004739',
-      'Mint-ChIP-seq': 'NTR:0005141',
-      'single nuclei ATAC-seq': 'NTR:0000438',
-      'icSHAPE': 'NTR:0000433'
-}
+assay_data = SourceFileLoader('module.name', 'src/encoded/types/assay_data.py').load_module()
+assay_terms = assay_data.assay_terms
+ontology_file = json.load(open('ontology.json'))
 
 verified_assays = []
+incorrect_assays = []
 
 for assay in assay_terms.keys():
     name_from_assay_data = assay
     id_from_assay_data = assay_terms[assay]
-    name_from_ontology_json = ont[id_from_assay_data]['name']
-    preferred_name_from_ontology = ont[id_from_assay_data]['preferred_name']
-    synonyms_from_ontology = ont[id_from_assay_data]['synonyms']
+    name_from_ontology_json = ontology_file[id_from_assay_data]['name']
+    preferred_name_from_ontology = ontology_file[id_from_assay_data]['preferred_name']
+    synonyms_from_ontology = ontology_file[id_from_assay_data]['synonyms']
 
     if name_from_assay_data == name_from_ontology_json or name_from_assay_data == preferred_name_from_ontology or name_from_assay_data in synonyms_from_ontology:
         verified_assays.append(name_from_assay_data)
     else:
+        incorrect_assays.append(name_from_assay_data)
         print("ERROR: " + name_from_assay_data + ' is paired with ' + id_from_assay_data + ' but ' + id_from_assay_data + '=[name:' + name_from_ontology_json + ',preferred_name:' + preferred_name_from_ontology + ',synonyms:' + str(synonyms_from_ontology) + ']')
 
 total_assay_count = len(assay_terms.keys())
@@ -81,4 +29,4 @@ print(str(total_verified_assay_count) + ' out of ' + str(total_assay_count) + ' 
 if total_assay_count == total_verified_assay_count:
     print("GOOD TO GO")
 else:
-    print("NEED FIXES")
+    print("NEED FIXES:" + str(incorrect_assays))
