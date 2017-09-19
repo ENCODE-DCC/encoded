79efa6275bded05895c443ea43be398dd1ecb2f0
Forrest Tanaka <forrest@forrest-tanaka.com>
2017-09-19 13:24:09 -0700

    Make GM column of replicate table conditional
    The “Genetic modification” column of the replicate table now only displays if the experiment has biosamples with genetic modifications. ENCD-3504

--- src/encoded/static/components/experiment.js
+++ src/encoded/static/components/experiment.js
@@ -739,7 +739,7 @@ const replicateTableColumns = {
                 return (
                     <span>
                         {gms.map((gm, i) => (
-                            <span>
+                            <span key={gm.uuid}>
                                 {i > 0 ? <span>, </span> : null}
                                 <a href={gm['@id']} title={`View genetic modification ${gm.accession}`}>{gm.accession}</a>
                             </span>
@@ -749,6 +749,10 @@ const replicateTableColumns = {
             }
             return null;
         },
+        hide: list => _(list).all((condensedReplicate) => {
+            const replicate = condensedReplicate[0];
+            return !(replicate.library && replicate.library.biosample && replicate.library.biosample.applied_modifications && replicate.library.biosample.applied_modifications.length);
+        }),
     },
 
     antibody_accession: {
