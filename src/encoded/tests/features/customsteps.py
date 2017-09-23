65071a6973828c58268b9dcce5b7f29afb9fe26a
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2017-09-22 19:01:57 -0700

    different css selector for state 'communicating'

--- src/encoded/tests/features/customsteps.py
+++ src/encoded/tests/features/customsteps.py
@@ -50,9 +50,9 @@ def click_element(browser, css):
 
 
 @when('I wait for the table to fully load')
-def wait_for_table(browser):
+def wait_for_table(browser): 
     assert browser.is_element_present_by_css("table.collection-table", wait_time=15)
-    assert browser.is_element_not_present_by_css("table.collection-table.communicating", wait_time=15)
+    assert browser.is_element_not_present_by_css("label-warning.spinner-warning", wait_time=15)
 
 
 @when('I wait for deferred content to fully load')
