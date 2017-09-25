3aa2cf3217f8fdb0015e471e4cff87c20d0544ab
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2017-09-25 14:13:11 -0700

    ff

--- src/encoded/tests/features/customsteps.py
+++ src/encoded/tests/features/customsteps.py
@@ -53,8 +53,7 @@ def click_element(browser, css):
 @when('I wait for the table to fully load')
 def wait_for_table(browser):
     assert browser.is_element_present_by_css("table.sticky-area.collection-table.table")
-    time.sleep(30)
-    assert browser.is_element_present_by_css("span.table-meta-data")
+    assert browser.is_element_not_present_by_css("table.sticky-area.collection-table.communicating")
 
 
 @when('I wait for deferred content to fully load')
