0ce2d07c70ad35d255914cfc463bf9692cae4b74
Idan Gabdank <gabdank@stanford.edu>
2017-10-16 14:51:00 -0700

    adding test for donor schema dependencies

---
+++ src/encoded/tests/test_schema_donor.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.fixture
+def mouse_donor_to_test(testapp, lab, award, mouse):
+    return {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'organism': mouse['@id'],
+    }
+
+
+def test_donor_with_three_parents(
+        testapp,
+        mouse_donor_to_test,
+        mouse_donor,
+        mouse_donor_1,
+        mouse_donor_2, ):
+    mouse_donor_to_test['parent_strains'] = [
+        mouse_donor['@id'],
+        mouse_donor_1['@id'],
+        mouse_donor_2['@id'],
+    ]
+    testapp.post_json('/mouse_donor', mouse_donor_to_test, status=422)
