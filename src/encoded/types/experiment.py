febba1c08c651163baa463e9f9b2b280d759297b
David Glick <david@glicksoftware.com>
2017-11-08 21:12:13 -0500

    Specify reverse link properties which should not be submittable

--- src/encoded/types/experiment.py
+++ src/encoded/types/experiment.py
@@ -339,6 +339,7 @@ class Experiment(Dataset,
             "type": ['string', 'object'],
             "linkFrom": "Series.related_datasets",
         },
+        "notSubmittable": True,
     })
     def related_series(self, request, related_series):
         return paths_filtered_by_status(request, related_series)
@@ -350,6 +351,7 @@ class Experiment(Dataset,
             "type": ['string', 'object'],
             "linkFrom": "Experiment.supersedes",
         },
+        "notSubmittable": True,
     })
     def superseded_by(self, request, superseded_by):
         return paths_filtered_by_status(request, superseded_by)
