62282d4fbd3cea7c6ae8ea3f63215c18516620da
estherc <etchan@stanford.edu>
2017-12-21 10:10:02 -0800

    ENCD-3721 Allow 'in vitro sample' to have biosample_term_name (#2067)

--- src/encoded/tests/test_audit_experiment.py
+++ src/encoded/tests/test_audit_experiment.py
@@ -188,7 +188,9 @@ def ctrl_experiment(testapp, lab, award, control_target):
     item = {
         'award': award['uuid'],
         'lab': lab['uuid'],
-        'biosample_type': 'in vitro sample',
+        'biosample_type': 'cell-free sample',
+        'biosample_term_id': 'NTR:0000471',
+        'biosample_term_name': 'none',
         'status': 'started',
         'assay_term_name': 'ChIP-seq'
     }
