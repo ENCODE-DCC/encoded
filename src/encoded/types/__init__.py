3c22d56d3d5c0d883a6fbabac93c6858d0a52f09
keenangraham <keenangraham@gmail.com>
2018-01-08 10:29:48 -0800

    ENCD-3755 Remove schemas for RNAi and construct characterizations (#2085)

--- src/encoded/types/__init__.py
+++ src/encoded/types/__init__.py
@@ -101,21 +101,7 @@ class Construct(Item):
     item_type = 'construct'
     schema = load_schema('encoded:schemas/construct.json')
     # XXX 'vector_name' as key?
-    rev = {
-        'characterizations': ('ConstructCharacterization', 'characterizes'),
-    }
     embedded = ['target']
-
-    @calculated_property(schema={
-        "title": "Characterizations",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "ConstructCharacterization",
-        },
-    })
-    def characterizations(self, request, characterizations):
-        return paths_filtered_by_status(request, characterizations)
 
 
 @collection(
@@ -129,9 +115,6 @@ class TALEN(Item):
     item_type = 'talen'
     schema = load_schema('encoded:schemas/talen.json')
     name_key = 'name'
-    rev = {
-        'characterizations': ('ConstructCharacterization', 'characterizes'),
-    }
     embedded = [
         'lab',
         'submitted_by',
@@ -140,17 +123,6 @@ class TALEN(Item):
         'documents.lab',
         'documents.submitted_by'
     ]
-
-    @calculated_property(schema={
-        "title": "Characterizations",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "ConstructCharacterization",
-        },
-    })
-    def characterizations(self, request, characterizations):
-        return paths_filtered_by_status(request, characterizations)
 
 
 @collection(
@@ -248,20 +220,6 @@ class RNAi(Item):
     item_type = 'rnai'
     schema = load_schema('encoded:schemas/rnai.json')
     embedded = ['source', 'documents', 'target']
-    rev = {
-        'characterizations': ('RNAiCharacterization', 'characterizes'),
-    }
-
-    @calculated_property(schema={
-        "title": "Characterizations",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "RNAiCharacterization",
-        },
-    })
-    def characterizations(self, request, characterizations):
-        return paths_filtered_by_status(request, characterizations)
 
 
 @collection(
