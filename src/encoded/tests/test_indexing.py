4caa6ccca3f2ed592044ce791bc366dbbc009498
Tim Dreszer <tdreszer@stanford.org>
2018-01-12 12:07:32 -0800

    Fix one merge issue.

--- src/encoded/tests/test_indexing.py
+++ src/encoded/tests/test_indexing.py
@@ -94,13 +94,9 @@ def test_indexing_workbook(testapp, indexer_testapp):
     res = indexer_testapp.post_json('/index', {'record': True})
     assert res.json['updated']
     assert res.json['indexed']
-<<<<<<< HEAD
-    assert res.json['pass2_took']
-=======
     ### OPTIONAL: audit via 2-pass is coming...
     #assert res.json['pass2_took']
     ### OPTIONAL: audit via 2-pass is coming...
->>>>>>> master
 
     res = indexer_testapp.post_json('/index_secondary', {'record': True})
     assert res.json['cycle_took']
