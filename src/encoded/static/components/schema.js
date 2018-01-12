fef387dfb2cc422689bda9478a0305234859b18b
Forrest Tanaka <forrest@forrest-tanaka.com>
2018-01-11 16:20:19 -0800

    Display the raw schema
    I put in tabs to select between viewing formatted and raw schema displays. ENCD_1934

--- src/encoded/static/components/schema.js
+++ src/encoded/static/components/schema.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
+import { Panel, PanelHeading, PanelBody, TabPanel, TabPanelPane } from '../libs/bootstrap/panel';
 import { collapseIcon } from '../libs/svg-icons';
 import { Param, FetchedData } from './fetched';
 import * as globals from './globals';
@@ -17,6 +18,23 @@ const titleMap = {
 };
 
 
+// List of schema terms we don't display.
+const excludedTerms = [
+    'type',
+    'additionalProperties',
+    'mixinProperties',
+    'title',
+    'description',
+    'id',
+    '$schema',
+    'facets',
+    'columns',
+    'boost_values',
+    'changelog',
+    '@type',
+];
+
+
 // Functions to display simple schema term types, keyed by `typeof` result.
 const simpleTypeDisplay = {
     string: item => <span>{item}</span>,
@@ -25,6 +43,48 @@ const simpleTypeDisplay = {
 };
 
 
+/**
+ * Create a brace read-only editor instance that goes into the HTML element with the given ID and
+ * displaying the given schema term.
+ *
+ * @param {*} elementId - HTML element ID to draw the editor into.
+ * @param {*} term - Schema term object to display.
+ */
+function createJsonDisplay(elementId, term) {
+    return new Promise((resolve) => {
+        require.ensure([
+            'brace',
+            'brace/mode/json',
+            'brace/theme/katzenmilch',
+        ], (require) => {
+            const ace = require('brace');
+            require('brace/mode/json');
+            require('brace/theme/katzenmilch');
+            const value = JSON.stringify(term, null, 4);
+            const editor = ace.edit(elementId);
+            const session = editor.getSession();
+            session.setMode('ace/mode/json');
+            editor.setTheme('ace/theme/katzenmilch');
+            editor.setValue(value);
+            editor.setOptions({
+                maxLines: 500,
+                minLines: 1,
+                readOnly: true,
+                highlightActiveLine: false,
+                highlightGutterLine: false,
+                showGutter: false,
+                showPrintMargin: false,
+            });
+            editor.clearSelection();
+            editor.textInput.getElement().disabled = true;
+            editor.renderer.$cursorLayer.element.style.opacity = 0;
+            resolve(editor);
+        }, 'brace');
+    });
+}
+
+
+// Display JSON for one schema term.
 class SchemaTermJsonDisplay extends React.Component {
     constructor() {
         super();
@@ -37,33 +97,12 @@ class SchemaTermJsonDisplay extends React.Component {
         // Now that the JSON display component has mounted, 
         const { schemaValue, term } = this.props;
 
-        require.ensure([
-            'brace',
-            'brace/mode/json',
-            'brace/theme/katzenmilch',
-        ], (require) => {
-            const ace = require('brace');
-            require('brace/mode/json');
-            require('brace/theme/katzenmilch');
-            const value = JSON.stringify(schemaValue[term], null, 4);
-            this.editor = ace.edit(term);
-            const session = this.editor.getSession();
-            session.setMode('ace/mode/json');
-            this.editor.setTheme('ace/theme/katzenmilch');
-            this.editor.setValue(value);
-            this.editor.setOptions({
-                maxLines: 500,
-                minLines: 1,
-                readOnly: true,
-                highlightActiveLine: false,
-                highlightGutterLine: false,
-                showGutter: false,
-                showPrintMargin: false,
-            });
-            this.editor.clearSelection();
-            this.editor.textInput.getElement().disabled = true;
-            this.editor.renderer.$cursorLayer.element.style.opacity = 0;
-        }, 'brace');
+        // Create a read-only brace editor to display the formatted JSON.
+        createJsonDisplay(term, schemaValue[term]).then((editor) => {
+            // The brace editor successfully created. Save the reference to the brace editor
+            // instance so we can get rid of it when the JSON display is closed.
+            this.editor = editor;
+        });
     }
 
     componentWillUnmount() {
@@ -90,6 +129,7 @@ SchemaTermJsonDisplay.propTypes = {
 };
 
 
+// Display one object-type term, with associated JSON.
 class SchemaTermItemDisplay extends React.Component {
     constructor() {
         super();
@@ -123,7 +163,7 @@ class SchemaTermItemDisplay extends React.Component {
                     <SchemaTermJsonDisplay schemaValue={schemaValue} term={term} />
                 : null}
             </div>
-        )
+        );
     }
 }
 
@@ -133,6 +173,7 @@ SchemaTermItemDisplay.propTypes = {
 };
 
 
+// Display all property terms of a schema object (usually dependencies and properties).
 const SchemaTermDisplay = props => (
     <div>
         {Object.keys(props.schemaValue).map(key =>
@@ -146,6 +187,7 @@ SchemaTermDisplay.propTypes = {
 };
 
 
+// Display one term of the schema regardless of its value type.
 const TermDisplay = (props) => {
     const { termSchema } = props;
     const termType = typeof termSchema;
@@ -189,22 +231,7 @@ TermDisplay.propTypes = {
 };
 
 
-const excludedTerms = [
-    'type',
-    'additionalProperties',
-    'mixinProperties',
-    'title',
-    'description',
-    'id',
-    '$schema',
-    'facets',
-    'columns',
-    'boost_values',
-    'changelog',
-    '@type',
-];
-
-
+// Display an entire formatted schema.
 const DisplayObject = (props) => {
     const { schema } = props;
     const schemaTerms = Object.keys(schema).filter(term => excludedTerms.indexOf(term) === -1);
@@ -227,6 +254,41 @@ DisplayObject.propTypes = {
 };
 
 
+// Display a complete raw schema object.
+class DisplayRawObject extends React.Component {
+    constructor() {
+        super();
+
+        // Set object properties.
+        this.editor = null;
+    }
+
+    componentDidMount() {
+        // Create a read-only brace editor to display the formatted JSON.
+        createJsonDisplay('raw-schema', this.props.schema).then((editor) => {
+            // The brace editor successfully created. Save the reference to the brace editor
+            // instance so we can get rid of it when the JSON display is closed.
+            this.editor = editor;
+        });
+    }
+
+    componentWillUnmount() {
+        if (this.editor) {
+            this.editor.destroy();
+            this.editor = null;
+        }
+    }
+
+    render() {
+        return <div id="raw-schema" />;
+    }
+}
+
+DisplayRawObject.propTypes = {
+    schema: PropTypes.object.isRequired, // Schema to display as a raw object
+};
+
+
 // Display markdown.
 const Markdown = (props) => {
     const html = marked(props.source, { sanitize: true });
@@ -244,9 +306,9 @@ Markdown.defaultProps = {
 
 // Display the Markdown-formatted change log.
 const ChangeLog = props => (
-    <section className="view-detail panel">
+    <div>
         <Markdown source={props.source} />
-    </section>
+    </div>
 );
 
 ChangeLog.propTypes = {
@@ -271,13 +333,31 @@ const SchemaPage = (props) => {
                 </div>
             </header>
             {typeof context.description === 'string' ? <p className="description">{context.description}</p> : null}
-            <section className="view-detail panel">
-                <DisplayObject schema={context} />
-            </section>
-            {changelog && <FetchedData>
-                <Param name="source" url={changelog} type="text" />
-                <ChangeLog />
-            </FetchedData>}
+            <Panel>
+                <TabPanel tabs={{ formatted: 'Formatted', raw: 'Raw' }}>
+                    <TabPanelPane key="formatted">
+                        <PanelBody>
+                            <DisplayObject schema={context} />
+                        </PanelBody>
+                    </TabPanelPane>
+                    <TabPanelPane key="raw">
+                        <PanelBody>
+                            <DisplayRawObject schema={context} />
+                        </PanelBody>
+                    </TabPanelPane>
+                </TabPanel>
+            </Panel>
+            <Panel>
+                <PanelHeading>
+                    <h4>ChangeLog</h4>
+                </PanelHeading>
+                <PanelBody>
+                    {changelog && <FetchedData>
+                        <Param name="source" url={changelog} type="text" />
+                        <ChangeLog />
+                    </FetchedData>}
+                </PanelBody>
+            </Panel>
         </div>
     );
 };
