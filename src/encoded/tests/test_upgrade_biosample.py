a05dfa7fd9bef7239903bf8d60c02a07d7253bc4
jahilton <jahilton@stanford.edu>
2018-03-02 11:05:00 -0800

    Undo change in biosample test upgrade
    This is a test that should be preserved, and thus, not included in the
    code-wide sweep

--- src/encoded/tests/test_upgrade_biosample.py
+++ src/encoded/tests/test_upgrade_biosample.py
@@ -174,7 +174,7 @@ def biosample_18(biosample_0, biosample, construct, rnai):
     item.update({
         'biosample_term_id': 'EFO:0002067',
         'biosample_term_name': 'K562',
-        'biosample_type': 'cell line',
+        'biosample_type': 'immortalized cell line',
         'constructs': list(construct),
         'rnais': list(rnai),
         'transfection_type': 'stable',
