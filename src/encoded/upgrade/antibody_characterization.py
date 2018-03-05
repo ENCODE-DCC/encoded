676dca5b509f38c4aa3e1574a005ad746ca9ca3a
jahilton <jahilton@stanford.edu>
2018-03-05 09:18:10 -0800

    move antibody_characterization upgrade

--- src/encoded/upgrade/antibody_characterization.py
+++
@@ -1,9 +0,0 @@
-from snovault import upgrade_step
-
-
-@upgrade_step('antibody_characterization', '11', '12')
-def antibody_characterization(value, system):
-    # https://encodedcc.atlassian.net/browse/ENCD-3848
-    for characterization_review in value.get('characterization_reviews'):
-        if characterization_review.get('biosample_type') == 'immortalized cell line':
-            characterization_review['biosample_type'] = "cell line"
--- src/encoded/upgrade/characterization.py
+++ src/encoded/upgrade/characterization.py
@@ -209,3 +209,11 @@ def characterization_10_11(value, system):
     if 'comment' in value:
         value['submitter_comment'] = value['comment']
         del value['comment']
+
+
+@upgrade_step('antibody_characterization', '11', '12')
+def antibody_characterization(value, system):
+    # https://encodedcc.atlassian.net/browse/ENCD-3848
+    for characterization_review in value.get('characterization_reviews'):
+        if characterization_review.get('biosample_type') == 'immortalized cell line':
+            characterization_review['biosample_type'] = "cell line"
