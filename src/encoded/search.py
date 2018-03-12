9d1cff2c126d338f40f40599d19a8ef6b7166e17
kakaly <reach.kkarthik@gmail.com>
2018-03-12 11:05:53 -0700

    Uncommented the line to add back the download button

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -556,8 +556,8 @@ def search_result_actions(request, doc_types, es_results, position=None):
     # TODO we could enable them for Datasets as well here, but not sure how well it will work
     # batch download disabled for region-search results
     if '/region-search/' not in request.url:
-        #if (doc_types == ['Experiment'] or doc_types == ['Annotation']) and any(
-        if (doc_types == ['Experiment']) and any(
+        if (doc_types == ['Experiment'] or doc_types == ['Annotation']) and any(
+        #if (doc_types == ['Experiment']) and any(
                 bucket['doc_count'] > 0
                 for bucket in aggregations['files-file_type']['files-file_type']['buckets']):
             actions['batch_download'] = request.route_url(
