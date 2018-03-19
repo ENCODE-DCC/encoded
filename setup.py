b1f06f9f3ddbf6d17957110de0d7c339a3eac48f
caseylitton <caseylitton@gmail.com>
2018-03-19 10:56:00 -0700

    Update to version 67.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ tests_require = [
 
 setup(
     name='encoded',
-    version='66.0',
+    version='67.0',
     description='Metadata database for ENCODE',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
