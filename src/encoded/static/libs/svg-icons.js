3ce1713736ca317bde0b67161035840cd1e6c98d
Phillip Adenekan <phil@DN525v2m.SUNet>
2018-05-18 12:40:05 -0700

    Updated file to better comply with ES6

--- src/encoded/static/libs/svg-icons.js
+++ src/encoded/static/libs/svg-icons.js
@@ -1,4 +1,4 @@
-const React = require('react');
+import React from 'react';
 
 const icons = {
     disclosure: <svg id="Disclosure" data-name="Disclosure" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 480" className="svg-icon svg-icon-disclosure"><circle class="cls-1" cx="240" cy="240" r="240"/><polyline class="cls-2" points="401.79 175.66 240 304.34 78.21 175.66"/></svg>,
@@ -10,18 +10,14 @@ const icons = {
     orientH: <svg id="Orient Horizontally" data-name="Orient Horizontally" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 360 220" className="svg-icon svg-icon-orient-v"><path d="M342,79H246a18,18,0,0,0-17.34,13.23L209.38,64.55l-4.83,10.28L132,40.8V31.33A18.3,18.3,0,0,0,114,13H18A18.32,18.32,0,0,0,0,31.33v26A17.75,17.75,0,0,0,18,75h96a17.72,17.72,0,0,0,18-17.67V54.06l67.45,31.63L194.64,96l33.8-2.88A18,18,0,0,0,228,97v26a17.91,17.91,0,0,0,.58,4.47l-33.94-2.9,4.81,10.28L132,166.48v-3.82A17.73,17.73,0,0,0,114,145H18A17.75,17.75,0,0,0,0,162.67v26A18.32,18.32,0,0,0,18,207h96a18.3,18.3,0,0,0,18-18.33v-8.93l72.55-34L209.37,156l19.39-27.82A18,18,0,0,0,246,141h96a18,18,0,0,0,18-18V97A18,18,0,0,0,342,79ZM120,57a6,6,0,0,1-6,6H18a6,6,0,0,1-6-6V31a6,6,0,0,1,6-6h96a6,6,0,0,1,6,6V57Zm0,132a6,6,0,0,1-6,6H18a6,6,0,0,1-6-6V163a6,6,0,0,1,6-6h96a6,6,0,0,1,6,6v26Zm228-66a6,6,0,0,1-6,6H246a6,6,0,0,1-6-6V97a6,6,0,0,1,6-6h96a6,6,0,0,1,6,6v26Z"/></svg>
 };
 
-
-const svgIcon = icon => icons[icon];
-module.exports.svgIcon = svgIcon;
-
+export const svgIcon = icon => icons[icon];
 
 // Render the icon used to collapse a panel from the title bar.
 //   collapsed: T if the icon should be rendered for the collapsed steate
 //   handlecollapse: function to call when the icon is clicked
-const collapseIcon = (collapsed, addClasses) => {
-    return (
-        <svg className={'collapsing-title-control' + (addClasses ? ' ' + addClasses : '')} data-name="Collapse Icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            {collapsed ?
+export const collapseIcon = (collapsed, addClasses) => (
+    <svg className={`collapsing-title-control ${(addClasses ? ` ${addClasses}` : '')}`} data-name="Collapse Icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        {collapsed ?
                 <g>
                     <title>Panel collapsed</title>
                     <circle className="bg" cx="256" cy="256" r="240" />
@@ -34,9 +30,6 @@ const collapseIcon = (collapsed, addClasses) => {
                     <circle className="bg" cx="256" cy="256" r="240" />
                     <line className="content-line" x1="151.87" y1="256" x2="360.13" y2="256" />
                 </g>
-            }
-        </svg>
-    );
-};
-
-module.exports.svgIcon = collapseIcon;
+        }
+    </svg>
+);
