d577108f9da1421e8ae7cec9acd6c6acda6f46bd
Keenan Graham <keenangraham@stanford.edu>
2018-05-31 14:46:11 -0700

    Add more assert statements

--- src/encoded/tests/test_file_download.py
+++ src/encoded/tests/test_file_download.py
@@ -27,6 +27,9 @@ def test_external_creds():
     creds = external_creds('mock_bucket', 'mock_object', 'mock_name')
     assert 'upload_credentials' in creds
     assert creds['bucket'] == 'mock_bucket'
+    assert creds['key'] == 'mock_object'
+    assert 'mock_name' in creds['upload_credentials']['federated_user_id']
+    assert creds['service'] == 's3'
 
 
 @mock_sts
