09d5ae1a0538b156477ce505e291c2e1ffc9d399
Tim Dreszer <tdreszer@stanford.org>
2018-06-04 16:33:53 -0700

    Updated tests for region/regulome indexing/search.

--- src/encoded/tests/test_indexing.py
+++ src/encoded/tests/test_indexing.py
@@ -87,6 +87,7 @@ def test_indexing_workbook(testapp, indexer_testapp):
     res = indexer_testapp.post_json('/index', {'record': True})
     assert res.json['indexed'] == 1
 
+    import time
     from ..loadxl import load_all
     from pkg_resources import resource_filename
     inserts = resource_filename('encoded', 'tests/data/inserts/')
@@ -95,9 +96,6 @@ def test_indexing_workbook(testapp, indexer_testapp):
     res = indexer_testapp.post_json('/index', {'record': True})
     assert res.json['updated']
     assert res.json['indexed']
-    ### OPTIONAL: audit via 2-pass is coming...
-    #assert res.json['pass2_took']
-    ### OPTIONAL: audit via 2-pass is coming...
 
     # NOTE: Both vis and region indexers are "followup" or secondary indexers
     #       and must be staged by the primary indexer
@@ -110,8 +108,20 @@ def test_indexing_workbook(testapp, indexer_testapp):
     assert res.json['title'] == 'region_indexer'
     assert res.json['indexed'] > 0
 
+    # primary indexer contents
     res = testapp.get('/search/?type=Biosample')
     assert res.json['total'] > 5
+
+    # vis indexer contents
+    res = testapp.get('/batch_hub/type%3DExperiment/hg19/trackDb.json')
+    assert res.json['ChIP']['view']['groups']['aOIDR']['type'] == 'bigNarrowPeak'
+    assert res.json['tkRNA']['vis_id'] == 'ENCSR000AEN_hg19'
+
+    # region indexer contents via region_search
+    time.sleep(1)  # For some reason this fails without some winks
+    res = testapp.get('/region-search/?region=chr13%3A61800000-78800000&genome=GRCh37')
+    assert res.json['total'] == 1
+    assert res.json['visualize_batch']['hg19']['UCSC']
 
 
 def test_indexing_simple(testapp, indexer_testapp):
