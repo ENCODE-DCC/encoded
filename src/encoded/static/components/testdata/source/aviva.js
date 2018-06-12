5559f08cdf153ff446d8070152b5f00ba13f5bfa
Keenan Graham <keenangraham@stanford.edu>
2018-06-12 13:50:19 -0700

    Update to standard_status

--- src/encoded/static/components/testdata/source/aviva.js
+++ src/encoded/static/components/testdata/source/aviva.js
@@ -4,7 +4,7 @@ module.exports = {
         "Source",
         "Item"
     ],
-    "status": "current",
+    "status": "released",
     "uuid": "fa701215-07e6-4ffe-a8f4-20356d66f3e0",
     "title": "Aviva",
     "url": "http://www.avivasysbio.com",
