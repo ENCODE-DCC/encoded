311404cde45e8903714f2f9449974b28eb506463
Keenan Graham <keenangraham@stanford.edu>
2018-06-12 11:46:37 -0700

    Move treatement to standard_statuses

--- src/encoded/tests/test_upgrade_treatment.py
+++ src/encoded/tests/test_upgrade_treatment.py
@@ -65,6 +65,18 @@ def treatment_8(treatment, document):
     return item
 
 
+@pytest.fixture
+def treatment_9(treatment, document):
+    item = treatment.copy()
+    item.update({
+        'schema_version': '9',
+        'treatment_type': 'protein',
+        'status': 'current'
+    })
+    item['treatment_term_id'] = 'UniprotKB:P03823'
+    return item
+
+
 def test_treatment_upgrade(upgrader, treatment_1):
     value = upgrader.upgrade('treatment', treatment_1, target_version='2')
     assert value['schema_version'] == '2'
@@ -75,7 +87,7 @@ def test_treatment_upgrade(upgrader, treatment_1):
 
 def test_treatment_upgrade_encode_dbxref(upgrader, treatment_1):
     treatment_1['encode2_dbxrefs'] = ["encode:hESC to endoderm differentiation treatment"]
-    value = upgrader.upgrade('treatment', treatment_1, current_version = '1', target_version='2')
+    value = upgrader.upgrade('treatment', treatment_1, current_version='1', target_version='2')
     assert value['schema_version'] == '2'
     assert 'encode2_dbxrefs' not in value
     assert value['dbxrefs'] == ['UCSC-ENCODE-cv:hESC to endoderm differentiation treatment']
@@ -83,19 +95,19 @@ def test_treatment_upgrade_encode_dbxref(upgrader, treatment_1):
 
 
 def test_treatment_upgrade_status(upgrader, treatment_2):
-    value = upgrader.upgrade('treatment', treatment_2, current_version = '2', target_version='3')
+    value = upgrader.upgrade('treatment', treatment_2, current_version='2', target_version='3')
     assert value['schema_version'] == '3'
     assert value['status'] == 'current'
 
 
 def test_treatment_upgrade_3_4(upgrader, treatment_3):
-    value = upgrader.upgrade('treatment', treatment_3, current_version = '3', target_version='4')
+    value = upgrader.upgrade('treatment', treatment_3, current_version='3', target_version='4')
     assert value['schema_version'] == '4'
     assert len(value['aliases']) == 1
 
 
 def test_treatment_upgrade_4_to_5(upgrader, treatment_4):
-    value = upgrader.upgrade('treatment', treatment_4, current_version = '4', target_version='5')
+    value = upgrader.upgrade('treatment', treatment_4, current_version='4', target_version='5')
     assert value['schema_version'] == '5'
     assert 'protocols' not in value
     assert 'documents' in value
@@ -111,3 +123,14 @@ def test_treatment_upgrade_8_to_9(upgrader, treatment_8):
     value = upgrader.upgrade('treatment', treatment_8, current_version='8', target_version='9')
     assert value['schema_version'] == '9'
     assert value['treatment_term_id'] == 'UniProtKB:P03823'
+
+
+def test_treatment_upgrade_9_10(upgrader, treatment_9):
+    value = upgrader.upgrade('treatment', treatment_9, current_version='9', target_version='10')
+    assert value['schema_version'] == '10'
+    assert value['status'] == 'released'
+    treatment_9['status'] = 'disabled'
+    treatment_9['schema_version'] == '9'
+    value = upgrader.upgrade('treatment', treatment_9, current_version='9', target_version='10')
+    assert value['schema_version'] == '10'
+    assert value['status'] == 'deleted'
