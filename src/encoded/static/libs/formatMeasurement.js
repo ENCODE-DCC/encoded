81a9ca18cdd61434c7ca81f41388947c546687fb
Philip Adenekan <zoldello@stanford.edu>
2018-06-19 09:47:10 -0700

    added return type in comment

--- src/encoded/static/libs/formatMeasurement.js
+++ src/encoded/static/libs/formatMeasurement.js
@@ -3,8 +3,10 @@
 * Takes a magnitude and unit (singular form) and returns the measurement
 * with the proper unit in singular or plural, depending on magnitude.
 * Invalid measurements are turned as- Unknown
-* @param {number} magnitude - Measured value
+* @param {string} magnitude - Measured value, range or unknown
 * @param {string} unit - Measurement system in singular form
+* @return {string} - Measurement with singular or plural unit
+* (depending on value/range) or "Unknown" without unit
 */
 const formatMeasurement = (magnitude, unit) => {
     if (!magnitude || (magnitude.toLowerCase && magnitude.toLowerCase()) === 'unknown') {
