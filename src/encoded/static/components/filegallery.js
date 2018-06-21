4291f3da3b788c2461559501b63b538ebb2762ed
Philip Adenekan <zoldello@stanford.edu>
2018-06-20 19:44:05 -0700

    cleaned up code to not use omit

--- src/encoded/static/components/filegallery.js
+++ src/encoded/static/components/filegallery.js
@@ -170,26 +170,15 @@ export class FileTable extends React.Component {
                 return 'proc';
             });
 
-            const procTableColumns = showReplicateNumber ?
-              FileTable.procTableColumns :
-              _.omit(FileTable.procTableColumns, 'biological_replicates');
-
-            // filesRaw should be falsy if files.raw is
-            const filesRaw = showReplicateNumber ?
-              files.raw :
-              _.omit(files.raw, 'biological_replicates');
-
-            // filesRawArray should be falsy if files.rawArray is
-            const filesRawArray = showReplicateNumber ?
-                files.rawArray :
-                _.omit(files.rawArray, 'biological_replicates');
+            // showReplicateNumber is NOT(!) -ed so it is in harmony with hide function
+            FileTable.procTableColumns.biological_replicates.hide = () => !showReplicateNumber;
 
             return (
                 <div>
                     {showFileCount ? <div className="file-gallery-counts">Displaying {filteredCount} of {unfilteredCount} files</div> : null}
                     <SortTablePanel header={filePanelHeader} noDefaultClasses={this.props.noDefaultClasses}>
                         <RawSequencingTable
-                            files={filesRaw}
+                            files={files.raw}
                             meta={{
                                 encodevers,
                                 replicationType: context.replication_type,
@@ -201,7 +190,7 @@ export class FileTable extends React.Component {
                             }}
                         />
                         <RawFileTable
-                            files={filesRawArray}
+                            files={files.rawArray}
                             meta={{
                                 encodevers,
                                 replicationType: context.replication_type,
@@ -223,7 +212,7 @@ export class FileTable extends React.Component {
                             rowClasses={this.rowClasses}
                             collapsed={this.state.collapsed.proc}
                             list={files.proc}
-                            columns={procTableColumns}
+                            columns={FileTable.procTableColumns}
                             sortColumn={showReplicateNumber ? 'biological_replicates' : 'date_created'}
                             meta={{
                                 encodevers,
