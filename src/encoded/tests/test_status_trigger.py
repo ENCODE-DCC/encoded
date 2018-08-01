dc74d914839df8704dc3cebfb583f46756c7c40c
Keenan Graham <keenangraham@stanford.edu>
2018-07-31 18:31:13 -0700

    Test force_transition, block_children default

--- src/encoded/tests/test_status_trigger.py
+++ src/encoded/tests/test_status_trigger.py
@@ -473,3 +473,48 @@ def test_set_status_changed_paths_experiment_rep_and_in_progress_file_block_chil
     res = testapp.patch_json(experiment['@id'] + '@@set_status?force_audit=true&block_children=true', {'status': 'released'}, status=200)
     assert len(res.json_body['changed']) == 1
     assert len(res.json_body['considered']) == 1
+
+
+@mock_sts
+@mock_s3
+def test_set_status_force_transition_block_children_default(testapp, experiment, file, replicate, dummy_request, root):
+    import boto3
+    client = boto3.client('s3')
+    client.create_bucket(Bucket='test_upload_bucket')
+    # Generate creds.
+    testapp.patch_json(file['@id'], {'status': 'uploading'})
+    dummy_request.registry.settings['file_upload_bucket'] = 'test_upload_bucket'
+    testapp.post_json(file['@id'] + '@@upload', {})
+    # Get bucket name and key.
+    file_item = root.get_by_uuid(file['uuid'])
+    external = file_item._get_external_sheet()
+    # Put mock object in bucket.
+    client.put_object(Body=b'ABCD', Key=external['key'], Bucket=external['bucket'])
+    # Set to in progress.
+    testapp.patch_json(file['@id'], {'status': 'released'})
+    res = testapp.patch_json(experiment['@id'] + '@@set_status?force_audit=true&force_transition=true', {'status': 'released'}, status=200)
+    assert len(res.json_body['changed']) == 1
+    assert len(res.json_body['considered']) == 1
+
+
+@mock_sts
+@mock_s3
+def test_set_status_force_transition_block_children_specified(testapp, experiment, file, replicate, dummy_request, root):
+    import boto3
+    client = boto3.client('s3')
+    client.create_bucket(Bucket='test_upload_bucket')
+    # Generate creds.
+    testapp.patch_json(file['@id'], {'status': 'uploading'})
+    dummy_request.registry.settings['file_upload_bucket'] = 'test_upload_bucket'
+    testapp.post_json(file['@id'] + '@@upload', {})
+    # Get bucket name and key.
+    file_item = root.get_by_uuid(file['uuid'])
+    external = file_item._get_external_sheet()
+    # Put mock object in bucket.
+    client.put_object(Body=b'ABCD', Key=external['key'], Bucket=external['bucket'])
+    # Set to in progress.
+    testapp.patch_json(file['@id'], {'status': 'released'})
+    res = testapp.patch_json(experiment['@id'] + '@@set_status?force_audit=true&force_transition=true&block_children=false', {'status': 'replaced'}, status=200)
+    assert len(res.json_body['changed']) == 2
+    assert len(res.json_body['considered']) == 2
+
