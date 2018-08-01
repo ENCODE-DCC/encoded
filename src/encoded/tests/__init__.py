40bc8cbefee37ee3aa6946b7af140d244472c5e0
Casey Litton <caseylitton@gmail.com>
2018-07-31 17:35:40 -0700

    ENCD-4149 Allow bin test log arg for workbook loading (#2351)

--- src/encoded/tests/__init__.py
+++ src/encoded/tests/__init__.py
@@ -12,3 +12,4 @@ def pytest_addoption(parser):
     parser.addoption('--browser-arg', nargs=2, dest='browser_args', action='append', type='string')
     parser.addoption('--browser-arg-int', nargs=2, dest='browser_args', action=AppendInt2, type='string')
     parser.addoption('--wsgi-arg', nargs=2, dest='wsgi_args', action='append', type='string')
+    parser.addoption("--log", action="store", default="INFO", help="set logging level")
