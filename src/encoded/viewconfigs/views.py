780a4cb8686a9f99a73c2e18b7c790e496223488
Philip Adenekan <zoldello@gmail.com>
2018-10-01 14:59:51 -0700

    moved download_tsv to encoded

--- src/encoded/viewconfigs/views.py
+++ src/encoded/viewconfigs/views.py
@@ -80,7 +80,9 @@ def search(context, request, search_type=None, return_generator=False):
 @view_config(route_name='report', request_method='GET', permission='search')
 def report(context, request):
     report = ReportView(context, request)
-    return report.preprocess_view()
+    process_view = report.preprocess_view()
+    process_view['download_tsv'] = request.route_path('report_download') + report.search_base
+    return process_view
 
 
 @view_config(route_name='news', request_method='GET', permission='search')
