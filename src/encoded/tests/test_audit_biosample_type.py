14c452007a729e8a24c4d55deca61dc689d289e5
Yunhai Luo <yunhail@stanford.edu>
2018-11-21 11:37:23 -0800

    ENCD-4319 update tests

--- src/encoded/tests/test_audit_biosample_type.py
+++ src/encoded/tests/test_audit_biosample_type.py
@@ -31,20 +31,26 @@ def inconsistent_biosample_type(testapp):
     return testapp.post_json('/biosample-types', item, status=201).json['@graph'][0]
 
 
-def test_audit_biosample_type_term(testapp,
-                                   ntr_biosample_type,
-                                   id_nonexist_biosample_type,
-                                   inconsistent_biosample_type):
+def test_audit_none(testapp, biosample_type):
+    res = testapp.get(biosample_type['@id'] + '@@index-data')
+    assert res.json['audit'] == {}
+
+
+def test_audit_ntr_biosample(testapp, ntr_biosample_type):
     res = testapp.get(ntr_biosample_type['@id'] + '@@index-data')
     errors = res.json['audit']
     assert len(errors) == len(errors['INTERNAL_ACTION']) == 1
     assert errors['INTERNAL_ACTION'][0]['category'] == 'NTR biosample'
 
+
+def test_audit_not_in_ontology(testapp, id_nonexist_biosample_type):
     res = testapp.get(id_nonexist_biosample_type['@id'] + '@@index-data')
     errors = res.json['audit']
     assert len(errors) == len(errors['INTERNAL_ACTION']) == 1
     assert errors['INTERNAL_ACTION'][0]['category'] == 'term_id not in ontology'
 
+
+def test_audit_inconsistent_type(testapp, inconsistent_biosample_type):
     res = testapp.get(inconsistent_biosample_type['@id'] + '@@index-data')
     errors = res.json['audit']
     assert len(errors) == len(errors['ERROR']) == 1
