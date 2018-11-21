167f04fe1f7e754c9de40bb0474cf3955bfbf3bc
Keenan Graham <keenangraham@stanford.edu>
2018-11-21 00:15:59 -1000

    Try get or create cart view

--- src/encoded/types/cart.py
+++ src/encoded/types/cart.py
@@ -2,9 +2,13 @@ from snovault import (
     collection,
     load_schema,
 )
+from snovault import COLLECTIONS
+from snovault.crud_views import create_item
+from pyramid.httpexception import HTTPBadRequest
 from pyramid.security import (
     Allow,
 )
+from pyramid.view import view_config
 from .base import (
     Item,
     DELETED,
@@ -20,7 +24,7 @@ from .base import (
         'description': 'Listing of cart contents',
     },
     acl=[
-        (Allow, 'group.submitter', 'save_carts'),
+        (Allow, 'group.submitter', 'save-carts'),
     ]
 )
 class Cart(Item):
@@ -38,3 +42,47 @@ class Cart(Item):
         return {
             owner: 'role.owner'
         }
+
+    class Collection(Item.Collection):
+        pass
+
+
+def _get_carts_by_user(request, userid):
+    return [
+        c['@id']
+        for c in request.embed('/carts/?datastore=database&limit=all&remove=elements')
+        if c['submitted_by'] == '/users/{}/'.format(userid)
+    ]
+
+
+def _create_cart(request, user):
+    carts = request.registry[COLLECTIONS]['carts']
+    cart = create_item(
+        carts.type_info,
+        request,
+        {
+            'submitted_by': user.path,
+            'status': 'current',
+            'name': '{} cart'.format(user.properties['title'])
+        }
+    )
+    return cart.path
+
+
+@view_config(context=Cart.Collection, request_method='GET', permission='save-carts')
+def get_or_create_cart_by_user(context, request):
+    userid = request.authenticated_userid
+    if not userid or '.' not in userid:
+        raise HTTPBadRequest()
+    user = request.registry[COLLECTIONS]['users'][userid.split('.')[0]]
+    if not user:
+        raise HTTPBadRequest()
+    carts = _get_carts_by_user(request, userid)
+    if not carts:
+        cart = _create_cart(user)
+    request.response.status = 200
+    return {
+        'status': 'success',
+        '@type': ['result'],
+        '@graph': carts or [cart]
+    }
