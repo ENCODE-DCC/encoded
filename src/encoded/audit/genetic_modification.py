b9db55f2950cabb17c36b345a6de6bfa0a45ceac
Paul Sud <paulsud@stanford.edu>
2018-11-28 11:06:43 -0800

    reorganize after code review, clarify docstring, and add RVD_sequence_pairs check for TALENs

--- src/encoded/audit/genetic_modification.py
+++ src/encoded/audit/genetic_modification.py
@@ -9,12 +9,11 @@ def audit_genetic_modification_reagents(value, system):
     '''
     Genetic modifications with missing reagents will be flagged by this audit.
     Modifications with acceptable substitutes for reagents will not be flagged,
-    depending on their method. See the method dependecy in file.json for more
-    detail.
+    depending on their method. See the method dependency description in
+    file.json for more detail.
     '''
-    missing_reagents = False
-
     if not value.get('reagents'):
+        missing_reagents = False
         method = value['method']
         if method == 'CRISPR':
             if not value.get('guide_rna_sequences'):
@@ -23,12 +22,14 @@ def audit_genetic_modification_reagents(value, system):
             if not value.get('rnai_sequences'):
                 missing_reagents = True
         elif method == 'TALEN':
-            missing_reagents = True
+            if not value.get('RVD_sequence_pairs'):
+                missing_reagents = True
         elif not value.get('documents'):
             missing_reagents = True
 
-    if missing_reagents:
-        detail = 'Genetic modification {} of method {} is missing reagents'.format(
-            value['@id'],
-            method)
-        yield AuditFailure('missing genetic modification reagents', detail, level='ERROR')
+        if missing_reagents:
+            detail = 'Genetic modification {} of method {} is missing reagents'.format(
+                value['@id'],
+                method,
+            )
+            yield AuditFailure('missing genetic modification reagents', detail, level='ERROR')
