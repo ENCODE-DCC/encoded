df7bf112491eb5ed4a4233a8c41cac3a1142b4a7
Philip Adenekan <zoldello@gmail.com>
2018-12-03 18:48:40 -0800

    move query and matrix from baseview and made instant variable

--- src/encoded/viewconfigs/matrix.py
+++ src/encoded/viewconfigs/matrix.py
@@ -32,23 +32,21 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
         'format', 'frame', 'datastore', 'field', 'region', 'genome',
         'sort', 'from', 'referrer',
     ]
+
     def __init__(self, context, request):
         super(MatrixView, self).__init__(context, request)
-        if self._view_name == 'matrix':  # hack
-            self._result['matrix'] = ''
-            self._matrix = ''
         self._view_item = View_Item(request, self._search_base)
         self._facets = []
         self._schema = None
 
     @staticmethod
     def _set_result_title(type_info):
-        '''Helper function for class and child classes'''
+        """Set class and child classes."""
         title = type_info.name + ' matrix'
         return title
 
-    def _construct_query(self):
-        '''Helper method for preprocessing view'''
+    def _construct_query(self, search_result, matrix):
+        """Preprocess query."""
         search_fields, _ = get_search_fields(self._request, self._doc_types)
         query = get_filtered_query(
             self._search_term,
@@ -63,7 +61,7 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
             query['query']['query_string']['fields'].extend(
                 ['_all', '*.uuid', '*.md5sum', '*.submitted_file_name']
             )
-        used_filters = self._set_query_aggs(query)
+        used_filters = self._set_query_aggs(query, search_result, matrix)
         return query, used_filters
 
     def _construct_result_views(self, type_info):
@@ -76,10 +74,10 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
             views.append(self._view_item.summary_report)
         return views
 
-    def _construct_xygroupings(self, query, filters, negative_filters):
-        '''Helper Method for constructing query'''
-        x_grouping = self._matrix['x']['group_by']
-        y_groupings = self._matrix['y']['group_by']
+    def _construct_xygroupings(self, query, filters, negative_filters, matrix):
+        """Construct xy group."""
+        x_grouping = matrix['x']['group_by']
+        y_groupings = matrix['y']['group_by']
         x_agg = {
             "terms": {
                 "field": 'embedded.' + x_grouping,
@@ -109,7 +107,7 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
         }
 
     def _validate_items(self, type_info):
-        '''Helper function for class and child classes'''
+        """Validate items."""
         msg = None
         if len(self._doc_types) != 1:
             msg = (
@@ -128,14 +126,14 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
         if msg:
             raise HTTPBadRequest(explanation=msg)
 
-    def _set_query_aggs(self, query):
-        '''Helper Method for constructing query'''
+    def _set_query_aggs(self, query, search_result, matrix):
+        """Construct facets of search query."""
         query_filters = query['post_filter'].pop('bool')
         filter_collector = {'post_filter': {'bool': query_filters}}
         used_filters = set_filters(
             self._request,
             filter_collector,
-            self._result,
+            search_result,
             filter_exclusion=self._filter_exclusion
         )
         filters = filter_collector['post_filter']['bool']['must']
@@ -144,38 +142,50 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
             (field, facet)
             for field, facet in self._schema['facets'].items()
             if (
-                field in self._matrix['x']['facets'] or
-                field in self._matrix['y']['facets']
+                field in matrix['x']['facets'] or
+                field in matrix['y']['facets']
                 )
         ]
-
-        # Display all audits if logged in, or all but INTERNAL_ACTION if logged out
+        # Show all audits if logged in or all but INTERNAL_ACTION if logged out
         for audit_facet in self._audit_facets:
             if (
                     self._search_audit and
                     'group.submitter' in self._principals or
                     'INTERNAL_ACTION' not in audit_facet[0]
-                ):
+            ):
                 self._facets.append(audit_facet)
-
         query['aggs'] = set_facets(
             self._facets,
             used_filters,
             self._principals,
             self._doc_types
         )
-        self._construct_xygroupings(query, filters, negative_filters)
+        self._construct_xygroupings(query, filters, negative_filters, matrix)
         return used_filters
 
-    def _summarize_buckets(self, x_buckets, outer_bucket, grouping_fields):
+    def _summarize_buckets(
+            self,
+            x_buckets,
+            outer_bucket,
+            grouping_fields,
+            matrix
+    ):
+        """
+        Set bucket summary.
+
+            :param x_buckets: x buckets
+            :param outer_bucket: Outer bucket
+            :param grouping_fields: Grouping fields
+            :param matrix: Matrix
+        """
         group_by = grouping_fields[0]
         grouping_fields = grouping_fields[1:]
         if not grouping_fields:
             counts = {}
             for bucket in outer_bucket[group_by]['buckets']:
                 doc_count = bucket['doc_count']
-                if doc_count > self._matrix['max_cell_doc_count']:
-                    self._matrix['max_cell_doc_count'] = doc_count
+                if doc_count > matrix['max_cell_doc_count']:
+                    matrix['max_cell_doc_count'] = doc_count
                 counts[bucket['key']] = doc_count
             summary = []
             for bucket in x_buckets:
@@ -183,14 +193,22 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
             outer_bucket[group_by] = summary
         else:
             for bucket in outer_bucket[group_by]['buckets']:
-                self._summarize_buckets(x_buckets, bucket, grouping_fields)
+                self._summarize_buckets(
+                    x_buckets,
+                    bucket,
+                    grouping_fields,
+                    matrix
+                )
 
     def preprocess_view(self):
-        """Construct query and build view results json."""
+        """Construct query and build view search result json."""
         matrix_route = self._request.route_path('matrix', slash='/')
-        self._result['@id'] = matrix_route + self._search_base
-        self._result['@type'] = ['Matrix']
-        self._result['notification'] = ''
+        search_result = {}
+        search_result['@context'] = self._request.route_path('jsonld_context')
+        search_result['filters'] = []
+        search_result['@id'] = matrix_route + self._search_base
+        search_result['@type'] = ['Matrix']
+        search_result['notification'] = ''
         # TODO: Validate doc types in base class in one location
         # Now we do it here and in _validate_items
         type_info = None
@@ -199,22 +217,26 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
                 type_info = self._types[self._doc_types[0]]
                 self._schema = type_info.schema
         self._validate_items(type_info)
-        self._result['title'] = self._set_result_title(type_info)
-        self._result['matrix'] = type_info.factory.matrix.copy()
-        self._matrix = self._result['matrix']
-        self._matrix['x']['limit'] = self._request.params.get('x.limit', 20)
-        self._matrix['y']['limit'] = self._request.params.get('y.limit', 5)
+        search_result['title'] = self._set_result_title(type_info)
+        search_result['matrix'] = type_info.factory.matrix.copy()
+        matrix = {}
+        matrix = search_result['matrix']
+        matrix['x']['limit'] = self._request.params.get('x.limit', 20)
+        matrix['y']['limit'] = self._request.params.get('y.limit', 5)
         search_route = self._request.route_path('search', slash='/')
-        self._matrix['search_base'] = search_route + self._search_base
-        self._matrix['clear_matrix'] = matrix_route + '?type=' + self._doc_types[0]
-        self._result['views'] = self._construct_result_views(type_info)
-        query, used_filters = self._construct_query()
-        es_results = self._elastic_search.search(body=query, index=self._es_index)
+        matrix['search_base'] = search_route + self._search_base
+        matrix['clear_matrix'] = matrix_route + '?type=' + self._doc_types[0]
+        search_result['views'] = self._construct_result_views(type_info)
+        query, used_filters = self._construct_query(search_result, matrix)
+        es_results = self._elastic_search.search(
+            body=query,
+            index=self._es_index
+        )
         aggregations = es_results['aggregations']
         total = aggregations['matrix']['doc_count']
-        self._result['matrix']['doc_count'] = total
-        self._result['matrix']['max_cell_doc_count'] = 0
-        self._result['facets'] = self._format_facets(
+        search_result['matrix']['doc_count'] = total
+        search_result['matrix']['max_cell_doc_count'] = 0
+        search_result['facets'] = self._format_facets(
             es_results,
             self._facets,
             used_filters,
@@ -222,26 +244,27 @@ class MatrixView(BaseView):  #pylint: disable=too-few-public-methods
             total,
             self._principals
         )
-        x_grouping = self._matrix['x']['group_by']
-        y_groupings = self._matrix['y']['group_by']
+        x_grouping = matrix['x']['group_by']
+        y_groupings = matrix['y']['group_by']
         self._summarize_buckets(
             aggregations['matrix']['x']['buckets'],
             aggregations['matrix'],
-            y_groupings + [x_grouping]
+            y_groupings + [x_grouping],
+            matrix
         )
-        self._result['matrix']['y'][y_groupings[0]] = aggregations['matrix'][y_groupings[0]]
-        self._result['matrix']['x'].update(aggregations['matrix']['x'])
-        self._result.update(
+        search_result['matrix']['y'][y_groupings[0]] = aggregations['matrix'][y_groupings[0]]
+        search_result['matrix']['x'].update(aggregations['matrix']['x'])
+        search_result.update(
             search_result_actions(
                 self._request,
                 self._doc_types,
                 es_results
             )
         )
-        self._result['total'] = es_results['hits']['total']
-        if self._result['total']:
-            self._result['notification'] = 'Success'
+        search_result['total'] = es_results['hits']['total']
+        if search_result['total']:
+            search_result['notification'] = 'Success'
         else:
             self._request.response.status_code = 404
-            self._result['notification'] = 'No results found'
-        return self._result
+            search_result['notification'] = 'No results found'
+        return search_result
