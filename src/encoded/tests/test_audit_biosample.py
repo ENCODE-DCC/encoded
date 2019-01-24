2aff0ba3059568601e5f7acc8a4cc772813ef0ff
Paul Sud <paulsud@stanford.edu>
2019-01-24 10:21:10 -0800

    add test for is_part_of helper function

--- src/encoded/tests/test_audit_biosample.py
+++ src/encoded/tests/test_audit_biosample.py
@@ -51,6 +51,49 @@ def base_chipmunk(testapp):
     return testapp.post_json('/organism', item, status=201).json['@graph'][0]
 
 
+@pytest.fixture
+def ontology():
+    ontology = {
+        'UBERON:0002469': {
+            'part_of': [
+                'UBERON:0001043',
+                'UBERON:0001096',
+                'UBERON:1111111'
+            ]
+        },
+        'UBERON:1111111': {
+            'part_of': []
+        },
+        'UBERON:0001096': {
+            'part_of': []
+        },
+        'UBERON:0001043': {
+            'part_of': [
+                'UBERON:0001007',
+                'UBERON:0004908'
+            ]
+        },
+        'UBERON:0001007': {
+            'part_of': []
+        },
+        'UBERON:0004908': {
+            'part_of': [
+                'UBERON:0001043',
+                'UBERON:1234567'
+            ]
+        },
+        'UBERON:1234567': {
+            'part_of': [
+                'UBERON:0006920'
+            ]
+        },
+        'UBERON:0006920': {
+            'part_of': []
+        }
+    }
+    return ontology
+
+
 def test_audit_biosample_modifications_whole_organism(
         testapp, base_biosample,
         fly_donor, fly, construct_genetic_modification,
@@ -303,3 +346,10 @@ def test_audit_biosample_depleted_in_term_name(testapp, base_biosample, single_c
     assert any(error['category'] == target_err_cat
                for error_cat in errors.values()
                for error in error_cat)
+
+
+def test_is_part_of(ontology):
+    from encoded.audit.biosample import is_part_of
+    assert is_part_of('UBERON:0002469', 'UBERON:0001007', ontology)
+    assert is_part_of('UBERON:0002469', 'UBERON:0006920', ontology)
+    assert not is_part_of('UBERON:1111111', 'UBERON:0001043', ontology)
