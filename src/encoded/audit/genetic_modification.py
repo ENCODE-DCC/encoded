cab11e94540526a4332eda9a4120d108f111297c
Idan Gabdank <gabdank@stanford.edu>
2019-01-31 23:53:54 -0800

    exclude RNAi from reagents audit

--- src/encoded/audit/genetic_modification.py
+++ src/encoded/audit/genetic_modification.py
@@ -3,8 +3,14 @@ from snovault import (
     audit_checker,
 )
 
-
-@audit_checker('GeneticModification', frame='object')
+@audit_checker('GeneticModification', 
+               frame=['object',
+                      'biosamples_modified',
+                      'biosamples_modified.organism',
+                      'donors_modified',
+                      'donors_modified.organism',
+                     ]
+                )
 def audit_genetic_modification_reagents(value, system):
     '''
     Genetic modifications with missing reagents will be flagged by this audit.
@@ -12,6 +18,21 @@ def audit_genetic_modification_reagents(value, system):
     depending on their method. See the method dependency description in
     file.json for more detail.
     '''
+    flies = ['ab546d43-8e2a-4567-8db7-a217e6d6eea0',
+             '5be68469-94ba-4d60-b361-dde8958399ca',
+             '74144f1f-f3a6-42b9-abfd-186a1ca93198',
+             'c3cc08b7-7814-4cae-a363-a16b76883e3f',
+             'd1072fd2-8374-4f9b-85ce-8bc2c61de122',
+             'b9ce90a4-b791-40e9-9b4d-ffb1c6a5aa2b',
+             '0bdd955a-57f0-4e4b-b93d-6dd1df9b766c']
+    if value['method'] == 'RNAi' and 'biosamples_modified' in value:
+        for biosample in value['biosamples_modified']:
+            if biosample['organism']['uuid'] in flies:
+                return
+    if value['method'] == 'RNAi' and 'donors_modified' in value:
+        for donor in value['donors_modified']:
+            if donor['organism']['uuid'] in flies:
+                return
     if not value.get('reagents'):
         missing_reagents = False
         method = value['method']
