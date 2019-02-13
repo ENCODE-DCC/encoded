b32e03469e11b573e2668e9257904d8f955dc7c1
Paul Sud <paulsud@stanford.edu>
2019-02-13 09:37:32 -0800

    clean up duplicated test

--- src/encoded/tests/test_audit_genetic_modification.py
+++ src/encoded/tests/test_audit_genetic_modification.py
@@ -2,25 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def genetic_modification(testapp, lab, award):
-    item = {
-        'award': award['@id'],
-        'lab': lab['@id'],
-        'modified_site_by_coordinates': {
-            'assembly': 'GRCh38',
-            'chromosome': '11',
-            'start': 20000,
-            'end': 21000
-        },
-        'purpose': 'repression',
-        'category': 'deletion',
-        'method': 'CRISPR',
-        'zygosity': 'homozygous'
-    }
-    return testapp.post_json('/genetic_modification', item).json['@graph'][0]
-
-
-@pytest.fixture
 def genetic_modification_RNAi(testapp, lab, award):
     item = {
         'award': award['@id'],
@@ -49,6 +30,7 @@ def tagged_target(testapp, gene):
     return testapp.post_json('/target', item, status=201).json['@graph'][0]
 
 
+@pytest.fixture
 def genetic_modification_source(testapp, lab, award, source, gene):
     item = {
         'lab': lab['@id'],
@@ -112,6 +94,10 @@ def test_audit_reagent_source(testapp, source, genetic_modification_source):
 
 def test_genetic_modification_reagents_fly(testapp, genetic_modification_RNAi, fly_donor):
     res = testapp.get(genetic_modification_RNAi['@id'] + '@@index-data')
+    errors = res.json['audit']
+    errors_list = []
+    for error_type in errors:
+        errors_list.extend(errors[error_type])
     assert any(error['category'] == 'missing genetic modification reagents' for
                error in errors_list)
     testapp.patch_json(fly_donor['@id'], {'genetic_modifications': [genetic_modification_RNAi['@id']]})
