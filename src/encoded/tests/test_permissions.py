cae79fc83caeebc4481909c5b6e57a81a2809682
Idan Gabdank <gabdank@stanford.edu>
2019-02-13 11:54:54 -0800

    added dependency tests

--- src/encoded/tests/test_permissions.py
+++ src/encoded/tests/test_permissions.py
@@ -140,7 +140,7 @@ def test_submitter_post_other_lab(submitter_testapp, other_lab, award, cell_free
     assert "not in user submits_for" in res.json['errors'][0]['description']
 
 
-def test_reviewer_patch_other_lab_characterization(submitter_testapp, testapp, submitter, attachment, construct_genetic_modification_N, other_lab, lab, award):
+def test_not_reviewer_patch_other_lab_characterization(submitter_testapp, testapp, submitter, attachment, construct_genetic_modification_N, other_lab, lab, award):
     testapp.patch_json(submitter['@id'], {'submits_for': [other_lab['@id']]}),
     item = {
         'characterizes': construct_genetic_modification_N['@id'],
@@ -159,13 +159,23 @@ def test_reviewer_patch_other_lab_characterization(submitter_testapp, testapp, s
                     'reviewed_by': submitter['@id'],
                     'status': 'compliant'}}, expect_errors=True)
     assert "not in user submits_for" in res.json['errors'][0]['description']
-    
-    testapp.patch_json(gm['@id'],
-        {'review': {'lab': other_lab['@id']}})
-    res = submitter_testapp.patch_json(
+
+
+def test_reviewer_patch_other_lab_characterization(submitter_testapp, testapp, submitter, attachment, construct_genetic_modification_N, other_lab, lab, award):
+    testapp.patch_json(submitter['@id'], {'submits_for': [other_lab['@id']]}),
+    item = {
+        'characterizes': construct_genetic_modification_N['@id'],
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'attachment': attachment,
+        'review': {
+            'lab': other_lab['@id']
+        }
+    }
+    gm = testapp.post_json('/genetic_modification_characterization', item).json['@graph'][0]
+    submitter_testapp.patch_json(
         gm['@id'],
         {'review': {'lab': other_lab['@id'], 'reviewed_by': submitter['@id'], 'status': 'compliant'}}, status=200)
-
 
 
 def test_wrangler_post_other_lab(wrangler_testapp, other_lab, award, cell_free):
