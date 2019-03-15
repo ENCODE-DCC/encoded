4e5bf38ac7b635fd75353b1264e71396acabb0ba
Idan Gabdank <gabdank@stanford.edu>
2019-03-15 13:41:38 -0700

    single quote to double quote

--- src/encoded/tests/test_schema_quality_metric.py
+++ src/encoded/tests/test_schema_quality_metric.py
@@ -5,15 +5,15 @@ TAR_BALL = "data:application/x-tar;base64,dG1wLwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 @pytest.fixture
 def generic_quality_metric(analysis_step_run, file, award, lab):
     return {
-        'award': award['uuid'],
-        'lab': lab['uuid'],
-        'name': 'Generic QC',
-        'step_run': analysis_step_run['uuid'],
-        'quality_metric_of': [file['uuid']],
-        'attachment': {
-            'download': 'test.tgz',
-            'type': 'application/x-tar',
-            'href': TAR_BALL
+        "award": award["uuid"],
+        "lab": lab["uuid"],
+        "name": "Generic QC",
+        "step_run": analysis_step_run["uuid"],
+        "quality_metric_of": [file["uuid"]],
+        "attachment": {
+            "download": "test.tgz",
+            "type": "application/x-tar",
+            "href": TAR_BALL
         }
     }
 
@@ -21,27 +21,27 @@ def generic_quality_metric(analysis_step_run, file, award, lab):
 @pytest.fixture
 def micro_rna_quality_metric(analysis_step_run, file, award, lab, replicate_1_1):
     return {
-        'award': award['uuid'],
-        'lab': lab['uuid'],
-        'step_run': analysis_step_run['uuid'],
-        'quality_metric_of': [file['uuid']],
-        'miRNA_alignments': [{
-                'replicate': replicate_1_1['uuid'],
-                'aligned_reads': 100
+        "award": award["uuid"],
+        "lab": lab["uuid"],
+        "step_run": analysis_step_run["uuid"],
+        "quality_metric_of": [file["uuid"]],
+        "miRNA_alignments": [{
+                "replicate": replicate_1_1["uuid"],
+                "aligned_reads": 100
         }],
-        'miRNA_expression' : [{
-                'replicate': replicate_1_1['uuid'],
-                'expressed_mirnas': 100
+        "miRNA_expression" : [{
+                "replicate": replicate_1_1["uuid"],
+                "expressed_mirnas": 100
         }]
     }
 
 
 def test_tarball_attachment(testapp, generic_quality_metric):
     # Make sure *.tar.gz and *.tgz files are accepted by the attachment property
-    res = testapp.post_json('/generic_quality_metric', generic_quality_metric, expect_errors=False)
+    res = testapp.post_json("/generic_quality_metric", generic_quality_metric, expect_errors=False)
     assert res.status_code == 201
 
 
 def test_micro_rna_quality_metric(testapp, micro_rna_quality_metric):
-    res = testapp.post_json('/micro_rna_quality_metric', micro_rna_quality_metric, expect_errors=False)
+    res = testapp.post_json("/micro_rna_quality_metric", micro_rna_quality_metric, expect_errors=False)
     assert res.status_code == 201
