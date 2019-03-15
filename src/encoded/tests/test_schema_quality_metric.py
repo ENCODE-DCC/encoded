5f9d4cc5d2d2143d837d859b068ed94bbd7bff03
Idan Gabdank <gabdank@stanford.edu>
2019-03-15 13:39:35 -0700

    adding insert

--- src/encoded/tests/test_schema_quality_metric.py
+++ src/encoded/tests/test_schema_quality_metric.py
@@ -25,14 +25,14 @@ def micro_rna_quality_metric(analysis_step_run, file, award, lab, replicate_1_1)
         'lab': lab['uuid'],
         'step_run': analysis_step_run['uuid'],
         'quality_metric_of': [file['uuid']],
-       'miRNA_alignments': [{
-            'replicate': replicate_1_1['uuid'],
-            'aligned_reads': 100
-       }],
-       'miRNA_expression' : [{
-            'replicate': replicate_1_1['uuid'],
-            'expressed_mirnas': 100
-       }]
+        'miRNA_alignments': [{
+                'replicate': replicate_1_1['uuid'],
+                'aligned_reads': 100
+        }],
+        'miRNA_expression' : [{
+                'replicate': replicate_1_1['uuid'],
+                'expressed_mirnas': 100
+        }]
     }
 
 
