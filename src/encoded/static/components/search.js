6c43536489197a01fbdd7af9887edaf8f1f17d5c
Emma O'Neill <emmaruthoneill@gmail.com>
2019-03-18 13:31:55 -0700

    ENCD-4084 date selector facets

--- src/encoded/static/components/search.js
+++ src/encoded/static/components/search.js
@@ -735,6 +735,289 @@ TypeTerm.propTypes = {
     total: PropTypes.number.isRequired,
 };
 
+class DateSelectorFacet extends React.Component {
+    constructor() {
+        super();
+
+        // Set initial React component state.
+        this.state = {
+            possibleYears: [], // all years with results
+            possibleMonths: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'], // all months are allowed
+            startYears: [], // possible years for the start year drop-down
+            endYears: [], // possible years for the end year drop-down
+            startMonths: [], // possible months for the start month drop-down
+            endMonths: [], // possible months for the end month drop-down
+            startYear: undefined, // chosen start year
+            startMonth: undefined, // chosen start month
+            endYear: undefined, // chosen end year
+            endMonth: undefined, // chosen end month
+            currentYear: moment().format('YYYY'), // current year
+            currentMonth: moment().format('MM'), // current month
+            currentDay: moment().format('DD'), // current day
+            activeFacet: 'date_released', // for toggle, either 'date_released' or 'date_submitted'
+        };
+
+        this.selectYear = this.selectYear.bind(this);
+        this.selectMonth = this.selectMonth.bind(this);
+        this.checkForSameYear = this.checkForSameYear.bind(this);
+        this.toggleDateFacet = this.toggleDateFacet.bind(this);
+        this.setActiveFacetParameters = this.setActiveFacetParameters.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.resetMonthDropDowns = this.resetMonthDropDowns.bind(this);
+    }
+
+    componentDidMount() {
+        this.setActiveFacetParameters();
+    }
+
+    setActiveFacetParameters() {
+        // Filter 'facets' to get selected 'facet' object (either 'date_released' or 'date_submitted' object)
+        const activeFacet = this.props.facets.filter(f => f.field === this.state.activeFacet)[0];
+        // Sorted terms for the selected facet object
+        const activeFacetTerms = _.sortBy(activeFacet.terms, obj => moment(obj.key, 'YYYY-MM-DD').toISOString());
+
+        // All possible years with results
+        const possibleYears = activeFacetTerms.map(term => term.key.split('-')[0]).filter((item, i, ar) => (ar.indexOf(item) === i));
+        this.setState({ possibleYears });
+
+        // Set dropdown lists to be full lists of possiblities
+        this.setState({ startYears: possibleYears });
+        this.setState({ endYears: possibleYears });
+        this.setState({ startMonths: this.state.possibleMonths });
+        this.setState({ endMonths: this.state.possibleMonths });
+
+        // Set dropdowns to be initialized to boundaries of full range
+        this.setState({ startYear: possibleYears[0] });
+        this.setState({ endYear: possibleYears[possibleYears.length - 1] });
+        this.setState({ startMonth: '01' });
+        this.setState({ endMonth: '12' });
+    }
+
+    selectYear(event) {
+        // We are changing the start year, which means we need to change the possibilities for the end years and also the possible start months
+        if (event.target.id === 'select-start-year') {
+            // Set startYear to be user choice
+            this.setState({ startYear: event.target.value }, () => {
+                // Check if now the years match and month lists need to be limited
+                this.checkForSameYear();
+            });
+            // Possibilities for endYears must now all be greater than the new startYear
+            const endYears = this.state.possibleYears.filter(year => +year >= event.target.value);
+            this.setState({ endYears });
+        // We are changing the end year, which means we need to change the possiblities for the starting year and also the possible end months
+        } else {
+            // Set endYear to be user choice
+            this.setState({ endYear: event.target.value }, () => {
+                // Check if now the years match and month lists need to be limited
+                this.checkForSameYear();
+            });
+            // Possiblities for startYears must now all be less than the new endYears
+            const startYears = this.state.possibleYears.filter(year => +year <= event.target.value);
+            this.setState({ startYears });
+        }
+    }
+
+    resetMonthDropDowns() {
+        this.setState({ startMonths: this.state.possibleMonths });
+        this.setState({ startMonth: '01' });
+        this.setState({ endMonths: this.state.possibleMonths });
+        this.setState({ endMonth: '12' });
+    }
+
+    // If the start year and the end year match, we have to be careful to not allow the user to pick an end month that is earlier than the start month
+    checkForSameYear() {
+        if (this.state.startYear === this.state.endYear) {
+            // If start month is later than the end month and years match, this is not allowed, so we reset
+            if (this.state.endMonth < this.state.startMonth) {
+                this.resetMonthDropDowns();
+            // If start and end months are allowed, we still need to filter dropdown possible lists so they can't select an unallowed combination
+            } else {
+                // endMonths can only display months that are after the chosen startMonth
+                const endMonths = this.state.possibleMonths.filter(month => +month >= +this.state.startMonth);
+                this.setState({ endMonths });
+                // startMonths can only display months that are before the chosen endMonth
+                const startMonths = this.state.possibleMonths.filter(month => +month <= +this.state.endMonth);
+                this.setState({ startMonths });
+            }
+        // If the start and end years previously matched (but now they don't), an incomplete list of months may be set and we need to update
+        } else {
+            if (this.state.possibleMonths !== this.state.startMonths) {
+                this.setState({ startMonths: this.state.possibleMonths });
+            }
+            if (this.state.possibleMonths !== this.state.endMonths) {
+                this.setState({ endMonths: this.state.possibleMonths });
+            }
+        }
+    }
+
+    selectMonth(event) {
+        // When a month changes, we need to check if the years match and filter the month dropdown possibilities if they do
+        if (event.target.id === 'select-start-month') {
+            this.setState({ startMonth: event.target.value }, () => {
+                this.checkForSameYear();
+            });
+        } else {
+            this.setState({ endMonth: event.target.value }, () => {
+                this.checkForSameYear();
+            });
+        }
+    }
+
+    // Toggle the 'activeFacet' state and also reset the drop down options by calling 'setActiveFacetParameters'
+    toggleDateFacet() {
+        this.setState(prevState => ({ activeFacet: prevState.activeFacet === 'date_released' ? 'date_submitted' : 'date_released' }), this.setActiveFacetParameters);
+    }
+
+    // Reset the dropdowns and state, and clear query
+    handleReset(resetString) {
+        this.setState({ activeFacet: 'date_released' }, () => {
+            this.setActiveFacetParameters();
+            this.context.navigate(resetString);
+        });
+    }
+
+    // Set dropdowns to match quick link query and nagivate to quick link
+    handleQuickLink(quickLinkString) {
+        this.setState({
+            startMonth: this.state.currentMonth,
+            endMonth: this.state.currentMonth,
+            startYear: this.state.currentYear - 1,
+            endYear: this.state.currentYear,
+        }, () => {
+            this.context.navigate(quickLinkString);
+        });
+    }
+
+    render() {
+        const { facet, searchBase, facets } = this.props;
+        const field = this.state.activeFacet;
+        const activeFacet = facets.filter(f => f.field === this.state.activeFacet)[0];
+
+        const daysInEndMonth = moment(`${this.state.endYear}-${this.state.endMonth}`, 'YYYY-MM').daysInMonth();
+
+        // if a date range has already been selected, we want to over-write that date range with a new one
+        const existingFilter = this.props.filters.filter(filter => filter.field === 'advancedQuery');
+        let resetString = '';
+        let searchBaseForDateRange = searchBase;
+        if (existingFilter.length > 0) {
+            resetString = `${existingFilter[0].remove}&`;
+            searchBaseForDateRange = `${existingFilter[0].remove}&`;
+        } else {
+            resetString = searchBase;
+        }
+
+        const searchString = `${searchBaseForDateRange}advancedQuery=@type:Experiment ${this.state.activeFacet}:[${this.state.startYear}-${this.state.startMonth}-01 TO ${this.state.endYear}-${this.state.endMonth}-${daysInEndMonth}]`;
+
+        const currentYearSearch = `${searchBaseForDateRange}advancedQuery=@type:Experiment ${field}:[${this.state.currentYear - 1}-${this.state.currentMonth}-${this.state.currentDay} TO ${this.state.currentYear}-${this.state.currentMonth}-${this.state.currentDay}]`;
+
+        // Print selected date range next to date selector facet
+        let dateRangeString = '';
+        if (existingFilter.length > 0) {
+            if (existingFilter[0].term.indexOf('date_released') > -1) {
+                dateRangeString = `Data released between ${existingFilter[0].term.substring(existingFilter[0].term.indexOf('[') + 1, existingFilter[0].term.indexOf(']')).replace('TO', 'and')}`;
+            } else {
+                dateRangeString = `Data submitted between ${existingFilter[0].term.substring(existingFilter[0].term.indexOf('[') + 1, existingFilter[0].term.indexOf(']')).replace('TO', 'and')}`;
+            }
+        }
+
+        if ((activeFacet.terms.length && activeFacet.terms.some(term => term.doc_count)) || (field.charAt(field.length - 1) === '!')) {
+            return (
+                <div className={`facet date-selector-facet ${facet.field === 'date_released' ? 'display-date-selector' : ''}`}>
+                    <h5>Date range selection</h5>
+                    {existingFilter.length > 0 ?
+                        <div className="selected-date-range">
+                            <div>Selected range: </div>
+                            {existingFilter.map(filter =>
+                                <div key={filter.term}>{dateRangeString}</div>
+                            )}
+                        </div>
+                    : null}
+
+                    <div className="date-selector-toggle-wrapper">
+                        <div className="date-selector-toggle"><input
+                            type="radio"
+                            name="released"
+                            value="released"
+                            checked={this.state.activeFacet === 'date_released'}
+                            onChange={this.toggleDateFacet}
+                        />Released
+                        </div>
+                        <div className="date-selector-toggle"><input
+                            type="radio"
+                            name="submitted"
+                            value="submitted"
+                            checked={this.state.activeFacet === 'date_submitted'}
+                            onChange={this.toggleDateFacet}
+                        />Submitted
+                        </div>
+                    </div>
+                    <button className="date-selector-btn" onClick={() => this.handleQuickLink(currentYearSearch)}>
+                        <i className="icon icon-caret-right" />
+                        See results for the past year
+                    </button>
+                    <div className="date-container">
+                        <div className="date-selector-module">
+                            <h6>Start date:</h6>
+                            <div className="date-selector">
+                                <select id="select-start-month" value={this.state.startMonth} onChange={this.selectMonth}>
+                                    {this.state.startMonths.map(month =>
+                                        <option value={month} key={month}>{month}</option>
+                                    )}
+                                </select>
+                                <select id="select-start-year" value={this.state.startYear} onChange={this.selectYear}>
+                                    {this.state.startYears.map(year =>
+                                        <option value={year} key={year}>{year}</option>
+                                    )}
+                                </select>
+                            </div>
+                        </div>
+                        <div className="date-arrow">
+                            <i className="icon icon-arrow-right" />
+                        </div>
+                        <div className="date-selector-module">
+                            <h6>End date:</h6>
+                            <div className="date-selector">
+                                <select id="select-end-month" value={this.state.endMonth} onChange={this.selectMonth}>
+                                    {this.state.endMonths.map(month =>
+                                        <option value={month} key={month}>{month}</option>
+                                    )}
+                                </select>
+                                <select id="select-end-year" value={this.state.endYear} onChange={this.selectYear}>
+                                    {this.state.endYears.map(year =>
+                                        <option value={year} key={year}>{year}</option>
+                                    )}
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <a href={searchString}>
+                        <button className="btn btn-info btn-sm apply-date-selector">
+                            Apply changes
+                        </button>
+                    </a>
+                    <button className="btn btn-info btn-sm reset-date-selector" onClick={() => this.handleReset(resetString)}>
+                        Reset
+                    </button>
+                </div>
+            );
+        }
+
+        // Facet had all zero terms and was not a "not" facet.
+        return null;
+    }
+}
+
+DateSelectorFacet.propTypes = {
+    facet: PropTypes.object.isRequired,
+    facets: PropTypes.array.isRequired,
+    filters: PropTypes.array.isRequired,
+    searchBase: PropTypes.string.isRequired, // Base URI for the search
+};
+
+DateSelectorFacet.contextTypes = {
+    navigate: PropTypes.func,
+};
+
 // Sanitize user input and facet terms for comparison: convert to lowercase, remove white space and asterisks (which cause regular expression error)
 const sanitizedString = inputString => inputString.toLowerCase()
     .replace(/ /g, '') // remove spaces (to allow multiple word searches)
@@ -1102,6 +1385,22 @@ export class FacetList extends React.Component {
                     {facets.map((facet) => {
                         if (hideTypes && facet.field === 'type') {
                             return <span key={facet.field} />;
+                        }
+                        if (facet.field === 'date_released') {
+                            return (
+                                <DateSelectorFacet
+                                    {...this.props}
+                                    key={facet.field}
+                                    facet={facet}
+                                    filters={filters}
+                                    width={width}
+                                    negationFilters={negationFilters}
+                                    facets={facets}
+                                />
+                            );
+                        }
+                        if (facet.field === 'date_submitted') {
+                            return null;
                         }
                         return (
                             <Facet
