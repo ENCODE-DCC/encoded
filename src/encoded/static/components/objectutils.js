f4615783a676a0d1675bd2ed8d3765d0c0a433e1
Forrest Tanaka <fytanaka@stanford.edu>
2019-03-21 12:20:02 -0700

    Redo wrapper

--- src/encoded/static/components/objectutils.js
+++ src/encoded/static/components/objectutils.js
@@ -635,15 +635,13 @@ export const AlternateAccession = (props) => {
 
     if (altAcc && altAcc.length) {
         return (
-            <div className="replacement-accessions">
-                <h4 className="replacement-accessions__alternate">
-                    {altAcc.length === 1 ?
-                        <span>Alternate accession: {altAcc[0]}</span>
-                    :
-                        <span>Alternate accessions: {altAcc.join(', ')}</span>
-                    }
-                </h4>
-            </div>
+            <h4 className="replacement-accessions__alternate">
+                {altAcc.length === 1 ?
+                    <span>Alternate accession: {altAcc[0]}</span>
+                :
+                    <span>Alternate accessions: {altAcc.join(', ')}</span>
+                }
+            </h4>
         );
     }
 
