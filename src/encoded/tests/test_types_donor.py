17e94454aa092f3b46aabe9f0064821f60c8d365
Jennifer Zamanian <jlz@stanford.edu>
2019-03-27 09:42:28 -0700

    Make more descriptive names

--- src/encoded/tests/test_types_donor.py
+++ src/encoded/tests/test_types_donor.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture
-def parent(testapp, award, lab, human):
+def parent_human_donor(testapp, award, lab, human):
     item = {
         'lab': lab['@id'],
         'award': award['@id'],
@@ -12,7 +12,7 @@ def parent(testapp, award, lab, human):
 
 
 @pytest.fixture
-def child(testapp, award, lab, human):
+def child_human_donor(testapp, award, lab, human):
     item = {
         'lab': lab['@id'],
         'award': award['@id'],
@@ -21,9 +21,8 @@ def child(testapp, award, lab, human):
     return testapp.post_json('/human_donor', item).json['@graph'][0]
 
 
-# patch parent to child, check that children are a calculated property for the parent
-def test_types_calculated_children(testapp, parent, child):
-    testapp.patch_json(child['@id'],
-                             {'parents': [parent['@id']]}).json['@graph'][0]
-    res = testapp.get(parent['@id'] + '@@index-data')
-    assert res.json['object']['children'] == [child['@id']]
+def test_types_calculated_children(testapp, parent_human_donor, child_human_donor):
+    testapp.patch_json(child_human_donor['@id'],
+                             {'parents': [parent_human_donor['@id']]}).json['@graph'][0]
+    res = testapp.get(parent_human_donor['@id'] + '@@index-data')
+    assert res.json['object']['children'] == [child_human_donor['@id']]
