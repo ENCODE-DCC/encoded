a337e5d73f6c9740a9377ecb1e51129049f07c60
caseylitton <caseylitton@gmail.com>
2019-04-01 17:28:05 -0700

    something with vis blobs update

--- src/encoded/visualization.py
+++ src/encoded/visualization.py
@@ -659,7 +659,7 @@ class VisDataset(object):
                 elif format_type == 'broadPeak' or "scoreFilter" in view:
                     view["type"] = "bigBed 6 +"  # scoreFilter implies score so 6 +
             #log.debug("%d files looking for type %s" % (len(dataset["files"]),view["type"]))
-            for a_file in self.dataset["files"]:
+            for a_file in self.dataset.get("files", []):
                 if a_file['status'] not in self.vis_defines.visible_file_statuses():
                     continue
                 if file_format != a_file['file_format']:
@@ -1139,6 +1139,7 @@ class VisDataset(object):
 def vis_cache_add(request, dataset, is_vis_indexer=False):
     '''For a single embedded dataset, builds and adds vis_dataset to es cache for each relevant assembly.'''
     if (
+            not dataset.get('accession') and
             not is_vis_indexer and
             not object_is_visualizable(dataset, exclude_quickview=True)
         ):
