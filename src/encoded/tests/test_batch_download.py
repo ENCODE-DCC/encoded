2ef03861ad90d7e5bc1e44bddc0eb683d9d3d6ed
Keenan Graham <keenangraham@stanford.edu>
2019-04-30 14:00:54 -0700

    Fix tests

--- src/encoded/tests/test_batch_download.py
+++ src/encoded/tests/test_batch_download.py
@@ -95,7 +95,7 @@ def test_batch_download_files_txt(testapp, workbook):
 
     metadata = (lines[0].decode('UTF-8')).split('/')
     assert metadata[-1] == 'metadata.tsv'
-    assert metadata[-2] == 'type=Experiment'
+    assert metadata[-2] == 'type%3DExperiment'
     assert metadata[-3] == 'metadata'
 
     assert len(lines[1:]) > 0
@@ -110,7 +110,7 @@ def test_batch_download_parse_file_plus_correctly(testapp, workbook):
     r = testapp.get(
         '/batch_download/type%3DExperiment%26files.file_type%3DbigBed%2Bbed3%252B%26format%3Djson'
     )
-    assert r.body.decode('utf-8') == 'http://localhost/metadata/type=Experiment&files.file_type=bigBed+bed3%2B&format=json/metadata.tsv\nhttp://localhost/files/ENCFF880XNW/@@download/ENCFF880XNW.bigBed'
+    assert r.body.decode('utf-8') == 'http://localhost/metadata/type%3DExperiment%26files.file_type%3DbigBed%2Bbed3%252B%26format%3Djson/metadata.tsv\nhttp://localhost/files/ENCFF880XNW/@@download/ENCFF880XNW.bigBed'
     
 
 def test_batch_download_restricted_files_present(testapp, workbook):
