ba94174605d8dd40f967a5b0afa0ffae5ecb2c61
Philip Adenekan <zoldello@gmail.com>
2019-05-09 13:11:46 -0700

    fix issue with mutating prop

--- src/encoded/static/components/news.js
+++ src/encoded/static/components/news.js
@@ -31,11 +31,12 @@ NewsPreviewItem.propTypes = {
 // Display a list of news preview items from search results.
 const NewsPreviews = (props) => {
     const { items } = props;
-    items.sort((item1, item2) => new Date(item2.date_created) - new Date(item1.date_created));
-    if (items && items.length) {
+    const newsItems = items && items.length ? [...items] : [];
+    newsItems.sort((item1, item2) => new Date(item2.date_created) - new Date(item1.date_created));
+    if (newsItems.length > 0) {
         return (
             <div className="news-listing" data-test="news-listing">
-                {items.map(item => (
+                {newsItems.map(item => (
                     <NewsPreviewItem key={item['@id']} item={item} />
                 ))}
             </div>
