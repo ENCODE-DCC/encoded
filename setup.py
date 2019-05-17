fb6cd6ae5ada7c0d28f87df94c69053dafa577c2
Casey Litton <caseylitton@gmail.com>
2019-05-17 10:17:56 -0700

    Update to 85.1

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ tests_require = [
 
 setup(
     name='encoded',
-    version='85.0',
+    version='85.1',
     description='Metadata database for ENCODE',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
