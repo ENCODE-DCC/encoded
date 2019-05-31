10a8ef9f49f2ec8c02a372d93b4d987cb1dc7d47
Keenan Graham <keenangraham@stanford.edu>
2019-05-31 11:44:36 -0400

    Remove change

--- src/encoded/batch_download.py
+++ src/encoded/batch_download.py
@@ -432,7 +432,7 @@ def metadata_tsv(context, request):
                 data_row.extend(audit_info)
                 rows.append(data_row)
     fout = io.StringIO()
-    writer = csv.writer(fout, delimiter='\t', lineterminator='\n')
+    writer = csv.writer(fout, delimiter='\t')
     header.extend([prop for prop in _audit_mapping])
     writer.writerow(header)
     writer.writerows(rows)
