185a845e52206032a89bb6421d28f5440d1405f4
Casey Litton <caseylitton@gmail.com>
2019-06-10 16:45:34 -0700

    Organize arguments

--- cloud-build/batchupgrade-run.sh
+++ cloud-build/batchupgrade-run.sh
@@ -4,4 +4,6 @@
 # apt deps:
 
 env_ini=$1
-sudo -i -u encoded bin/batchupgrade $env_ini --app-name app
+batchsize=$2
+processes=$3
+sudo -i -u encoded bin/batchupgrade $env_ini --app-name app --batchsize $batchsize --processes $processes
