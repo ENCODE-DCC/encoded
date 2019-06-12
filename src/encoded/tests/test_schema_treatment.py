cdcb191277778d044ad6c016e9cdf12becd43ca6
Meenakshi <mkagd001@ucr.edu>
2019-06-12 11:32:24 -0700

    Added test schema treatment

---
+++ src/encoded/tests/test_schema_treatment.py
@@ -0,0 +1,17 @@
+import pytest
+
+@pytest.fixture
+def test_treatment_with_bad_prod (testapp,treatment, source):
+    item = {
+        "treatment_term_name": treatment['treatment_term_name'],
+        "treatment_type": treatment['treatment_type'],
+        "product_id": " Illumina",
+        "source": source['@id']
+    }
+    return item
+
+def test_treatment_prod_source_id (testapp, test_treatment_with_bad_prod):
+    testapp.post_json('/treatment', test_treatment_with_bad_prod, status=422)
+    test_treatment_with_bad_prod["product_id"] = "Illumina"
+    testapp.post_json('/treatment', test_treatment_with_bad_prod, status=201)
+
