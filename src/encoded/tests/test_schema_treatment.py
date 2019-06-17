eccfa6ca663f9589be50f3fe84d08c3b3a3c8784
Meenakshi <mkagd001@ucr.edu>
2019-06-17 10:34:05 -0700

    Changed test schema treament

--- src/encoded/tests/test_schema_treatment.py
+++ src/encoded/tests/test_schema_treatment.py
@@ -1,16 +1,7 @@
 import pytest
 
 @pytest.fixture
-def test_treatment_with_bad_prod (testapp,treatment, source):
-    item = {
-        "treatment_term_name": treatment['treatment_term_name'],
-        "treatment_type": treatment['treatment_type'],
-        "product_id": " Abcam",
-        "source": source['@id']
-    }
-    return item
-
-def test_treatment_prod_source_id (testapp, test_treatment_with_bad_prod):
-    testapp.post_json('/treatment', test_treatment_with_bad_prod, status=422)
-    test_treatment_with_bad_prod["product_id"] = "Illumina"
-    testapp.post_json('/treatment', test_treatment_with_bad_prod, status=201)
+def test_treatment_patch_prod_source(testapp, treatment):
+    treatment["product_id"] = "HT-904"
+    treatment["source"] = "sigma"
+    testapp.patch_json('/treatment', test_treatment_patch_prod_source, status=200)
