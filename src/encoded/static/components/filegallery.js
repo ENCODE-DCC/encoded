d5d07fd89ae2291bfde388be752b7c563f90703c
Forrest Tanaka <fytanaka@stanford.edu>
2019-07-08 06:09:17 -0700

    ENCD-4699 Fix Javascript crash display series file tables (#2829)

--- src/encoded/static/components/filegallery.js
+++ src/encoded/static/components/filegallery.js
@@ -181,6 +181,7 @@ export class FileTable extends React.Component {
             // TODO: (1)Move .hide to FileTable.procTableColumns declaration
             // (2) move showReplicateNumber to meta
             FileTable.procTableColumns.biological_replicates.hide = () => !showReplicateNumber;
+            FileTable.procTableColumns.visualize.hide = () => browserOptions.browserFileSelectHandler === null;
 
             return (
                 <div>
@@ -283,7 +284,7 @@ FileTable.propTypes = {
         /** Currently selected genome browser */
         currentBrowser: PropTypes.string,
         /** Called when user selects a browser */
-        browserFileSelectHandler: PropTypes.func.isRequired,
+        browserFileSelectHandler: PropTypes.func,
         /** Files selected for browsing */
         selectedBrowserFiles: PropTypes.array,
     }),
@@ -318,6 +319,7 @@ FileTable.defaultProps = {
     showFileCount: false,
     browserOptions: {
         currentBrowser: '',
+        browserFileSelectHandler: null,
         selectedBrowserFiles: [],
     },
     setInfoNodeId: null,
