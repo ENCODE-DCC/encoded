8425914d1e93dc6e574bfa9f42e64a83f4fcaf63
Idan Gabdank <gabdank@stanford.edu>
2019-07-18 14:59:13 -0700

    another round of refactoring

--- src/encoded/types/file.py
+++ src/encoded/types/file.py
@@ -66,10 +66,16 @@ def property_closure(request, propname, root_uuid):
     return seen
 
 
-def is_md5sum_constrained_file(file_properties, unique_md5_output_categories, uniform_processing_pipeline_uuid):
-    if ((file_properties.get('lab') != uniform_processing_pipeline_uuid or 
-        file_properties.get('output_category') in unique_md5_output_categories) and 
-        'md5sum' in file_properties):
+ENCODE_PROCESSING_PIPELINE_UUID = 'a558111b-4c50-4b2e-9de8-73fd8fd3a67d'
+RAW_DATA = 'raw data'
+
+
+def file_is_md5sum_constrained(properties):
+    conditions = [
+        properties.get('lab') != ENCODE_PROCESSING_PIPELINE_UUID,
+        properties.get('output_category') == RAW_DATA
+    ]
+    if any(conditions):
         return True
     return False
 
@@ -143,15 +149,11 @@ class File(Item):
         if properties.get('status') == 'replaced':
             return self.uuid
         return properties.get(self.name_key, None) or self.uuid
-    
+
     def unique_keys(self, properties):
         keys = super(File, self).unique_keys(properties)
-        ENCODE_PROCESSING_PIPELINE_UUID = "a558111b-4c50-4b2e-9de8-73fd8fd3a67d"
         if properties.get('status') != 'replaced':
-            if is_md5sum_constrained_file(
-                properties,
-                ['raw data'],
-                ENCODE_PROCESSING_PIPELINE_UUID):    
+            if 'md5sum' in properties and file_is_md5sum_constrained(properties):   
                 value = 'md5:{md5sum}'.format(**properties)
                 keys.setdefault('alias', []).append(value)
             # Ensure no files have multiple reverse paired_with
