74356c7c0638531232f05a9733366a19c04e9be9
Ben Hitz <hitz@stanford.edu>
2019-10-06 20:36:42 -0700

    [REVERT] ENCD-4833 replication type calculation (#2920) (#2950)
    This reverts commit fe6ba70099abaa1ef22835d846b6750d4ba2462b.

--- src/encoded/tests/test_types_experiment.py
+++ src/encoded/tests/test_types_experiment.py
@@ -11,183 +11,80 @@ def base_experiment(testapp, lab, award, cell_free):
     }
     return testapp.post_json('/experiment', item, status=201).json['@graph'][0]
 
-
-@pytest.fixture
-def fly_experiment(
-    testapp,
-    base_experiment,
-    fly, 
-    whole_organism, 
-    fly_donor, 
-    biosample_1, 
-    biosample_2, 
-    library_1, 
-    library_2, 
-    replicate_1_1, 
-    replicate_2_1, 
-    fastq_file_1, 
-    fastq_file_2,
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
-    testapp.patch_json(biosample_1['@id'], {'donor': fly_donor['@id'], 'organism': fly['@id'], 'biosample_ontology': whole_organism['@id']})
-    testapp.patch_json(biosample_2['@id'], {'donor': fly_donor['@id'], 'organism': fly['@id'], 'biosample_ontology': whole_organism['@id']})
-    testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
-    testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
-    return testapp.get(base_experiment['@id']+'@@index-data') 
-
-
-def test_fly_isogenic_replicate_type(testapp, fly_experiment):
-    assert fly_experiment.json['object']['replication_type']=='isogenic' 
-
-
-def test_fly_unreplicated_replicate_type(testapp, fly_experiment, replicate_2_1):
-    testapp.patch_json(replicate_2_1['@id'], {'status': 'deleted'})
-    res = testapp.get(fly_experiment.json['object']['@id']+'@@index-data') 
-    assert res.json['object']['replication_type']=='unreplicated' 
- 
-def test_isogenic_replicate_type(
-    testapp,
-    base_experiment,
-    donor_1,
-    donor_2,
-    biosample_1,
-    biosample_2,
-    library_1,
-    library_2,
-    replicate_1_1,
-    replicate_2_1,
-    fastq_file_1,
-    fastq_file_2,
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
+def test_isogenic_replicate_type(testapp, base_experiment, donor_1, donor_2,biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1 ):
     testapp.patch_json(donor_1['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_1['@id'], {'sex': 'female' })
     testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(biosample_2['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
     testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
+    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id']})
+    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
+    testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id']+'@@index-data') 
     assert res.json['object']['replication_type']=='isogenic' 
 
-def test_anisogenic_replicate_type_sex_age_matched(
-    testapp, 
-    base_experiment, 
-    donor_1, 
-    donor_2,
-    biosample_1, 
-    biosample_2, 
-    library_1, 
-    library_2, 
-    replicate_1_1, 
-    replicate_2_1, 
-    fastq_file_1, 
-    fastq_file_2
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
+def test_anisogenic_replicate_type_sex_age_matched(testapp, base_experiment, donor_1, donor_2,biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1 ):
     testapp.patch_json(donor_1['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult'})
     testapp.patch_json(donor_2['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_1['@id'], {'sex': 'female' })
     testapp.patch_json(donor_2['@id'], {'sex': 'female' })    
     testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id']})
+
     testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
     testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
+    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id']})
+    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
+    testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id']+'@@index-data') 
     assert res.json['object']['replication_type']=='anisogenic' 
 
-def test_anisogenic_replicate_type_sex_matched(
-    testapp, 
-    base_experiment, 
-    donor_1, 
-    donor_2,
-    biosample_1, 
-    biosample_2, 
-    library_1, 
-    library_2, 
-    replicate_1_1, 
-    replicate_2_1, 
-    fastq_file_1, 
-    fastq_file_2
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
+def test_anisogenic_replicate_type_sex_matched(testapp, base_experiment, donor_1, donor_2,biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1 ):
     testapp.patch_json(donor_1['@id'], {'age_units': 'year', 'age': '15', 'life_stage': 'adult' })
     testapp.patch_json(donor_2['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_1['@id'], {'sex': 'female' })
     testapp.patch_json(donor_2['@id'], {'sex': 'female' })    
     testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id']})
+
     testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
     testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
+    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id']})
+    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
+    testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id']+'@@index-data') 
     assert res.json['object']['replication_type']=='anisogenic' 
 
-def test_anisogenic_replicate_type_age_matched(
-    testapp, 
-    base_experiment, 
-    donor_1, 
-    donor_2,
-    biosample_1, 
-    biosample_2, 
-    library_1, 
-    library_2, 
-    replicate_1_1, 
-    replicate_2_1, 
-    fastq_file_1, 
-    fastq_file_2
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
+def test_anisogenic_replicate_type_age_matched(testapp, base_experiment, donor_1, donor_2,biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1 ):
     testapp.patch_json(donor_1['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_2['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_1['@id'], {'sex': 'female' })
     testapp.patch_json(donor_2['@id'], {'sex': 'mixed' })    
     testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id']})
+
     testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
     testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
+    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id']})
+    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
+    testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id']+'@@index-data') 
     assert res.json['object']['replication_type']=='anisogenic'     
 
-def test_anisogenic_replicate_type(
-    testapp, 
-    base_experiment, 
-    donor_1, 
-    donor_2,
-    biosample_1, 
-    biosample_2, 
-    library_1, 
-    library_2, 
-    replicate_1_1, 
-    replicate_2_1, 
-    fastq_file_1, 
-    fastq_file_2
-):
-    testapp.patch_json(fastq_file_1['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_1_1['@id']})
-    testapp.patch_json(fastq_file_2['@id'], {'dataset': base_experiment['@id'], 'replicate': replicate_2_1['@id']})
+def test_anisogenic_replicate_type(testapp, base_experiment, donor_1, donor_2,biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1 ):
     testapp.patch_json(donor_1['@id'], {'age': 'unknown' })
     testapp.patch_json(donor_2['@id'], {'age_units': 'year', 'age': '55', 'life_stage': 'adult' })
     testapp.patch_json(donor_1['@id'], {'sex': 'female' })
     testapp.patch_json(donor_2['@id'], {'sex': 'unknown' })    
     testapp.patch_json(biosample_1['@id'], {'donor': donor_1['@id']})
     testapp.patch_json(biosample_2['@id'], {'donor': donor_2['@id']})
+
     testapp.patch_json(library_1['@id'], {'biosample': biosample_1['@id']})
     testapp.patch_json(library_2['@id'], {'biosample': biosample_2['@id']})
-    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id'], 'experiment': base_experiment['@id']})
-    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id'], 'experiment': base_experiment['@id']})
+    testapp.patch_json(replicate_1_1['@id'], {'library': library_1['@id']})
+    testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
+    testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id']+'@@index-data') 
     assert res.json['object']['replication_type']=='anisogenic'   
 
