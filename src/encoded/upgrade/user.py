8e050f8c137126957e2dd2a93cb81780e5825808
Bonita Lam <bonitarlam@gmail.com>
2019-10-24 09:13:00 -0700

    ENCD-4861 add enum to user groups(#2964)

--- src/encoded/upgrade/user.py
+++ src/encoded/upgrade/user.py
@@ -42,3 +42,9 @@ def user_7_8(value, system):
             value['viewing_groups'].append('community')
     else:
         value['viewing_groups'] = ['community']
+
+
+@upgrade_step('user', '8', '9')
+def user_8_9(value, system):
+    if 'groups' in value:
+        value['groups'] = [group for group in value['groups'] if group in ['admin', 'verified', 'read-only-admin']]
