2324470a232ec50e5b4eda4fcd4db7c9d11e6a05
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2019-11-02 17:50:36 +0500

    drop! try teardown without deleting

--- src/encoded/tests/test_indexing.py
+++ src/encoded/tests/test_indexing.py
@@ -58,9 +58,9 @@ def DBSession(app):
 
 @pytest.fixture(autouse=True)
 def teardown(app, dbapi_conn):
-    from snovault.elasticsearch import ELASTIC_SEARCH
+    # from snovault.elasticsearch import ELASTIC_SEARCH
     from snovault.elasticsearch import create_mapping
-    app.registry[ELASTIC_SEARCH].delete_by_query(index='_all',
+    #app.registry[ELASTIC_SEARCH].delete_by_query(index='_all',
                                                  body={'query': {'match_all': {}}},
                                                  conflicts='proceed')
     create_mapping.run(app)
