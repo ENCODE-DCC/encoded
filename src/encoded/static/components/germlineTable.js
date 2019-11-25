9be7f1a441bbf3e128c76e2eb209999649889bd0
Sensecode <mznie@hotmail.com>
2019-11-25 10:59:12 -0600

    cleaning code, ready for push

--- src/encoded/static/components/germlineTable.js
+++ src/encoded/static/components/germlineTable.js
@@ -6,19 +6,13 @@ import { SortTablePanel, SortTable } from './sorttable';
 class GermlineTable extends React.Component {
     constructor(props) {
         super(props);
-       // this.data = this.props.data;
         this.germlineFilters = [];
         this.transformData = [];
-       // this.tableTitle = this.props.tableTitle;
     }
 
     filterData() {
-        //let data=this.props.data;
         this.germlineFilters = this.props.data.filter(i => (i.significance === 'Positive' || i.significance === 'Variant' || i.significance === 'Positive and Variant'));
         this.transformData = this.germlineFilters.map(i => ({ target: i.target, significance: i.significance }));
-        console.log(this.props.data);
-        console.log(this.transformData);
-        console.log(this.germlineFilters);
     }
 
 
@@ -29,33 +23,25 @@ class GermlineTable extends React.Component {
                     </SortTablePanel>
             );
         }
-        else {
-            if(this.transformData.length>0) {
-                const germlineTableColumns = {
-                    target: {
-                        title: 'Target Gene',
-                    },
-                    significance: {
-                        title: 'Clinical Significance',
-                    },
-                };
-                return (
-                    <SortTablePanel title={this.props.tableTitle}>
-                        <SortTable list={this.transformData} columns={germlineTableColumns} />
-                    </SortTablePanel>
-                );
-            }
-            else {
-                return (<SortTablePanel title={this.props.tableTitle}>
-                        <div className="table-body"><h5>No Positive for mutations!</h5></div>
-                    </SortTablePanel>
-                );
-            }
+        if (this.transformData.length>0) {
+            const germlineTableColumns = {
+                target: {
+                    title: 'Target Gene',
+                },
+                significance: {
+                    title: 'Clinical Significance',
+                },
+            };
+            return (
+                <SortTablePanel title={this.props.tableTitle}>
+                    <SortTable list={this.transformData} columns={germlineTableColumns} />
+                </SortTablePanel>
+            );
         }
-
-        
-        //Transformat data to <SortTable/> format
-
+        return (<SortTablePanel title={this.props.tableTitle}>
+                    <div className="table-body"><h5>No Positive for mutations!</h5></div>
+                </SortTablePanel>
+         );
     }
 
     render() {
@@ -71,75 +57,3 @@ class GermlineTable extends React.Component {
 }
 
 export default GermlineTable;
-// ============================================================
-//http://localhost:6543/report/?type=Page&news=true&limit=all&sort=-%40id
-{/* <SortTablePanel title={tableTitle}>
-<SortTable list={condensedReplicates} columns={replicateTableColumns} />
-</SortTablePanel> */}
-// this.tableConfig = {
-//     type: 'grid',
-//     plotarea: {
-//         margin: 20,
-//     },
-//     globals: {
-//         fontFamily: 'Arial',
-//         fontStyle: 'normal',
-//         fontWeight: 'normal',
-//     },
-//     backgroundColor: '',
-//     options: {
-//         'col-labels': [
-//             'Target',
-//             'Significance',
-//         ],
-//         style: {
-//             '.td': {
-//                 paddingTop: '9px',
-//                 align: 'center',
-//                 borderTop: 'none',
-//                 borderRight: '1px solid #2f3341',
-//                 borderBottom: '1px solid #2f3341',
-//                 borderLeft: '1px solid #2f3341',
-//                 borderWidth: '1px',
-//                 fontColor: '#000000',
-//                 fontSize: '16px',
-//                 height: '34px',
-//             },
-//             '.th': {
-//                 align: 'center',
-//                 backgroundColor: '#074361',
-//                 borderTop: '1px solid #2f3341',
-//                 borderRight: '1px solid #2f3341',
-//                 borderBottom: '1px solid #2f3341',
-//                 borderLeft: '1px solid #2f3341',
-//                 fontColor: '#FFFFFF',
-//                 fontSize: '20px',
-//                 fontWeight: 'normal',
-//                 height: '38px',
-//             },
-//             '.tr_even': {
-
-//                 backgroundColor: 'none',
-//             },
-//             '.tr_odd': {
-//                 backgroundColor: 'none',
-//             },
-//         },
-//     },
-
-//     series: this.transferData,
-// };
-// This module displays a table that can be sorted by any column. You can set one up for display with:
-//     <SortTablePanel>
-//         <SortTable list={array of objects} columns={object describing table columns} meta={table-specific data} />
-//     </SortTablePanel>
-//
-// <SortTablePanel> supports multiple <SortTable> components as children, so you can have multiple tables,
-// each with their own header, inside the table panel.
-//
-// The list array (required) must be an array of objects whose properties get displayed in each cell of the
-// table. The meta object (optional) has any extra data relevant to the specific table and doesn't get
-// processed -- only passed on -- by the SortTable code.
-//
-// The columns object (required) describes the columns of the table, and optionally how they get displayed,
-// sorted, and hidden.
