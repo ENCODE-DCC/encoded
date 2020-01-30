42ac78fef01bd6a2d85d27628e424fc00e9d8919
Sensecode <mznie@hotmail.com>
2020-01-30 17:16:19 -0600

    format IHC json schema and add IHC json data.

---
+++ src/encoded/static/components/ihcTable.js
@@ -0,0 +1,49 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+import React from 'react';
+import { SortTablePanel, SortTable } from './sorttable';
+
+class IHCTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ihcFilters = [];
+    }
+
+    filterData() {
+        this.ihcFilters = this.props.data.map(i =>({ "target proteins": i.proteins, "result": i.result }) ) ;
+        console.log("IHCfilter",this.ihcFilters);
+    }
+
+
+    renderData() {
+        
+        if (this.ihcFilters.length>0) {
+            const ihcTableColumns = {
+                "target proteins": {
+                    title: 'Target protein',
+                },
+                "result": {
+                    title: 'Result',
+                },
+            };
+            return (
+                <SortTablePanel title={this.props.tableTitle}>
+                    <SortTable list={this.ihcFilters} columns={ihcTableColumns} />
+                </SortTablePanel>
+            );
+        }
+    }
+        
+    render() {
+        return (
+            <div> {this.renderData()}</div>
+        );
+    }
+
+    componentDidMount() {
+        this.filterData();
+        this.renderData();
+    }
+}
+
+export default IHCTable;
