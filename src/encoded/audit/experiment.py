e1b8e71b44820629686442766d1a37af7a5b4af1
Bonita Lam <bonitarlam@gmail.com>
2020-03-13 12:13:48 -0700

    Add additional comparison and tests

--- src/encoded/audit/experiment.py
+++ src/encoded/audit/experiment.py
@@ -4023,6 +4023,21 @@ def audit_experiment_inconsistent_analyses_files(value, system, files_structure)
                         )
                     )
                 yield AuditFailure('inconsistent analyses files', detail, level='INTERNAL_ACTION')
+        files_not_in_processed_data = []
+        for analysis_file in analysis_outputs:
+            if analysis_file['@id'] in processed_data:
+                continue
+            if analysis_file['@id'] not in processed_data:
+                files_not_in_processed_data.append(analysis_file)
+                files_not_in_processed_data_links = [audit_link(path_to_text(file), file) for file in files_not_in_processed_data]
+                detail = ('Experiment {} '
+                        'contains file(s) in an analysis {} '
+                        'not in processed data'.format(
+                            audit_link(path_to_text(value['@id']), value['@id']),
+                            ', '.join(files_not_in_processed_data_links)
+                        )
+                    )
+                yield AuditFailure('inconsistent analyses files', detail, level='INTERNAL_ACTION')
 
 
 #######################
