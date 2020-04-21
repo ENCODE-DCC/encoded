51773f82a34c8beac3208a8990a3fb95a09e1cd1
Philip Adenekan <zoldello@gmail.com>
2020-04-21 15:34:52 -0700

    added url to file id

--- src/encoded/static/components/file.js
+++ src/encoded/static/components/file.js
@@ -582,7 +582,12 @@ class SequenceFileInfo extends React.Component {
                         {file.index_of ?
                             <div data-test="outputtype">
                                 <dt>Index of</dt>
-                                <dd>{file.index_of.map(f => f.replace(/\/files\/|\//g, '')).join(', ')}</dd>
+                                <dd>
+                                    {file.index_of.map((f) => {
+                                        const fileId = f.replace(/\/files\/|\//g, '');
+                                        return (<a href={`/files/${fileId}`} title={fileId}>{fileId}</a>);
+                                    }).reduce((f, fs) => [f, ', ', fs])}
+                                </dd>
                             </div>
                         : null}
 
