f9e261bb2b23e44850f5c4c57b0c413be7bbc43e
Yunhai Luo <yunhail@stanford.edu>
2020-05-18 10:39:45 -0700

    oneOf-and-test

--- src/encoded/tests/test_schema_library.py
+++ src/encoded/tests/test_schema_library.py
@@ -36,3 +36,55 @@ def test_library_size_SD_and_CV_properties(testapp, library_size_range, library_
     testapp.post_json('/library', library_fragment_length_CV, status=422)
     library_fragment_length_CV.pop('fragment_length_CV')
     testapp.post_json('/library', library_fragment_length_CV, status=201)
+
+
+def test_library_adapters(testapp, library, file):
+    file_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+            },
+        ]
+    }
+    testapp.post_json('/library', file_adapters, status=201)
+    sequence_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNA',
+            },
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNAT',
+            },
+        ]
+    }
+    testapp.post_json('/library', sequence_adapters, status=201)
+    file_sequence_adapter = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+                'sequence': 'GGGGGGCNA',
+            },
+        ]
+    }
+    testapp.post_json('/library', file_sequence_adapter, status=422)
+    mixed_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+            },
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNA',
+            },
+        ]
+    }
+    testapp.post_json('/library', mixed_adapters, status=422)
