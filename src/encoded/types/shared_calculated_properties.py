87e009758fe79559e39f68b1c99c2528b68f6a25
Bonita Lam <bonitarlam@gmail.com>
2020-05-18 13:35:52 -0700

    Handle biosample summary calculation

--- src/encoded/types/shared_calculated_properties.py
+++ src/encoded/types/shared_calculated_properties.py
@@ -6,10 +6,8 @@ from encoded.vis_defines import (
     vis_format_url,
     browsers_available
     )
-from .biosample import (
-    construct_biosample_summary,
-    generate_summary_dictionary
-)
+from .biosample import construct_biosample_summary
+from .shared_biosample import biosample_summary_dictionary
 from .base import (
     paths_filtered_by_status
 )
@@ -220,7 +218,6 @@ class CalculatedBiosampleSummary:
                           replicates=None):
         drop_age_sex_flag = False
         dictionaries_of_phrases = []
-        biosample_accessions = set()
         if replicates is not None:
             for rep in replicates:
                 replicateObject = request.embed(rep, '@@object')
@@ -232,97 +229,10 @@ class CalculatedBiosampleSummary:
                         continue
                     if 'biosample' in libraryObject:
                         biosampleObject = request.embed(libraryObject['biosample'], '@@object')
-                        if biosampleObject['status'] == 'deleted':
-                            continue
-                        if biosampleObject['accession'] not in biosample_accessions:
-                            biosample_accessions.add(biosampleObject['accession'])
-
-                            biosampleTypeObject = request.embed(
-                                biosampleObject['biosample_ontology'],
-                                '@@object'
-                            )
-                            if biosampleTypeObject.get('classification') in [
-                                    'in vitro differentiated cells']:
-                                drop_age_sex_flag = True
-
-                            organismObject = None
-                            if 'organism' in biosampleObject:
-                                organismObject = request.embed(biosampleObject['organism'],
-                                                               '@@object')
-                            donorObject = None
-                            if 'donor' in biosampleObject:
-                                donorObject = request.embed(biosampleObject['donor'], '@@object')
-
-                            treatment_objects_list = None
-                            treatments = biosampleObject.get('treatments')
-                            if treatments is not None and len(treatments) > 0:
-                                treatment_objects_list = []
-                                for t in treatments:
-                                    treatment_objects_list.append(request.embed(t, '@@object'))
-
-                            part_of_object = None
-                            if 'part_of' in biosampleObject:
-                                part_of_object = request.embed(biosampleObject['part_of'],
-                                                               '@@object')
-                            originated_from_object = None
-                            if 'originated_from' in biosampleObject:
-                                originated_from_object = request.embed(biosampleObject['originated_from'],
-                                                                       '@@object')
-
-                            modifications_list = None
-                            genetic_modifications = biosampleObject.get('applied_modifications')
-                            if genetic_modifications:
-                                modifications_list = []
-                                for gm in genetic_modifications:
-                                    gm_object = request.embed(gm, '@@object')
-                                    modification_dict = {'category': gm_object.get('category')}
-                                    if gm_object.get('modified_site_by_target_id'):
-                                        modification_dict['target'] = request.embed(
-                                            gm_object.get('modified_site_by_target_id'),
-                                                          '@@object')['label']
-                                    if gm_object.get('introduced_tags_array'):
-                                        modification_dict['tags'] = []
-                                        for tag in gm_object.get('introduced_tags_array'):
-                                            tag_dict = {'location': tag['location']}
-                                            if tag.get('promoter_used'):
-                                                tag_dict['promoter'] = request.embed(
-                                                    tag.get('promoter_used'),
-                                                            '@@object').get['label']
-                                            modification_dict['tags'].append(tag_dict)
-
-                                    modifications_list.append((gm_object['method'], modification_dict))
-
-                            preservation_method = None
-                            dictionary_to_add = generate_summary_dictionary(
-                                request,
-                                organismObject,
-                                donorObject,
-                                biosampleObject.get('age'),
-                                biosampleObject.get('age_units'),
-                                biosampleObject.get('life_stage'),
-                                biosampleObject.get('sex'),
-                                biosampleTypeObject.get('term_name'),
-                                biosampleTypeObject.get('classification'),
-                                biosampleObject.get('starting_amount'),
-                                biosampleObject.get('starting_amount_units'),
-                                biosampleObject.get('depleted_in_term_name'),
-                                biosampleObject.get('phase'),
-                                biosampleObject.get('subcellular_fraction_term_name'),
-                                biosampleObject.get('synchronization'),
-                                biosampleObject.get('post_synchronization_time'),
-                                biosampleObject.get('post_synchronization_time_units'),
-                                biosampleObject.get('post_treatment_time'),
-                                biosampleObject.get('post_treatment_time_units'),
-                                biosampleObject.get('post_nucleic_acid_delivery_time'),
-                                biosampleObject.get('post_nucleic_acid_delivery_time_units'),
-                                treatment_objects_list,
-                                preservation_method,
-                                part_of_object,
-                                originated_from_object,
-                                modifications_list,
-                                True)
-
-                            dictionaries_of_phrases.append(dictionary_to_add)
+                        biosample_dict = biosample_summary_dictionary(request, biosampleObject)
+                        dictionaries_of_phrases.append(biosample_dict[0])
+                        if biosample_dict[1] is True:
+                            drop_age_sex_flag is True
 
         if drop_age_sex_flag is True:
             sentence_parts = [
