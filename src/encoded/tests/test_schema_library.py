f1a7688c878623da65dff082a527613f580f12b8
iyoungworth <iay@stanford.edu>
2020-05-20 09:50:20 -0700

    ENCD-5276-add-kallisto-properties-to-library

--- src/encoded/tests/test_schema_library.py
+++ src/encoded/tests/test_schema_library.py
@@ -21,3 +21,18 @@ def test_library_fragmentation_method_list(testapp, library_with_valid_fragmenta
     testapp.post_json('/library', library_with_valid_fragmentation_method_list, status=201)
     library_with_valid_fragmentation_method_list.pop('fragmentation_methods')
     testapp.post_json('/library', library_with_valid_fragmentation_method_list, status=422)
+
+
+def test_library_size_SD_and_CV_properties(testapp, library_size_range, library_fragment_length_CV):
+    # https://encodedcc.atlassian.net/browse/ENCD-5276
+    testapp.post_json('/library', library_size_range, status=201)
+    library_size_range.update({'average_fragment_size': 350})
+    testapp.post_json('/library', library_size_range, status=422)
+    library_size_range.pop('size_range')
+    testapp.post_json('/library', library_size_range, status=201)
+
+    testapp.post_json('/library', library_fragment_length_CV, status=201)
+    library_fragment_length_CV.update({'fragment_length_SD': 45})
+    testapp.post_json('/library', library_fragment_length_CV, status=422)
+    library_fragment_length_CV.pop('fragment_length_CV')
+    testapp.post_json('/library', library_fragment_length_CV, status=201)
