0e8d4a678db993541cabd2b60873c0c554fbd26f
Idan Gabdank <gabdank@users.noreply.github.com>
2020-05-21 17:55:30 -0700

    ENCD-5270-library-adapter (#3312)

--- src/encoded/tests/test_schema_library.py
+++ src/encoded/tests/test_schema_library.py
@@ -36,3 +36,70 @@ def test_library_size_SD_and_CV_properties(testapp, library_size_range, library_
     testapp.post_json('/library', library_fragment_length_CV, status=422)
     library_fragment_length_CV.pop('fragment_length_CV')
     testapp.post_json('/library', library_fragment_length_CV, status=201)
+
+
+def test_library_adapters(testapp, library, file):
+    file_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+            },
+        ]
+    }
+    testapp.post_json('/library', file_adapters, status=201)
+    sequence_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNA',
+            },
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNAT',
+            },
+        ]
+    }
+    testapp.post_json('/library', sequence_adapters, status=201)
+    file_sequence_adapter1 = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+                'sequence': 'GGGGGGCNA',
+            },
+        ]
+    }
+    testapp.post_json('/library', file_sequence_adapter1, status=422)
+    file_sequence_adapter2 = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+            },
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+                'sequence': 'GGGGGGCNA',
+            },
+        ]
+    }
+    testapp.post_json('/library', file_sequence_adapter2, status=422)
+    mixed_adapters = {
+        **library,
+        'adapters': [
+            {
+                'type': "3' adapter",
+                'file': file['@id'],
+            },
+            {
+                'type': "3' adapter",
+                'sequence': 'GGGGGGCNA',
+            },
+        ]
+    }
+    testapp.post_json('/library', mixed_adapters, status=422)
