ce702fb59d9517b21278deeb598a1d791c387d68
Phil <zoldello@gmail.com>
2020-07-10 15:46:06 -0700

    ENCD-5391-fix-internal-status-facet-not-opening (#3410)

--- src/encoded/static/components/facets/internal_status.js
+++ src/encoded/static/components/facets/internal_status.js
@@ -8,7 +8,7 @@ import FacetRegistry from './registry';
  * The Internal Status facet only displays with an admin login. Its display gets supressed with the
  * null output from this component otherwise.
  */
-const InternalStatusFacet = ({ facet, results, mode, relevantFilters, pathname, queryString }, reactContext) => (
+const InternalStatusFacet = ({ facet, results, mode, relevantFilters, pathname, queryString, isExpanded, handleExpanderClick, handleKeyDown }, reactContext) => (
     <React.Fragment>
         {reactContext.session_properties && reactContext.session_properties.admin ?
             <DefaultFacet
@@ -18,6 +18,9 @@ const InternalStatusFacet = ({ facet, results, mode, relevantFilters, pathname, 
                 relevantFilters={relevantFilters}
                 pathname={pathname}
                 queryString={queryString}
+                isExpanded={isExpanded}
+                handleExpanderClick={handleExpanderClick}
+                handleKeyDown={handleKeyDown}
             />
         : null}
     </React.Fragment>
@@ -36,11 +39,20 @@ InternalStatusFacet.propTypes = {
     pathname: PropTypes.string.isRequired,
     /** Query-string portion of current URL without initial ? */
     queryString: PropTypes.string,
+    /** True if facet is expanded */
+    isExpanded: PropTypes.bool,
+    /** Expand or collapse facet */
+    handleExpanderClick: PropTypes.func,
+    /** Handles key-press and toggling facet */
+    handleKeyDown: PropTypes.func,
 };
 
 InternalStatusFacet.defaultProps = {
     mode: '',
     queryString: '',
+    isExpanded: false,
+    handleExpanderClick: () => {},
+    handleKeyDown: () => {},
 };
 
 InternalStatusFacet.contextTypes = {
