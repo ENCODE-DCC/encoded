5b415444539b42376bdd1f6beda1605b56c72b1d
Yunhai Luo <yunhail@stanford.edu>
2020-07-14 18:22:36 -0700

    ENCD-5398-fix-tests

--- src/encoded/tests/test_batch_download.py
+++ src/encoded/tests/test_batch_download.py
@@ -327,7 +327,7 @@ def test_batch_download_report_download(testapp, workbook):
         b'Post-synchronization time', b'Post-synchronization time units',
         b'Replicates',
     ]
-    assert len(lines) == 67
+    assert len(lines) == 68
 
 
 def test_batch_download_matched_set_report_download(testapp, workbook):
