9dcaf3c36ae6c1ab034e8c58623f4e5e2dc9aa73
Keenan Graham <keenangraham@stanford.edu>
2020-08-05 17:14:15 -0700

    Add build request and batched_values and tests

--- src/encoded/reports/metadata.py
+++ src/encoded/reports/metadata.py
@@ -353,18 +353,41 @@ class CSVGenerator:
 
 class BatchedSearchGenerator:
 
+    SEARCH_PATH = '/search/'
+
     def __init__(self, request, batch_field='@id', batch_size=5000):
-        self.requeset = request
+        self.request = request
         self.batch_field = batch_field
         self.batch_size = batch_size
         self.query_string = QueryString(request)
         self.param_list = self.query_string.group_values_by_key()
         self.batch_param_values = self.param_list.get(batch_field, []).copy()
 
-    def _make_batches_from_batch_params(self):
+    def _make_batched_values_from_batch_param_values(self):
         end = len(self.batch_param_values)
         for start in range(0, end, self.batch_size):
             yield self.batch_param_values[start:min(start + self.batch_size, end)]
+
+    def _make_batched_params_from_batched_values(self, batched_values):
+        return [
+            (self.batch_field, batched_value)
+            for batched_value in batched_values
+        ]
+
+    def _build_new_request(self, batched_params):
+        self.query_string.drop(self.batch_field)
+        self.query_string.extend(batched_params)
+        request = self.query_string.get_request_with_new_query_string()
+        request.path_info = self.SEARCH_PATH
+        request.registry = self.request.registry
+        return request
+
+    def results(self):
+        for batched_values in self._make_batched_values_from_batch_param_values():
+            batched_params = self._make_batched_params_from_batched_values(batched_values)
+            request = self._build_new_request(batched_params)
+            yield from search_generator(request)
+
 
 
 def _get_metadata(context, request):
--- src/encoded/tests/test_metadata.py
+++ src/encoded/tests/test_metadata.py
@@ -1578,35 +1578,96 @@ def test_metadata_batched_search_generator_init(dummy_request):
     assert bsg.batch_param_values == []
 
 
-def test_metadata_batched_search_generator_make_batches_from_batch_params(dummy_request):
-       from encoded.reports.metadata import BatchedSearchGenerator
-       dummy_request.environ['QUERY_STRING'] = (
-           'type=Experiment'
-       )
-       bsg = BatchedSearchGenerator(dummy_request)
-       assert list(bsg._make_batches_from_batch_params()) == []
-       from encoded.reports.metadata import BatchedSearchGenerator
-       dummy_request.environ['QUERY_STRING'] = (
-           'type=Experiment&@id=/files/ENCFFABC123/'
-           '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
-           '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
-           '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
-       )
-       bsg = BatchedSearchGenerator(dummy_request, batch_size=2)
-       assert list(bsg._make_batches_from_batch_params()) == [
-           ['/files/ENCFFABC123/', '/files/ENCFFABC345/'],
-           ['/files/ENCFFABC567/', '/files/ENCFFABC789/'],
-           ['/files/ENCFFDEF123/', '/files/ENCFFDEF345/'],
-           ['/files/ENCFFDEF567/']
-       ]
-       bsg = BatchedSearchGenerator(dummy_request, batch_field='accession', batch_size=2)
-       assert list(bsg._make_batches_from_batch_params()) == []
-       dummy_request.environ['QUERY_STRING'] = (
-           'type=Experiment&@id=/files/ENCFFABC123/'
-           '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
-           '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
-           '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
-           '&accession=ENCFFAAA111'
-       )
-       bsg = BatchedSearchGenerator(dummy_request, batch_field='accession')
-       assert next(bsg._make_batches_from_batch_params()) == ['ENCFFAAA111']
+def test_metadata_batched_search_generator_make_batched_values_from_batch_param_values(dummy_request):
+    from encoded.reports.metadata import BatchedSearchGenerator
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment'
+    )
+    bsg = BatchedSearchGenerator(dummy_request)
+    assert list(bsg._make_batched_values_from_batch_param_values()) == []
+    from encoded.reports.metadata import BatchedSearchGenerator
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment&@id=/files/ENCFFABC123/'
+        '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
+        '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
+        '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
+    )
+    bsg = BatchedSearchGenerator(dummy_request, batch_size=2)
+    assert list(bsg._make_batched_values_from_batch_param_values()) == [
+        ['/files/ENCFFABC123/', '/files/ENCFFABC345/'],
+        ['/files/ENCFFABC567/', '/files/ENCFFABC789/'],
+        ['/files/ENCFFDEF123/', '/files/ENCFFDEF345/'],
+        ['/files/ENCFFDEF567/']
+    ]
+    bsg = BatchedSearchGenerator(dummy_request, batch_field='accession', batch_size=2)
+    assert list(bsg._make_batched_values_from_batch_param_values()) == []
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment&@id=/files/ENCFFABC123/'
+        '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
+        '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
+        '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
+        '&accession=ENCFFAAA111'
+    )
+    bsg = BatchedSearchGenerator(dummy_request, batch_field='accession')
+    assert next(bsg._make_batches_from_batch_params()) == ['ENCFFAAA111']
+
+
+def test_metadata_batched_search_generator_make_batched_params_from_batched_values(dummy_request):
+    from encoded.reports.metadata import BatchedSearchGenerator
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment&@id=/files/ENCFFABC123/'
+        '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
+        '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
+        '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
+    )
+    bsg = BatchedSearchGenerator(dummy_request, batch_size=2)
+    actual_batched_params = []
+    for batched_values in bsg._make_batched_values_from_batch_param_values():
+        actual_batched_params.append(
+            bsg._make_batched_params_from_batched_values(batched_values)
+        )
+    expected_batched_params = [
+        [('@id', '/files/ENCFFABC123/'), ('@id', '/files/ENCFFABC345/')],
+        [('@id', '/files/ENCFFABC567/'), ('@id', '/files/ENCFFABC789/')],
+        [('@id', '/files/ENCFFDEF123/'), ('@id', '/files/ENCFFDEF345/')],
+        [('@id', '/files/ENCFFDEF567/')]
+    ]
+    assert expected_batched_params == actual_batched_params
+
+
+
+def test_metadata_batched_search_generator_build_new_request(dummy_request):
+    from encoded.reports.metadata import BatchedSearchGenerator
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment&@id=/files/ENCFFABC123/'
+        '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
+        '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
+        '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
+    )
+    bsg = BatchedSearchGenerator(dummy_request, batch_size=2)
+    batched_params = [('@id', '/files/ENCFFABC123/'), ('@id', '/files/ENCFFABC345/')]
+    request = bsg._build_new_request(batched_params)
+    assert request.query_string == (
+        'type=Experiment'
+        '&%40id=%2Ffiles%2FENCFFABC123%2F'
+        '&%40id=%2Ffiles%2FENCFFABC345%2F'
+    )
+    assert request.path_info == '/search/'
+    assert request.registry
+    dummy_request.environ['QUERY_STRING'] = (
+        'type=Experiment&@id=/files/ENCFFABC123/'
+        '&@id=/files/ENCFFABC345/&@id=/files/ENCFFABC567/'
+        '&@id=/files/ENCFFABC789/&@id=/files/ENCFFDEF123/'
+        '&@id=/files/ENCFFDEF345/&@id=/files/ENCFFDEF567/'
+        '&field=accession&files.status=released'
+    )
+    bsg = BatchedSearchGenerator(dummy_request, batch_size=2)
+    batched_params = [('@id', '/files/ENCFFABC123/'), ('@id', '/files/ENCFFABC345/')]
+    request = bsg._build_new_request(batched_params)
+    assert request.query_string == (
+        'type=Experiment&field=accession&files.status=released'
+        '&%40id=%2Ffiles%2FENCFFABC123%2F'
+        '&%40id=%2Ffiles%2FENCFFABC345%2F'
+    )
+    assert request.path_info == '/search/'
+    assert request.registry
