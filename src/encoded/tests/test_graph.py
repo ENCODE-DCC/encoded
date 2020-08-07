6862f0764bfc5fc04846e2498114f546a0ae88b5
Casey Litton <caseylitton@gmail.com>
2020-08-06 17:47:19 -0700

    SNO-163-install-graphviz (#3460)

--- src/encoded/tests/test_graph.py
+++ src/encoded/tests/test_graph.py
@@ -16,3 +16,15 @@ def test_graph_svg(testapp):
     res = testapp.get('/profiles/graph.svg', status=200)
     assert res.content_type == 'image/svg+xml'
     assert res.text
+
+
+def test_graph_dot(testapp):
+    res = testapp.get('/profiles/graph.svg', status=200)
+    if not res.content_type == 'image/svg+xml' and res.json.get('status_code') == 404:
+        # graphviz is probably not installed
+        msg = res.json.get('message')
+        assert msg == 'graph.svg is not available'
+        # Force fail since graphviz is not installed on the system
+        assert False
+    assert res.content_type == 'image/svg+xml'
+    assert res.text
