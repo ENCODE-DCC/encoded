ddc0367961ba87b68353a8283e7461459fd4ac19
mingjiecn <mingjiecn@gmail.com>
2020-08-07 14:03:51 -0500

    change treatment chart range

--- src/encoded/static/components/radiation.js
+++ src/encoded/static/components/radiation.js
@@ -166,13 +166,11 @@ for (let i = 0; i < ganttData.length; i++) {
 }
 
 let diagnosisDate;
-let minX;
+let minX =new Date(this.props.first_treatment_date + ' 00:00:00');
 if (this.props.diagnosis_date != "Not available") {
   diagnosisDate = new Date(this.props.diagnosis_date + ' 00:00:00' );
-  minX = new Date(this.props.diagnosis_date + ' 00:00:00');;
-} else {
-  minX = new Date(ganttData[0].startDate);
-}
+  
+} 
 let deceasedDate;
 let lastFollowUpDate;
 let maxX;
@@ -323,6 +321,7 @@ this.plotly.newPlot(this.props.chartId, data, layout, this.plotlyConfig);
 }
 
 export default Radiation;
+
 
 
 
