392eacc76cb8ca64115a499223c25eeb7e629a5a
Pedro Assis <pedroh@stanford.edu>
2020-08-21 15:54:21 -0700

    layout changes for landing page

--- src/encoded/static/components/home.js
+++ src/encoded/static/components/home.js
@@ -137,32 +137,47 @@ class HomeContent extends React.Component {
     render() {
         return (
             <div>
-                <div className="overall-classic">
+                <div className="overall-classic homepage">
+                    <div className="header">
+                        <img src="static/img/lattice-cells.png" alt="Lattice DB" className="logo" />
+                        <h1>Lattice</h1>
+		    </div>
 
-                    <h1>Lattice DB: Seed Networks Database</h1>
-                    <div className="site-banner">
-                        <div className="site-banner-img">
+                    <div className="site-banner__intro main-content">
+                       <p>Lattice is the acting Data Coordination Center for the CZI Seed Networks.</p>
+                       <div className="quote">
+                          <i>"The Seed Networks for the Human Cell Atlas projects bring together experimental scientists, computational biologists, software engineers, and physicians to support the continued development of the Human Cell Atlas (HCA), an international effort to map all cells in the human body as a resource for better understanding health and disease." - <a href="https://chanzuckerberg.com/science/programs-resources/humancellatlas/seednetworks/" target="_blank">Human Cell Atlas Seed Networks</a></i>
+                        </div>
+                     </div>
+                     <div className="site-banner main-content">
+                        <div className="site-banner__intro text-side">
+                            <p>The Lattice team collaborates with researchers in each of the 38 Seed Networks to represent sample acquisition, experimental procedures, and data processing steps in structured metadata [<a href="/data-organization">learn more about our Data Organization</a>]. We wrangle metadata, raw data, and analysis outputs into the Lattice database which provides Seed Network contributors the opportunity for intra- and inter-Seed Network data sharing prior to data release. Through tight collaboration with the computational biologists in the Seed Networks, Lattice ensures essential information is captured to facilitate the generation of a reference atlas of the cells in the human body.</p>
+		            <p>As stewards for the Seed Network data, the Lattice team actively seeks out opportunities to maximize the value of the data through enhanced findability and reuse. We collaborate with other single-cell data centers to migrate the standardized data corpus from the Lattice database to their open data resources, allowing for Seed Network data to be easily integrated with community tools and data collections.</p>
+                        </div>
+                        <div className="img-side">
                             <img src="static/img/lattice-banner.png" alt="Lattice DB" />
-		            <p><strong>Figure.</strong> Data flow from Seed Networks into the Lattice DB and subsequently, to community resources.</p>
                         </div>
+                    </div>
 
-                        <div className="site-banner__intro">
-		            <p>Lattice is the acting Data Coordination Center for the CZI Seed Networks.</p>
-		            <i>The Seed Networks for the Human Cell Atlas projects bring together experimental scientists, computational biologists, software engineers, and physicians to support the continued development of the Human Cell Atlas (HCA), an international effort to map all cells in the human body as a resource for better understanding health and disease. - <a href="https://chanzuckerberg.com/science/programs-resources/humancellatlas/seednetworks/" target="_blank">CZI Seed Networks</a></i>
-		            <p>Lattice collaborates with researchers in each of the 38 Seed Networks to represent sample acquisition, experimental procedures, and data processing steps in structured metadata. We wrangle metadata, raw data, and analysis outputs into the Lattice database which provides Seed Network contributors the opportunity for intra- and inter-Seed Network data sharing prior to data release. Through tight collaboration with the computational biologists in the Seed Networks, Lattice ensures essential information is captured to facilitate the generation of a reference atlas of the cells in the human body.</p>
-		            <p>As the steward for the Seed Network data, Lattice will actively seek out opportunities to maximize the value of the data through enhanced findability and reuse. We collaborate with other single-cell data centers to migrate the standardized data corpus from the Lattice database to their open data resources, allowing for Seed Network data to be easily integrated with community tools and data collections.</p>
-                            <p>The Lattice team can be reached at <a href="mailto:lattice-info@lists.stanford.edu">lattice-info@lists.stanford.edu</a> for:</p>
-                            <ul>
-                                <li>Seed Network researchers interested in submitting data</li>
-                                <li>Community resources interested in integrating Seed Network data</li>
-                            </ul>
-                            <p>Resources</p>
-                            <ul>
-                                <li>Lattice <a href="https://github.com/Lattice-Data" target="_blank">Github repository</a></li>
-                                <li>Data organization strategy: server/data-organization</li>
-                                <li>Metadata schema: server/profiles</li>
-                            </ul>
-                        </div>
+                    <div className="site-banner__intro contacts">
+                       <p className="email-us">Contact the Lattice team at <a href="mailto:lattice-info@lists.stanford.edu"> lattice-info@lists.stanford.edu</a>.</p>
+		    </div>
+                    <div className="site-banner__intro contacts">
+                       <div className="contact-group">
+                          <p className="call-contact">Seed Network researchers</p>
+                          <p>Do you have data to submit?</p>
+                          <p>Have questions about metadata collection?</p>
+                       </div>
+                       <div className="contact-group">
+                          <p className="call-contact">Community data resources</p>
+                          <p>Interested in integrating Seed Network data?</p>
+                          <p>Have questions about the Lattice data model & schema?</p>
+                       </div>
+                    </div>
+                    <div className="site-banner__intro credits">
+                        <p>The Lattice team consists of data wranglers & software developers within the Cherry Lab at the Stanford University Department of Genetics. <a href="https://cherrylab.stanford.edu/people/human-cell-atlas/grid" target="_bank">Meet the team</a></p>
+                        <p className="italic">Lattice is funded by the Chan Zuckerberg Initiative (CZI).</p>
+                        <p className="italic">Lattice logo created by Idan Gabdank, Ph.D.</p>
                     </div>
                 </div>
             </div>
