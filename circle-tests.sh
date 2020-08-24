ddcf27b23e97bd95d83bb9b93402b9bfe3c10ad5
Keenan Graham <keenangraham@stanford.edu>
2020-08-24 10:49:23 -0700

    Update local tests and readme

--- circle-tests.sh
+++ circle-tests.sh
@@ -12,17 +12,22 @@
 ##
 
 if [ "$1" == "bdd" ]; then
-    bin/test -v -v --timeout=300 -m "bdd" --tb=short --splinter-implicit-wait 10 --splinter-webdriver chrome --splinter-socket-timeout 300 --chrome-options "--headless --disable-gpu --no-sandbox --disable-dev-shm-usage --disable-extensions --whitelisted-ips --window-size=1920,1080"
+    bin/test --exitfirst -s -vv -m "bdd" --tb=short --splinter-implicit-wait=10 --splinter-webdriver=chrome --splinter-socket-timeout=300 --splinter-session-scoped-browser=false --splinter-headless=true --chrome-options "--disable-gpu --no-sandbox --disable-dev-shm-usage --disable-extensions --whitelisted-ips --window-size=1920,1080"
     exit
 fi
 
-if [ "$1" == "not-bdd-indexing" ]; then
-    bin/test -s -vv -m "indexing"
+if [ "$1" == "indexing" ]; then
+    bin/test --exitfirst -s -vv -m "indexing"
+    exit
+fi
+
+if [ "$1" == "indexer" ]; then
+    bin/test --exitfirst -s -vv -m "indexer"
     exit
 fi
 
 if [ "$1" == "not-bdd-non-indexing" ]; then
-    bin/test -s -vv -m "not bdd and not indexing" 
+    bin/test --exitfirst -s -vv -m "not bdd and not indexing and not indexer"
     exit
 fi
 
