34484e549f6ef50937efa9d5bba14dafdb5f62d9
jason Hilton <jahilton@stanford.edu>
2020-08-31 12:41:56 -0700

    remove unnecessary library calcuated properties

--- src/encoded/types/library.py
+++ src/encoded/types/library.py
@@ -46,6 +46,7 @@ class Library(Item):
         'lab'
     ]
 
+
     @calculated_property(condition='derived_from', schema={
         "title": "Donor accessions",
         "type": "array",
@@ -65,39 +66,3 @@ class Library(Item):
             if 'organism' in props
         }
         return donor_accs
-
-    @calculated_property(condition='biological_macromolecule_term_name', schema={
-        "title": "Nucleic acid term ID",
-        "type": "string",
-    })
-    def biological_macromolecule_term_id(self, request, biological_macromolecule_term_name):
-        term_lookup = {
-            'DNA': 'SO:0000352',
-            'RNA': 'SO:0000356',
-            'polyadenylated mRNA': 'SO:0000871',
-            'miRNA': 'SO:0000276',
-            'protein': 'SO:0000104'
-        }
-        term_id = None
-        if biological_macromolecule_term_name in term_lookup:
-            term_id = term_lookup.get(biological_macromolecule_term_name)
-        return term_id
-
-    @calculated_property(condition='depleted_in_term_name', schema={
-        "title": "Depleted in term ID",
-        "type": "string",
-    })
-    def depleted_in_term_id(self, request, depleted_in_term_name):
-        term_lookup = {
-            'rRNA': 'SO:0000252',
-            'polyadenylated mRNA': 'SO:0000871',
-            'capped mRNA': 'SO:0000862'
-        }
-        term_id = list()
-        for term_name in depleted_in_term_name:
-            if term_name in term_lookup:
-                term_id.append(term_lookup.get(term_name))
-            else:
-                term_id.append('Term ID unknown')
-        return term_id
-
