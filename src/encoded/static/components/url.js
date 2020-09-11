52b6b6c3a570eeae3ccfac9c34e113f9947ce23e
Pedro Assis <pedroh@stanford.edu>
2020-09-11 11:06:35 -0700

    duplicate fix

--- src/encoded/static/components/url.js
+++ src/encoded/static/components/url.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import _ from 'underscore';
 
 
 export default function urlList(values) {
     // Render each of the links, with null for each value without an identifier property
     if (values && values.length > 0) {
-        const links = _.compact(values.map((value) => {
-            return values.map((value, index) =>
-                <li key={index}>
+        const links = values.map((value) => {
+            return (
+                <li>
                     <a href={value}>{value}</a>
                 </li>
             );
-        }));
+        });
 
-        // Render any links into a ul. Just return null if no links to render.
-        if (links.length > 0) {
-            return (
-                <ul>{links}</ul>
-            );
-        }
+        return (
+            <ul>{links}</ul>
+        );
     }
     return null;
 }
