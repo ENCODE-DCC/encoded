9796c9c71474f366fee2b23644b83e9cedbfffc7
Emma O'Neill <emmaruthoneill@gmail.com>
2020-09-11 14:23:15 -0700

    address code review, fix python

--- src/encoded/glossary.py
+++ src/encoded/glossary.py
@@ -1,13 +1,10 @@
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPBadRequest
-from urllib.parse import (
-    parse_qs,
-    urlencode,
-)
+
 
 def includeme(config):
     config.add_route('glossary', '/glossary{slash:/?}')
     config.scan(__name__)
+
 
 @view_config(route_name='glossary', request_method='GET', permission='search')
 def glossary(context, request):
