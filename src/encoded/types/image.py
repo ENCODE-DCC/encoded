5105c74b3768a6a0a7ace1a2ce3e0ee20479d7cc
Pedro Assis <pedroh@stanford.edu>
2020-09-17 13:20:31 -0700

    making released images public

--- src/encoded/types/image.py
+++ src/encoded/types/image.py
@@ -4,6 +4,8 @@ from snovault import (
 )
 from .base import (
     Item,
+    ALLOW_EVERYONE_VIEW,
+    ONLY_ADMIN_VIEW
 )
 from snovault.attachment import ItemWithAttachment
 
@@ -24,6 +26,12 @@ class Image(ItemWithAttachment, Item):
         'image/gif',
     ]
     embedded = ['submitted_by']
+
+    STATUS_ACL = {
+        'released': ALLOW_EVERYONE_VIEW,
+        'in progress': ONLY_ADMIN_VIEW,
+        'deleted': ONLY_ADMIN_VIEW
+    }
 
     def unique_keys(self, properties):
         keys = super(Image, self).unique_keys(properties)
