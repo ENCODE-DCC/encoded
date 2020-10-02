075eec52787f7ecc682e8357753dcdd90b7638ec
Bonita Lam <bonitarlam@gmail.com>
2020-10-02 15:30:15 -0700

    Script for human and mouse, update to test json

--- src/encoded/commands/generate_annotations.py
+++ src/encoded/commands/generate_annotations.py
@@ -6,8 +6,7 @@ import time
 
 EPILOG = __doc__
 
-_HUMAN_URL = 'https://www.encodeproject.org/search/?type=Gene&format=json&locations=*&field=geneid&field=name&field=symbol&field=synonyms&field=dbxrefs&field=locations&organism.scientific_name=Homo+sapiens&limit=all'
-_MOUSE_URL = 'https://www.encodeproject.org/search/?type=Gene&format=json&locations=*&field=geneid&field=name&field=symbol&field=synonyms&field=dbxrefs&field=locations&organism.scientific_name=Mus+musculus&limit=all'
+_GENE_URL = 'https://www.encodeproject.org/search/?type=Gene&format=json&locations=*&field=geneid&field=name&field=symbol&field=synonyms&field=dbxrefs&field=locations&field=organism.scientific_name&organism.scientific_name=Homo+sapiens&organism.scientific_name=Mus+musculus&limit=all'
 
 
 def get_annotation():
@@ -19,45 +18,51 @@ def get_annotation():
     }
 
 
-def human_annotations(url):
+def all_annotations(url):
     annotations = []
-    species = ' (homo sapiens)'
-    species_for_payload = re.split('[(|)]', species)[1]
     response = requests.get(url)
     for gene in response.json()['@graph']:
         doc = {'annotations': []}
-        if 'dbxrefs' in gene:
-            hgnc_id = [x for x in gene['dbxrefs'] if x.startswith('HGNC:')]
-            if len(hgnc_id) == 1:
-                hgnc_id = ''.join(hgnc_id)
-                if 'name' in gene:
-                    doc['suggest'] = {
-                        'input': [gene['name'] + species, gene['symbol'] + species, hgnc_id, gene['geneid'] + ' (Gene ID)']
-                    }
-                    doc['payload'] = {'id': hgnc_id, 'species': species_for_payload}
-                    doc['id'] = hgnc_id
 
-                if 'synonyms' in gene:
-                    synonyms = [s + species for s in gene['synonyms']]
-                    doc['suggest']['input'] = doc['suggest']['input'] + synonyms
+        if 'organism' in gene:
+            organism = gene['organism'].get('scientific_name')
+            if organism == 'Homo sapiens':
+                species = ' (homo sapiens)'
+            elif organism == 'Mus musculus':
+                species = ' (mus musculus)'
 
-                annotation = get_annotation()
+            if 'dbxrefs' in gene:
+                identifier = [x for x in gene['dbxrefs'] if x.startswith(('HGNC:', 'MGI:'))]
+                if len(identifier) == 1:
+                    identifier = ''.join(identifier)
+                    if 'name' in gene:
+                        species_for_payload = re.split('[(|)]', species)[1]
+                        doc['suggest'] = {
+                            'input': [gene['name'] + species, gene['symbol'] + species, identifier, gene['geneid'] + ' (Gene ID)']
+                        }
+                        doc['payload'] = {'id': identifier, 'species': species_for_payload}
+                        doc['id'] = identifier
 
-                if 'locations' in gene:
-                    for location in gene['locations']:
-                        annotation['assembly_name'] = location['assembly']
-                        annotation['chromosome'] = location['chromosome'][3:]
-                        annotation['start'] = location['start']
-                        annotation['end'] = location['end']
-                        doc['annotations'].append(annotation)
+                    if 'synonyms' in gene:
+                        synonyms = [s + species for s in gene['synonyms']]
+                        doc['suggest']['input'] = doc['suggest']['input'] + synonyms
 
-                annotations.append({
-                    'index': {
-                        '_index': 'annotations',
-                        '_type': 'default',
-                        '_id': doc['id']
-                    }
-                })
+                    if 'locations' in gene:
+                        for location in gene['locations']:
+                            annotation = get_annotation()
+                            annotation['assembly_name'] = location['assembly']
+                            annotation['chromosome'] = location['chromosome'][3:]
+                            annotation['start'] = location['start']
+                            annotation['end'] = location['end']
+                            doc['annotations'].append(annotation)
+
+                    annotations.append({
+                        'index': {
+                            '_index': 'annotations',
+                            '_type': 'default',
+                            '_id': doc['id']
+                        }
+                    })
 
         annotations.append(doc)
 
@@ -66,7 +71,7 @@ def human_annotations(url):
 
 def main():
     '''
-    Get annotations from multiple sources
+    Get annotations from portal Gene objects
     This helps to implement autocomplete for region search
     '''
     import argparse
@@ -75,8 +80,8 @@ def main():
         epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    human = human_annotations(_HUMAN_URL)
-    annotations = human
+
+    annotations = all_annotations(_GENE_URL)
 
     # Create annotations JSON file
     with open('annotations_local.json', 'w') as outfile:
--- src/encoded/docs/updating_annotations.md
+++ src/encoded/docs/updating_annotations.md
@@ -1,69 +1,66 @@
 Updating annotations
 =========================
 
-This document describes how to update the annotations used for searching and validation in the encoded application, ```annotations.json``` .
+This document describes how to update the annotations used for searching and validation in the encoded application, ```annotations.json```.
 
 Annotations used
 ---------------- 
 
-* [Ensembl Gene ID (Ensembl)]
 * [Entrez Gene ID (NCBI)]
+* [Refseq (NCBI)]
 * [HUGO Gene Nomenclature Committee HGNC ID (HGNC)]
 * [Mouse Genome Informatics (MGI)]
+* [ENCODE Genes]
 
 How to update the annotations versions
 ---------------- 
 
-1. Annotation files to use:
+1. The ```annotations.json``` uses official identifiers for the annotated genes. We retrieve location information from our Gene objects ```locations``` property. The ```locations``` property uses RefSeq identifiers in gff assembly files from [NCBI Genomes] to obtain coordinates. The RefSeq IDs used can be found in the ```dbxrefs``` property of the Gene objects.
 	
-	* Human (HGNC file): tsv using HGNC [HUGO Gene Nomenclature Committee HGNC ID (HGNC)]
-	* Mouse (Mouse file): tsv using MGI [Mouse Genome Informatics (MGI)]
+	* Human identifiers: [HUGO Gene Nomenclature Committee HGNC ID (HGNC)]
+	* Mouse identifiers: [Mouse Genome Informatics (MGI)]
+	* All gene identifiers: [Entrez Gene ID (NCBI)]
+	* RefSeq identifiers: [Refseq (NCBI)]
 
-2. Archive the old annotation files in the reference fileset [ENCSR763ATQ] used to generate the prior ```annotations.json``` . The archived file should have a *superseded_by* value for the new file that replaces it.
 
-3. Update the path to new tsv files uploaded on portal in [generate_annotations.py]
-
-4. Run generate-annotations on demo machine as encoded user:
+2. Run [generate-annotations] locally to generate a new ```annotations_local.json``` file:
 ```
-cd encoded/srv/encoded/
-sudo -su encoded
-bin/generate-annotations development.ini --app-name app
+$ bin/generate-annotations
 ```
 
-5. Transfer ```annotations_local.json``` to local machine:
+3. Rename the ```annotations_local.json``` to one with the date that it was generated:
 ```
-scp ubuntu@PublicDNS(IPv4):/srv/encoded/annotations_local.json annotations_local.json
-```
-
-6. Rename the ```annotations_local.json``` to one with the date that it was generated:
-```
-cp annotations_local.json annotations_YYYY_MM_DD.json
+$ cp annotations_local.json annotations_YYYY_MM_DD.json
 ```
 
-7. Load new annotations file into the encoded-build/annotations directory on S3
+4. Load new annotations file into the encoded-build/annotations directory on S3
 ```
-aws s3 cp annotations_YYYY_MM_DD.json s3://encoded-build/annotations/
+$ aws s3 cp annotations_YYYY_MM_DD.json s3://encoded-build/annotations/
 ```
 
-8.  Update the annotations version in the [buildout.cfg]:
+5.  Update the annotations version in the [buildout.cfg]:
+```
+curl -o annotations.json https://s3-us-west-1.amazonaws.com/encoded-build/annotations/annotations_YYYY_MM_DD.json
+```
 
-	curl -o annotations.json https://s3-us-west-1.amazonaws.com/encoded-build/annotations/annotations_YYYY_MM_DD.json
-
-9.  Update the following information
+6.  Update the following information
     
-    Site release version: 92
-    annotations.json file: annotations-2019-10-18.json
-    [Ensembl release date]: 2020-08-20
-	[MGI release date]: 2020-03-09
-	[HGNC release date]: 2020-09-02
+	* Site release version: 108
+	* annotations.json file: annotations-2020-10-02.json
+	* [GRCh38 gff assembly release date]: 2019-02-28
+	* [hg19 gff assembly release date]: 2013-06-28
+	* [mm10 gff assembly release date]: 2017-09-15
+	* [mm9 gff assembly release date]: 2010-10-21
 
-[Ensembl Gene ID (Ensembl)]: http://ensembl.org/
+[ENCODE Genes]: https://www.encodeproject.org/search/?type=Gene
+[NCBI Genomes]: https://ftp.ncbi.nlm.nih.gov/genomes/
+[Refseq (NCBI)]: https://www.ncbi.nlm.nih.gov/refseq/
 [Entrez Gene ID (NCBI)]: http://ncbi.nlm.nih.gov/gene/
 [HUGO Gene Nomenclature Committee HGNC ID (HGNC)]: http://genenames.org
 [Mouse Genome Informatics (MGI)]: http://informatics.jax.org
-[ENCSR763ATQ]: http://www.encodeproject.org/references/ENCSR763ATQ/
 [buildout.cfg]: ../../../buildout.cfg
-[generate_annotations.py]: ../../../src/encoded/commands/generate_annotations.py
-[Ensembl release date]: http://ensembl.info
-[MGI release date]: http://informatics.jax.org/mgihome/other/mgicron.shtml
-[HGNC release date]: http://www.genenames.org/download/statistics-and-files/
+[generate_annotations]: https://github.com/ENCODE-DCC/encoded/blob/dev/src/encoded/commands/generate_annotations.py
+[GRCh38 gff assembly release date]: https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.39/
+[hg19 gff assembly release date]: https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.25/
+[mm10 gff assembly release date]: https://www.ncbi.nlm.nih.gov/assembly/GCF_000001635.26/
+[mm9 gff assembly release date]: https://www.ncbi.nlm.nih.gov/assembly/GCF_000001635.18
