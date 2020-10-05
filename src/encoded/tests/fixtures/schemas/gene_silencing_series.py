bcd9b895fdaff21dbabc6eb3b5459d2ec74ef228
Idan Gabdank <gabdank@stanford.edu>
2020-10-05 08:54:09 -0700

    fixing a typo in the fixture

--- src/encoded/tests/fixtures/schemas/gene_silencing_series.py
+++ src/encoded/tests/fixtures/schemas/gene_silencing_series.py
@@ -8,4 +8,4 @@ def base_gene_silencing_series(testapp, lab, base_experiment_submitted, award):
         'lab': lab['uuid'],
         'related_datasets': [base_experiment_submitted['@id']]
     }
-    return testapp.post_json('/single_cell_rna_series', item, status=201).json['@graph'][0]
+    return testapp.post_json('/gene_silencing_series', item, status=201).json['@graph'][0]
