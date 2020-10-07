42d1ab6493a8b764c7085d18ef627b26b6d852de
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2020-10-07 15:21:59 -0500

    comment out upgrade.

--- cloud-config/run-scripts/app-wrapper.sh
+++ cloud-config/run-scripts/app-wrapper.sh
@@ -77,9 +77,9 @@ if [ -z "$ENCD_PG_IP" ]; then
 fi
 $ENCD_SCRIPTS_DIR/app-encd.sh
 sudo -u root $ENCD_SCRIPTS_DIR/app-a2en.sh
-if [ "$ENCD_BATCHUPGRADE" == "true" ]; then
-    $ENCD_SCRIPTS_DIR/app-batchupgrade.sh production.ini $BUP_VAR_1 $BUP_VAR_2 $BUP_VAR_3 $BUP_VAR_4
-fi
+#if [ "$ENCD_BATCHUPGRADE" == "true" ]; then
+#    $ENCD_SCRIPTS_DIR/app-batchupgrade.sh production.ini $BUP_VAR_1 $BUP_VAR_2 $BUP_VAR_3 $BUP_VAR_4
+#fi
 $ENCD_SCRIPTS_DIR/app-final.sh
 
 # Set flag, eventhough the install could have failed
