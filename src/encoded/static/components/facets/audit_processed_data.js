777ce1e0f09889f35ec2698c8ea367f744860c61
Forrest Tanaka <fytanaka@stanford.edu>
2020-10-23 13:42:56 -0700

    ENCD-5559-fcc-facet
    Converted from boolean switch to dropdown.

--- src/encoded/static/components/facets/audit_processed_data.js
+++ src/encoded/static/components/facets/audit_processed_data.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import QueryString from '../../libs/query_string';
-import BooleanSwitch from '../../libs/ui/boolean-switch';
 import { SpecialFacetRegistry } from './registry';
 
 
 /**
- * Special facet to display a switch to select between having the search result query string
- * contain: "audit.WARNING.category!=lacking processed data" and without that term at all, with the
- * boolean switch component showing the "on" state with the former and the "off" state with the
- * latter.
+ * Special facet to display a dropdown to select between having the search result query string
+ * contain "audit.WARNING.category!=lacking processed data" and without that term at all.
  */
 const AuditProcessedData = ({ results, queryString }, reactContext) => {
-    /** Keeps track of the switch state for `handleSwitch` */
-    const switchState = React.useRef(false);
-
     /**
-     * Called when the user clicks the term to flip the switch and modify the query string
-     * accordingly.
+     * Called when the user changes the dropdown to a new value.
      */
-    const handleSwitch = () => {
+    const handleChange = (event) => {
         const query = new QueryString(queryString);
-        if (switchState.current) {
+        if (event.target.value === 'all-data') {
+            // "All data" selected. Clear out any audit.WARNING.category from the query string.
             query.deleteKeyValue('audit.WARNING.category');
         } else {
+            // "Processed data" selected. Replace any audit.WARNING.category in the query string
+            // with audit.WARNING.category!=lacking+processed+data.
             query.replaceKeyValue('audit.WARNING.category', 'lacking processed data', true);
         }
         const href = `?${query.format()}`;
@@ -37,17 +33,14 @@ const AuditProcessedData = ({ results, queryString }, reactContext) => {
         // `handleSwitch` doesn't have to do this same calculation later.
         const query = new QueryString(queryString);
         const auditWarningValues = query.getKeyValues('audit.WARNING.category', true);
-        switchState.current = auditWarningValues.length === 1 && auditWarningValues[0] === 'lacking processed data';
+        const dataSelection = (auditWarningValues.length === 1 && auditWarningValues[0] === 'lacking processed data') ? 'processed-data' : 'all-data';
 
         return (
             <div className="facet facet--audit-warning">
-                <BooleanSwitch
-                    id="facet-audit-warning"
-                    state={switchState.current}
-                    title={switchState.current ? 'With processed data' : 'All data'}
-                    triggerHandler={handleSwitch}
-                    options={{ cssTitle: 'facet-audit-switch-title'}}
-                />
+                <select value={dataSelection} onChange={handleChange}>
+                    <option value="processed-data">With processed data</option>
+                    <option value="all-data">All data</option>
+                </select>
             </div>
         );
     }
