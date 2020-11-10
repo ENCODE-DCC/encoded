1c3d3e83c67128fb1d1d5256ff5f1836bccce6b7
emro <emmaruthoneill@gmail.com>
2020-11-10 15:35:09 -0800

    ENCD-5566 fix cropped tooltips (#3588)

--- src/encoded/static/libs/ui/tooltip.js
+++ src/encoded/static/libs/ui/tooltip.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+
+// Render tooltips to the document body
+// This prevents cropping when the tooltips overflow their parent containers
+// Code modified from: http://jamesknelson.com/rendering-react-components-to-the-document-body/
+class RenderInBody extends React.Component {
+    componentDidMount() {
+        this.popup = document.createElement('div');
+        document.body.appendChild(this.popup);
+        this._renderLayer();
+    }
+
+    componentDidUpdate() {
+        this._renderLayer();
+    }
+
+    componentWillUnmount() {
+        ReactDOM.unmountComponentAtNode(this.popup);
+        document.body.removeChild(this.popup);
+    }
+
+    _renderLayer() {
+        ReactDOM.render(this.props.children, this.popup);
+    }
+
+    render() {
+        // Render a placeholder
+        return <div />;
+    }
+}
+
+RenderInBody.propTypes = {
+    children: PropTypes.object.isRequired,
+};
 
 // This module lets you have a tooltip pop up when any component is hovered over or focused. The
 // general usage is:
@@ -14,41 +48,62 @@ import PropTypes from 'prop-types';
 // that's all you need, or a full-fledged component.
 
 const Tooltip = (props) => {
-    const [showDefinition, setDefinition] = React.useState(false);
+    const [placeholder, setPlaceholder] = React.useState(null);
+    const [visibility, setVisibility] = React.useState(null);
+    const [eType, setEType] = React.useState(null);
     const [isMobile, setIsMobile] = React.useState(false);
-    const [tooltipPosition, setTooltipPosition] = React.useState(0);
+    const [tooltipLeft, setTooltipLeft] = React.useState(0);
+    const [tooltipTop, setTooltipTop] = React.useState(0);
     const { trigger, position, tooltipId, css, innerCss, children, timerFlag, relativeTooltipFlag } = props;
     const wrapperCss = `tooltip-container${css ? ` ${css}` : ''}`;
     const tooltipCss = `${innerCss || `tooltip ${position}`}`;
     const buttonRef = React.useRef(null);
+    const timeoutRef = React.useRef(null);
 
     // Depending on the tooltip trigger location on the viewport, we may need to move the tooltip bubble right or left to prevent cropping
     // Overlap to the right of the page is stored as a negative number, overlap to the left of the page is stored as a positive number and are used to set left margins
     // The tooltip bubble is assumed to be 250px wide and an update is required if that is no longer true
     // Code could determine the width of the tooltip upon load but then the tooltip will need to be rendered in an incorrect position -- or the mechanics of hiding/showing the tooltip would need to be changed
     const positionTooltip = React.useCallback(() => {
+        // Determining the position of the tooltip trigger and accounting for vertical scroll
+        const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
         const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-        const tooltipRight = buttonRef.current.getBoundingClientRect().right;
-        const tooltipLeft = buttonRef.current.getBoundingClientRect().left;
-        const rightOverlap = viewportWidth - (tooltipRight + 125);
-        const leftOverlap = tooltipLeft - 125;
+        const tooltipTriggerRight = buttonRef.current.getBoundingClientRect().right;
+        const tooltipTriggerLeft = buttonRef.current.getBoundingClientRect().left;
+        const tooltipTriggerTop = scrollTop + buttonRef.current.getBoundingClientRect().top;
+        // Ascertaining if tooltip bubble needs to be placed to the left or right to stay on document body
+        const rightOverlap = viewportWidth - (tooltipTriggerRight + 125);
+        const leftOverlap = tooltipTriggerLeft - 125;
         const tooltipOverlap = (rightOverlap < 0) ? rightOverlap : (leftOverlap < 0) ? -leftOverlap : 0;
-        setTooltipPosition(tooltipOverlap);
+        // Setting tooltip bubble parameters
+        setTooltipLeft(tooltipTriggerLeft + tooltipOverlap);
+        setTooltipTop(tooltipTriggerTop + 20);
     }, []);
 
     // Display or hide tooltip pop-up
-    const setDefinitionVisibility = (param, e) => {
+    const SetDefinitionVisibility = (param, e) => {
+        // Conditional is for mobile: if link is clicked within tooltip bubble, "blur" event executes after tooltip bubble "mouseenter" event and closes the bubble, preventing link execution
+        if (!(e.type === 'blur' && eType === 'mouseenter')) {
+            setPlaceholder(param);
+        }
+        setEType(e.type);
+    };
+
+    React.useEffect(() => {
+        if (timeoutRef.current !== null) {
+            clearTimeout(timeoutRef.current);
+        }
         // Set delay on hiding the pop-up on "mouseleave" unless timerFlag is set to false
-        if (timerFlag && e.type === 'mouseleave') {
-            setTimeout(() => {
-                setDefinition(param);
+        if (timerFlag && eType === 'mouseleave') {
+            timeoutRef.current = setTimeout(() => {
+                setVisibility(placeholder);
                 positionTooltip();
             }, 400);
         } else {
-            setDefinition(param);
+            setVisibility(placeholder);
             positionTooltip();
         }
-    };
+    }, [eType, placeholder, positionTooltip, timerFlag, visibility]);
 
     // Check to see if device is mobile (small width with touch screen)
     React.useEffect(() => {
@@ -68,22 +123,26 @@ const Tooltip = (props) => {
         return (
             <React.Fragment>
                 <button
-                    aria-describedby={showDefinition ? tooltipId : ''}
-                    onMouseEnter={e => setDefinitionVisibility(true, e)}
-                    onMouseLeave={e => setDefinitionVisibility(false, e)}
-                    onFocus={e => setDefinitionVisibility(true, e)}
-                    onBlur={e => setDefinitionVisibility(false, e)}
-                    className={`tooltip-container__trigger ${showDefinition ? 'show' : ''}`}
+                    aria-describedby={visibility ? tooltipId : ''}
+                    onMouseEnter={e => SetDefinitionVisibility(true, e)}
+                    onMouseLeave={e => SetDefinitionVisibility(false, e)}
+                    onFocus={e => SetDefinitionVisibility(true, e)}
+                    onBlur={e => SetDefinitionVisibility(false, e)}
+                    className={`tooltip-container__trigger ${visibility ? 'show' : ''}`}
                     ref={buttonRef}
                     type="button"
                 >
                     {trigger}
                 </button>
-                {showDefinition ?
+                {(visibility && children) ?
                     <div
                         className={tooltipCss}
                         role="tooltip"
                         id={tooltipId}
+                        onMouseEnter={e => SetDefinitionVisibility(true, e)}
+                        onMouseLeave={e => SetDefinitionVisibility(false, e)}
+                        onFocus={e => SetDefinitionVisibility(true, e)}
+                        onBlur={e => SetDefinitionVisibility(false, e)}
                     >
                         <div className="tooltip-inner">{children}</div>
                     </div>
@@ -94,31 +153,35 @@ const Tooltip = (props) => {
     return (
         <div className={wrapperCss}>
             <button
-                aria-describedby={showDefinition ? tooltipId : ''}
-                onMouseEnter={e => setDefinitionVisibility(true, e)}
-                onMouseLeave={e => setDefinitionVisibility(false, e)}
-                onFocus={e => setDefinitionVisibility(true, e)}
-                onBlur={e => setDefinitionVisibility(false, e)}
-                className={`tooltip-container__trigger ${showDefinition ? 'show' : ''}`}
+                aria-describedby={visibility ? tooltipId : ''}
+                onMouseEnter={e => SetDefinitionVisibility(true, e)}
+                onMouseLeave={e => SetDefinitionVisibility(false, e)}
+                onFocus={e => SetDefinitionVisibility(true, e)}
+                onBlur={e => SetDefinitionVisibility(false, e)}
+                className={`tooltip-container__trigger ${visibility ? 'show' : ''}`}
                 ref={buttonRef}
                 type="button"
             >
                 {trigger}
             </button>
-            {showDefinition ?
-                <div
-                    className={tooltipCss}
-                    role="tooltip"
-                    id={tooltipId}
-                >
-                    <div className="tooltip-arrow" />
+            {(visibility && children) ?
+                <RenderInBody>
                     <div
-                        className="tooltip-inner"
-                        style={(tooltipPosition < 0) ? ({ marginLeft: `${tooltipPosition * 2}px` }) : (tooltipPosition > 0) ? ({ marginLeft: `${tooltipPosition * 2}px` }) : ({})}
+                        className={tooltipCss}
+                        role="tooltip"
+                        id={tooltipId}
+                        style={{ left: `${tooltipLeft}px`, top: `${tooltipTop}px` }}
+                        onMouseEnter={e => SetDefinitionVisibility(true, e)}
+                        onMouseLeave={e => SetDefinitionVisibility(false, e)}
+                        onFocus={e => SetDefinitionVisibility(true, e)}
+                        onBlur={e => SetDefinitionVisibility(false, e)}
                     >
-                        {children}
+                        <div className="tooltip-arrow" />
+                        <div className="tooltip-inner">
+                            {children}
+                        </div>
                     </div>
-                </div>
+                </RenderInBody>
             : null}
         </div>
     );
