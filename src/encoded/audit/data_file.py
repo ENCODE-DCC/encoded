83c5207ad6a120436a03a0a57b9e70bed965c494
jason Hilton <jahilton@stanford.edu>
2020-11-15 15:12:08 -0800

    add genome_annotation audit for derived_froms consistency, add suspension audit checking donor

--- src/encoded/audit/data_file.py
+++ src/encoded/audit/data_file.py
@@ -30,12 +30,33 @@ def audit_file_assembly(value, system):
             return
 
 
+def audit_file_genome_annotation(value, system):
+    if 'derived_from' not in value:
+        return
+    for f in value['derived_from']:
+        if f.get('genome_annotation') and value.get('genome_annotation') and \
+           f.get('genome_annotation') != value.get('genome_annotation'):
+            detail = ('Processed file {} genome_annotation {} '
+                'does not match genome_annotation {} of the file {} '
+                'it was derived from.'.format(
+                    audit_link(path_to_text(value['@id']), value['@id']),
+                    value['genome_annotation'],
+                    f['genome_annotation'],
+                    audit_link(path_to_text(f['@id']), f['@id'])
+                )
+            )
+            yield AuditFailure('inconsistent genome_annotation',
+                               detail, level='WARNING')
+            return
+
+
 function_dispatcher = {
-    'audit_file_assembly': audit_file_assembly
+    'audit_file_assembly': audit_file_assembly,
+    'audit_file_genome_annotation': audit_file_genome_annotation
 }
 
 
-@audit_checker('DataFile',
+@audit_checker('File',
                frame=['derived_from',
                       'library',
                       'dataset',
--- src/encoded/audit/suspension.py
+++ src/encoded/audit/suspension.py
@@ -8,15 +8,15 @@ from .formatter import (
 )
 
 
-def audit_biosample_donor(value, system):
+def audit_suspension_donor(value, system):
     '''
-    A biosample should have a donor.
+    A Suspension should have a donor.
     '''
     if value['status'] in ['deleted']:
         return
 
     if not value['donors']:
-        detail = ('Biosample {} is not associated with any donor.'.format(
+        detail = ('Suspension {} is not associated with any donor.'.format(
             audit_link(value['accession'], value['@id'])
             )
         )
@@ -25,12 +25,12 @@ def audit_biosample_donor(value, system):
 
 
 function_dispatcher = {
-    'audit_donor': audit_biosample_donor,
+    'audit_donor': audit_suspension_donor,
 }
 
-@audit_checker('Biosample',
+@audit_checker('Suspension',
                frame=['donors'])
-def audit_biosample(value, system):
+def audit_suspension(value, system):
     for function_name in function_dispatcher.keys():
         for failure in function_dispatcher[function_name](value, system):
             yield failure
