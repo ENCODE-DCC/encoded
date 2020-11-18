8596bb3e40718fef9d7d38dd4259896527b529fa
Phil <zoldello@gmail.com>
2020-11-18 14:38:40 -0800

    ENCD-5356-add-examined-loci-on-FCC-experiments (#3624)

--- src/encoded/static/components/experiment.js
+++ src/encoded/static/components/experiment.js
@@ -16,6 +16,7 @@ import { SortTablePanel, SortTable } from './sorttable';
 import Status from './status';
 import { BiosampleSummaryString, BiosampleOrganismNames, CollectBiosampleDocs, AwardRef, ReplacementAccessions, ControllingExperiments, ExperimentTable } from './typeutils';
 import Tooltip from '../libs/ui/tooltip';
+import getNumberWithOrdinal from '../libs/ordinal_suffix';
 
 
 const anisogenicValues = [
@@ -656,6 +657,26 @@ const ExperimentComponent = ({ context, auditIndicators, auditDetail }, reactCon
                                 <div data-test="biosample-type">
                                     <dt>Biosample Type</dt>
                                     <dd>{context.biosample_ontology.classification}</dd>
+                                </div>
+                            : null}
+
+                            {context.examined_loci && context.examined_loci.length > 0 ?
+                                <div data-test="examined-loci">
+                                    <dt>Examined Loci</dt>
+                                    <dd>
+                                        {/* A user can have a loci repeat. Therefore, uuid alone is not sufficient as an identifier */}
+                                        {context.examined_loci.map((loci, i) => (
+                                            loci.gene ?
+                                                <span key={`${loci.gene.uuid}-${i}`}>
+                                                    {i > 0 ? <span>, </span> : null}
+                                                    <a href={loci.gene['@id']}>{loci.gene.symbol}</a>
+                                                    {/* 0 is falsy but we still want it to display, so 0 is explicitly checked for */}
+                                                    {loci.expression_percentile || loci.expression_percentile === 0 ? <span>{' '}({getNumberWithOrdinal(loci.expression_percentile)} percentile)</span> : null}
+                                                    {(loci.expression_range_maximum && loci.expression_range_minimum) || (loci.expression_range_maximum === 0 || loci.expression_range_minimum === 0) ? <span>{' '}({loci.expression_range_minimum}-{loci.expression_range_maximum}%)</span> : null}
+                                                </span>
+                                            : null
+                                        ))}
+                                    </dd>
                                 </div>
                             : null}
 
