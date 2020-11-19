2cd3dd44e00650c95e71c037ab1d3923ac747321
jason Hilton <jahilton@stanford.edu>
2020-11-18 16:14:21 -0800

    add audit to flag if a sample has a death property but is from a living donor

--- src/encoded/audit/biosample.py
+++ src/encoded/audit/biosample.py
@@ -24,8 +24,32 @@ def audit_biosample_donor(value, system):
         return
 
 
+def audit_death_prop_living_donor(value, system):
+    '''
+    A biosample should not have a property indicating time since death
+    if it is associated with a living donor.
+    '''
+    if value['status'] in ['deleted']:
+        return
+
+    living_donor_flag = False
+    for donor in value['donors']:
+        if donor.get('living_at_sample_collection') == True:
+            living_donor_flag = True
+    for death_prop in ['death_to_preservation_interval']:
+        if living_donor_flag == True and value.get(death_prop):
+            detail = ('Biosample {} has property {} but is associated with at least one donor that is living at sample collection.'.format(
+                audit_link(value['accession'], value['@id']),
+                death_prop
+                )
+            )
+            yield AuditFailure('death interval for living donor', detail, level='ERROR')
+            return
+
+
 function_dispatcher = {
     'audit_donor': audit_biosample_donor,
+    'audit_death_prop_living_donor': audit_death_prop_living_donor
 }
 
 @audit_checker('Biosample',
--- src/encoded/audit/suspension.py
+++ src/encoded/audit/suspension.py
@@ -24,8 +24,32 @@ def audit_suspension_donor(value, system):
         return
 
 
+def audit_death_prop_living_donor(value, system):
+    '''
+    A suspension should not have a property indicating time since death
+    if it is associated with a living donor.
+    '''
+    if value['status'] in ['deleted']:
+        return
+
+    living_donor_flag = False
+    for donor in value['donors']:
+        if donor.get('living_at_sample_collection') == True:
+            living_donor_flag = True
+    for death_prop in ['death_to_dissociation_interval']:
+        if living_donor_flag == True and value.get(death_prop):
+            detail = ('Biosample {} has property {} but is associated with at least one donor that is living at sample collection.'.format(
+                audit_link(value['accession'], value['@id']),
+                death_prop
+                )
+            )
+            yield AuditFailure('death interval for living donor', detail, level='ERROR')
+            return
+
+
 function_dispatcher = {
     'audit_donor': audit_suspension_donor,
+    'audit_death_prop_living_donor': audit_death_prop_living_donor
 }
 
 @audit_checker('Suspension',
