8f9f340ceb52f2fd600ce8e8a8d54c2cabc4442b
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2020-12-16 09:41:19 -0600

    Add back breadcrubs.

--- src/encoded/static/components/biospecimen.js
+++ src/encoded/static/components/biospecimen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, PanelBody, PanelHeading } from '../libs/ui/panel';
 import * as globals from './globals';
-import { ItemAccessories } from './navigation';
+import { ItemAccessories, Breadcrumbs } from './navigation';
 import Status from './status';
 import CollapsiblePanel from './collapsiblePanel';
 import { PanelLookup } from './objectutils';
