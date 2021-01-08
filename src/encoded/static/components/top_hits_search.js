58a7cdb8bbbd431e93ec919d3108a95357115312
Keenan Graham <keenangraham@stanford.edu>
2021-01-07 19:02:27 -0800

    Random mismash

--- src/encoded/static/components/top_hits_search.js
+++ src/encoded/static/components/top_hits_search.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as globals from './globals';
 import {InputSuggest} from './home';
 
 
 const topHitsUrl = '/top-hits-raw/';
+const topHitsParams = (
+    '&field=description&field=accession' +
+    '&field=title&field=biosample_summary' +
+    '&format=json'
+);
+
+
+class TopHits {
+    constructor() {
+
+    }
+}
+
+
+class Hit extends React.Component {
+
+    asString() {
+        const embedded = this.props.embedded;
+        return `${embedded.accession || embedded["@id"]} - ${embedded.description || embedded.title || embedded.biosample_summary}`;
+    }
+
+    render() {
+        return <h5>{this.asString()}</h5>;
+    }
+}
+
+
+function Type(props) {
+    return <h3>{props.type}</h3>;
+}
+
+
+function Hits(props) {
+    return props.hits.map(
+        hit => {
+            const embedded = hit._source.embedded;
+            return <Hit key={embedded["@id"]} embedded={embedded} />
+        }
+    );
+}
+
+
+function HitsByType(props) {
+    return (
+        <div>
+            <Type type={props.type} />
+            <Hits hits={props.hits} />
+        </div>
+    );
+}
 
 
 export class TopHitsSearch extends React.Component {
     constructor() {
         super();
         this.state = {
+            a: null,
             currentSearchTerm: '',
             suggestedSearchTerms: [],
         };
@@ -30,30 +81,24 @@ export class TopHitsSearch extends React.Component {
         }
     }
 
-    parseHit(hit) {
-        const embedded = hit._source.embedded;
-        return `${embedded.accession || embedded["@id"]} - ${embedded.description || embedded.title || embedded.biosample_summary}`
-    }
-
     parseHits(hits, type) {
-        return hits.map(
-            hit => `${this.parseHit(hit)} - ${type}`
-        );
+        return <HitsByType key={type} hits={hits} type={type} />;
     }
 
     parseResults(results) {
         let parsedResults = results.aggregations.types.types.buckets.map(
             result => this.parseHits(result.top_hits.hits.hits, result.key)
         );
-        return parsedResults.flat();
+        return parsedResults;
     }
 
-    fetchTopHits(newSearchTerm) {
-        let topHitsUrlWithParams = (
-            `${topHitsUrl}?searchTerm=${newSearchTerm}&field=description&field=accession&field=title&field=biosample_summary&format=json`
-        );
+    makeTopHitsUrl(newSearchTerm) {
+        return `${topHitsUrl}?searchTerm=${newSearchTerm}${topHitsParams}`;
+    }
+
+    fetchTopHits(url) {
         return fetch(
-            topHitsUrlWithParams,
+            url,
             {
                 method: 'GET',
                 headers: {
@@ -64,7 +109,7 @@ export class TopHitsSearch extends React.Component {
             response => (response.ok ? response.json() : [])
         ).then(
             results => {
-                return this.parseResults(results).flat();
+                return this.parseResults(results);
             }
         );
     }
@@ -72,9 +117,10 @@ export class TopHitsSearch extends React.Component {
     updateTopHits(newSearchTerm) {
         this.startDelayTimer();
         this.lastSearchTerm = newSearchTerm;
-        this.fetchTopHits(newSearchTerm).then(
+        const url = this.makeTopHitsUrl(newSearchTerm);
+        this.fetchTopHits(url).then(
             results => this.setState(
-                {suggestedSearchTerms: results}
+                {suggestedSearchTerms: results, a: results}
             )
         );
     }
@@ -119,10 +165,11 @@ export class TopHitsSearch extends React.Component {
         const context = this.props.context;
         const isSearchDisabled = this.state.currentSearchTerm.length === 0;
         return (
+            <div>
             <div className="site-search__screen">
                 <form>
                     <fieldset>
-                        <legend className="sr-only">Top hits search</legend>
+                <legend className="sr-only">Top hits search</legend>
                         <div className="site-search__input">
                             <label htmlFor="top-hits-search" id="top-hits-search-label">
                                 Search for top hits by type
@@ -138,24 +185,113 @@ export class TopHitsSearch extends React.Component {
                                 itemSelectHandler={this.termSelectHandler}
                             />
                         </div>
-                        <div className="site-search__submit">
-                            <a
-                                disabled={isSearchDisabled}
-                                aria-label="Human GRCh38 search"
-                                title="Human GRCh38 search"
-                                className="btn btn-info btn-sm site-search__submit-element"
-                                role="button"
-                                href={`/search/?searchTerm=${this.state.currentSearchTerm}`}
-                            >
-                                Search<i className="icon icon-search" />
-                            </a>
-                        </div>
                     </fieldset>
                 </form>
+            </div>
             </div>
         );
     }
 }
 
 
-globals.contentViews.register(TopHitsSearch, 'TopHitsSearch');
+const Title = (props) => {
+    return (
+        <li>
+          <button className={props.styling}>
+            {props.value}
+          </button>
+        </li>
+    );
+};
+
+
+const Item = (props) => {
+    return (
+        <li>
+          <button className={props.styling}>
+            {props.value}
+          </button>
+        </li>
+    );
+};
+
+
+const Items = (props) => {
+    return (
+        <ul className={props.styling}>
+          <Title value={props.title} styling={props.titleStyling}/>
+          {
+              props.items.map(
+                  item => (
+                      <Item
+                        key={item.key}
+                        value={item.value}
+                        styling={item.styling}
+                      />
+                  )
+              )
+          }
+        </ul>
+    );
+};
+
+
+
+
+const TopHitsInput = (props) => {
+};
+
+
+const TopHitsResults = (props) => {
+};
+
+
+const TopHitsMenu = (props) => {
+};
+
+
+const NewTopHitsSearch = (props) => {
+    const [input, setInput] = useState('');
+    const [topHits, setTopHits] = useState(["a", "b"]);
+    const [displayResults, setDisplayResults] = useState(true);
+
+    const styling = {
+        selectedItem: 'top-hits-search__suggested-results--selected',
+        results: 'top-hits-search__suggested-results',
+        title: 'top-hits-search__suggested-results--title'
+
+    };
+
+    const items = [
+        {
+            key: "abc",
+            value: "new item list",
+            styling: null
+        },
+        {
+            key: "xyz",
+            value: "second item list",
+            styling: styling['selectedItem']
+        },
+    ];
+    
+    return (
+        <div className="top-hits-search__input">
+          <div className="top-hits-search__input-field">
+            <input
+              type="text"
+              autoComplete="off"
+            />
+            <Items
+              title="My section"
+              items={items}
+              styling={styling['results']}
+              titleStyling={styling['title']}
+            />
+          </div>
+        </div>
+    );
+};
+
+
+globals.contentViews.register(NewTopHitsSearch, 'TopHitsSearch');
