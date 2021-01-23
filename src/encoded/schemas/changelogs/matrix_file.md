01f19050c38841c1fc49991c688c7d3fe122ff74
jason Hilton <jahilton@stanford.edu>
2021-01-22 16:33:25 -0800

    add layers to matrixfile schema, move properties within layers

--- src/encoded/schemas/changelogs/matrix_file.md
+++ src/encoded/schemas/changelogs/matrix_file.md
@@ -1,4 +1,8 @@
 ## Changelog for matrix_file.json
 
+### Schema version 3
+* Added *layers* to hold list of metadata describing each layer in the matrix
+* *feature_counts*, *value_scale*, *value_units*, *normalized*, *normalization_method*, *filtering_cutoffs* all moved within *layers*
+
 ### Schema version 2
 * *award* is calculated through *dataset*, instead of submitted
--- src/encoded/upgrade/file.py
+++ src/encoded/upgrade/file.py
@@ -5,11 +5,30 @@ from snovault import upgrade_step
 @upgrade_step('sequence_alignment_file', '1', '2')
 @upgrade_step('matrix_file', '1', '2')
 def analysis_file_1_2(value, system):
-    if 'award' in value:
-        del value['award']
+	if 'award' in value:
+		del value['award']
 
 
 @upgrade_step('reference_file', '1', '2')
 def reference_file_1_2(value, system):
-    if 'fileset' in value:
-        del value['fileset']
+	if 'fileset' in value:
+		del value['fileset']
+
+
+@upgrade_step('matrix_file', '2', '3')
+def matrix_file_2_3(value, system):
+	properties = [
+		'feature_counts',
+		'value_scale',
+		'value_units',
+		'normalized',
+		'normalization_method',
+		'filtering_cutoffs'
+	]
+	temp = {}
+	temp['label'] = 'raw'
+	for prop in properties:
+		if prop in value:
+			temp[prop] = value[prop]
+			del value[prop]
+	value['layers'] = [temp]
