db72ced1fccffc57f47f6d4a0b9bacbd82686a67
Idan <gabdank@stanford.edu>
2021-02-18 14:45:59 -0800

    fixing typo

--- src/encoded/tests/test_types_analysis_step_run.py
+++ src/encoded/tests/test_types_analysis_step_run.py
@@ -2,8 +2,8 @@ import pytest
 
 
 def test_admin_allowed_add_analysis_step_run(testapp, analysis_step_run_with_no_status):
-    testapp.post_json('/analysis-step-runs/', analysis_step_run_with_no_status, status=201)
+    testapp.post_json('/analysis_step_run', analysis_step_run_with_no_status, status=201)
 
 
 def test_submitter_allowed_add_analysis_step_run(submitter_testapp, analysis_step_run_with_no_status):
-    submitter_testapp.post_json('/analysis-step-runs/', analysis_step_run_with_no_status, status=201)
+    submitter_testapp.post_json('/analysis_step_run', analysis_step_run_with_no_status, status=201)
