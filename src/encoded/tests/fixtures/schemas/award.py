db87abb27c41cf21210799342aebd9e056634f32
jason Hilton <jahilton@stanford.edu>
2021-03-02 15:45:41 -0800

    set Award name pattern, allow HCA General Community Awards, add general HCA Lab and Award inserts

--- src/encoded/tests/fixtures/schemas/award.py
+++ src/encoded/tests/fixtures/schemas/award.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def award_base(testapp):
     item = {
-        'name': 'seed-network-award',
+        'name': 'CZI000AAA',
         'project': 'HCA Seed Networks',
         'title': 'A Generic HCA Award',
         'uuid': '62ba2fba-8ee9-4996-b804-f9a673d137c3',
