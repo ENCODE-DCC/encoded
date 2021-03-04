d8d5f04257a955dd23e4d28ad4623e11251ada83
Jennifer Jou <jjou@stanford.edu>
2021-03-04 13:09:19 -0800

    ENCD-5766 Add audit for differentiation series time units

--- src/encoded/audit/series.py
+++ src/encoded/audit/series.py
@@ -72,3 +72,35 @@ def audit_treatment_concentration_series_mixed_units(value, system):
         yield AuditFailure('inconsistent treatment units',
                            detail, level='INTERNAL_ACTION')
         return
+
+
+@audit_checker('DifferentiationSeries', frame=[
+    'related_datasets',
+    'related_datasets.replicates',
+    'related_datasets.replicates.library',
+    'related_datasets.replicates.library.biosample'])
+def audit_differentation_time_series_mixed_units(value, system):
+    if value['status'] in ['deleted', 'replaced', 'revoked']:
+        return
+
+    if 'related_datasets' not in value:
+        return
+
+    post_differentation_time_units = set()
+    for assay in value['related_datasets']:
+        if assay['status'] not in ['deleted', 'replaced', 'revoked']:
+            if 'replicates' in assay:
+                for rep in assay['replicates']:
+                    if rep['status'] not in ['deleted'] and \
+                       'library' in rep and 'biosample' in rep['library']:
+                        biosample_object = rep['library']['biosample']
+                        if 'post_differentiation_time_units' in biosample_object:
+                            post_differentation_time_units.add(biosample_object['post_differentiation_time_units'])
+    if len(post_differentation_time_units) > 1:
+        detail = (
+            f"Biosamples associated with series {audit_link(path_to_text(value['@id']), value['@id'])} "
+            f"use inconsistent post differentation time units {post_differentation_time_units}."
+        )
+        yield AuditFailure('inconsistent differentation time units',
+                           detail, level='INTERNAL_ACTION')
+        return
