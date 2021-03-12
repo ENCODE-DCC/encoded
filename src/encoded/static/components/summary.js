8bf8f69bfa55ec13d42f5d046b68dec1b99ecea8
jason Hilton <jahilton@stanford.edu>
2021-03-12 14:22:08 -0800

    add cell count to summary page, remove unused summary code

--- src/encoded/static/components/summary.js
+++ src/encoded/static/components/summary.js
@@ -5,13 +5,12 @@ import url from 'url';
 import * as encoding from '../libs/query_encoding';
 import QueryString from '../libs/query_string';
 import { Panel, PanelBody } from '../libs/ui/panel';
-import { LabChart, CategoryChart, createNewBarChart } from './award';
+import { CategoryChart, createNewBarChart } from './award';
 import * as globals from './globals';
 import { requestSearch } from './objectutils';
 import { FacetList, ClearFilters } from './search';
 import { getObjectStatuses, sessionToAccessLevel } from './status';
 import { ViewControls } from './view_controls';
-import { systemsField, organField } from './body_map';
 
 /**
  * Generate an array of data from one facet bucket for displaying in a chart, with one array entry
@@ -22,16 +21,6 @@ import { systemsField, organField } from './body_map';
  * @param {array} labels - Experiment status labels.
  * @return {array} - Data extracted from buckets with an order of values corresponding to `labels`.
  */
-
-// Data field for organism
-// We will display different facets depending on the selected organism
-const organismField = 'donors.organism.scientific_name';
-
-// Mapping of shortened organism name and full scientific organism name
-const organismTerms = [
-    'Homo sapiens',
-    'Mus musculus',
-];
 
 function generateStatusData(buckets, labels) {
     // Fill the array to the proper length with zeroes to start with. Actual non-zero data will
@@ -345,38 +334,43 @@ SummaryData.defaultProps = {
 class SummaryBody extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            CellCount: 0
+        }
         const searchQuery = url.parse(this.props.context['@id']).search;
         const terms = queryString.parse(searchQuery);
-        this.state = {
-            selectedOrganism: terms[organismField] ? terms[organismField] : [],
-        };
-        this.chooseOrganism = this.chooseOrganism.bind(this);
     }
 
-    chooseOrganism(e) {
-        this.setState({
-            selectedOrganism: e.currentTarget.id,
-        });
-        const parsedUrl = url.parse(this.props.context['@id']);
-        const query = new QueryString(parsedUrl.query);
-        query.deleteKeyValue(systemsField);
-        query.deleteKeyValue(organField);
-        query.replaceKeyValue(organismField, e.currentTarget.id, '');
-        const href = `?${query.format()}`;
-        this.context.navigate(href);
+    componentDidMount() {
+        const query_url = this.props.context.search_base.replace('/search/?', '')
+        this.getCellCount(query_url);
     }
+
+    getCellCount(searchBase) {
+        requestSearch(searchBase + '&limit=all').then((results) => {
+            if (Object.keys(results).length > 0 && results['@graph'].length > 0) {
+                var mx_query = 'type=MatrixFile&layers.value_scale=linear&layers.normalized=false&derivation_process=cell calling&field=observation_count&limit=all'
+                results['@graph'].forEach(x => mx_query += '&libraries=' + x['@id']);
+                requestSearch(mx_query).then((results2) => {
+                    var cell_count = 0
+                    results2['@graph'].forEach(y => cell_count += y['observation_count']);
+                    this.setState({
+                        CellCount: cell_count
+                    })
+                })
+            }
+        })
+    }
+
     render() {
         const searchQuery = url.parse(this.props.context['@id']).search;
-        const query = new QueryString(searchQuery);
-        const nonPersistentQuery = query.clone();
-        nonPersistentQuery.deleteKeyValue('?type');
-        const clearButton = nonPersistentQuery.queryCount() > 0 && query.queryCount('?type') > 0;
         const context = this.props.context;
         const vertFacetNames = ['assay', 'protocol.title', 'biosample_classification', 'biosample_ontologies.system_slims', 'biosample_ontologies.organ_slims', 'biosample_ontologies.term_name', 'award.project', 'award.coordinating_pi.title'];
         const vertFacets = []
         context.facets.forEach(x => {
             if (vertFacetNames.includes(x.field)) vertFacets.push(x);
             })
+        const cell_count = this.state.CellCount.toLocaleString();
         return (
             <div className="search-results">
                 <div className="search-results__facets">
@@ -384,6 +378,7 @@ class SummaryBody extends React.Component {
                 </div>
                 <div className="search-results__report-list">
                     <h4>{this.props.context.total} {this.props.context.total > 1 ? 'libraries' : 'library'}</h4>
+                    <h4>{cell_count} cells</h4>
                     <div className="view-controls-container">
                         <ViewControls results={this.props.context} alternativeNames={['Tabular report']} />
                     </div>
