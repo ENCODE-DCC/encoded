61ff52582b2c4f3239808906b9c10d8c0017aba9
Keenan Graham <keenangraham@stanford.edu>
2021-04-16 15:44:47 -0700

    Add custom generic config

---
+++ src/encoded/searches/configs.py
@@ -0,0 +1,36 @@
+from snovault.elasticsearch.searches.configs import search_config
+
+
+@search_config(
+    name='custom'
+)
+def custom_search_config():
+    return {
+        'facets': {
+            'assay_title': {
+                'title': 'Assay title',
+                'type': 'typeahead',
+                'open_on_load': True
+            },
+            'status': {
+                'title': 'Status',
+                "open_on_load": True
+            },
+            'target.label': {
+                'title': 'Target of assay',
+                'type': 'typeahead',
+                'length': 'long',
+                'open_on_load': True
+            },
+            'biosample_ontology.term_name' : {
+                'title': 'Biosample term name',
+                'type': 'typeahead',
+                'length': 'long',
+                "open_on_load": True
+            },
+        },
+        'columns': {},
+        'matrix': {},
+        'boost_values': {},
+        'fields': {},
+    }
