7e4647883357b71ba08d17a810f6620fc745e769
Bonita Lam <bonitarlam@gmail.com>
2021-04-19 10:56:28 -0700

    Add test for calculated assay slims

--- src/encoded/tests/fixtures/schemas/single_cell_rna_series.py
+++ src/encoded/tests/fixtures/schemas/single_cell_rna_series.py
@@ -9,3 +9,13 @@ def base_single_cell_series(testapp, lab, base_experiment_submitted, award):
         'related_datasets': [base_experiment_submitted['@id']]
     }
     return testapp.post_json('/single_cell_rna_series', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def single_cell_series(testapp, lab, base_single_cell_experiment_submitted, award):
+    item = {
+        'award': award['uuid'],
+        'lab': lab['uuid'],
+        'related_datasets': [base_single_cell_experiment_submitted['@id']]
+    }
+    return testapp.post_json('/single_cell_rna_series', item, status=201).json['@graph'][0]
