ffdcd9889f37d122a55a26555e495be7cb5ee7e7
Keenan Graham <keenangraham@stanford.edu>
2021-04-26 21:55:57 -0700

    Add example default and alias configuration

--- src/encoded/searches/configs.py
+++ src/encoded/searches/configs.py
@@ -1,8 +1,11 @@
+from snovault.elasticsearch.searches.interfaces import SEARCH_CONFIG
 from snovault.elasticsearch.searches.configs import search_config
 
 
 def includeme(config):
     config.scan(__name__)
+    config.registry[SEARCH_CONFIG].add_aliases(ALIASES)
+    config.registry[SEARCH_CONFIG].add_defaults(DEFAULTS)
 
 
 @search_config(
@@ -38,3 +41,18 @@ def custom_search_config():
         'boost_values': {},
         'fields': {},
     }
+
+
+ALIASES = {
+    'DonorSubtypes': [
+        'HumanDonor',
+        'FlyDonor',
+        'WormDonor',
+        'MouseDonor',
+    ]
+}
+
+
+DEFAULTS = {
+    'Donor': ['DonorSubtypes']
+}
