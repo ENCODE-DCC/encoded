f0127cb07aebd436612bf61b3b1314af5d9ef577
Keenan Graham <keenangraham@stanford.edu>
2021-05-11 16:25:36 -0700

    Add CollectionsQuery

--- src/encoded/static/components/top_hits/query.js
+++ src/encoded/static/components/top_hits/query.js
@@ -1,4 +1,6 @@
 import {
+    COLLECTIONS,
+    COLLECTIONS_KEY,
     TOP_HITS_URL,
     TOP_HITS_PARAMS,
 } from './constants';
@@ -90,6 +92,99 @@ class Query {
     getResults() {
         return fetchAndParseTopHits(
             this.makeTopHitsUrl()
+        );
+    }
+}
+
+
+export const makeCollectionUrl = (searchUrl, searchTerm) => `${searchUrl}&searchTerm=${searchTerm}`;
+
+
+export const fetchTotalResultsFromCollection = (url) => (
+    fetch(
+        url,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    ).then(
+        (response) => (response.ok ? response.json() : [])
+    ).then(
+        (results) => results.total
+    )
+);
+
+
+export const getHitsFromCollections = (collections, searchTerm) => (
+    collections.map(
+        (collection) => (
+            {
+                total: fetchTotalResultsFromCollection(
+                    makeCollectionUrl(
+                        collection.searchUrl,
+                        searchTerm,
+                    )
+                ),
+                ...collection,
+            }
+        )
+    )
+);
+
+
+export const getCountFromCollectionsHits = (hits) => (
+    hits.reduce(
+        (accumulator, {total}) => accumulator + total, 0
+    )
+);
+
+
+class CollectionsQuery {
+    constructor(searchTerm) {
+        this.searchTerm = searchTerm;
+        this.collections = COLLECTIONS;
+    }
+
+    getCollectionsWithResults() {
+        let hits = getHitsFromCollections(this.collections, this.searchTerm);
+        return [
+            {
+                key: COLLECTIONS_KEY,
+                count: getCountFromCollectionsHits(hits),
+                hits: hits,
+            }
+        ];
+    }
+
+    getResults() {
+        return this.getCollectionsWithResults();
+    }
+}
+
+
+export const getTopHitsQuery = (searchTerm) => (
+    new Query(searchTerm)
+);
+
+
+export const getCollectionsQuery = (searchTerm) => (
+    new CollectionsQuery(searchTerm)
+);
+
+
+class TopHitsAndCollectionQuery {
+    constructor(searchTerm) {
+        this.searchTerm = searchTerm;
+        this.topHitsQuery = getTopHitsQuery(searchTerm);
+        this.collectionsQuery = getCollectionsQuery(searchTerm);
+    }
+
+    getResults() {
+        return (
+            this.collectionsQuery.getResults() +
+            this.topHitsQuery.getResults()
         );
     }
 }
