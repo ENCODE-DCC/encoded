507ee38fffef92aac440ac756c3f41deb64c5485
ottojolanki <ojolanki@stanford.edu>
2021-05-18 16:39:15 -0700

    install python3.6-dev to get all the headers

--- cloud-config/run-scripts/app-pg.sh
+++ cloud-config/run-scripts/app-pg.sh
@@ -151,7 +151,7 @@ copy_with_permission "$PG_CONF_DEST" "$WALE_ENV" 'WALE_S3_PREFIX'
 ## Install python3.6 to run wal-e with
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3.6 python3.6-venv
+sudo apt-get install -y python3.6 python3.6-dev python3.6-venv
 ## Install wal-e
 sudo -u root mkdir -p "$WALE_DIR"
 sudo -u root chown postgres:postgres "$WALE_DIR"
