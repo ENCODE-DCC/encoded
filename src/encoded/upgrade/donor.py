b8dbad641d68da36e0708e89215349cbf273c956
jason Hilton <jahilton@stanford.edu>
2021-05-18 17:18:27 -0700

    update height and body_mass_index from number to string values

--- src/encoded/upgrade/donor.py
+++ src/encoded/upgrade/donor.py
@@ -6,3 +6,11 @@ def human_postnatal_donor_1_2(value, system):
 	if 'family_history_breast_cancer' in value:
 		value['family_members_history_breast_cancer'] = value['family_history_breast_cancer']
 		del value['family_history_breast_cancer']
+
+
+@upgrade_step('human_postnatal_donor', '2', '3')
+def human_postnatal_donor_2_3(value, system):
+	if 'height' in value:
+		value['height'] = str(value['height'])
+	if 'body_mass_index' in value:
+		value['body_mass_index'] = str(value['body_mass_index'])
