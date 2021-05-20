6888334059747583e14185298553b03c8e1d7143
Keenan Graham <keenangraham@stanford.edu>
2021-05-20 11:32:02 -0700

    Add comment on queries constant

--- src/encoded/static/components/top_hits/multi/constants.js
+++ src/encoded/static/components/top_hits/multi/constants.js
@@ -5,6 +5,16 @@ import TopHitsResults from '../results';
 import CollectionsResults from './results';
 
 
+/**
+* Used for specifying query objects and result components for multisearch.
+* Format:
+* [
+*      {string} name - key used to store results in React state,
+*      {object} query - query object that has getResults method,
+*      {element} results - React component that knows how to render results returned by query,
+*      {string} title - Title used to display group of results,
+* ]
+*/
 const QUERIES = [
     ['dataCollections', CollectionsQuery, CollectionsResults, 'Data collections'],
     ['topHits', TopHitsQuery, TopHitsResults, 'Top results by type'],
