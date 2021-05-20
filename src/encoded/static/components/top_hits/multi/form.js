0854146d95eb6dd97ca51c27dd61746f13421fd8
Keenan Graham <keenangraham@stanford.edu>
2021-05-20 11:43:31 -0700

    Add comments to form

--- src/encoded/static/components/top_hits/multi/form.js
+++ src/encoded/static/components/top_hits/multi/form.js
@@ -6,11 +6,24 @@ import {
 import QUERIES from './constants';
 
 
+/**
+* Checks if the result key is defined and if it has any results to render.
+* @param {string} name - Key in results object.
+* @param {object} results - Results state.
+* @return {bool} Should render results.
+*/
 const shouldRenderResults = (name, results) => (
     results[name] && results[name].length > 0
 );
 
 
+/**
+* Check if a group has results and renders a Group component.
+* @param {string} input - User input to pass to Group component.
+* @param {object} results - Results state.
+* @param {func} handleClickAway - Callback to pass to Group component.
+* @return {array[Group]} List of Groups with results for rendering.
+*/
 const makeGroupsForResults = (input, results, handleClickAway) => (
     QUERIES.map(
         ([name, , Component, title]) => (
@@ -31,6 +44,7 @@ const makeGroupsForResults = (input, results, handleClickAway) => (
 
 
 const Form = ({ children, input, results, handleClickAway }) => {
+    // Calculate groups first to avoid rendering container div if not needed.
     const groups = makeGroupsForResults(input, results, handleClickAway);
     return (
         <form className="multisearch__multiform" action="/search/">
