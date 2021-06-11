8b252f7e44529edac5e10094b5c6cba7826e8577
Jennifer Jou <jjou@stanford.edu>
2021-06-11 13:14:59 -0700

    Address eslint error

--- src/encoded/static/components/dataset.js
+++ src/encoded/static/components/dataset.js
@@ -1512,6 +1512,39 @@ function sortStage(a, b) {
     return 0;
 }
 
+const ElementsReferences = ({ reference }) => {
+    // Render all examined_loci as links, and all examined_regions as text, and combine them
+    // together for final rendering.
+    const examinedLoci = (reference.examined_loci && reference.examined_loci.length > 0)
+        ? reference.examined_loci.map((locus) => <a key={locus['@id']} href={locus['@id']}>{locus.symbol}</a>)
+        : [];
+    const examinedRegions = (reference.examined_regions && reference.examined_regions.length > 0)
+        ? reference.examined_regions.map((region, i) => <span key={i}>{region.assembly} {region.chromosome}:{region.start}-{region.end}</span>)
+        : [];
+    const examinedAreas = examinedLoci.concat(examinedRegions);
+
+    // Use the combined components to render to the page.
+    if (examinedAreas.length > 0) {
+        return (
+            <>
+                &nbsp;(
+                {examinedAreas.map((examinedArea, i) => (
+                    <React.Fragment key={i}>
+                        {i > 0 ? ', ' : null}
+                        {examinedArea}
+                    </React.Fragment>
+                ))}
+                )
+            </>
+        );
+    }
+    return null;
+};
+
+ElementsReferences.propTypes = {
+    reference: PropTypes.object.isRequired,
+};
+
 const organismDevelopmentSeriesTableColumns = {
     accession: {
         title: 'Accession',
@@ -1970,35 +2003,6 @@ export const SeriesComponent = (props, reactContext) => {
             return accumulatedDiseases.concat(...diseasesTermNames);
         }, [])),
     ].filter((disease) => disease);
-
-    const ElementsReferences = ({ reference }) => {
-        // Render all examined_loci as links, and all examined_regions as text, and combine them
-        // together for final rendering.
-        const examinedLoci = (reference.examined_loci && reference.examined_loci.length > 0)
-            ? reference.examined_loci.map((locus) => <a key={locus['@id']} href={locus['@id']}>{locus.symbol}</a>)
-            : [];
-        const examinedRegions = (reference.examined_regions && reference.examined_regions.length > 0)
-            ? reference.examined_regions.map((region, i) => <span key={i}>{region.assembly} {region.chromosome}:{region.start}-{region.end}</span>)
-            : [];
-        const examinedAreas = examinedLoci.concat(examinedRegions);
-
-        // Use the combined components to render to the page.
-        if (examinedAreas.length > 0) {
-            return (
-                <>
-                    &nbsp;(
-                    {examinedAreas.map((examinedArea, i) => (
-                        <React.Fragment key={i}>
-                            {i > 0 ? ', ' : null}
-                            {examinedArea}
-                        </React.Fragment>
-                    ))}
-                    )
-                </>
-            );
-        }
-        return null;
-    };
 
     return (
         <div className={itemClass}>
