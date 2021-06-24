b092d6828a0575fb05326339229c6718d2030aaf
jason Hilton <jahilton@stanford.edu>
2021-06-23 17:30:33 -0700

    upgrade to remove flowcell_details from existing sequencingrun objects

--- src/encoded/tests/test_upgrade_sequencing_run.py
+++ src/encoded/tests/test_upgrade_sequencing_run.py
@@ -6,3 +6,10 @@ def test_sequencing_run_upgrade_1_2(upgrader, sequencing_run_base):
 	value = upgrader.upgrade('sequencing_run', sequencing_run_base, current_version='1', target_version='2')
 	assert value['derived_from'] == ['LATLB234DEF']
 	assert value['schema_version'] == '2'
+
+
+def test_sequencing_run_upgrade_2_3(upgrader, sequencing_run_base):
+	sequencing_run_base['flowcell_details'] = {"machine":"A00437","lane":"4","flowcell":"HH22VDSXX"}
+	value = upgrader.upgrade('sequencing_run', sequencing_run_base, current_version='2', target_version='3')
+	assert 'flowcell_details' not in value
+	assert value['schema_version'] == '3'
--- src/encoded/upgrade/sequencing_run.py
+++ src/encoded/upgrade/sequencing_run.py
@@ -5,3 +5,9 @@ from snovault import upgrade_step
 def sequencing_run_1_2(value, system):
 	if 'derived_from' in value:
 		value['derived_from'] = [value['derived_from']]
+
+
+@upgrade_step('sequencing_run', '2', '3')
+def sequencing_run_2_3(value, system):
+	if 'flowcell_details' in value:
+		del value['flowcell_details']
