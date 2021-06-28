8081367bfd87e1b2951fc23b20c2d02d21dfd196
Paul Sud <paulsud@stanford.edu>
2021-06-28 11:25:44 -0700

    reduce maxmemory again

--- cloud-config/run-scripts/ami-redis.sh
+++ cloud-config/run-scripts/ami-redis.sh
@@ -17,7 +17,7 @@ sudo sed -i -e "s/port 6379/port $ENCD_REDIS_PORT/" /etc/redis/redis.conf
 # Allow remote connections
 sudo sed -i -e 's/bind 127\.0\.0\.1/bind 0\.0\.0\.0/' /etc/redis/redis.conf
 # Add config for LRU cache
-echo "maxmemory 32gb" | sudo tee -a /etc/redis/redis.conf > /dev/null
+echo "maxmemory 24gb" | sudo tee -a /etc/redis/redis.conf > /dev/null
 echo "maxmemory-policy allkeys-lru" | sudo tee -a /etc/redis/redis.conf > /dev/null
 # restart
 sudo service redis-server restart
