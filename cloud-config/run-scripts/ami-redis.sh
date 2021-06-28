aabe28ba76f0b1ee4c10f83498847711c847b2af
Paul Sud <paulsud@stanford.edu>
2021-06-28 14:25:24 -0700

    add redis conf

--- cloud-config/run-scripts/ami-redis.sh
+++ cloud-config/run-scripts/ami-redis.sh
@@ -19,5 +19,20 @@ sudo sed -i -e 's/bind 127\.0\.0\.1/bind 0\.0\.0\.0/' /etc/redis/redis.conf
 # Add config for LRU cache
 echo "maxmemory 24gb" | sudo tee -a /etc/redis/redis.conf > /dev/null
 echo "maxmemory-policy allkeys-lru" | sudo tee -a /etc/redis/redis.conf > /dev/null
+# Turn off persistence
+sudo sed -i -e 's/^save/#\ save/' /etc/redis/redis.conf
+# Turn off daemonize, for some reason it is set to yes in default conf which conflicts with systemd
+sudo sed -i -e 's/^daemonize/#\ daemonize/' /etc/redis/redis.conf
+# Make sure supervised is set correctly, was `no` in original conf
+sudo sed -i -e 's/^supervised\ no/supervised\ systemd/' /etc/redis/redis.conf
+echo 'save ""' | sudo tee -a /etc/redis/redis.conf > /dev/null
+
+# Disable THP, see https://docs.mongodb.com/manual/tutorial/transparent-huge-pages/
+echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
+sudo cp "$ENCD_CC_DIR/configs/redis/disable-transparent-huge-pages.service" /etc/systemd/system/disable-transparent-huge-pages.service
+sudo systemctl daemon-reload
+sudo systemctl start disable-transparent-huge-pages
+sudo systemctl enable disable-transparent-huge-pages
+
 # restart
 sudo service redis-server restart
