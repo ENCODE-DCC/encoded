c1a1ef553a455933e0f2ee7498abfb022502242d
forresttanaka <fytanaka@stanford.edu>
2021-07-21 13:36:31 -0700

    ENCD-6065-cart-upgrade-error

--- src/encoded/upgrade/cart.py
+++ src/encoded/upgrade/cart.py
@@ -1,6 +1,12 @@
 from snovault import upgrade_step
 
 
+@upgrade_step('cart', '', '2')
+def cart_0_2(value, system):
+    if 'file_views' not in value:
+        value['file_views'] = []
+
+
 @upgrade_step('cart', '1', '2')
 def cart_1_2(value, system):
     if 'file_views' not in value:
