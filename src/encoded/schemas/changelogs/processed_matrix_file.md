f84e50fcf93b665d0019aca45d015207e52f8909
jason Hilton <jahilton@stanford.edu>
2021-07-29 17:46:48 -0700

    remove reference fields from processedmatrixfile and replaced with calculated props

---
+++ src/encoded/schemas/changelogs/processed_matrix_file.md
@@ -0,0 +1,4 @@
+## Changelog for processed_matrix_file.json
+
+### Schema version 4
+* Removed submitted *assembly* and *genome_annotation* in favor of calculated *assemblies* and *genome_annotations*
--- src/encoded/upgrade/file.py
+++ src/encoded/upgrade/file.py
@@ -6,3 +6,10 @@ from snovault import upgrade_step
 def analysis_file_1_2(value, system):
 	if 'award' in value:
 		del value['award']
+
+@upgrade_step('processed_matrix_file', '3', '4')
+def processed_matrix_file_3_4(value, system):
+	if 'assembly' in value:
+		del value['assembly']
+	if 'genome_annotation' in value:
+		del value['genome_annotation']
