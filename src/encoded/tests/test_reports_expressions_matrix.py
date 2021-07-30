71a83313df3b6b76baf71b75534a9a8e9d599265
Keenan Graham <keenangraham@stanford.edu>
2021-07-30 16:56:23 -0700

    Update tests

--- src/encoded/tests/test_reports_expressions_matrix.py
+++ src/encoded/tests/test_reports_expressions_matrix.py
@@ -156,8 +156,8 @@ def test_reports_expressions_matrix_expression_matrix_as_matrix():
     matrix = list(em.as_matrix())
     expected_matrix = [
         ['featureID', 'geneSymbol', '/files/ENCFF004JWA/', '/files/ENCFF006IHP/', '/files/ENCFF008KUV/'],
-        ['ENSG00000100393.12', 'EP300', 19.19, 75.65, 8.0],
         ['ENSG00000102974.15', 'CTCF', 14.1, 55.53, 16.36],
+        ['ENSG00000100393.12', 'EP300', 19.19, 75.65, 8.0],
         ['ENSG00000115138.10', 'POMC', 0.0, 2.94, 0.02]
     ]
     assert matrix == expected_matrix
@@ -170,8 +170,8 @@ def test_reports_expressions_matrix_expression_matrix_as_tsv():
     actual_tsv = list(em.as_tsv())
     expected_tsv = [
         b'featureID\tgeneSymbol\t/files/ENCFF004JWA/\t/files/ENCFF006IHP/\t/files/ENCFF008KUV/\n',
-        b'ENSG00000100393.12\tEP300\t19.19\t75.65\t8.0\n',
         b'ENSG00000102974.15\tCTCF\t14.1\t55.53\t16.36\n',
+        b'ENSG00000100393.12\tEP300\t19.19\t75.65\t8.0\n',
         b'ENSG00000115138.10\tPOMC\t0.0\t2.94\t0.02\n'
     ]
     assert actual_tsv == expected_tsv
