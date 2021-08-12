fdea780149d032b5c7b24f8fc46fec90048d790c
Ben Hitz <hitz@stanford.edu>
2021-08-12 15:53:24 -0700

    update-snovault-1.0.60

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 # Edit Snovault version after the `@` here, can be a branch or tag
-SNOVAULT_DEP = "git+https://github.com/ENCODE-DCC/snovault.git@SNO-209-move-search-code-to-independent-package"
+SNOVAULT_DEP = "git+https://github.com/ENCODE-DCC/snovault.git@1.0.60"
 
 INSTALL_REQUIRES = [
     "PasteDeploy==2.1.0",
