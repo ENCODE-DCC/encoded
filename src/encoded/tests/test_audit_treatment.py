94cfe8b0c469986ab64fee54e911dcd4713d5c74
Idan <gabdank@stanford.edu>
2021-08-19 13:57:31 -0700

    fixing the test

--- src/encoded/tests/test_audit_treatment.py
+++ src/encoded/tests/test_audit_treatment.py
@@ -1,8 +1,8 @@
 import pytest
 
 
-def test_audit_pipeline_assay_term_names(testapp, treatment):
-    res = testapp.get(treatment['@id'] + '@@index-data')
+def test_audit_pipeline_assay_term_names(testapp, treatment_12):
+    res = testapp.get(treatment_12['@id'] + '@@index-data')
     errors = res.json['audit']
     errors_list = []
     for error_type in errors:
