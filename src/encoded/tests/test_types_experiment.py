293e2ef73c751a02362447335a13270e4cb0404c
forresttanaka <fytanaka@stanford.edu>
2021-08-26 22:02:39 -0700

    ENCD-5687-replication-count
    Update the tests.

--- src/encoded/tests/test_types_experiment.py
+++ src/encoded/tests/test_types_experiment.py
@@ -508,7 +508,7 @@ def test_experiment_default_analysis(
 
 def test_experiment_replication_count_0(testapp, base_experiment):
     res = testapp.get(base_experiment['@id'] + '@@index-data') 
-    assert res.json['object']['replication_count'] == 0
+    assert res.json['object']['bio_replicate_count'] == 0 and res.json['object']['tech_replicate_count'] == 0
 
 
 def test_experiment_replication_count_2(testapp, base_experiment, biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1):
@@ -518,7 +518,7 @@ def test_experiment_replication_count_2(testapp, base_experiment, biosample_1, b
     testapp.patch_json(replicate_2_1['@id'], {'library': library_2['@id']})
     testapp.patch_json(base_experiment['@id'], {'replicates': [replicate_1_1['@id'], replicate_2_1['@id']]})
     res = testapp.get(base_experiment['@id'] + '@@index-data') 
-    assert res.json['object']['replication_count'] == 2
+    assert res.json['object']['bio_replicate_count'] == 2 and res.json['object']['tech_replicate_count'] == 1
 
 
 def test_experiment_biosample_replicates_1_2(testapp, base_experiment, biosample_1, biosample_2, library_1, library_2, replicate_1_1, replicate_2_1):
