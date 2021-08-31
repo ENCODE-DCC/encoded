763dc60ddf76ab441bb75e0bd81413b18b657ec3
Idan <gabdank@stanford.edu>
2021-08-30 22:35:39 -0700

    adding fixture

--- src/encoded/tests/fixtures/schemas/treatment.py
+++ src/encoded/tests/fixtures/schemas/treatment.py
@@ -152,6 +152,16 @@ def treatment_12(testapp):
 
 
 @pytest.fixture
+def treatment_not_perturbed(testapp, organism):
+    item = {
+        'treatment_term_name': 'ethanol',
+        'treatment_type': 'chemical',
+        'purpose': 'activation'
+    }
+    return testapp.post_json('/treatment', item).json['@graph'][0]
+
+
+@pytest.fixture
 def treatment_perturbed(testapp, organism):
     item = {
         'treatment_term_name': 'ethanol',
