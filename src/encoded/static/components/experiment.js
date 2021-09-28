cf7758d7278818728052addf5ba4651134839fe8
Jennifer Jou <jjou@stanford.edu>
2021-09-28 16:33:28 -0700

    Remove braces flagged by eslint

--- src/encoded/static/components/experiment.js
+++ src/encoded/static/components/experiment.js
@@ -691,7 +691,7 @@ const ExperimentComponent = ({ context, auditIndicators, auditDetail }, reactCon
                                 <div data-test="biosample-summary">
                                     <dt>Biosample summary</dt>
                                     <dd>
-                                        {context.biosample_summary ? <div> {<BiosampleSummaryDisplay summary={context.biosample_summary} organisms={organismNames.concat(GeneticModificationOrganismNames(biosamples))} />} </div> : null}
+                                        {context.biosample_summary ? <div><BiosampleSummaryDisplay summary={context.biosample_summary} organisms={organismNames.concat(GeneticModificationOrganismNames(biosamples))}/></div> : null}
                                     </dd>
                                 </div>
                             : null}
@@ -1081,7 +1081,7 @@ const replicateTableColumns = {
 
             // Else, display biosample summary if the biosample exists
             if (replicate.library && replicate.library.biosample) {
-                return <span>{<BiosampleSummaryDisplay summary={replicate.library.biosample.summary} organisms={[replicate.library.biosample.organism.scientific_name].concat(GeneticModificationOrganismNames([replicate.library.biosample]))} />}</span>;
+                return <span><BiosampleSummaryDisplay summary={replicate.library.biosample.summary} organisms={[replicate.library.biosample.organism.scientific_name].concat(GeneticModificationOrganismNames([replicate.library.biosample]))} /></span>;
             }
 
             // Else, display nothing
