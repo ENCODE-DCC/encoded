3c8065b9e6044ca5d75dd184866122d910e6ce6e
Keenan Graham <keenangraham@stanford.edu>
2021-10-06 20:32:22 -0700

    test.tgz gets mappend to application/gzip by browser

--- src/encoded/tests/test_schema_quality_metric.py
+++ src/encoded/tests/test_schema_quality_metric.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_tarball_attachment(testapp, generic_quality_metric):
-    # Make sure *.tar.gz and *.tgz files are accepted by the attachment property
+    # Make sure *.tar files are accepted by the attachment property
     res = testapp.post_json('/generic_quality_metric', generic_quality_metric, expect_errors=False)
     assert res.status_code == 201
 
