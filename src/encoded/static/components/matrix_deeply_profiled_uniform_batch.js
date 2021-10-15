6d7bb95f88125e9602408f49f2d79251af158b01
Philip Adenekan <zoldello@gmail.com>
2021-10-14 18:26:25 -0700

    fixed issue with color not saying the same in uniform matrix

--- src/encoded/static/components/matrix_deeply_profiled_uniform_batch.js
+++ src/encoded/static/components/matrix_deeply_profiled_uniform_batch.js
@@ -665,7 +665,7 @@ class DeeplyProfiledUniformBatchMatrix extends React.Component {
      */
     getRowCategories() {
         const rowCategory = this.props.context.matrix.y.group_by[0];
-        const rowCategoryData = this.props.context.matrix.y[rowCategory].buckets.filter((rowCategoryItem) => globals.DeeplyProfiledCellLineList.includes(rowCategoryItem.key));
+        const rowCategoryData = this.props.context.matrix.y[rowCategory].buckets.filter((rowCategoryItem) => globals.DeeplyProfiledCellLineList.includes(rowCategoryItem.key)).sort((a, b) => sortByDescending(a.key, b.key));
         const rowCategoryColors = globals.DeeplyProfiledCellLineListColors.colorList(rowCategoryData.map((rowCategoryDatum) => rowCategoryDatum.key));
         const rowCategoryNames = {};
         rowCategoryData.forEach((datum) => {
