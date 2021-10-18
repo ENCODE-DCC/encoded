4f7de7e1cafb23c5e3ce59527a4b49ceead233f3
Bonita Lam <bonitarlam@gmail.com>
2021-10-18 15:39:14 -0700

    Update treatment component

--- src/encoded/static/components/biosample.js
+++ src/encoded/static/components/biosample.js
@@ -646,6 +646,13 @@ const Treatment = (props) => {
                 <dt>Type</dt>
                 <dd>{treatmentDetails}</dd>
             </div>
+
+            {context.purpose ?
+                <div data-test="purpose">
+                    <dt>Purpose</dt>
+                    <dd>{context.purpose}</dd>
+                </div>
+            : null}
         </dl>
     );
 };
