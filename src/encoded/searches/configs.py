412607e78d92fb7c1bbba9b43fd2d8d4ecadb439
Idan <gabdank@stanford.edu>
2021-11-10 16:41:58 +0000

    updating config.py

--- src/encoded/searches/configs.py
+++ src/encoded/searches/configs.py
@@ -89,6 +89,173 @@ def custom_columns_config():
     }
 
 
+# To allow FunctionalCharacterizationSeries columns properties to pass through for the search:
+# type=FunctionalCharacterizationExperiment&type=FunctionalCharacterizationSeries&type=TransgenicEnhancerExperiment
+# with effectively the FunctionalCharacterizationExperiment config.
+@search_config(
+    name='FunctionalCharacterization'
+)
+def functional_characterization_data_view():
+    return {
+        'facets': {
+            "assay_slims": {
+                "title": "Assay type"
+            },
+            'assay_title': {
+                'title': 'Assay title',
+                'open_on_load': True
+            },
+            'status': {
+                'title': 'Status',
+                'open_on_load': True
+            },
+            'elements_references.examined_loci.symbol': {
+                'title': 'Targeted loci',
+                'type': 'typeahead',
+                'open_on_load': True
+            },
+            'elements_references.elements_selection_method': {
+                'title': 'Elements selection method'
+            },
+            'elements_references.crispr_screen_tiling': {
+                'title': 'CRISPR screen tiling'
+            },
+            'examined_loci.gene.symbol': {
+                'title': 'Examined loci',
+                'type': 'typeahead',
+                'open_on_load': True
+            },
+            'replicates.library.biosample.applied_modifications.guide_type': {
+                'title': 'Guide type'
+            },
+            'replicates.library.biosample.applied_modifications.reagents.promoter_details': {
+                'title': 'Promoter details'
+            },
+            'replicates.library.biosample.applied_modifications.MOI': {
+                'title': 'Multiplicity of infection'
+            },
+            'replicates.library.biosample.donor.organism.scientific_name': {
+                'title': 'Organism',
+                'open_on_load': True
+            },
+            "replicates.library.biosample.life_stage": {
+                "title": "Life stage"
+            },
+            'biosample_ontology.classification': {
+                'title': 'Biosample classification'
+            },
+            'biosample_ontology.term_name' : {
+                'title': 'Biosample',
+                'type': 'typeahead',
+                'length': 'long',
+                'open_on_load': True
+            },
+            'biosample_ontology.organ_slims': {
+                'title': 'Organ',
+                'type': 'typeahead'
+            },
+            'biosample_ontology.cell_slims': {
+                'title': 'Cell',
+                'type': 'typeahead'
+            },
+            'replicates.library.biosample.disease_term_name': {
+                'title': 'Disease'
+            },
+            'replicates.library.biosample.treatments.treatment_term_name': {
+                'title': 'Biosample treatment'
+            },
+            'control_type': {
+                'type': 'exists',
+                'title': 'Hide control experiments'
+            },
+            'award.project': {
+                'title': 'Project'
+            },
+            "award.rfa": {
+                "title": "RFA"
+            },
+            'assembly': {
+                'title': 'Genome assembly'
+            },
+            'files.file_type': {
+                'title': 'Available file types'
+            }
+            'date_released': {
+                'title': 'Date released'
+            },
+            'date_submitted': {
+                'title': 'Date submitted'
+            },
+            'lab.title': {
+                'title': 'Lab'
+            }
+        },
+        "facet_groups": [
+            {
+                "title": "Assay",
+                "facet_fields": [
+                    "assay_slims",
+                    "assay_title",
+                    "control_type",
+                    "elements_references.crispr_screen_tiling",
+                    "replicates.library.biosample.applied_modifications.guide_type",
+                    "replicates.library.biosample.applied_modifications.MOI",
+                    "replicates.library.biosample.applied_modifications.reagents.promoter_details"
+                ]
+            },
+            {
+                "title": "Elements",
+                "facet_fields": [
+                    "elements_references.examined_loci.symbol",
+                    "examined_loci.gene.symbol",
+                    "elements_references.elements_selection_method"
+                ]
+            },
+            {
+                "title": "Biosample",
+                "facet_fields": [
+                    "replicates.library.biosample.donor.organism.scientific_name",
+                    "biosample_ontology.term_name",
+                    "biosample_ontology.classification",
+                    "biosample_ontology.organ_slims",
+                    "biosample_ontology.cell_slims",
+                    "replicates.library.biosample.life_stage",
+                    "replicates.library.biosample.treatments.treatment_term_name",
+                    "replicates.library.biosample.disease_term_name"
+                ]
+            },
+            {
+                "title": "Analysis",
+                "facet_fields": [
+                    "assembly",
+                    "files.file_type"
+                ]
+            },
+            {
+                "title": "Provenance",
+                "facet_fields": [
+                    "award.project",
+                    "award.rfa",
+                    "lab.title",
+                    "date_submitted",
+                    "date_released"
+                ]
+            },
+            {
+                "title": "Quality",
+                "facet_fields": [
+                    "status",
+                    "audit.ERROR.category",
+                    "audit.NOT_COMPLIANT.category",
+                    "audit.WARNING.category",
+                    "audit.INTERNAL_ACTION.category"
+                ]
+            }
+        ]
+    }
+
+
+
 ALIASES = {
     'DonorSubtypes': [
         'HumanDonor',
