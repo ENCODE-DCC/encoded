70b9c94c3869e9f240a85b235a4ce70a17c4eb34
Idan Gabdank <gabdank@users.noreply.github.com>
2021-12-16 16:29:39 -0800

    ENCD-6228 gene dependency update (#4209)

--- src/encoded/upgrade/gene.py
+++ src/encoded/upgrade/gene.py
@@ -6,3 +6,9 @@ def gene_1_2(value, system):
     # https://encodedcc.atlassian.net/browse/ENCD-5005
     # go_annotations are replaced by a link on UI to GO
     value.pop('go_annotations', None)
+
+@upgrade_step('gene', '2', '3')
+def gene_2_3(value, system):
+    # https://encodedcc.atlassian.net/browse/ENCD-6228
+    if value.get('locations') == []:
+        value.pop('locations', None)
