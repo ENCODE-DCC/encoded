ae76c3b427895d9e47d1549716834895f716e9d2
Mingjie Li <mingjie@Mingjies-MacBook-Pro.local>
2022-01-04 14:52:53 -0600

    fix eslint error

--- src/encoded/static/components/quality_metric.js
+++ src/encoded/static/components/quality_metric.js
@@ -100,6 +100,7 @@ const qcAttachmentProperties = {
         { total_counts_vs_pct_mitochondria: 'Total counts vs pct mitochondria' },
         { total_counts_vs_genes_by_count: 'Total counts vs genes by count' },
         { counts_violin_plot: 'Counts violin plot' },
+    ],
     SegwayQualityMetric: [
         { trackname_assay: 'Trackname assay' },
         { feature_aggregation_tab: 'Feature aggregation tab' },
