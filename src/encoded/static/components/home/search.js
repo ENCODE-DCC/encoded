338316806eb332438c10c3f1955b7768bf854bc4
forresttanaka <fytanaka@stanford.edu>
2022-01-11 15:52:02 -0800

    ENCD-6266-immune-cells-home
    Correct the search help icon color.

--- src/encoded/static/components/home/search.js
+++ src/encoded/static/components/home/search.js
@@ -407,7 +407,7 @@ const SearchSection = ({ onReceiveCollectionTitles }) => {
                                 <Tooltip
                                     trigger={svgIcon('questionCircle')}
                                     tooltipId="search-encode"
-                                    css="tooltip-home-info"
+                                    css="tooltip-container--home-search"
                                 >
                                     Search for candidate Cis-Regulatory Elements by entering a gene name or
                                     alias, SNP rsID, ccRE accession, or genomic region in the form
