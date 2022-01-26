e37e18e16b5e627d10c7beede82a515bc4d97f97
keenangraham <keenangraham@gmail.com>
2022-01-26 15:26:55 -0800

    ENCD-6214-remove-data-collections (#4248)

--- src/encoded/static/components/top_hits/multi/constants.js
+++ src/encoded/static/components/top_hits/multi/constants.js
@@ -1,8 +1,5 @@
-import TopHitsQuery, {
-    CollectionsQuery,
-} from '../query';
+import TopHitsQuery from '../query';
 import TopHitsResults from '../results';
-import CollectionsResults from './results';
 
 
 /**
@@ -16,7 +13,6 @@ import CollectionsResults from './results';
 * ]
 */
 const QUERIES = [
-    ['dataCollections', CollectionsQuery, CollectionsResults, 'Data collections'],
     ['topHits', TopHitsQuery, TopHitsResults, 'Top results by type'],
 ];
 
