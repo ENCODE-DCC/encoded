2e0b4babdac918a972f2fdfce0c379c037d81c67
Ben Hitz <hitz@stanford.edu>
2022-02-11 15:42:49 -0800

    update-version-v127.0

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"]
 
 setup(
     name='encoded',
-    version='126.0',
+    version='127.0',
     description='Metadata database for ENCODE',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
