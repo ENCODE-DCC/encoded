3b9aea64386ec7ead334293fd200c79f87a53391
Khine Lin <khzlin@stanford.edu>
2022-06-27 20:42:24 +0700

    upgrade qc

--- src/encoded/tests/fixtures/schemas/sc_atac_alignment_quality_metric.py
+++ src/encoded/tests/fixtures/schemas/sc_atac_alignment_quality_metric.py
@@ -12,3 +12,14 @@ def sc_atac_alignment_quality_metric_1(analysis_step_run, file, award, lab):
         'pct_mapped_reads': 0.3,
         'pct_singletons': 0.4
     }
+
+
+@pytest.fixture
+def sc_atac_alignment_quality_metric_2(analysis_step_run, file, award, lab):
+    return {
+        'award': award['uuid'],
+        'lab': lab['uuid'],
+        'step_run': analysis_step_run['uuid'],
+        'quality_metric_of': [file['uuid']],
+        'usable_fragments': 3165,
+    }
