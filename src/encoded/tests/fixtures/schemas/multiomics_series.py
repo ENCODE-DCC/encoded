4d4fad45fda5a0b99222fecd0a3f52d293237c92
Jennifer Jou <jjou@stanford.edu>
2023-03-03 16:25:06 -0800

    Fix fixtures

--- src/encoded/tests/fixtures/schemas/multiomics_series.py
+++ src/encoded/tests/fixtures/schemas/multiomics_series.py
@@ -16,7 +16,7 @@ def multiomics_series_1(testapp, lab, base_experiment_submitted, award):
     item = {
         'award': award['uuid'],
         'lab': lab['uuid'],
-        'related_datasets': [base_experiment_submitted['@id']]
+        'related_datasets': [base_experiment_submitted['@id']],
         'schema_version': '1',
         'internal_tags': ['ENCYCLOPEDIAv3', 'ENCYCLOPEDIAv4', 'ENCYCLOPEDIAv5', 'ENCYCLOPEDIAv6']
     }
