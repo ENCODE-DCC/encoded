87c132b5decec98468c47e23b117103bdcaad773
Mingjie Li <mingjiecn@gmail.com>
2023-03-20 17:55:35 -0500

    lint

--- src/encoded/static/components/matrix_entex.js
+++ src/encoded/static/components/matrix_entex.js
@@ -484,11 +484,11 @@ const MatrixHeader = ({ context }) => {
                         <div className="matrix-description__text">
                             <span>ENTEx is a collaboration with the GTEx Consortium to profile approximately 30 overlapping tissues from four donors.<br /></span>
                             <span>Publication: Rozowsky et al. Cell 2023. (</span>
-                            <a href="https://doi.org/10.1016/j.cell.2023.02.018" rel="noopener noreferrer" target="_blank"><span>https://doi.org/10.1016/j.cell.2023.02.018</span></a>
+                            <a href='https://doi.org/10.1016/j.cell.2023.02.018' rel="noopener noreferrer" target="_blank"><span>https://doi.org/10.1016/j.cell.2023.02.018</span></a>
                             <span>)<br /></span>
                             <span>Additional ancillary files are available from </span>
-                            <a href="http://entex.encodeproject.org/" rel="noopener noreferrer" target="_blank"><span>http://entex.encodeproject.org/</span></a>
-                            <span>.<br/></span>
+                            <a href='http://entex.encodeproject.org/' rel="noopener noreferrer" target="_blank"><span>http://entex.encodeproject.org/</span></a>
+                            <span>.<br /></span>
                         </div>
                     </div>
                 </div>
