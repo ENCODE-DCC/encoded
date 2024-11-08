1c586c5e1f7f22e32242e6cde2d8301ff980514a
forresttanaka <fytanaka@stanford.edu>
2024-11-07 19:49:48 -0800

    ENCM-206-more-stuff
    Change all occurrences of “single cell” and “sc” to “single nucleus” and “sn.”

--- src/encoded/static/components/single_cell.js
+++ src/encoded/static/components/single_cell.js
@@ -10,7 +10,7 @@ const SingleCellReferences = () => (
     <>
         <ul className="single-cell-references">
             <li>
-                Metadata for all unique cell types and cell states annotated from all single cell
+                Metadata for all unique cell types and cell states annotated from all single nucleus
                 experiments [
                 <a
                     href="/documents/6267390f-76d3-4a93-a842-f8612164a229/"
@@ -23,7 +23,7 @@ const SingleCellReferences = () => (
             </li>
             <li>
                 Unified metadata and cell type annotation for all QC filtered cells from all single
-                cell experiments [
+                nucleus experiments [
                 <a
                     href="/documents/0f99810a-697c-4631-bc0c-def084b56042/"
                     target="_blank"
@@ -35,14 +35,14 @@ const SingleCellReferences = () => (
             </li>
         </ul>
         <p>
-            Three levels of scRNA-seq and scATAC-seq pseudobulks were constructed in a hierarchical
+            Three levels of snRNA-seq and snATAC-seq pseudobulks were constructed in a hierarchical
             manner. Level 1 pseudobulks: pools cells of the same cell type and collected from a
-            single scRNA-seq sample. First-level pseudobulks serve as building blocks for the
+            single snRNA-seq sample. First-level pseudobulks serve as building blocks for the
             second and third levels of pseudobulks.
         </p>
         <ul>
             <li>
-                Metadata and quality control metrics for all scRNA-seq Level 1 pseudobulks [
+                Metadata and quality control metrics for all snRNA-seq Level 1 pseudobulks [
                 <a
                     href="/documents/fe984cd5-e9dd-45e2-b083-a83d810dd672/"
                     target="_blank"
@@ -53,7 +53,7 @@ const SingleCellReferences = () => (
                 ]
             </li>
             <li>
-                Metadata and quality control metrics for all scATAC-seq Level 1 pseudobulks [
+                Metadata and quality control metrics for all snATAC-seq Level 1 pseudobulks [
                 <a
                     href="/documents/f4f9d6b4-1b6f-49ed-8af6-c0f575a5461b/"
                     target="_blank"
@@ -71,7 +71,7 @@ const SingleCellReferences = () => (
         </p>
         <ul>
             <li>
-                Metadata and quality control metrics for all scRNA-seq Level 2 pseudobulks [
+                Metadata and quality control metrics for all snRNA-seq Level 2 pseudobulks [
                 <a
                     href="/documents/3a5a3644-2877-426a-9df3-2cd3259449b7/"
                     target="_blank"
@@ -82,7 +82,7 @@ const SingleCellReferences = () => (
                 ]
             </li>
             <li>
-                Metadata and quality control metrics for all scATAC-seq Level 2 pseudobulks [
+                Metadata and quality control metrics for all snATAC-seq Level 2 pseudobulks [
                 <a
                     href="/documents/82ae885c-afd4-4cfc-9cc6-98fa2b6dd84d/"
                     target="_blank"
@@ -100,7 +100,7 @@ const SingleCellReferences = () => (
         </p>
         <ul>
             <li>
-                Metadata and quality control metrics for all scRNA-seq Level 3 pseudobulks [
+                Metadata and quality control metrics for all snRNA-seq Level 3 pseudobulks [
                 <a
                     href="/documents/3745dc72-1fae-49bd-b42d-f15bddf2edec/"
                     target="_blank"
@@ -111,7 +111,7 @@ const SingleCellReferences = () => (
                 ]
             </li>
             <li>
-                Metadata and quality control metrics for all scATAC-seq Level 3 pseudobulks [
+                Metadata and quality control metrics for all snATAC-seq Level 3 pseudobulks [
                 <a
                     href="/documents/2cc13b15-181e-4cd4-ad4d-cc8fc5ea445d/"
                     target="_blank"
@@ -147,7 +147,7 @@ const singleCellList = {
         search: '?type=Annotation&annotation_type=cell+type+annotation&status=released&annotation_type=pseudobulk',
         description: (
             <div>
-                Cell type and pseudobulk annotations for high-throughput single-cell RNA-seq and ATAC-seq datasets.
+                Cell type and pseudobulk annotations for high-throughput single-nucleus RNA-seq and ATAC-seq datasets.
                 <h5>Metadata Tables for Cell Type and Pseudobulk Annotations from the ENCODE 4 Single Cell Working Group</h5>
                 <SingleCellReferences />
             </div>
